e6e7f9c1900708aa8ddb2607150ab466282dd9ed
Jim0607 <jinjun.ding@gmail.com>
2019-12-21 16:36:13 -0700

    Update Binary-Search.py

--- Binary-Search.py
+++ Binary-Search.py
@@ -11,6 +11,6 @@
 4. 理解二分法的三个境界：
     1. 熟记背诵模板
     2. OOXX的思考方法解决first X或者last O的问题，注意要画图可以帮助理解好问题，有时候空想是想不出来的。
-      eg: First bad version; Search in a big sorted array, Find minimum in Rotate sorted Array II, maximum number in mountain
-    3. Half half的二分法本质。
+      eg: sqrt(x); search a 2D matrix; search insertion position; First bad version; Wood count; Search in a big sorted array, Recover rotated array; Find minimum in Rotate sorted Array II, maximum number in mountain
+    3. Half half是二分法本质，每次去掉没有答案的那一半，保留有答案的那一半。
       eg: Find peak element; Search in rotated sorted array
