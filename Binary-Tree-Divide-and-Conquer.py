13c560be2ff5cc656c1c1c3d012c21280b1f7f32
Jim0607 <jinjun.ding@gmail.com>
2019-12-25 00:46:40 -0700

    Update Binary-Tree-Divide-and-Conquer.py

--- Binary-Tree-Divide-and-Conquer.py
+++ Binary-Tree-Divide-and-Conquer.py
@@ -1,7 +1,9 @@
-递归三要素：
+碰到二叉树的问题，就想想整棵树在该问题上的结果和左右儿子在该问题上的结果之间的联系是什么!!
+
+递归三要素：eg: 257.Binary Tree Paths
 1. 递归的定义（这个递归函数要干一件什么事情）
-2. 递归的拆解：对于Binary Tree一般就是处理 根、左、右 三步就可以了
-3. 递归的出口（结束条件）
+2. 递归的拆解：对于Binary Tree一般就是处理 根、左、右 三步就可以了，用divide and conquer的思想
+3. 递归的出口（结束条件，一般最后写）
 
 实现递归有两种方法：Traverse; Divide and Conquer
 Traverse vs Divide Conquer:
