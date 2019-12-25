d1b04497587b826c48c7f11f61e7e56b89539468
Jim0607 <jinjun.ding@gmail.com>
2019-12-24 21:47:40 -0700

    Create Binary-Tree-Divide-and-Conquer.py

---
+++ Binary-Tree-Divide-and-Conquer.py
@@ -0,0 +1,9 @@
+递归三要素：
+1. 递归的定义（这个递归函数要干一件什么事情）
+2. 递归的拆解：对于Binary Tree一般就是处理 根、左、右 三步就可以了
+3. 递归的出口（结束条件）
+
+Traverse vs Divide Conquer:
+  • They are both Recursion Algorithm
+  • Result in parameter vs Result in return value
+  • Top down vs Bottom up
