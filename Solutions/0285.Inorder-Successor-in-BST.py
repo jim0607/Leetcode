8c1f8ce24a326ff396ea4bc49cbe13da8dcaede3
Jim0607 <jinjun.ding@gmail.com>
2019-12-26 22:56:53 -0700

    Create 0285.Inorder-Successor-in-BST.py

---
+++ Solutions/0285.Inorder-Successor-in-BST.py
@@ -0,0 +1,86 @@
+#
+# @lc app=leetcode id=285 lang=python3
+#
+# [285] Inorder Successor in BST
+#
+# https://leetcode.com/problems/inorder-successor-in-bst/description/
+#
+# algorithms
+# Medium (37.45%)
+# Likes:    890
+# Dislikes: 55
+# Total Accepted:    127.5K
+# Total Submissions: 340.4K
+# Testcase Example:  '[2,1,3]\n1'
+#
+# Given a binary search tree and a node in it, find the in-order successor of
+# that node in the BST.
+# 
+# The successor of a node p is the node with the smallest key greater than
+# p.val.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [2,1,3], p = 1
+# Output: 2
+# Explanation: 1's in-order successor node is 2. Note that both p and the
+# return value is of TreeNode type.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [5,3,6,2,4,null,null,1], p = 6
+# Output: null
+# Explanation: There is no in-order successor of the current node, so the
+# answer is null.
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# If the given node has no in-order successor in the tree, return null.
+# It's guaranteed that the values of the tree are unique.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+"""solution 1: in-order traverse"""
+class Solution:
+    # a list of TreeNode
+    inOrderArr = []
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        self._inOrder_(root)
+        if p == self.inOrderArr[-1]:
+            return None
+        for i, node in enumerate(self.inOrderArr):
+            if node == p:
+                return self.inOrderArr[i + 1]
+
+    def _inOrder_(self, root: "TreeNode"):
+        """
+        in order traverse the tree, and put the nodes value in the arr
+        """
+        if not root:
+            return
+        self._inOrder_(root.left)
+        self.inOrderArr.append(root)
+        self._inOrder_(root.right)
+
+# @lc code=end
+
