5dcb55b9674e9c5efbbc61d134b3b8be3d26722f
Jim0607 <jinjun.ding@gmail.com>
2020-01-04 19:33:02 -0700

    Create 102.Binary-Tree-Level-Order-Traversal.py

---
+++ Solutions/102.Binary-Tree-Level-Order-Traversal.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode id=102 lang=python3
+#
+# [102] Binary Tree Level Order Traversal
+#
+# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+#
+# algorithms
+# Medium (51.36%)
+# Likes:    2090
+# Dislikes: 57
+# Total Accepted:    486.7K
+# Total Submissions: 945.4K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the level order traversal of its nodes' values.
+# (ie, from left to right, level by level).
+# 
+# 
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+# 
+# return its level order traversal as:
+# 
+# [
+# ⁠ [3],
+# ⁠ [9,20],
+# ⁠ [15,7]
+# ]
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        if not root:
+            return []
+        q = deque()
+        q.append(root)
+        while q:
+            level = []
+            lens = len(q)
+            # put the node of this level into array level
+            for _ in range(lens):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return res
+
+# @lc code=end
