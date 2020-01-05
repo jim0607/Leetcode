f2e306e49dc104cd365816ab7df4cb1ba6722346
Jim0607 <jinjun.ding@gmail.com>
2020-01-05 00:00:29 -0700

    Update 0102.Binary-Tree-Level-Order-Traversal.py

--- Solutions/0102.Binary-Tree-Level-Order-Traversal.py
+++ Solutions/0102.Binary-Tree-Level-Order-Traversal.py
@@ -51,19 +51,22 @@ from collections import deque
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        res = []
         if not root:
             return []
+        # 非常typical的BFS解binary tree的模板
+        res = []
         q = deque()
         q.append(root)
         while q:
             level = []
-            lens = len(q)
-            # put the node of this level into array level
+            lens = len(q)  # important
+            # 用一个for循环来处理每一层
             for _ in range(lens):
+                # 在这一层要做两件事情：1. 将该层的所有的node.val依次放入level中
                 node = q.popleft()
                 level.append(node.val)
-                if node.left:
+                # 2. 将该层所有的node的左右子节点依次入队列
+                if node.left:  # 注意这里判断是为了不把None放到队列里去，这样res出来的结果就没有None了。
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
