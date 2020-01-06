51a84027dab1dd48e39c6d822694dcea28b78102
Jim0607 <jinjun.ding@gmail.com>
2020-01-05 20:03:03 -0700

    Update 0200.Number-of-Islands.py

--- Solutions/0200.Number-of-Islands.py
+++ Solutions/0200.Number-of-Islands.py
@@ -44,9 +44,7 @@
 #
 
 """
-Linear scan the 2d grid map, if a node contains a '1', then it is a root node that triggers a Breadth First Search. 
-Put it into a queue and set its value as '0' to mark as visited node. 
-Iteratively search the neighbors of enqueued nodes until the queue becomes empty.
+Linear scan the 2d grid map, if a node contains a '1', then it is a root node that triggers a Breadth First Search.
 """
 # @lc code=start
 class Solution:
