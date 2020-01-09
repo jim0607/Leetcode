b4351339c498944c4be5530521a4b2e4ae44282c
Jim0607 <jinjun.ding@gmail.com>
2020-01-08 23:49:30 -0700

    Update 0025.Reverse-Nodes-in-k-Group.py

--- Solutions/0025.Reverse-Nodes-in-k-Group.py
+++ Solutions/0025.Reverse-Nodes-in-k-Group.py
@@ -1,42 +1,29 @@
 #
-# @lc app=leetcode id=25 lang=python3
+# @lc app=leetcode id=24 lang=python3
 #
-# [25] Reverse Nodes in k-Group
+# [24] Swap Nodes in Pairs
 #
-# https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+# https://leetcode.com/problems/swap-nodes-in-pairs/description/
 #
 # algorithms
-# Hard (38.91%)
-# Likes:    1610
-# Dislikes: 317
-# Total Accepted:    225.9K
-# Total Submissions: 578.3K
-# Testcase Example:  '[1,2,3,4,5]\n2'
+# Medium (47.54%)
+# Likes:    1651
+# Dislikes: 146
+# Total Accepted:    393K
+# Total Submissions: 823.9K
+# Testcase Example:  '[1,2,3,4]'
 #
-# Given a linked list, reverse the nodes of a linked list k at a time and
-# return its modified list.
+# Given a linked list, swap every two adjacent nodes and return its head.
 # 
-# k is a positive integer and is less than or equal to the length of the linked
-# list. If the number of nodes is not a multiple of k then left-out nodes in
-# the end should remain as it is.
-# 
+# You may not modify the values in the list's nodes, only nodes itself may be
+# changed.
 # 
 # 
 # 
 # Example:
 # 
-# Given this linked list: 1->2->3->4->5
 # 
-# For k = 2, you should return: 2->1->4->3->5
-# 
-# For k = 3, you should return: 3->2->1->4->5
-# 
-# Note:
-# 
-# 
-# Only constant extra memory is allowed.
-# You may not alter the values in the list's nodes, only nodes itself may be
-# changed.
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
 # 
 # 
 #
@@ -49,43 +36,39 @@
 #         self.next = None
 
 class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        # dummy.next is always head of the Linked List
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        
         dummy = ListNode(0)
         dummy.next = head
 
         curr = dummy
         while curr:
-            curr = self.reverse(curr, k)
-
-        return dummy.next
-    
-    # reverse n1 -> n2 -> ..... -> nk
-    # head -> n1 -> n2 -> ..... -> nk -> nk+1
-    # after reverse shoule be : head -> nk -> ....n1 -> nk+1
-    # return head
-    def reverse(self, head: ListNode, k: int) -> ListNode:
-        n1 = head.next
-        nk = head
-        for i in range(k):
-            nk = nk.next
-            if not nk:
-                return None
-
-        # reverse
-        nkplus = nk.next
-        prev, curr = None, n1
-        while curr != nkplus:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+            curr = self.reverse(curr)
         
-        # connect
-        head.next = nk
-        n1.next = nkplus
+        return dummy.next
 
+    # The input is n0 (not None)
+    # swap the n0 -> n1 -> n2 -> n3
+    # to be n0 -> n2 -> n1 -> n3
+    # return n0 for hte next pair, which is n1 for this pair
+    def reverse(self, curr: ListNode) -> ListNode:
+        if not curr.next or not curr.next.next:
+            return None
+        
+        n0, n1, n2, n3 = curr, curr.next, curr.next.next, curr.next.next.next
+
+        # reverse n1 and n2
+        n2.next = n1
+
+        # connect
+        n0.next = n2
+        n1.next = n3
+
+        # return the n0 for the next pair, which is n1 for this pair
         return n1
 
+        
 # @lc code=end
 
