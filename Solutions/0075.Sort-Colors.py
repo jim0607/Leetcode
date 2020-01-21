a00d03c0dcc8bf03d71131b03314e4fc366ad092
Jim0607 <jinjun.ding@gmail.com>
2020-01-20 17:11:42 -0700

    Create 0075.Sort-Colors.py

---
+++ Solutions/0075.Sort-Colors.py
@@ -0,0 +1,40 @@
+75. Sort Colors
+
+Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note: You are not suppose to use the library's sort function for this problem.
+
+Example:
+
+Input: [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+"""使用一次扫描的办法。
+设立三根指针，left, index, right。定义如下规则：
+left 的左侧都是 0（不含 left）
+right 的右侧都是 2（不含 right）
+index 从左到右扫描每个数，如果碰到 0 就丢给 left，碰到 2 就丢给 right。碰到 1 就跳过不管。"""
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return nums
+        
+        lens = len(nums)
+        left, index, right = 0, 0, lens - 1
+        while index <= right:       # be careful, index < right is not correct
+            if nums[index] == 0:
+                nums[index], nums[left] = nums[left], nums[index]
+                left += 1
+                index += 1
+            elif nums[index] == 2:
+                nums[index], nums[right] = nums[right], nums[index]
+                right -= 1
+            elif nums[index] == 1:  
+                index += 1
+        
+        return nums
