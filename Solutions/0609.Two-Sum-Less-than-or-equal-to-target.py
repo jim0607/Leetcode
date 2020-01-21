77a33f15775187dc935464c8facc33cd798cb30f
Jim0607 <jinjun.ding@gmail.com>
2020-01-20 22:04:58 -0700

    Update 0609.Two-Sum-Less-than-or-equal-to-target.py

--- Solutions/0609.Two-Sum-Less-than-or-equal-to-target.py
+++ Solutions/0609.Two-Sum-Less-than-or-equal-to-target.py
@@ -15,4 +15,15 @@ Return 5.
 先排序。
 左右两个指针，一个往右走，一个往左走。
 假设我们发现nums[i] + nums[j] <= target，由于这个数组是排序的，所以nums[i] + i 到 j之间的任何数，一定也是小于等于target的。那么我们就不用重复计算了。
-def twoSUm5()
+
+def twoSUm5(self, nums, target):
+  i, j = 0, len(nums) - 1
+  cnt = 0
+  while i < j:
+    if nums[i] + nums[j] <= target:
+      cnt += j - i		# 注意这里是 cnt += j - i 表示nums[i] 加上 i 到 j之间的任何数，一定也是小于等于target的
+      i += 1
+    else:
+      j -= 1
+      
+  return cnt
