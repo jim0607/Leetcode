57858a192fb70b60e19afec7172c525640787108
Jim0607 <jinjun.ding@gmail.com>
2020-01-27 20:20:17 -0700

    Create 0341.Flatten-Nested-List-Iterator.py

---
+++ Solutions/0341.Flatten-Nested-List-Iterator.py
@@ -0,0 +1,60 @@
+341. Flatten Nested List Iterator
+
+Given a nested list of integers, implement an iterator to flatten it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+
+Input: [[1,1],2,[1,1]]
+Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, 
+             the order of elements returned by next should be: [1,1,2,1,1].
+
+
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = []
+        # put every item in the nestedList into a stack with reversed order
+        for item in nestedList[::-1]:
+            self.stack.append(item)
+    
+    def next(self) -> int:
+        return self.stack.pop().getInteger()
+    
+    def hasNext(self) -> bool:
+        while self.stack:
+            topItem = self.stack[-1]
+            if topItem.isInteger():
+                return True
+            else:            # if it is a nestedList, 就展开。
+                self.stack = self.stack[:-1] + topItem.getList()[::-1]
+                
+        return False
+            
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
