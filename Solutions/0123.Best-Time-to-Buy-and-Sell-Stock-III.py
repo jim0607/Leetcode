892e1526c47698965db1f3c3986b6a667d6159ab
Jim0607 <jinjun.ding@gmail.com>
2020-02-02 17:07:21 -0700

    Create 0123.Best-Time-to-Buy-and-Sell-Stock-III.py

---
+++ Solutions/0123.Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -0,0 +1,33 @@
+123. Best Time to Buy and Sell Stock III
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+             Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+
+
+
+
+
+
+"""O(N), O(1) solution using DP"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sell1, sell2 = 0, 0
+        buy1, buy2 = -float("inf"), -float("inf")
+        for price in prices:
+            buy1 = max(-price, buy1)            # buy1 = the minimum money you can 欠下 after the first purchase
+            sell1 = max(sell1, buy1 + price)    # sell1 = the maximum money you can 挣下 for the first sell
+            buy2 = max(sell1 - price, buy2)     # buy2 = the minimum money you can 欠下 after the second purchase
+            sell2 = max(sell2, buy2 + price)    # sell2 = the maximum money you can 挣下 after the second purchase
+            
+        return sell2
