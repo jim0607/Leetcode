75aaaa0db6d8b0a54001e675d4307579e8a7b5f7
Jim0607 <jinjun.ding@gmail.com>
2020-02-05 22:43:33 -0700

    Update 0279.Perfect-Squares.py

--- Solutions/0279.Perfect-Squares.py
+++ Solutions/0279.Perfect-Squares.py
@@ -1,3 +1,5 @@
+279. Perfect Squares
+
 Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
 Example 1:
@@ -5,23 +7,23 @@ Example 1:
 Input: n = 12
 Output: 3 
 Explanation: 12 = 4 + 4 + 4.
-Example 2:
-
-Input: n = 13
-Output: 2
-Explanation: 13 = 4 + 9.
 
 
-"""grandyang DP: dp[i]表示组成i的完全平方数的最小个数
-递推公式：dp[i+j**2] = dp[i]+1
-初始值：dp[1] = 1;  返回值：dp[n+1]"""
+
+
+"""f[i]=the least number os perfect square numbers which sum to i
+f[j] = min(f[j-i^2]+1) for i^2<=j
+Time complexity: j is from 0 to n, i is from 0 to j^0.5, so O(N^1.5)"""
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [5] * (n+1)   # 四平方和定理：任意一个正整数均可表示为4个整数的平方和，其实是可以表示为4个以内的平方数之和，那么就是说返回结果只有 1,2,3 或4其中的一个
-        dp[0], dp[1] = 0, 1   # 这里必须从dp[0]=0开始算，不然dp[4]就无法等于1了！
-        for i in range(0, n+1):
-            j = 1
-            while i+j**2 <= n:
-                dp[i+j**2] = min(dp[i+j**2], dp[i] + 1)
-                j += 1
-        return dp[-1]
+        dp = [float("inf")] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        
+        for j in range(2, n + 1):
+            i = 1
+            while i**2 <= j:
+                dp[j] = min(dp[j], dp[j - i**2] + 1)
+                i += 1
+                
+        return dp[n]
