c09d1d618b96fa5df82814f9b249a9c3d504e410
Jim0607 <jinjun.ding@gmail.com>
2020-02-11 21:34:13 -0700

    Update 0714.Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.py

--- Solutions/0714.Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.py
+++ Solutions/0714.Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.py
@@ -19,30 +19,46 @@ Note:
 0 < prices[i] < 50000.
 0 <= fee < 50000.
 
+"""DP solution: 两种状态：hold and unhold
+O(N), O(N)"""
+"""
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        if not prices:
+            return 0
+        
+        lens = len(prices)
+        if lens == 1:
+            return 0
+        
+        hold = [-float("inf")] * lens
+        unhold = [-float("inf")] * lens
+        
+        hold[0] = -prices[0]
+        unhold[0] = 0
+        
+        for i in range(1, lens):
+            hold[i] = max(hold[i - 1], unhold[i - 1] - prices[i])
+            unhold[i] = max(unhold[i - 1], hold[i - 1] + prices[i] - fee)
+            
+        return unhold[-1]
+"""
 
-"""提示：Consider the first K stock prices. At the end, the only legal states are that you don't own a share of stock, or that you do. Calculate the most profit you could have under each of these two cases.
-定义dp[i][0]为i天后手里有股票的最大收益，dp[i][1]为i天后手里没有股票的最大收益
-递推关系：dp[i][0] = max(dp[i-1][1]-prices[i], dp[i-1][0])； dp[i][1] = max(dp[i-1][0]+prices[i]-fee, dp[i-1][1])
-初始条件：dp[0][0], dp[0][1] = -prices[0], 0
-返回值：max(dp[lens-1][0], dp[lens-1][1])"""
+"""DP, 空间优化"""
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        lens = len(prices)
-        dp = [[0]*2 for _ in range(lens)]
-        dp[0][0], dp[0][1] = -prices[0], 0
-        for i in range(1, lens):
-            dp[i][0] = max(dp[i-1][1]-prices[i], dp[i-1][0])
-            dp[i][1] = max(dp[i-1][0]+prices[i]-fee, dp[i-1][1])
-        return max(dp[lens-1][0], dp[lens-1][1])
+        if not prices:
+            return 0
         
-        
-""" 这一题就解决了，但是这样处理 base case 很麻烦，而且注意一下状态转移方程，新状态只和相邻的一个状态有关，其实不用整个 dp 数组，只需要一个变量储存相邻的那个状态就足够了，这样可以把空间复杂度降到 O(1):"""
-class Solution:
-    def maxProfit(self, prices: List[int], fee: int) -> int:
         lens = len(prices)
-        dp_0, dp_1 = -prices[0], 0
+        if lens == 1:
+            return 0
+        
+        hold = -prices[0]
+        unhold = 0
+        
         for i in range(1, lens):
-            temp = dp_0
-            dp_0 = max(dp_1-prices[i], dp_0)
-            dp_1 = max(temp+prices[i]-fee, dp_1)
-        return max(dp_0, dp_1)
+            hold = max(hold, unhold - prices[i])
+            unhold = max(unhold, hold + prices[i] - fee)
+            
+        return unhold
