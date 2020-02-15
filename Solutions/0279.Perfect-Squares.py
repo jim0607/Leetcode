df969768300e7834ac38905f7b83c35e62d7810f
Jim0607 <jinjun.ding@gmail.com>
2020-02-15 15:05:41 -0700

    Update 0279.Perfect-Squares.py

--- Solutions/0279.Perfect-Squares.py
+++ Solutions/0279.Perfect-Squares.py
@@ -1,17 +1,12 @@
-279. Perfect Squares
-
 Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
 Example 1:
-
 Input: n = 12
 Output: 3 
 Explanation: 12 = 4 + 4 + 4.
 
 
-
-
-"""f[i]=the least number os perfect square numbers which sum to i
+"""f[i]=the least number of perfect square numbers which sum to i
 f[j] = min(f[j-i^2]+1) for i^2<=j
 Time complexity: j is from 0 to n, i is from 0 to j^0.5, so O(N^1.5)"""
 class Solution:
