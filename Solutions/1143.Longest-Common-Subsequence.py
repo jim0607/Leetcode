1cf59114719233659f98f517c476f4ddb5f67404
Jim0607 <jinjun.ding@gmail.com>
2020-02-16 15:18:40 -0700

    Update 1143.Longest-Common-Subsequence.py

--- Solutions/1143.Longest-Common-Subsequence.py
+++ Solutions/1143.Longest-Common-Subsequence.py
@@ -2,11 +2,7 @@ Given two strings text1 and text2, return the length of their longest common sub
 
 A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
 
- 
-
 If there is no common subsequence, return 0.
-
- 
 
 Example 1:
 
@@ -14,18 +10,20 @@ Input: text1 = "abcde", text2 = "ace"
 Output: 3  
 Explanation: The longest common subsequence is "ace" and its length is 3.
 
-"""解法一：二维DP：Try dynamic programming. DP[i][j] represents the longest common subsequence of text1[0 ... i] & text2[0 ... j].
-DP[i][j] = DP[i - 1][j - 1] + 1 , if text1[i] == text2[j] DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]) , otherwise"""
+"""设f[i][j]为A前i个字符A[0..i)和B前j个字符[0..j)的最长公共子串的长度，注意不包括i和j，前面有一层buffer layer非常重要，就像sputtering那样重要！f[i][j]=max(f[i][j-1], f[i-1][j], f[i-1][j-1] when A[i-1]=B[j-1])"""
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        len_1, len_2 = len(text1), len(text2)
-        if len_1 == 0 or len_2 == 0:
+        if not text1 or not text2:
             return 0
-        dp = [[0]*(len_2+1) for _ in range(len_1+1)]
-        for i in range(1, len_1+1):
-            for j in range(1, len_2+1):
+        
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+                
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
+                    
         return dp[-1][-1]
