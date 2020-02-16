c693038bdeae683b3c946ded6ec2c552b85a15c2
Jim0607 <jinjun.ding@gmail.com>
2020-02-15 17:06:57 -0700

    Update 0486.Predict-the-Winner.py

--- Solutions/0486.Predict-the-Winner.py
+++ Solutions/0486.Predict-the-Winner.py
@@ -11,6 +11,24 @@ So, final score of player 1 is 1 + 2 = 3, and player 2 is 5.
 Hence, player 1 will never be the winner and you need to return False.
 
 
+"""f[i][j]=当石子还剩i到j时，先手最多能赢多少
+f[i][j] = max(取左边A[i]-f[i+1][j], 取右边A[j]-f[i][j-1])"""
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        if not nums:
+            return False
+        
+        lens = len(nums)
+        dp = [[False] * lens for _ in range(lens)]
+        
+        for i in range(lens - 1, -1, -1):
+            dp[i][i] = nums[i]
+            for j in range(i + 1, lens):
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1])
+                
+        return dp[0][lens - 1] >= 0
+
+
 """时间复杂度：O(2^N)，其中 N 是数组的长度。空间复杂度：O(N)，为递归时栈使用的空间。"""
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
@@ -28,20 +46,3 @@ class Solution:
         return max(
             nums[i] + min(self._getMax(nums, i+1, j-1), self._getMax(nums, i+2, j)), 
             nums[j] + min(self._getMax(nums, i+1, j-1), self._getMax(nums, i, j-2))    )
-            
-            
-"""方法二：动态规划    时间复杂度：O(N^2); 空间复杂度：O(N^2)
-我们同样可以使用动态规划来解决这个问题。用 dp(i, j) 表示当剩下的数为 nums[i .. j] 时，当前操作的选手（注意，不一定是先手）与另一位选手最多的分数差。当前操作的选手可以选择 nums[i] 并留下 nums[i+1 .. j]，或选择 nums[j] 并留下 nums[i .. j-1]，因此状态转移方程为：
-dp(i, j) = max(nums[i] - dp(i+1, j), nums[j] - dp(i, j-1))， dp(i,j)依赖前面的值，前面的值分别来自下面一行和左边一列，因此是往二维数组的右上方移动，直到移动到第零行，如果 dp(0, n - 1) >= 0，那么先手必胜。
-初始条件为dp(i, i) = nums[i]
-https://leetcode-cn.com/problems/predict-the-winner/solution/san-chong-dpsi-lu-jie-jue-duo-si-lu-by-a-fei-8/"""
-class Solution:
-    def PredictTheWinner(self, nums: List[int]) -> bool:
-        lens = len(nums)
-        dp = [[0]*lens for _ in range(lens)]
-        for i in range(lens):
-            dp[i][i] = nums[i]
-        for row in range(lens-1, -1, -1):
-            for col in range(row+1, lens):
-                dp[row][col] = max(nums[row]-dp[row+1][col], nums[col]-dp[row][col-1])
-        return dp[0][lens-1] >= 0
