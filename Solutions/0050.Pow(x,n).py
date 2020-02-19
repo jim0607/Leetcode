d77638d60ef1ffa6c03362167fd8260f27d4dda2
Jim0607 <jinjun.ding@gmail.com>
2020-02-19 15:09:07 -0700

    Create 0050.Pow(x,n).py

---
+++ Solutions/0050.Pow(x,n).py
@@ -0,0 +1,47 @@
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n == -1:
+            return 1 / x
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        if x == -1:
+            return 1 if n % 2 == 0 else -1
+        
+        k = n if n > 0 else -n
+        x = x if n > 0 else 1 / x
+        res = 1
+        while k >= 1:
+            mod = k % 2
+            div = k // 2
+            if mod == 0:
+                res *= x**div
+            else:
+                res *= x**div * x
+                
+            k = div
+                
+        return res
