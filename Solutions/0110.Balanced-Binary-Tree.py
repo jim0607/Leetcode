4be0ebe0c929f690132cd4dcdb1663426cbdcf5d
Jim0607 <jinjun.ding@gmail.com>
2020-02-23 00:59:55 -0700

    Update 0110.Balanced-Binary-Tree.py

--- Solutions/0110.Balanced-Binary-Tree.py
+++ Solutions/0110.Balanced-Binary-Tree.py
@@ -31,12 +31,32 @@ Given the following tree [1,2,2,3,3,null,null,4,4]:
 Return false.
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        self.is_balanced = True
+        
+        self.helper(root)
+        
+        return self.is_balanced
+    
+    def helper(self, root):
+        """递归的定义: return the depth of the tree with root as its root"""
+        # 1. 初始条件
+        if not root:
+            return 0
+        # 2. 初始条件，养成好习惯，把叶子节点单独做判断
+        if not root.left and not root.right:
+            return 1
+        
+        # 3. 递归的拆解：divide
+        leftDepth = self.helper(root.left)
+        rightDepth = self.helper(root.right)
+        
+        # 4. 递归的拆解：merge
+        if abs(leftDepth - rightDepth) > 1:
+            self.is_balanced = False
+            
+        return max(leftDepth, rightDepth) + 1
 
 # this is a good example of ResultType in Python. 也就是需要return不止一个值的时候，我们需要将result打包起来。
 class Solution:
