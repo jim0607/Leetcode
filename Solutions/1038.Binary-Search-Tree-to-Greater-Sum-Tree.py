339ffefbeba3222aacc03226ae62226bce6b8b24
Jim0607 <jinjun.ding@gmail.com>
2020-02-23 22:48:18 -0700

    Create 1038.Binary-Search-Tree-to-Greater-Sum-Tree.py

---
+++ Solutions/1038.Binary-Search-Tree-to-Greater-Sum-Tree.py
@@ -0,0 +1,26 @@
+Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
+
+
+"""do a in order traversal (reversed version) to keep track the sums"""
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return
+        
+        stack = []
+        curr = root
+        sums = 0
+        
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.right       # reverse the in order traversal (output right nodes first)
+                
+            curr = stack.pop()
+            
+            sums += curr.val
+            curr.val = sums
+            
+            curr = curr.left
+            
+        return root
