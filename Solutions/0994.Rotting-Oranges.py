e114869c92a8500c25752f0c04edbb2a029b35a0
Jim0607 <jinjun.ding@gmail.com>
2020-02-26 20:13:29 -0700

    Update 0994.Rotting-Oranges.py

--- Solutions/0994.Rotting-Oranges.py
+++ Solutions/0994.Rotting-Oranges.py
@@ -73,7 +73,6 @@
 # 
 #
 
-# @lc code=start
 class Solution:
     EMPTY = 0
     FRESH = 1
@@ -86,57 +85,56 @@ class Solution:
         
         # 如果本来就没有FRESH的，就直接返回0
         found = False
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == self.FRESH:
                     found = True
+                    
         if not found:
-            return 0        
+            return 0  
         
         visited = set()
-        minutes = self.bfs(grid, visited)  # 需要分层遍历，所以需要加入一个参数minutes
-
-        # 判断有没有不能变质的
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        minites = self.bfs(grid, visited)
+        
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == self.FRESH:
                     return -1
-
-        return minutes - 1
-
-    # bfs 要做的事：1. 遍历ROTTEN周边，发现FRESH就变成ROTTEN，需要层序遍历来记录需要多少minutes, 最后返回minutes
+        
+        return minites
+    
     def bfs(self, grid, visited):
+        """bfs to reach the fresh ones and change them into rotten ones, return the minites to reach all fresh ones"""
+        
+        # 1. append the rotten ones to the first level, 由于需要做层序遍历，所以要想将所有的ROTTEN都放进q （作为第一层），然后再开始q的遍历
+        m, n = len(grid), len(grid[0])
         q = collections.deque()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == self.ROTTEN:
                     q.append((i, j))
-                    visited.add((i, j)) # 一对孪生兄弟
+                    visited.add((i, j))     # 一对孪生兄弟
         
-        # 由于需要做层序遍历，所以要想将所有的ROTTEN都放进q （作为第一层），然后再开始q的遍历
-        minutes = 0
+        # 2. bfs
+        minites = -1        # 需要分层遍历，所以需要加入一个参数minutes
         while q:
+            minites += 1
             lens = len(q)
-            minutes += 1
-            # 层序遍历
-            for i in range(lens):
+            for _ in range(lens):   # 一层一层的来
                 (x, y) = q.popleft()
                 for delta_x, delta_y in self.MOVES:
-                    new_x = x + delta_x
-                    new_y = y + delta_y
-                    if self.isBound(new_x, new_y, grid) and grid[new_x][new_y] == self.FRESH and (new_x, new_y) not in visited:
-                        grid[new_x][new_y] = self.ROTTEN
-                        q.append((new_x, new_y))
-                        visited.add((new_x, new_y)) # 一对孪生兄弟
+                    neighbor_x, neighbor_y = x + delta_x, y + delta_y
+                    if self.inBound(grid, neighbor_x, neighbor_y) and grid[neighbor_x][neighbor_y] == self.FRESH and (neighbor_x, neighbor_y) not in visited:
+                        grid[neighbor_x][neighbor_y] = self.ROTTEN
+                        q.append((neighbor_x, neighbor_y))
+                        visited.add((neighbor_x, neighbor_y))       # 一对孪生兄弟
 
-        return minutes
-
-    def isBound(self, x, y, grid):
-        if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
+        return minites
+                        
+    def inBound(self, grid, x, y):
+        m, n = len(grid), len(grid[0])
+        if 0 <= x < m and 0 <= y < n:
             return True
         
         return False
-
-
-# @lc code=end
-
