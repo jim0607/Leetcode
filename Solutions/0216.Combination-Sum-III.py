bbab71c922a5200fe29bed13048202244ca2ebf6
Jim0607 <jinjun.ding@gmail.com>
2020-02-28 16:10:41 -0700

    Create 0216.Combination-Sum-III.py

---
+++ Solutions/0216.Combination-Sum-III.py
@@ -0,0 +1,35 @@
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+Note:
+
+All numbers will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Example 2:
+
+Input: k = 3, n = 9
+Output: [[1,2,6], [1,3,5], [2,3,4]]
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        nums = [i for i in range(1, 10)]
+        res = []
+        self.dfs(nums, k, n, 0, [], res)
+        return res
+    
+    def dfs(self, nums, k, n, startIndex, curr, res):
+        if k < 0 or n < 0:
+            return 
+        
+        if k == 0 and n == 0:
+            res.append(curr.copy())
+            return
+        
+        for i in range(startIndex, len(nums)):
+            curr.append(nums[i])
+            self.dfs(nums, k - 1, n - nums[i], i + 1, curr, res)    # 不能出现重复数字，所以从i+1开始
+            curr.pop()
