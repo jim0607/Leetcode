6f25bcdc3635d6211e3bf82ede08ece666497dae
Jim0607 <jinjun.ding@gmail.com>
2020-02-28 23:22:34 -0700

    Update 0051.N-Queens.py

--- Solutions/0051.N-Queens.py
+++ Solutions/0051.N-Queens.py
@@ -52,73 +52,52 @@
 又由于Queen还可以斜着走，所以横纵坐标的和与差不能相同，也需要用visited标记。
 用一个visited数组存储 列号，横纵坐标之和，横纵坐标之差 有没有被用过
 """
-# @lc code=start
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
-        if n == 0:
-            return []
-
         visited_col = {i: False for i in range(n)}
         visited_sum = {i: False for i in range(2 * n - 1)}
         visited_diff = {i: False for i in range(1 - n, n)}
-
-        # 打印出数组nums中所有的可能排列：[[0,1,2,3], [1,3,0,2].....]
-        # 这里的数组nums其实就是[0, 1, 2, 3....n]，下标与数值相等。len(nums) = n, nums[i] = i
-        self.dfs(n, [], visited_col, visited_sum, visited_diff, res)
-
+        
+        self.dfs(n, [], res, visited_col, visited_sum, visited_diff)
         print(res)
- 
         return self.draw(res)
-
-    def dfs(self, n: int, curr: List[int], visited_col, visited_sum, visited_diff, res: List[List[int]]):
+    
+    def dfs(self, n, curr, res, visited_col, visited_sum, visited_diff):
         if len(curr) == n:
-            res.append(curr.copy())     # deep copy
+            res.append(curr.copy())
             return
         
         for i in range(n):
-            if not self.is_valid(curr, visited_col, visited_sum, visited_diff, i):
+            if self.isVisited(curr, i, visited_col, visited_sum, visited_diff):
                 continue
-            
+                
             curr.append(i)
-            row, col = len(curr) - 1, i
+            row, col = len(curr) - 1, i     # 比如curr = [1,3,0,2]，数字对应的下标位置是放那个Queen的行，数字的值是放那个Queen的列
             visited_col[col] = True
             visited_sum[row + col] = True
             visited_diff[row - col] = True
-
-            self.dfs(n, curr, visited_col, visited_sum, visited_diff, res)
-
+            
+            self.dfs(n, curr, res, visited_col, visited_sum, visited_diff)
+            
             curr.pop()
             visited_col[col] = False
             visited_sum[row + col] = False
             visited_diff[row - col] = False
-
-    def is_valid(self, curr, visited_col, visited_sum, visited_diff, i):
+            
+    def isVisited(self, curr, i, visited_col, visited_sum, visited_diff):
         row, col = len(curr), i
-        if visited_col[col]:
-            return False
-        if visited_sum[row + col]:
-            return False
-        if visited_diff[row - col]:
-            return False
-
-        return True
-
-    def draw(self, res: List[List[int]]) -> List[List[str]]:
-        draw_results = []       # a 2D List, List[List[str]]
-        for permutation in res:
-            draw_result = []
-            for col in permutation:
-                draw_str = ""
-                for c in range(len(permutation)):
-                    if c == col:
-                        draw_str += "Q"
-                    else:
-                        draw_str += "."
-                draw_result.append(draw_str)
-            draw_results.append(draw_result)
-
-        return draw_results
+        if visited_col[col] or visited_sum[row + col] or visited_diff[row - col]:
+            return True
         
-# @lc code=end
-
+        return False
+    
+    def draw(self, grid):
+        results = []
+        for nums in grid:
+            result = []
+            for col in nums:
+                result.append("." * col + "Q" + "." * (len(nums) - col - 1))    
+            results.append(result)
+            
+        return results
