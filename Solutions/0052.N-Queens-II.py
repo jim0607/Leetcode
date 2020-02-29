b637119dd07c16067e5aff133a4568b5a81d3bb4
Jim0607 <jinjun.ding@gmail.com>
2020-02-28 23:42:51 -0700

    Update 0052.N-Queens-II.py

--- Solutions/0052.N-Queens-II.py
+++ Solutions/0052.N-Queens-II.py
@@ -43,50 +43,33 @@
 # 
 #
 
-# @lc code=start
 class Solution:
     def totalNQueens(self, n: int) -> int:
         res = []
-        if n == 0:
-            return 0
-
         visited_col = {i: False for i in range(n)}
         visited_sum = {i: False for i in range(2 * n - 1)}
         visited_diff = {i: False for i in range(1 - n, n)}
         self.dfs(n, [], res, visited_col, visited_sum, visited_diff)
-
+        
         return len(res)
-
+    
     def dfs(self, n, curr, res, visited_col, visited_sum, visited_diff):
         if len(curr) == n:
-            res.append(curr.copy())
-
+            res.append(curr.copy)
+            
         for i in range(n):
-            row, col = len(curr), i     #准备把col放到len(curr)的位置，所以row=len(curr)而不是len(curr)-1
-            if not self.is_valid(row, col, visited_col, visited_sum, visited_diff):
+            row, col = len(curr), i     #准备把col放到len(curr)的位置，还没放，所以row=len(curr)而不是len(curr)-1
+            if visited_col[col] or visited_sum[row + col] or visited_diff[row - col]:
                 continue
-
+            
             curr.append(col)
             visited_col[col] = True
             visited_sum[row + col] = True
             visited_diff[row - col] = True
-
+            
             self.dfs(n, curr, res, visited_col, visited_sum, visited_diff)
-
+            
             curr.pop()
             visited_col[col] = False
             visited_sum[row + col] = False
             visited_diff[row - col] = False
-
-    def is_valid(self, row, col, visited_col, visited_sum, visited_diff):
-        if visited_col[col]:
-            return False
-        if visited_sum[row + col]:
-            return False
-        if visited_diff[row - col]:
-            return False
-
-        return True
-        
-# @lc code=end
-
