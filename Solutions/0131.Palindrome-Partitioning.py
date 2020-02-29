180a633348a64a276f2d43fb2f639fdb4ebcc35d
Jim0607 <jinjun.ding@gmail.com>
2020-02-28 20:26:02 -0700

    Update 0131.Palindrome-Partitioning.py

--- Solutions/0131.Palindrome-Partitioning.py
+++ Solutions/0131.Palindrome-Partitioning.py
@@ -34,40 +34,36 @@
 """
 这种输出所有组合问题无脑使用dfs，想想如何套用Subsets的模板，然后再开始写
 """
-# @lc code=start
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        if not s:
-            return res
         
         self.dfs(s, 0, [], res)
-
+        
         return res
-
-    # 递归的定义：从s中的start位置开始，挑一些位置切割，判断从start到i的部分是否为回文，如果是就放入curr中，如果i到了string末尾了则说明此事curr是一种组合方式，放入res中
-    def dfs(self, s: str, start: int, curr: list, res: list):
-        # 递归的出口
-        if start == len(s):
+    
+    def dfs(self, s, startIndex, curr, res):
+        """# 递归的定义：从s中的start位置开始，挑一些位置切割，判断从start到i的部分是否为回文，如果是就放入curr中，如果i到了string末尾了则说明此事curr是一种组合方式，放入res中"""
+        
+        if startIndex == len(s):
             res.append(curr.copy())     # always remember this is deep copy
             return
         
-        for i in range(start, len(s)):
-            if not self.is_palindrome(s[start:i+1]):
+        for i in range(startIndex, len(s)):
+            if not self.isPalin(s[startIndex:i + 1]):
                 continue
-            curr.append(s[start:i+1])
+            
+            curr.append(s[startIndex:i + 1])
             self.dfs(s, i + 1, curr, res)
             curr.pop()
-
-    def is_palindrome(self, s: str) -> bool:
+            
+    def isPalin(self, s):
         i, j = 0, len(s) - 1
         while i < j:
             if s[i] != s[j]:
                 return False
+            
             i += 1
             j -= 1
         
         return True
-
-# @lc code=end
-
