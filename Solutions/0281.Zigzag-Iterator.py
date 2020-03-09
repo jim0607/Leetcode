4fd58054c8e83a6923e7874412d86aa050539d91
Jim0607 <jinjun.ding@gmail.com>
2020-03-09 14:54:00 -0600

    Update 0281.Zigzag-Iterator.py

--- Solutions/0281.Zigzag-Iterator.py
+++ Solutions/0281.Zigzag-Iterator.py
@@ -9,10 +9,6 @@ Output: [1,3,2,4,5,6]
 Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,3,2,4,5,6].
  
 
-Follow up:
-
-What if you are given k 1d vectors? How well can your code be extended to such cases?
-
 Clarification for the follow up question:
 The "Zigzag" order is not clearly defined and is ambiguous for k > 2 cases. If "Zigzag" does not look right to you, replace "Zigzag" with "Cyclic". For example:
 
@@ -56,6 +52,8 @@ class ZigzagIterator:
         return False
         
 
-# Your ZigzagIterator object will be instantiated and called as such:
-# i, v = ZigzagIterator(v1, v2), []
-# while i.hasNext(): v.append(i.next())
+Follow up:
+
+What if you are given k 1d vectors? How well can your code be extended to such cases?
+
+Maybe we can use a list = [True] * k to represent whether or not the kth list has been chosen?
