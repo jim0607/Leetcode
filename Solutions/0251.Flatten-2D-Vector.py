337094e69cb8f49b4b2b2ec4ee32854ce62f8bd1
Jim0607 <jinjun.ding@gmail.com>
2020-03-09 14:30:15 -0600

    Update 0251.Flatten-2D-Vector.py

--- Solutions/0251.Flatten-2D-Vector.py
+++ Solutions/0251.Flatten-2D-Vector.py
@@ -83,3 +83,25 @@ class Vector2D:
 # param_2 = obj.hasNext()
 # @lc code=end
 
+
+"""Solution 2: using a queue"""
+
+class Vector2D:
+
+    def __init__(self, v: List[List[int]]):
+        self.q = collections.deque()
+        for lst in v:
+            for num in lst:
+                self.q.append(num)
+
+    def next(self) -> int:
+        return self.q.popleft()
+
+    def hasNext(self) -> bool:
+        return len(self.q) != 0
+
+
+# Your Vector2D object will be instantiated and called as such:
+# obj = Vector2D(v)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
