a5dbb28ba63bcd2843d4c2cb98e07f4b090b475d
Jim0607 <jinjun.ding@gmail.com>
2020-04-10 15:10:36 -0700

    Update and rename 0256.Paint-House.py to 0256.Paint-House.java

--- Solutions/0256.Paint-House.java
+++ Solutions/0256.Paint-House.java
@@ -29,3 +29,24 @@ class Solution:
             dp[i][2] = costs[i][2] + min(dp[i - 1][0], dp[i - 1][1])
         
         return min(dp[-1])
+
+      
+      
+class Solution {
+    public int minCost(int[][] costs) {
+        if (costs.length == 0) return 0;
+        
+        // dp[i][j] = the min cost to paint house i to be color j
+        int[][] dp = new int[costs.length][3];
+        for (int i = 0; i < costs.length; i++) {
+            if (i == 0) {
+                dp[i] = costs[i];
+            } else {
+                dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+                dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+                dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+            }
+        }
+        return Math.min(Math.min(dp[costs.length - 1][0], dp[costs.length - 1][1]), dp[costs.length - 1][2]);
+    }
+}
