c9d8a85daddc05227769bc7d2b9987be7400c091
Jim0607 <jinjun.ding@gmail.com>
2020-04-10 01:06:13 -0700

    Update 0120.Triangle.py

--- Solutions/0120.Triangle.py
+++ Solutions/0120.Triangle.py
@@ -30,3 +30,26 @@ class Solution:
                 dp[i][j] = min(triangle[i][j] + dp[i - 1][j], triangle[i][j] + dp[i - 1][j - 1])
                 
         return min(dp[-1])
+
+
+
+Note:
+
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+"""
+Rolling array to reduce the space to O(N)
+"""
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        m = len(triangle)
+        dp = [[0] * m for _ in range(2)]
+        dp[0][m - 1] = triangle[0][0]
+        
+        for i in range(1, m):
+            dp[i % 2][m - len(triangle[i])] = dp[(i - 1) % 2][m - len(triangle[i]) + 1] + triangle[i][0]
+            dp[i % 2][-1] = dp[(i - 1) % 2][-1] + triangle[i][-1]
+            
+            for j in range(1, len(triangle[i]) - 1):
+                dp[i % 2][m - j - 1] = min(dp[(i - 1) % 2][m - j - 1], dp[(i - 1) % 2][m - j]) + triangle[i][j]
+                
+        return min(dp[(m - 1) % 2])
