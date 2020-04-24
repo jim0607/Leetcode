f2b87f530a5363dbe604112782030159dffac2bf
Jim0607 <jinjun.ding@gmail.com>
2020-04-24 11:19:50 -0700

    Update 0334.Increasing-Triplet-Subsequence.py

--- Solutions/0334.Increasing-Triplet-Subsequence.py
+++ Solutions/0334.Increasing-Triplet-Subsequence.py
@@ -3,23 +3,21 @@ O(N^2), O(N)"""
 """
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        if not nums:
-            return False
-        
         lens = len(nums)
         if lens < 3:
             return False
         
-        dp = [1] * lens
-
+        dp = [1] * lens     # dp[i]=max lens of increasing sub end with i
         for j in range(1, lens):
             for i in range(j):
                 if nums[j] > nums[i]:
                     dp[j] = max(dp[j], dp[i] + 1)
-            if dp[j] >= 3:
-                return True
-            
+                    
+                    if dp[j] >= 3:
+                        return True
+                    
         return False
+        
 """    
 """how to solve it in O(N), O(1)"""
 class Solution:
@@ -35,7 +33,7 @@ class Solution:
         min_1, min_2 = float("inf"), float("inf")
         for num in nums:
             if num < min_1:             # first renew
-                min_1 = num              
+                min_1 = num             # 很容易错写成 min1, min2 = num, min1
             elif min_1 < num < min_2:   # second renew
                 min_2 = num
             elif min_2 < num:           # third renew
