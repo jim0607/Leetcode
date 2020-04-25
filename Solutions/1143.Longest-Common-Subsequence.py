95934857dee4c18cc12bbf412da86c7949a29774
Jim0607 <jinjun.ding@gmail.com>
2020-04-24 19:32:54 -0700

    Update 1143.Longest-Common-Subsequence.py

--- Solutions/1143.Longest-Common-Subsequence.py
+++ Solutions/1143.Longest-Common-Subsequence.py
@@ -13,17 +13,18 @@ Explanation: The longest common subsequence is "ace" and its length is 3.
 """设f[i][j]为A前i个字符A[0..i)和B前j个字符[0..j)的最长公共子串的长度，注意不包括i和j，前面有一层buffer layer非常重要，就像sputtering那样重要！f[i][j]=max(f[i][j-1], f[i-1][j], f[i-1][j-1] when A[i-1]=B[j-1])"""
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        if not text1 or not text2:
+        lens1, lens2 = len(text1), len(text2)
+        if lens1 == 0 or lens2 == 0:
             return 0
         
-        m, n = len(text1), len(text2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp = [[0] * (lens2 + 1) for _ in range(lens1 + 1)]
         
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
-                
-                if text1[i-1] == text2[j-1]:
+        for i in range(1, lens1 + 1):               # 注意点1: 因为有buffer layer, 所以范围是(1, lens1 + 1)
+            for j in range(1, lens2 + 1):
+                if text1[i - 1] == text2[j - 1]:    # 注意点2: 有了buffer layer之后，dp中的i对应的是text中的i-1
                     dp[i][j] = dp[i - 1][j - 1] + 1
-                   
-        return dp[-1][-1]
+                    
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    
+        return dp[lens1][lens2]
