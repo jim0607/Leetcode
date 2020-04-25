38f071d143bfc16708861e80dfe75448e3dce77e
Jim0607 <jinjun.ding@gmail.com>
2020-04-25 12:41:02 -0700

    Update 0973.K-Closest-Points-to-Origin.py

--- Solutions/0973.K-Closest-Points-to-Origin.py
+++ Solutions/0973.K-Closest-Points-to-Origin.py
@@ -17,6 +17,7 @@ Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
 We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
 
 
+
 """以squre来构建heap就可以了，heap中的元素是(square, point)
 O(NlogN)"""
 
@@ -32,5 +33,17 @@ class Solution:
         res = []
         for _ in range(K):
             res.append(heapq.heappop(heap)[1])
+            
+        return res
+
+# the following solution is just to sort, easy to think.
+# O(NlogN)
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        points.sort(key = lambda x: (x[0]**2 + x[1]**2))
+        
+        res = []
+        for i in range(K):
+            res.append(points[i])
             
         return res
