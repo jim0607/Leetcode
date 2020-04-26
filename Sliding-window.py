c4fc73321366508be607f776fc84847f4b4a3551
Jim0607 <jinjun.ding@gmail.com>
2020-04-26 00:25:53 -0700

    Update Sliding-window.py

--- Sliding-window.py
+++ Sliding-window.py
@@ -1,13 +1,8 @@
 滑动窗口的模板：
-两层for 循环
+两层for 循环:
 for i in range(lens):
-    while j < lens:
-        if 满足条件:
-            更新带有 j 的信息
-            j += 1
-        else:
-            break
-
-    更新 res
-
+    while j < lens and 满足条件:
+        更新带有 j 的信息
+        j += 1
+    更新 res if 满足条件
     更新带有 i 的信息
