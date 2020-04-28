6bb661ee9a7f15af84e9f68a463d17a2860ccd14
Jim0607 <jinjun.ding@gmail.com>
2020-04-28 15:09:35 -0700

    Update 0042.Trapping-Rain-Water.py

--- Solutions/0042.Trapping-Rain-Water.py
+++ Solutions/0042.Trapping-Rain-Water.py
@@ -41,3 +41,44 @@ class Solution:
                 water += rightHighest - height[i]
                 
         return water
+
+    
+    
+    
+使用九章算法班中讲过的相向型双指针算法。跟山景城一姐一个意思，稍微好一点。
+整个算法的思想是计算每个位置上可以盛放的水，累加起来。
+
+记录如下几个值：
+
+left, right => 左右指针的位置
+left_max, right_max => 从左到右和从右到左到 left, right 为止，找到的最大的 height
+每次比较 left_max 和 right_max，如果 left_max 比较小，就挪动 left 到 left + 1。
+与此同时，查看 left 这个位置上能够盛放多少水，这里有两种情况：
+
+一种是 left_max > heights[left]，这种情况下，水可以盛放 left_max - heights[left] 那么多。因为右边有 right_max 挡着，左侧可以到 left_max。
+一种是 left_max <= heights[left]，这种情况下，水无法盛放，会从左侧流走，此时更新 left_max 为 heights[left]
+left_max >= right_max 的情况类似处理。
+
+时间复杂度：O(n)，空间复杂度 O(1)  
+    
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        lens = len(height)
+        if lens <= 2:
+            return 0
+        
+        left, right = 0, len(height) - 1
+        leftMax, rightMax = height[left], height[right]
+        water = 0
+        while left <= right:
+            if height[left] < height[right]:
+                leftMax = max(leftMax, height[left])
+                water += leftMax - height[left]
+                left += 1
+                
+            else:
+                rightMax = max(rightMax, height[right])
+                water += rightMax - height[right]
+                right -= 1
+                
+        return water
