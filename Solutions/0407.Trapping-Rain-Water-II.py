22fc1058c9efd9c73954757f92dd2f9fb7537da5
Jim0607 <jinjun.ding@gmail.com>
2020-04-28 18:24:51 -0700

    Update 0407.Trapping-Rain-Water-II.py

--- Solutions/0407.Trapping-Rain-Water-II.py
+++ Solutions/0407.Trapping-Rain-Water-II.py
@@ -13,16 +13,9 @@ Given the following 3x6 height map:
 
 Return 4.
 
-
 The above image represents the elevation map [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]] before the rain.
 
- 
-
-
-
 After the rain, water is trapped between the blocks. The total volume of water trapped is 4.
-
-
 
 """
 Recall 1D trapping water problem.  We are using the two pointers left and right, from each end of the array.
@@ -37,8 +30,9 @@ In order to access the min height in the array fast, we can use a heapq, from wh
 So here is the algorithm:
 Step 1: store all the outliners of the matrix in heapq (the four corners doesn't matter).  Maintain a visited set to mark all the visited locations.
 Step 2: starting from the min height position, do BFS the 4 possible moves. If found a height < the min Height, then we can store water, else we cannot store water and we should update this leaking point by putting the new height into the heapq
-Step3: do step 2 until heapq is empty
+Step3: in this way, we start from out liners to inside. do step 2 until heapq is empty
 """
+
 import heapq
 
 class Solution:
