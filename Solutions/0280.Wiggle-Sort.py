87ec7c9b2adcedb4d4ef5934d8fe3874ca20ca62
Jim0607 <jinjun.ding@gmail.com>
2020-04-30 19:42:19 -0700

    Create 0280.Wiggle-Sort.py

---
+++ Solutions/0280.Wiggle-Sort.py
@@ -0,0 +1,35 @@
+280. Wiggle Sort
+
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+Example:
+
+Input: nums = [3,5,2,1,6,4]
+Output: One possible answer is [3,5,1,6,2,4]
+
+
+"""
+从左到右扫一遍，不满足条件的交换就好了。
+需要证明的是，当我们 交换了 nums[i] 和 nums[i - 1] 以后：
+
+... nums[i - 2], nums[i], nums[i - 1]
+nums[i - 2] 不会和 nums[i] 形成逆序（不满足条件的大小关系）
+
+那假如原来是 nums[i - 2] <= nums[i - 1]，那么 nums[i - 1] 和 nums[i] 交换的条件是，nums[i - 1] <= nums[i]。
+那我们就推导出此时 nums[i] >= nums[i - 2]，因此交换之后，不会让 nums[i] 和 nums[i - 2] 的大小关系出现变化。
+
+反过来如果 nums[i - 2] >= nums[i - 1] 的情况同理。
+"""
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        lens = len(nums)     
+        prevShouldLessCurr = True
+        for i in range(1, lens):
+            if prevShouldLessCurr:
+                if nums[i - 1] > nums[i]:
+                    nums[i], nums[i - 1] = nums[i - 1], nums[i]
+            else:
+                if nums[i - 1] < nums[i]:
+                    nums[i], nums[i - 1] = nums[i - 1], nums[i]
+                    
+            prevShouldLessCurr = not prevShouldLessCurr
