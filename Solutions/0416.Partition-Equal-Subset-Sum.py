73b98763ec5f6fce94b9be40ed27868ee6fdc46b
Jim0607 <jinjun.ding@gmail.com>
2020-05-03 21:52:53 -0700

    Update 0416.Partition-Equal-Subset-Sum.py

--- Solutions/0416.Partition-Equal-Subset-Sum.py
+++ Solutions/0416.Partition-Equal-Subset-Sum.py
@@ -18,25 +18,29 @@ Explanation: The array can be partitioned as [1, 5, 5] and [11].
 """背包问题：将A中的物品放入容量为target的背包中，问是否存在？
 f[i]=将前i个物品放入背包中，能否拼出t (背包问题重量一定要入状态)
 f[i][t]=True if 不放最后一个进背包: f[i-1][t]=True or 放最后一个进背包: f[i-1][t-A[i-1]]=True"""
+"""
+dp[i][m]=can fill up to m using the nums till ith, not including i
+dp[i][m] = 1. not put nums[i-1]: dp[i-1][m] or put nums[i-1]: dp[i-1][m-nums[i-1]]
+"""
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        sumNums = sum(nums)
-        if sumNums % 2 != 0:
+        lens = len(nums)
+        sums = sum(nums)
+        if sums % 2 == 1:
             return False
         
-        target = sumNums // 2
-        lens = len(nums)
-        dp = [[False] * (target + 1) for _ in range(lens + 1)] 
+        target = sums // 2
         
+        dp = [[False for _ in range(target + 1)] for _ in range(lens + 1)]
         for i in range(lens + 1):
-            for t in range(target + 1):
-                if i == 0:
-                    dp[i][t] = True if t == 0 else False
-                    continue
-                    
-                dp[i][t] = dp[i - 1][t]
-                if t >= nums[i - 1]:
-                    dp[i][t] = dp[i][t] or dp[i - 1][t- nums[i -1]]
+            dp[i][0] = True
+        
+        for i in range(1, lens + 1):
+            for m in range(1, target + 1):
+                dp[i][m] = dp[i - 1][m]
+            
+                if m >= nums[i - 1]:
+                    dp[i][m] = dp[i][m] or dp[i - 1][m - nums[i - 1]]
                     
         return dp[lens][target]
        
