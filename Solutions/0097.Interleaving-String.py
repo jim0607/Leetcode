ec688c61825a412bd0e942199b1610b68cea1dfa
Jim0607 <jinjun.ding@gmail.com>
2020-05-03 23:52:57 -0700

    Update 0097.Interleaving-String.py

--- Solutions/0097.Interleaving-String.py
+++ Solutions/0097.Interleaving-String.py
@@ -8,6 +8,36 @@ Output: true
 
 """f[i][j]=s3的前[0..i+j)个字符能否由s1前i个字符[0..i)和s2前j个字符[0..j)交错形成
 f[i][j]=True when (s3[i+j-1]=s1[i-1] 且 f[i-1][j]=True 即s3的前[0..i+j-1)个字符能否由s1前i-1个字符[0..i-1)和s2前j个字符[0..j)交错形成) or (s3[i+j-1]=s2[j-1] and f[i][j-1]=True)"""
+
+"""
+dp[i][j]=can form?
+dp[i][j]=true if s1[i-1]==s3[i+j-1] and dp[i-1][j]=true; or if s2[j-1]==s3[i+j-1] and dp[i][j-1]=true
+"""
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        lens1, lens2, lens3 = len(s1), len(s2), len(s3)
+        if lens1 + lens2 != lens3:
+            return False
+        
+        dp = [[False for _ in range(lens2 + 1)] for _ in range(lens1 + 1)]
+        for i in range(lens1 + 1):
+            dp[i][0] = (s1[:i] == s3[:i])
+        for j in range(lens2 + 1):
+            dp[0][j] = (s2[:j] == s3[:j])
+            
+        for i in range(1, lens1 + 1):
+            for j in range(1, lens2 + 1):
+                if s1[i - 1] == s3[i + j - 1] and dp[i - 1][j]:
+                    dp[i][j] = True
+                    continue
+                if s2[j - 1] == s3[i + j - 1] and dp[i][j - 1]:
+                    dp[i][j] = True
+                    continue
+        
+        return dp[lens1][lens2]
+
+
+"""另一种initilization的方法"""
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if not s1:
