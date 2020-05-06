c4187800e98f123b0aab5e60ea8de455df8db1f8
Jim0607 <jinjun.ding@gmail.com>
2020-05-05 18:12:19 -0700

    Update 0155.Min-Stack.py

--- Solutions/0155.Min-Stack.py
+++ Solutions/0155.Min-Stack.py
@@ -19,44 +19,58 @@ minStack.top();      --> Returns 0.
 minStack.getMin();   --> Returns -2.
 
 
+"""
+2 5 4
+
+2 
+2 5 
+2 4 5     2  4
+
+st, minSt
+st     2   2 5      2 5     2 5 4
+minSt  2            2        2
+
+"""
 class MinStack:
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.stack = []
-        self.minStack = []  # use a stack to store minVal 
-        
+        self.st = []
+        self.minSt = []
+
     def push(self, x: int) -> None:
-        self.stack.append(x)
+        self.st.append(x)
         
         # push一个元素进stack之后，要判断这个元素是不是<=最小的元素，如果是，那就append进去
-        if not self.minStack or x <= self.minStack[-1]:
-            self.minStack.append(x)
+        if len(self.minSt) == 0 or x <= self.minSt[-1]:
+            self.minSt.append(x)
 
     def pop(self) -> None:
-        if not self.stack:
-            return None
-        
-        popItem = self.stack.pop()
-        
-        # pop出一个元素之后，要判断这个元素是不是最小的元素，如果是，那就要在minStack中pop出这个元素
-        if popItem == self.minStack[-1]:
-            self.minStack.pop()
+        if len(self.st) == 0:
+            raise IndexError("The stack is empty")
+        else:
+            poppedItem = self.st.pop()
+            
+            # pop出一个元素之后，要判断这个元素是不是最小的元素，如果是，那就要在minStack中pop出这个元素
+            if poppedItem == self.minSt[-1]:
+                self.minSt.pop()
+                
+            return poppedItem
 
     def top(self) -> int:
-        if not self.stack:
-            return None
-        
-        return self.stack[-1]
+        if len(self.st) == 0:
+            raise IndexError("The stack is empty")
+        else:
+            return self.st[-1]
 
     def getMin(self) -> int:
-        if not self.stack:
-            return None
-        
-        return self.minStack[-1]
-            
+        if len(self.st) == 0:
+            raise IndexError("The stack is empty")
+        else:
+            return self.minSt[-1]
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(x)
