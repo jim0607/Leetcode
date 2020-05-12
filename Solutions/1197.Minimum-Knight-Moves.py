cd196652a1ca8ee7e30690ec70458cfbe73742dc
Jim0607 <jinjun.ding@gmail.com>
2020-05-12 13:07:39 -0700

    Update 1197.Minimum-Knight-Moves.py

--- Solutions/1197.Minimum-Knight-Moves.py
+++ Solutions/1197.Minimum-Knight-Moves.py
@@ -55,37 +55,35 @@
 
 # 为了解决TLE问题，尝试利用对称性质: x, y = abs(x), abs(y); append neighbor to q only if (neighbor_x >= -2 and neighbor_y >= -2)
 class Solution:
-    MOVES = [(1, 2), (-1, -2), (2, 1), (-2, -1), (-1, 2), (1, -2), (2, -1), (-2, 1)]
+    
+    MOVES = [(-1, 2), (1, 2), (-2, 1), (2, 1), (-1, -2), (1, -2), (-2, -1), (2, -1)]
     
     def minKnightMoves(self, x: int, y: int) -> int:
-        if (x, y) == (0, 0):
+        if x == 0 and y == 0:
             return 0
         
         x, y = abs(x), abs(y)
-        steps = self.bfs(x, y)
         
-        return steps
-    
-    def bfs(self, destination_x, destination_y):
-        """bfs the board layer by layer to find the layer number of (x, y), return the layer number"""
         q = collections.deque()
         visited = set()
         q.append((0, 0))
         visited.add((0, 0))
         
-        steps = 0
+        step = 0
         while q:
-            steps += 1
+            step += 1
             lens = len(q)
+            
             for _ in range(lens):
-                (x, y) = q.popleft()
-                for delta_x, delta_y in self.MOVES:
-                    neighbor_x, neighbor_y = x + delta_x, y + delta_y
-                    if (neighbor_x, neighbor_y) == (destination_x, destination_y):
-                        return steps
-                    if (neighbor_x, neighbor_y) not in visited and (neighbor_x >= -2 and neighbor_y >= -2):
-                        q.append((neighbor_x, neighbor_y))
-                        visited.add((neighbor_x, neighbor_y))
+                curr_x, curr_y = q.popleft()
+                for move in self.MOVES:
+                    next_x, next_y = curr_x + move[0], curr_y + move[1]
+                    if (next_x, next_y) == (x, y):
+                        return step
+                    
+                    if x >= -2 and y >= -2 and (next_x, next_y) not in visited:     # 这里用x >= -2 and y >= -2 pruning
+                        q.append((next_x, next_y))
+                        visited.add((next_x, next_y))
                         
                         
 """从source和destination两端同时进行bfs!!!!
@@ -132,19 +130,23 @@ class Solution:
         return q, visited
                         
                         
-"""DP solution: so brilliant
-The basic idea is that doesn't mater where the destination is, we can always "rotate" the board in such a way that the knight only needs to make (1,2) or (2,1) move to approach the destination. In other words, the destination is always in the first quadrant. Except (0, 0), all other initial values in cache are the points that we have to temporarily "step out" of the first quadrant and then come back in order to reach them. Therefore, they have to be manually added at the beginning."""
+"""DP solution / recurssion with memorization: so brilliant
+The basic idea is that doesn't mater where the destination is, we can always "rotate" the board in such a way that the knight only needs to make (1,2) or (2,1) move to approach the destination. 
+In other words, the destination is always in the first quadrant. Except (0, 0), all other initial values in cache are the points that we have to temporarily "step out" of the first quadrant and then come back in order to reach them. 
+Therefore, they have to be manually added at the beginning."""
 
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        cache = {(0, 0): 0, (1, 1): 2, (1, 0): 3, (0, 1): 3}
+        x, y = abs(x), abs(y)
         
-        def dp(x, y):
-            if (x, y) in cache: 
-                return cache[(x, y)]
-            
-            cache[(x, y)] = min(dp(abs(x-1), abs(y-2)), dp(abs(x-2), abs(y-1))) + 1
-            
-            return cache[(x, y)]
+        memo = {(0, 0): 0, (1, 1): 2, (1, 0): 3, (0, 1): 3}
         
-        return dp(abs(x), abs(y))
+        def memorization(x, y):                
+            if (x, y) in memo:
+                return memo[(x, y)]
+
+            memo[(x, y)] = min(memorization(abs(x-2), abs(y-1)), memorization(abs(x-1), abs(y-2))) + 1
+            
+            return memo[(x, y)]
+        
+        return memorization(x, y)
