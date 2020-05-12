28b23746b14363872a03babe0d3da6506c03dc14
Jim0607 <jinjun.ding@gmail.com>
2020-05-11 23:57:59 -0700

    Update 0215.Kth-Largest-Element-in-an-Array.py

--- Solutions/0215.Kth-Largest-Element-in-an-Array.py
+++ Solutions/0215.Kth-Largest-Element-in-an-Array.py
@@ -35,6 +35,50 @@
 #
 """use quick select method
 Python quick select (use partition) average time O(N) and worst O(N^2)"""
+
+
+
+"""  Celia's template for partition  """
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums or k < 1 or k > len(nums):
+            return None
+
+        return self.partition(nums, 0, len(nums) - 1, len(nums) - k)
+
+    def partition(self, nums, start, end, k):
+        if start == end:
+            return nums[k]
+
+        left, right = start, end
+        pivot = nums[(start + end) // 2]
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1 
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        if k <= right:
+            return self.partition(nums, start, right, k)
+        if k >= left:
+            return self.partition(nums, left, end, k)
+
+        return nums[k]
+
+
+
+
+
+
+
+
+
+
+
 # @lc code=start
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
