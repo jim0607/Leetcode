a49c01a5767be8145e7825b6a1ba44c9e0ffbf09
Jim0607 <jinjun.ding@gmail.com>
2020-05-14 12:42:15 -0700

    Update 0112.Path-Sum.py

--- Solutions/0112.Path-Sum.py
+++ Solutions/0112.Path-Sum.py
@@ -16,6 +16,40 @@ Given the below binary tree and sum = 22,
 return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
 
+"""
+Solution 1: similar with 257, find all the paths and put all the pathSums in a set. 
+"""
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        
+        def helper(root):
+            """
+            return a set of all the possible sums in the tree rooted as root
+            """
+            if not root:
+                return {float("inf")}       # 注意这里不要返回{0}, 因为我们的sum的终点必须是叶子节点，而不是None节点，想想一个[1,2], sum=1的例子，我们不能返回True因为右节点不是叶子节点而是None
+            if not root.left and not root.right:
+                return {root.val}
+
+            leftPathSums = helper(root.left)
+            rightPathSums = helper(root.right)
+        
+            rootPathSums = set()
+            for leftPathSum in leftPathSums:
+                rootPathSums.add(leftPathSum + root.val)
+            for rightPathSum in rightPathSums:
+                rootPathSums.add(rightPathSum + root.val)
+                
+            return rootPathSums
+        
+        return sum in helper(root)
+
+
+"""
+Solution 2: I would say this is good ans smart!
+"""
 class Solution:
     # the function returns whether of not starting from root, there is a path sums to target
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
