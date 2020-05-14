4321d61a255cd2037203676ca4304fb65a50eed0
Jim0607 <jinjun.ding@gmail.com>
2020-05-14 15:02:08 -0700

    Update 0110.Balanced-Binary-Tree.py

--- Solutions/0110.Balanced-Binary-Tree.py
+++ Solutions/0110.Balanced-Binary-Tree.py
@@ -61,27 +61,27 @@ class Solution:
 # this is a good example of ResultType in Python. 也就是需要return不止一个值的时候，我们需要将result打包起来。
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        return self.helper(root)[0]        
-        
-    # 1. 递归的定义：返回以root为根的二叉树（是不是balanced, 高度）    
-    def helper(self, root: TreeNode) -> (bool, int):
-        # 3. 递归的出口：an empty tree is balanced and has a height=-1
         if not root:
-            return True, -1
-        # 养成好习惯，判断一下
-        if not root.left and not root.right:
-            return True, 0
+            return True
         
-        # 2. 递归的拆解divide
-        leftBalanced, leftHeight = self.helper(root.left)
-        rightBalanced, rightHeight = self.helper(root.right)
+        def helper(root):
+            """
+            return (if the tree is balanced, maxDepth)
+            """
+            if not root:
+                return (True, 0)
+            
+            leftIsBalan, leftMaxDepth = helper(root.left)
+            rightIsBalan, rightMaxDepth = helper(root.right)
+            
+            rootIsBalan = leftIsBalan and rightIsBalan and abs(leftMaxDepth - rightMaxDepth) <= 1
+            rootMaxDepth = max(leftMaxDepth, rightMaxDepth) + 1
+            
+            return (rootIsBalan, rootMaxDepth)
         
-        # 2. 递归的拆解merge
-        if not leftBalanced or not rightBalanced or abs(leftHeight - rightHeight) > 1:
-            return False, -1
-        
-        return True, 1 + max(leftHeight, rightHeight)
-
+        return helper(root)[0]
+       
+       
 Time complexity : O(n)
 For every subtree, we compute its height in constant time as well as compare the height of its children.
 Space complexity : O(n). The recursion stack may go up to O(n) if the tree is unbalanced.
