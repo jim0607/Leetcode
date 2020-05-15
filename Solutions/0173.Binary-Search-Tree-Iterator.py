818750c82675d6d74ee398185c4ba69de555270a
Jim0607 <jinjun.ding@gmail.com>
2020-05-14 19:38:24 -0700

    Update 0173.Binary-Search-Tree-Iterator.py

--- Solutions/0173.Binary-Search-Tree-Iterator.py
+++ Solutions/0173.Binary-Search-Tree-Iterator.py
@@ -23,7 +23,41 @@ next() and hasNext() should run in average O(1) time and uses O(h) memory, where
 You may assume that next() call will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
 
 
-"""Solution 1: use queue"""
+"""solution 2: use a stack with controlled recursion, some part of the algorithm is similar with the in order traversal of a tree using a stack
+this algorithm has space complexity of O(h)"""
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self.root = root
+        
+        self.getLeftMost(self.root)
+        
+    def getLeftMost(self, root):
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        smallestNode = self.stack.pop()
+        if smallestNode.right:
+            self.getLeftMost(smallestNode.right)
+            
+        return smallestNode.val
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.stack) != 0
+
+
+
+
+"""Solution 1: use queue - trivial solution """
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -62,46 +96,3 @@ class BSTIterator:
         @return whether we have a next smallest number
         """
         return len(self.q) != 0
-
-
-# Your BSTIterator object will be instantiated and called as such:
-# obj = BSTIterator(root)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
-
-"""solution 2: use a stack with controlled recursion, some part of the algorithm is similar with the in order traversal of a tree using a stack
-this algorithm has space complexity of O(h)"""
-class BSTIterator:
-
-    def __init__(self, root: TreeNode):
-        self.stack = []
-        self.root = root
-        
-        self.getLeftMost(self.root)
-        
-    def getLeftMost(self, root):
-        while root:
-            self.stack.append(root)
-            root = root.left
-
-    def next(self) -> int:
-        """
-        @return the next smallest number
-        """
-        smallestNode = self.stack.pop()
-        if smallestNode.right:
-            self.getLeftMost(smallestNode.right)
-            
-        return smallestNode.val
-
-    def hasNext(self) -> bool:
-        """
-        @return whether we have a next smallest number
-        """
-        return len(self.stack) != 0
-
-
-# Your BSTIterator object will be instantiated and called as such:
-# obj = BSTIterator(root)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
