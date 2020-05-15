49fc9d9c320fde60969ee8dcd534e3a752c83ef4
Jim0607 <jinjun.ding@gmail.com>
2020-05-14 19:23:04 -0700

    Update 0114.Flatten-Binary-Tree-to-Linked-List.py

--- Solutions/0114.Flatten-Binary-Tree-to-Linked-List.py
+++ Solutions/0114.Flatten-Binary-Tree-to-Linked-List.py
@@ -22,12 +22,32 @@ The flattened tree should look like:
           6
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+"""方法二：divide and conquer"""
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return None
+        
+        leftHead = self.flatten(root.left)
+        rightHead = self.flatten(root.right)
+        
+        temp = rightHead
+        root.right = leftHead
+        root.left = None
+        
+        tail = root
+        while tail.right:
+            tail = tail.right
+            
+        tail.right = temp
+        
+        return root
+
+            
+            
 
 """If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal."""
 class Solution:
@@ -54,45 +74,3 @@ class Solution:
         # pre-order 3. right
         self.flatten(right)
         
-        
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-"""方法二：divide and conquer"""
-class Solution:
-    def flatten(self, root: TreeNode) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        self.helper(root)
-        
-    def helper(self, root: TreeNode) -> "TreeNode":
-        """
-        return the last node of root
-        """
-        if not root:
-            return
-        
-        # divide
-        left_last = self.helper(root.left)
-        right_last = self.helper(root.right)
-        
-        # conquer
-        # connect the left_last with the right_first, which is root.right
-        if left_last:
-            left_last.right = root.right
-            root.right = root.left
-            root.left = None
-        
-        # 这里顺序很重要，一定是先return right_last，再return left_last，都为None的话return root，因为如果把root排好了之后right_last应该是最后一个节点了
-        if right_last: 
-            return right_last
-        
-        if left_last:
-            return left_last
-
-        return root
