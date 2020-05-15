adcfcb320f53fe025174742c886dfd2c935cc6cb
Jim0607 <jinjun.ding@gmail.com>
2020-05-15 12:40:40 -0700

    Update 0078.Subsets.py

--- Solutions/0078.Subsets.py
+++ Solutions/0078.Subsets.py
@@ -43,7 +43,7 @@ O(N*S), S是solution的个数，这里S=2^N"""
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        n = len(nums)
+
         self.dfs(nums, 0, [], res)
         
         return res
@@ -58,8 +58,8 @@ class Solution:
             self.dfs(nums, i + 1, curr, res)    # 从nums中的start位置开始，挑选一些数，放到curr（此时为[1,2]）中，然后存入res里面。这就相当于把所有的以[1, 2]开头的子集都找到，且放进res里面
             curr.pop()                          # [1, 2] -> [1]，然后在进入for循环，然后curr再append 2后面的数 3, curr 变成[1, 3]，然后......周而复始，直至for循环结束。
 
-
-
+            
+            
 
 模板二：花花的模板，更加通用化，可以通过比较输出结果的数组排列顺序直到区别。
 上面程序的输出是[[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]]
