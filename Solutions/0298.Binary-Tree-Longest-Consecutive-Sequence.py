439e3361d6da547af63b396df2d5effb99bb9621
Jim0607 <jinjun.ding@gmail.com>
2020-05-16 17:36:14 -0700

    Update 0298.Binary-Tree-Longest-Consecutive-Sequence.py

--- Solutions/0298.Binary-Tree-Longest-Consecutive-Sequence.py
+++ Solutions/0298.Binary-Tree-Longest-Consecutive-Sequence.py
@@ -36,6 +36,9 @@ Output: 2
 Explanation: Longest consecutive sequence path is 2-3, not 3-2-1, so return 2.
 
 
+"""
+solution 1: dfs + backtracking using recurssion
+"""
 class Solution:
     def longestConsecutive(self, root: TreeNode) -> int:
         if not root:
@@ -62,3 +65,64 @@ class Solution:
                 
             else:   # 如果断掉了，就重新开始搜
                 self.backtracking(node, 1)
+                  
+                  
+                  
+"""
+Solution 2: interative way to implement dfs + backtrack
+"""        
+class Solution:
+    def longestConsecutive(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        stack = [(root, 1)]
+        res = 1
+        while stack:
+            root, curr = stack.pop()
+            res = max(res, curr)
+            
+            for node in (root.left, root.right):
+                if not node:
+                    continue
+                    
+                if node.val == root.val + 1:
+                    stack.append((node, curr + 1))
+                else:
+                    stack.append((node, 1))
+                    
+        return res
+
+
+"""
+soluiton 3: divide and conquer
+"""
+class Solution:
+    def longestConsecutive(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        self.res = 1
+        self.helper(root)
+        
+        return self.res
+        
+    def helper(self, root):
+        """
+        return the Longest Consecutive Sequence started with root node
+        """
+        if not root:
+            return 0
+
+        leftLCS = self.helper(root.left)
+        rightLCS = self.helper(root.right)
+
+        rootLCS = 1
+        if root.left and root.left.val == root.val + 1:
+            rootLCS = max(rootLCS, leftLCS + 1)
+        if root.right and root.right.val == root.val + 1:
+            rootLCS = max(rootLCS, rightLCS + 1)
+
+        self.res = max(self.res, rootLCS)
+
+        return rootLCS
