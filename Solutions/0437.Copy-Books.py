2c267c543d1dd0ae746e685b6ee76f1aa6071bc6
Jim0607 <jinjun.ding@gmail.com>
2020-05-19 19:21:57 -0700

    Update 0437.Copy-Books.py

--- Solutions/0437.Copy-Books.py
+++ Solutions/0437.Copy-Books.py
@@ -17,6 +17,48 @@ Explanation:
     Second person spends 4 minutes to copy book 3.
 
 
+    
+"""
+binary search: O(nlog(sum(pages)-max(pages))), n is the number of books
+"""
+class Solution:
+    def copyBooks(self, pages, k):
+        """
+        k is the number of person to copy the books
+        """
+        if not pages:
+            return 0
+            
+        startTime, endTime = max(pages), sum(pages)
+        while startTime + 1 < endTime:
+            midTime = startTime + (endTime - startTime) // 2
+            if self.leastPersonNeeded(pages, midTime) <= k:
+                endTime = midTime
+            else:
+                startTime = midTime
+                
+        return startTime if self.leastPersonNeeded(pages, startTime) <= k else endTime
+        
+    def leastPersonNeeded(self, pages, timeLimit):
+        """
+        return the least person needed in order to finish all the pages in the timeLimit.  Algorithm: greedy. 每次发现要超时了就加一个人。
+        sweep the books and keep track of the timeNeeded. 
+        when we find we cannot finish all pages in timeLimit, we add a new person and set the timeNeeded for the new person to be zero
+        """
+        personNeeded = 1
+        timeNeeded = 0      # initialize the timeNeeded for person 1 is zero
+        for pages in pages:
+            # whenever timeNeeded is over the timelimit, we need a new person to join and help, so that we can reduce the timeCost within timeLimit
+            # 每次发现要超时了就加一个人
+            if timeNeeded + page > timeLimit:    
+                personNeeded += 1
+                timeNeeded = 0        # timeNeeded resets to 0 for the new persion joined
+            timeNeeded += page
+            
+        return personNeeded
+    
+    
+    
 
 """需要找到一种分段方式，使得所有段的数字之和的最大值最小
 f[k][i]=前k个(不包括k)抄写员最多需要多长时间抄完前i本书(不包括i)
