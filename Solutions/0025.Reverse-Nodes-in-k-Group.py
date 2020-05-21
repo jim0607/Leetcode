48354ac05d9063c2a9dd80690361e0bc96a40bff
Jim0607 <jinjun.ding@gmail.com>
2020-05-21 13:45:22 -0700

    Update 0025.Reverse-Nodes-in-k-Group.py

--- Solutions/0025.Reverse-Nodes-in-k-Group.py
+++ Solutions/0025.Reverse-Nodes-in-k-Group.py
@@ -80,3 +80,37 @@ class Solution:
         n1.next = n_k_plus
         
         return n1
+
+    
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        """
+        reverse the list started with head
+        return new head
+        """
+        # step 1: exit of recursion: when less than k elements left, return head directly
+        if not head or not head.next:
+            return head
+        currNode = head
+        for _ in range(k - 1):
+            currNode = currNode.next
+            if not currNode:
+                return head
+            
+        # step 2: revursivey find the new head of the reversed list
+        nextGroupHead = self.reverseKGroup(currNode.next, k)
+        
+        # step 3: reverse the first k elements
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        prev, curr = None, head
+        for _ in range(k):
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        # step 4: hook up the reversedNewHead with head
+        head.next = nextGroupHead
+        
+        return prev
