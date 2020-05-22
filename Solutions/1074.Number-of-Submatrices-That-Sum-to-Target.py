2cf91662d3f268f02c82e53b7597b4c34ac4b498
Jim0607 <jinjun.ding@gmail.com>
2020-05-21 22:32:32 -0700

    Update 1074.Number-of-Submatrices-That-Sum-to-Target.py

--- Solutions/1074.Number-of-Submatrices-That-Sum-to-Target.py
+++ Solutions/1074.Number-of-Submatrices-That-Sum-to-Target.py
@@ -18,6 +18,69 @@ Output: 5
 Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
 
 
+
+    
+    
+"""
+solution 2: same idea O(M*M*N)
+也可以先把行处理好，让每一行里面保存上面所有行的和
+接下来就是在每一行里面去求560问题了，注意一点不同的是需要遍历upRow和downRow的, 如果不遍历就是solution 3的错误写法
+举一个反例想明白solution 3为什么行不通，自然就会改成solution 2了
+"""   
+class Solution:
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        for row in range(1, m):
+            for col in range(n):
+                matrix[row][col] += matrix[row - 1][col]
+            
+        cnt = 0
+        for upRow in range(m):
+            for downRow in range(upRow, m):
+                prefixSumDict = collections.defaultdict(int)
+                prefixSumDict[0] = 1
+                prefixSum = 0
+                for col in range(n):
+                    prefixSum += matrix[downRow][col] - (matrix[upRow-1][col] if upRow >= 1 else 0)
+                    if prefixSum - target in prefixSumDict:
+                        cnt += prefixSumDict[prefixSum - target]
+                    prefixSumDict[prefixSum] += 1
+                
+        return cnt
+
+
+    
+"""
+solution 3: 行不通的solution: 让每一行里面保存上面所有行的和， 接下来就是在每一行里面去求560问题了
+举一个反例想明白solution 3为什么行不通，自然就会改成solution 2了
+反例: [[5, 1], [1, 5]], 如果简单地认为不用遍历upRow和downRow，那就会丢掉下面一行[1, 5]的信息，从而输出3而不是4
+"""   
+class Solution:
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        for row in range(1, m):
+            for col in range(n):
+                matrix[row][col] += matrix[row - 1][col]
+            
+        # 下面与560一模一样
+        cnt = 0
+        for row in range(m):
+            prefixSumDict = collections.defaultdict(int)
+            prefixSumDict[0] = 1
+            prefixSum = 0
+            for num in matrix[row]:
+                prefixSum += num
+                if prefixSum - target in prefixSumDict:
+                    cnt += prefixSumDict[prefixSum - target]
+                prefixSumDict[prefixSum] += 1
+                
+        return cnt
+    
+    
+    
+    
+    
+    
 """
 用前缀和优化, 令 matrix[i][0] = matrix[i][0] + matrix[i][1] + ... + matrix[i][j], 这样matrix的行里保存的就是上面所有列的和了
 然后枚举左右边界left and right, 确定左右边界left and right 之后，接下来就相当于在一列内(指的是right那一列), 求一个数组连续子串和为0的问题了
