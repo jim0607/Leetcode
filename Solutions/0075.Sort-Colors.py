60accc76f58305b95a33eafaaa25f6413c129e37
Jim0607 <jinjun.ding@gmail.com>
2020-05-22 14:58:43 -0700

    Update 0075.Sort-Colors.py

--- Solutions/0075.Sort-Colors.py
+++ Solutions/0075.Sort-Colors.py
@@ -84,7 +84,8 @@ class Solution:
 设立三根指针，left, index, right。定义如下规则：
 left 的左侧都是 0（不含 left）
 right 的右侧都是 2（不含 right）
-index 从左到右扫描每个数，如果碰到 0 就丢给 left，碰到 2 就丢给 right。碰到 1 就跳过不管。"""
+index 从左到右扫描每个数，如果碰到 0 就丢给 left，碰到 2 就丢给 right。碰到 1 就跳过不管。
+这种解法相当于设置了left, right两个anchor"""
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
