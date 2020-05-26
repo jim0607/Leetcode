34aa44dc52f6a4920c2d95ebd893e6ecbe1aab10
Jim0607 <jinjun.ding@gmail.com>
2020-05-26 00:46:24 -0700

    Update 0567.Permutation-in-String.py

--- Solutions/0567.Permutation-in-String.py
+++ Solutions/0567.Permutation-in-String.py
@@ -20,8 +20,35 @@ The length of both given strings is in range [1, 10,000].
 
 
 
+
 """
-solution 1: sliding window O(M+26*(N-M))
+套用九章模板 O(M+N) if cnt1 == cnt2 is O(1)
+"""
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cnt1 = collections.Counter(s1)
+        cnt2 = collections.Counter()
+            
+        j = 0
+        for i in range(len(s2)):
+            while j < len(s2) and j - i < len(s1):
+                cnt2[s2[j]] += 1
+                j += 1
+            
+            if cnt1 == cnt2:
+                return True
+            
+            cnt2[s2[i]] -= 1
+            if cnt2[s2[i]] == 0:
+                del cnt2[s2[i]]
+            
+        return False
+
+
+       
+
+"""
+solution 2: sliding window with fix lens: O(M+M*(N-M))
 """
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
@@ -29,12 +56,12 @@ class Solution:
         for ch in s1:
             cntDict[ch] += 1
             
-        for i in range(len(s2)-len(s1)+1):
+        for i in range(len(s2)-len(s1)+1):  # O(N-M)
             tempDict = collections.defaultdict(int)
-            for ch in s2[i:i+len(s1)]:
+            for ch in s2[i:i+len(s1)]:    # O(M)
                 tempDict[ch] += 1
 
-            if tempDict == cntDict:
+            if tempDict == cntDict:       # O(?)
                 return True
             
         return False
