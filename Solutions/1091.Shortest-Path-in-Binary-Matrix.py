91d4a49d1c20259cf7659746e815c54b5d683f97
Jim0607 <jinjun.ding@gmail.com>
2020-05-29 01:43:57 -0700

    Update 1091.Shortest-Path-in-Binary-Matrix.py

--- Solutions/1091.Shortest-Path-in-Binary-Matrix.py
+++ Solutions/1091.Shortest-Path-in-Binary-Matrix.py
@@ -71,7 +71,9 @@ class Solution:
         
         return False
         
-        
+"""
+bfs: O(N) where N is the number of nodes in the graph
+"""    
         
         
 """
@@ -84,11 +86,58 @@ g((i,j)) = shortest path length found so far from start to (i, j)
 h((i,j)) = estimation to reach goal - bottom-right cell. For cell (i, j) it is calculated simply by max(m - i - 1, n - j - 1).
 Important for A* search is that h((i,j)) does not overestimate the actual cost to get to the goal.
 """
+"""
+A*: time complexity: 不好算，depends on the graph (how 0s and 1s are distributed)
+"""
+from heapq import *
+class Solution:
+    MOVES = [(1, 0), (-1, 0), (1, 1), (-1, 1), (0, 1), (0, -1), (1, -1), (-1, -1)]
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        source = (0, 0)
+        target = (m-1, n-1)
+        if grid[0][0] == 1 or grid[m-1][n-1] == 1:
+            return -1
+        
+        steps = [[999999] * n for _ in range(m)]     # 记录从source到(i, j)的步数，这个extra space在A*是必须的，用空间换时间
+        steps[0][0] = 1
+        
+        hq = []
+        # heapq stores (1. heustic estimation of min # of steps from source to target if 经过currNode, 
+        # 2. currNode_pos, 3. steps from source to currNode)
+        heappush(hq, (max(m, n), source, 1))  # max(m, n) is a heuristic ⁄hjuˈrɪstɪk⁄ estimate of the distance of source node to target, not accurate, but works
+        while hq:
+            heuristicEstimate, (curr_x, curr_y), step = heappop(hq)  # 精华，每次都pop出从source到target的最小步数(by heuristic estimation)
+            steps[curr_x][curr_y] = step
+            
+            if (curr_x, curr_y) == target:
+                return steps[curr_x][curr_y]
 
-
-
-
-
+            for delta_x, delta_y in self.MOVES:
+                next_x, next_y = curr_x + delta_x, curr_y + delta_y
+                if self.isNotValid(next_x, next_y, grid):
+                    continue
+                # 这一句是精华，如果之前访问过(next_x, next_y)，但是这次访问的时候发现能更快到达，
+                # 那就把这个(next_x, next_y)重新再次放进去（这就是为什么不要用visited set in A*），并更新从source到(next_x, next_y)的距离
+                if step + 1 < steps[next_x][next_y]:    
+                    heapq.heappush(hq, (step + max(m - next_x, n - next_y), (next_x, next_y), step + 1))
+                    steps[next_x][next_y] = step + 1    # 更新从source到(next_x, next_y)的距离
+       
+        return -1
+    
+    def isNotValid(self, x, y, grid):
+        m, n = len(grid), len(grid[0])
+        if x < 0 or x >= m or y < 0 or y >= n:
+            return True
+        if grid[x][y] == 1:
+            return True
+        
+        return False
+"""
+想想这个题的执行顺序for this example: [[0,1,0,1,0],[1,0,0,0,1],[0,0,1,1,1],[0,0,0,0,0],[1,0,1,0,0]]
+还真和wiki上的动画执行顺序一模一样
+https://en.wikipedia.org/wiki/A*_search_algorithm
+"""
 
 
 
