1d9b4402cea90e7b8d43d229827e1787248c8da2
Jim0607 <jinjun.ding@gmail.com>
2020-06-02 22:42:31 -0700

    Update 0076.Minimum-Window-Substring.py

--- Solutions/0076.Minimum-Window-Substring.py
+++ Solutions/0076.Minimum-Window-Substring.py
@@ -11,7 +11,7 @@ Note:
 If there is no such window in S that covers all characters in T, return the empty string .
 If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
 
-""""""
+"""
 """
 below solution is easy, but time comsuming O(MN) 
 """
