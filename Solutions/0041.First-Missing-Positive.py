1f5774f6391d40e94c69ecfcb03cd772329690b1
Jim0607 <jinjun.ding@gmail.com>
2020-06-16 00:11:00 -0700

    Create 0041.First-Missing-Positive.py

---
+++ Solutions/0041.First-Missing-Positive.py
@@ -0,0 +1,51 @@
+41. First Missing Positive
+
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)   # the answer must be in range 1 to n
+        # Base case.
+        if 1 not in nums:
+            return 1
+        if n == 1:     # nums = [1]
+            return 2
+        
+        # 1st pass: change all negtive numbers to be 1, so that there will be no negtive numbers
+        # [3,4,-1,1] --> [3,4,1,1]
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = 1
+
+        print(nums)
+        # 2nd pass: change the positive numbers into negative [3,4,1,1] --> [-3,-4,1,-1]
+        for num in nums:
+            idx = abs(num)
+            if idx == n:
+                nums[0] = - abs(nums[0])
+            else:
+                nums[idx] = -abs(nums[idx])
+                
+        # 3rd pass: find the first positive number, and the corresponding idx is missing
+        for idx in range(1, n):
+            if nums[idx] > 0:
+                return idx
+            
+        return n + 1 if nums[0] < 0 else n
