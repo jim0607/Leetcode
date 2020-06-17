b6b81f3e53755be397a832281576e3f8a70f91e8
Jim0607 <jinjun.ding@gmail.com>
2020-06-17 00:33:49 -0700

    Update 0227.Basic-Calculator-II.py

--- Solutions/0227.Basic-Calculator-II.py
+++ Solutions/0227.Basic-Calculator-II.py
@@ -55,3 +55,105 @@ class Solution:
                 prevSign = s[i]
                 
         return res + prevNum
+    
+    
+Google 06/2020 真题  
+第四轮：实现一个calculator，语法是 (operator node0 node1...),  op只能是加法或者是乘法, node可以是数字或者另一个(operator node0 ....)。
+比如(+ 9 1) = 10,
+(+ 2 (* 1 (+ 1 2)) 3) = 2 + (1*3) + 3 = 8
+(+ 2 3 3) 2 + 3 + 3 = 8
+
+followup是node 还可以是 if(a b c) 如果a！=0 是b 反之是c。要自己设计输入和testcase。
+思路就是recursive的找（），类似蠡口幺零九六。
+public static int calculate(String str) {
+   if(str == null) return 0;
+   Stack<String> opStack = new Stack<>();
+   Stack<String> numStack = new Stack<>();
+   int index = 0;
+   int num = 0;
+   while(index < str.length()) {
+       if(str.charAt(index) == '(') {
+           numStack.push("(");
+           index++;
+       } else if (str.charAt(index) == '+' || str.charAt(index) == '*') {
+           opStack.push(str.charAt(index) + "");
+           index++;
+       } else if (str.charAt(index) >= '0' && str.charAt(index) <= '9') {
+           while(index < str.length() && str.charAt(index) >= '0' && str.charAt(index) <= '9') {
+               num = 10 * num + str.charAt(index) + 0 - '0';
+               index++;
+           }
+           numStack.push(num + "");
+           num = 0;
+       } else if (str.charAt(index) == ')') {
+           String operator = opStack.pop();
+           int num1 = Integer.parseInt(numStack.pop());
+           int num2 = Integer.parseInt(numStack.pop());
+           int res = operate(operator, num1, num2);
+           while(!numStack.peek().equals("(")) {
+               res = operate(operator, res, Integer.parseInt(numStack.pop()));
+           }
+           numStack.pop();
+           numStack.push(res + "");
+           index++;
+       } else {
+           index++;
+       }
+   }
+   return Integer.parseInt(numStack.pop());
+}
+
+public static int calculateFollowUp(String str) {
+   if(str == null) return 0;
+   Stack<String> opStack = new Stack<>();
+   Stack<String> numStack = new Stack<>();
+   int index = 0;
+   int num = 0;
+   while(index < str.length()) {
+       if(str.charAt(index) == '(') {
+           numStack.push("(");
+           index++;
+       } else if (str.charAt(index) == '+' || str.charAt(index) == '*') {
+           opStack.push(str.charAt(index) + "");
+           index++;
+       } else if (str.charAt(index) == 'i') {
+           opStack.push("if");
+           index++;
+           index++;
+       } else if (str.charAt(index) >= '0' && str.charAt(index) <= '9') {
+           while(index < str.length() && str.charAt(index) >= '0' && str.charAt(index) <= '9') {
+               num = 10 * num + str.charAt(index) + 0 - '0';
+               index++;
+           }
+           numStack.push(num + "");
+           num = 0;
+       } else if (str.charAt(index) == ')') {
+           String operator = opStack.pop();
+           int res = 0;
+           if(operator.equals("if")) {
+               int num1 = Integer.parseInt(numStack.pop());
+               int num2 = Integer.parseInt(numStack.pop());
+               int num3 = Integer.parseInt(numStack.pop());
+               if(num3 != 0) {
+                   res = num2;
+               } else {
+                   res = num1;
+               }
+           } else {
+               int num1 = Integer.parseInt(numStack.pop());
+               int num2 = Integer.parseInt(numStack.pop());
+               res = operate(operator, num1, num2);
+               while(!numStack.peek().equals("(")) {
+                   res = operate(operator, res, Integer.parseInt(numStack.pop()));
+               }
+           }
+           numStack.pop();
+           numStack.push(res + "");
+           index++;
+       } else {
+           index++;
+       }
+   }
+   return Integer.parseInt(numStack.pop());
+}
+
