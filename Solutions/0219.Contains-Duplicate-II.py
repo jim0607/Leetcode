5f24e667abb18e92688b17b5d481fb3ef0a70d28
Jim0607 <jinjun.ding@gmail.com>
2020-06-17 11:45:43 -0700

    Update 0219.Contains-Duplicate-II.py

--- Solutions/0219.Contains-Duplicate-II.py
+++ Solutions/0219.Contains-Duplicate-II.py
@@ -19,26 +19,22 @@ Output: false
 
 """
 solution 1: hash set to store the positions where the same num appears
-O(N), O(N)
+O(N), O(M), where N is the number of num in nums, M is the number of distinct num in nums
 """
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        posDict = collections.defaultdict(list)
+        posDict = collections.defaultdict(int)
         for i, num in enumerate(nums):
-            posDict[num].append(i)
+            if num in posDict:
+                if i - posDict[num] <= k:
+                    return True
+            posDict[num] = i
             
-        # note that the lst in the posDict is already sorted
-        for num, lst in posDict.items():
-            if len(lst) >= 2:
-                for i in range(len(lst)-1):
-                    if lst[i+1] - lst[i] <= k:
-                        return True
-                    
         return False
 
 
 """
-sliding window: fix the sliding window to be k.  O(N), O(k)
+sliding window: fix the sliding window to be k.  O(N), O(k), where k is the size of the window
 """
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
@@ -48,10 +44,9 @@ class Solution:
             while j < len(nums) and j - i <= k:
                 if nums[j] in numSet:
                     return True
-                else:
-                    numSet.add(nums[j])
+                numSet.add(nums[j])
                 j += 1
-                    
-            numSet.remove(nums[i])
             
+            numSet.remove(nums[i])
+                
         return False
