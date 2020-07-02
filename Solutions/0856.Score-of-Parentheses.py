3ec086c3f86d011a653a91986d5bfe7d58fd2558
Jim0607 <jinjun.ding@gmail.com>
2020-07-01 18:19:10 -0700

    Update 0856.Score-of-Parentheses.py

--- Solutions/0856.Score-of-Parentheses.py
+++ Solutions/0856.Score-of-Parentheses.py
@@ -30,21 +30,16 @@ stack is good for parentheses problems
 class Solution:
     def scoreOfParentheses(self, S: str) -> int:
         res = 0
-        st = [0]         # store how many "(" needs to be paired and the res so far
-        for i in range(1, len(S)):
+        st = [0]         # store how many "(" needs to be paired and the res so far, 初始化先放一个dummy val进去垫底
+        for i in range(len(S)):
             if S[i] == "(":
-                st.append(0)    # "(" 表示开始新一轮的计算了
+                st.append(0)        # "(" 表示开始新一轮的计算了
+                
             if S[i] == ")":
                 top = st.pop()
-                if st:
-                    if S[i - 1] == ")":
-                        st[-1] += 2 * top
-                    else:
-                        st[-1] += top + 1
-                else:           # 如果为空了表示到目前为止左括号和右括号匹配了，也就是"()()"前面两个括号处理完了 
-                    if S[i - 1] == ")":
-                        res += 2 * top
-                    else:
-                        res += top + 1
+                if S[i - 1] == ")":
+                    st[-1] += 2 * top
+                else:
+                    st[-1] += top + 1
                         
-        return res
+        return st[-1]
