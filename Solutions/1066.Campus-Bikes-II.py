df288a4b8ba93b996b4a04bb715f9c35ed5a4ffe
Jim0607 <jinjun.ding@gmail.com>
2020-07-07 12:11:43 -0700

    Create 1066.Campus-Bikes-II.py

---
+++ Solutions/1066.Campus-Bikes-II.py
@@ -0,0 +1,65 @@
+1066. Campus Bikes II
+
+On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. Each worker and bike is a 2D coordinate on this grid.
+
+We assign one unique bike to each worker so that the sum of the Manhattan distances between each worker and their assigned bike is minimized.
+
+The Manhattan distance between two points p1 and p2 is Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
+
+Return the minimum possible sum of Manhattan distances between each worker and their assigned bike.
+
+ 
+
+Example 1:
+
+
+
+Input: workers = [[0,0],[2,1]], bikes = [[1,2],[3,3]]
+Output: 6
+Explanation: 
+We assign bike 0 to worker 0, bike 1 to worker 1. The Manhattan distance of both assignments is 3, so the output is 6.
+Example 2:
+
+
+
+Input: workers = [[0,0],[1,1],[2,0]], bikes = [[1,0],[2,2],[2,1]]
+Output: 4
+Explanation: 
+We first assign bike 0 to worker 0, then assign bike 1 to worker 1 or worker 2, bike 2 to worker 2 or worker 1. Both assignments lead to sum of the Manhattan distances as 4.
+
+
+
+class Solution:
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:   
+        def dist(w, b):
+            return abs(w[0] - b[0]) + abs(w[1] - b[1])
+        
+        def dfs(curr_w, assigned_bikes, memo):
+            """
+            curr_w: curr idx for workers
+            """
+            if curr_w == len(workers):
+                return 0
+            
+            if (curr_w, tuple(assigned_bikes)) in memo:     # change set to tuple cuz set is not hashable
+                return memo[(curr_w, tuple(assigned_bikes))]
+            
+            res = float("inf")
+            for next_b in range(len(bikes)):
+                if next_b in assigned_bikes:
+                    continue
+                
+                next_dist = dist(workers[curr_w], bikes[next_b])
+                
+                assigned_bikes.add(next_b)
+                res = min(res, next_dist + dfs(curr_w + 1, assigned_bikes, memo))
+                assigned_bikes.remove(next_b)       # backtrack, 指数级别的复杂度
+                
+            memo[(curr_w, tuple(assigned_bikes))] = res
+            
+            return res
+        
+        memo = collections.defaultdict()
+        assigned_bikes = set()
+        
+        return dfs(0, assigned_bikes, memo)
