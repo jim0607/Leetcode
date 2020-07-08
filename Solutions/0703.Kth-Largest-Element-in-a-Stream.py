4998ffc6bd1c7369e5eb920b40ca8bcafd41d229
Jim0607 <jinjun.ding@gmail.com>
2020-07-08 13:17:04 -0700

    Create 0703.Kth-Largest-Element-in-a-Stream.py

---
+++ Solutions/0703.Kth-Largest-Element-in-a-Stream.py
@@ -0,0 +1,43 @@
+703. Kth Largest Element in a Stream
+
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+
+Example:
+
+int k = 3;
+int[] arr = [4,5,8,2];
+KthLargest kthLargest = new KthLargest(3, arr);
+kthLargest.add(3);   // returns 4
+kthLargest.add(5);   // returns 5
+kthLargest.add(10);  // returns 5
+kthLargest.add(9);   // returns 8
+kthLargest.add(4);   // returns 8
+
+
+""""
+heap solution: maintain a hq with size k, the kth largest is always hq[0] - O(klogk)
+Maybe we can do log time complexity for add method using a red–black tree, 
+red-black tree is a type of self-balancing binary search tree, Google requires at least talk about it.
+"""
+from heapq import heappush, heappop, heapify
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.k = k
+        heapify(self.nums)
+        while len(self.nums) > k:
+            heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heappop(self.nums)
+        
+        return self.nums[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
