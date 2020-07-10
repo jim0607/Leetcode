0c317a83e7a954cb8659087b04cf8b323fde3ce1
Jim0607 <jinjun.ding@gmail.com>
2020-07-09 17:40:58 -0700

    Update List, String, HashMap, Set.py

--- List, String, HashMap, Set.py
+++ List, String, HashMap, Set.py
@@ -72,7 +72,7 @@ In python, hash map is dictionary
    dictNums = collections.defaultdict()
    如果想声明dict里面的val是int：dictNums = collections.defaultdict(int)
    如果想将默认的int值设置为1,可以使用lambda表达式: dictNums = collections.defaultdict(lambda: 1), eg: 1027. Longest Arithmetic Sequence
-   edges = collections.defaultdict(list) means that value of the dictionary is a list
+   edges = collections.defaultdict(list) means that value of the dictionary is a list, defaultdict里面必须传入val的类型可以是list, bool, str, TrieNode等等.
       
 3. delete a key, val pair in a dict: del dictNums[key]
       
