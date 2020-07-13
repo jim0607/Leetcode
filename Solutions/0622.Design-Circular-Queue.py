472c7b4b6149d3b9ddcbfbfad32f54fb120b82e4
Jim0607 <jinjun.ding@gmail.com>
2020-07-12 23:44:25 -0700

    Create 0622.Design-Circular-Queue.py

---
+++ Solutions/0622.Design-Circular-Queue.py
@@ -0,0 +1,120 @@
+622. Design Circular Queue
+
+Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Your implementation should support following operations:
+
+MyCircularQueue(k): Constructor, set the size of the queue to be k.
+Front: Get the front item from the queue. If the queue is empty, return -1.
+Rear: Get the last item from the queue. If the queue is empty, return -1.
+enQueue(value): Insert an element into the circular queue. Return true if the operation is successful.
+deQueue(): Delete an element from the circular queue. Return true if the operation is successful.
+isEmpty(): Checks whether the circular queue is empty or not.
+isFull(): Checks whether the circular queue is full or not.
+ 
+
+Example:
+
+MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
+circularQueue.enQueue(1);  // return true
+circularQueue.enQueue(2);  // return true
+circularQueue.enQueue(3);  // return true
+circularQueue.enQueue(4);  // return false, the queue is full
+circularQueue.Rear();  // return 3
+circularQueue.isFull();  // return true
+circularQueue.deQueue();  // return true
+circularQueue.enQueue(4);  // return true
+circularQueue.Rear();  // return 4
+
+
+"""
+we can use a Singly-Linked List. 
+enqueue: we append the value to the tail; dequeue: we remove node from head.
+front: the head; rear: the tail; isempty: cnt=0; isFull: cnt = k
+"""
+class ListNode:
+    
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
+        
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.head = None
+        self.tail = None
+        self.capacity = k
+        self.cnt = 0
+
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        if self.cnt == self.capacity:
+            return False
+        
+        new_node = ListNode(value)
+        if self.cnt == 0:               # 注意这里用易出错
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next = new_node   # 注意这里insert new_node的方法是append到tail后面，然后tail移到后面
+            self.tail = self.tail.next
+            
+        self.cnt += 1
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        if self.cnt == 0:
+            return False
+        
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        self.cnt -= 1
+        return True
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        return -1 if self.cnt == 0 else self.head.val 
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        return -1 if self.cnt == 0 else self.tail.val
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.cnt == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        return self.cnt == self.capacity
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
