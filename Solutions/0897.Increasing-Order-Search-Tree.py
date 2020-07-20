9ed94c3806d500816b80d9d051fdf52f2a532b67
Jim0607 <jinjun.ding@gmail.com>
2020-07-19 19:06:08 -0700

    Create 0897.Increasing-Order-Search-Tree.py

---
+++ Solutions/0897.Increasing-Order-Search-Tree.py
@@ -0,0 +1,73 @@
+897. Increasing Order Search Tree
+
+Given a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
+
+Example 1:
+Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+       5
+      / \
+    3    6
+   / \    \
+  2   4    8
+ /        / \ 
+1        7   9
+
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+ 1
+  \
+   2
+    \
+     3
+      \
+       4
+        \
+         5
+          \
+           6
+            \
+             7
+              \
+               8
+                \
+                 9  
+ 
+
+Constraints:
+
+The number of nodes in the given tree will be between 1 and 100.
+Each node will have a unique integer value from 0 to 1000.
+
+
+"""
+step 1: in order traversal to get a list of node vals;
+step 2: construct the tree based on the in_order list
+"""
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        in_order = self._in_order(root)
+        return self._construct(in_order)
+    
+    def _in_order(self, root):
+        if not root:
+            return []
+        
+        res = []
+        if root.left:
+            res += self._in_order(root.left)
+        res.append(root.val)
+        if root.right:
+            res += self._in_order(root.right)
+            
+        return res
+            
+    def _construct(self, arr):
+        if len(arr) == 0:
+            return None
+    
+        root = TreeNode(arr[0])
+        root.left = None
+        root.right = self._construct(arr[1:])
+        
+        return root        
