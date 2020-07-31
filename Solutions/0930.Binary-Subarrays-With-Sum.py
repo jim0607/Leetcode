6fec34d7102cb65dfc263f595eaac899aff945d4
Jim0607 <jinjun.ding@gmail.com>
2020-07-31 11:24:04 -0700

    Update 0930.Binary-Subarrays-With-Sum.py

--- Solutions/0930.Binary-Subarrays-With-Sum.py
+++ Solutions/0930.Binary-Subarrays-With-Sum.py
@@ -16,6 +16,7 @@ The 4 subarrays are bolded below:
 
 """
 (number of subarrays having sum S) = (number of subarrays having sum at most S) - (number of subarrays having sum at most S-1)
+第一种写法是让后面的指针逐渐远离前面的指针 - 非常适合at least problems
 """
 class Solution:
     def numSubarraysWithSum(self, A: List[int], S: int) -> int:
@@ -39,5 +40,31 @@ class Solution:
                 res += j - i - 1
                 
             sums -= A[i]
+            
+        return res
+
+    
+"""
+第二种写法是让前面的指针去追后面的指针 - 非常适合at most problems
+"""
+class Solution:
+    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
+        if S == 0:
+            return self._at_most(A, S) 
+        
+        return self._at_most(A, S) - self._at_most(A, S - 1)
+    
+    def _at_most(self, A, S):
+        res = 0
+        i = 0
+        sums = 0
+        for j in range(len(A)):
+            sums += A[j]        # 先更新后面的指针
+            
+            while i <= j and sums > S:
+                sums -= A[i]    # 再更新前面的指针
+                i += 1
+            
+            res += j - i + 1    # 更新res
             
         return res
