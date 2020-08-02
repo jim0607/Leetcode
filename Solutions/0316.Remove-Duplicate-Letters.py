25f517628f1c8e82d8e0f43faff4bde8c170806d
Jim0607 <jinjun.ding@gmail.com>
2020-08-01 22:16:01 -0700

    Create 0316.Remove-Duplicate-Letters.py

---
+++ Solutions/0316.Remove-Duplicate-Letters.py
@@ -0,0 +1,37 @@
+316. Remove Duplicate Letters
+
+Given a string which contains only lowercase letters, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+Example 1:
+
+Input: "bcabc"
+Output: "abc"
+Example 2:
+
+Input: "cbacdcbc"
+Output: "acdb"
+
+
+
+"""
+we use a stack to store the solution we have built as we iterate over the string.
+We delete a ch if two conditions are meet:
+1. the ch can occur later on
+2. the ch is greater than the curr ch;
+"""
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last_occur = collections.defaultdict(int)   # store the pos where ch last occured
+        for i, ch in enumerate(s):
+            last_occur[ch] = i
+
+        st = []
+        included = set()
+        for i, ch in enumerate(s):
+            if ch not in included:      # if ch is in included, we just skip
+                while st and ord(st[-1]) > ord(ch) and last_occur[st[-1]] > i:
+                    included.remove(st.pop())
+                st.append(ch)
+                included.add(ch)
+                
+        return "".join(st)
