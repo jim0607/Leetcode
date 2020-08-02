9761f0cee6b8936b276bf873ca6193ca8c6f3a43
Jim0607 <jinjun.ding@gmail.com>
2020-08-01 17:09:18 -0700

    Create 0991.Broken-Calculator.py

---
+++ Solutions/0991.Broken-Calculator.py
@@ -0,0 +1,60 @@
+991. Broken Calculator
+
+On a broken calculator that has a number showing on its display, we can perform two operations:
+
+Double: Multiply the number on the display by 2, or;
+Decrement: Subtract 1 from the number on the display.
+Initially, the calculator is displaying the number X.
+
+Return the minimum number of operations needed to display the number Y.
+
+ 
+
+Example 1:
+
+Input: X = 2, Y = 3
+Output: 2
+Explanation: Use double operation and then decrement operation {2 -> 4 -> 3}.
+Example 2:
+
+Input: X = 5, Y = 8
+Output: 2
+Explanation: Use decrement and then double {5 -> 4 -> 8}.
+Example 3:
+
+Input: X = 3, Y = 10
+Output: 3
+Explanation:  Use double, decrement and double {3 -> 6 -> 5 -> 10}.
+Example 4:
+
+Input: X = 1024, Y = 1
+Output: 1023
+Explanation: Use decrement operations 1023 times.
+
+
+
+"""
+先将y除下来，除到y < x之后再减，出的过程中遇到y为奇数就加一
+eg: x = 5, y = 120. 
+y // 2 = 60; step = 1
+y // 2 = 30; step = 2
+y // 2 = 15; step = 3
+y = y + 1;   step = 4   # 遇到y为奇数就加一，不能减一因为对x的操作只能是减一，所以对y的操作只能是加一
+y // 2 = 8;  step = 5
+y // 2 = 4;  step = 6
+出while loop cuz y < x; step += x - y
+"""
+class Solution:
+    def brokenCalc(self, x: int, y: int) -> int:
+        step = 0
+        while y > x:
+            if y % 2 == 0:
+                y //= 2
+            else:
+                y += 1
+            step += 1
+        return step + x - y
+    
+"""
+注意这题不能将x乘上去，将x乘上去肯定没有将y除下来步数更少，eg:x = 1, y = 80, x 乘上去会变成64，距离100还有16步；而将100除下来会变成5，距离1只有4步
+"""
