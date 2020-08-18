7e4485501ccfb3608510cdef99ba790b5c7ab0f9
Jim0607 <jinjun.ding@gmail.com>
2020-08-17 22:11:29 -0700

    Update 0946.Validate-Stack-Sequences.py

--- Solutions/0946.Validate-Stack-Sequences.py
+++ Solutions/0946.Validate-Stack-Sequences.py
@@ -17,6 +17,21 @@ Output: false
 Explanation: 1 cannot be popped before 2.
 
 
+
+"""
+pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+[]              empty:   append(1)
+[1]             1 != 4:  append(2)
+[1, 2]          2 != 4:  append(3)
+[1, 2, 3]       3 != 4:  append(4)
+[1, 2, 3, 4]    4 == 4:  pop from st [1, 2, 3], i+=1, i = 1
+[1, 2, 3]       3 != 5:  append(5)
+[1, 2, 3, 5]    5 == 5:  pop from st [1, 2, 3], i+=1, i = 2
+[1, 2, 3]       3 == 3:  pop from st [1, 2], i+=1, i = 3
+[1, 2]          2 == 2:  pop from st [1], i = 4
+[1]             1 == 1:  pop from st [], i = 5
+return st is empty
+"""
 """
 使用一个栈st来模拟push和pop的过程，用一个指针在popped list里面跑，如果popped[i]==st[-1]那就一直pop, 最后判断st能不能pop为空
 """
@@ -26,7 +41,7 @@ class Solution:
         i = 0
         for num in pushed:
             st.append(num)
-            while st and st[-1] == popped[i]:
-                i += 1
+            while len(st) > 0 and st[-1] == popped[i]:
                 st.pop()
-        return not st
+                i += 1
+        return len(st) == 0
