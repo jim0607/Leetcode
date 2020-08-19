c23c8b4904a364fcb4978750b9abc8f14dff73af
Jim0607 <jinjun.ding@gmail.com>
2020-08-18 22:05:30 -0700

    Update 1249.Minimum-Remove-to-Make-Valid-Parentheses.py

--- Solutions/1249.Minimum-Remove-to-Make-Valid-Parentheses.py
+++ Solutions/1249.Minimum-Remove-to-Make-Valid-Parentheses.py
@@ -32,46 +32,76 @@ Output: "a(b(c)d)"
 
 
 """
-借鉴32. Longest Valid Parentheses的做法：first sweep left to right, and store the ")" that should be deleted, eg: "())", the last ")" should be deleted;
+solution 1: use a st to store "(". 
+"""
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        st = []
+        res = ""
+        for ch in s:
+            if ch == "(":
+                st.append(ch)
+                res += ch
+                
+            elif ch == ")":
+                if len(st) != 0:    # if not st, we don't append ")" to res
+                    st.pop()
+                    res += ch
+            else:
+                res += ch
+
+        # if there is still "(" left in st, we delete them form the end of the string
+        ans = []
+        i = len(res) - 1
+        while i >= 0 and len(st) > 0:
+            if res[i] == "(":   # if it is "(", then we don't append it to ans
+                st.pop()
+            else:
+                ans.append(res[i])
+            i -= 1
+                
+        ans.append(res[:i+1])
+        
+        return "".join(ans[::-1])
+ 
+ 
+ 
+"""
+solution 2: 借鉴32. Longest Valid Parentheses的做法：first sweep left to right, and store the ")" that should be deleted, eg: "())", the last ")" should be deleted;
 then sweep right to left, and store "(" that should be deleted, eg: eg: "(()", the first "(" should be deleted; 
 lastly delete the prarentheses that should be deleted.
 """
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         left, right = 0, 0
-        idx = set()    # store the idx where a parentheses should be deleted
+        pos = set()     # store the pos where a parentheses should be deleted
         
         # first sweep left to right, and store the ")" that should be deleted, eg: "())", the last ")" should be deleted
         for i, ch in enumerate(s):
             if ch == "(":
                 left += 1
-            if ch == ")":
+            elif ch == ")":
                 right += 1
             if right > left:
-                idx.add(i)
+                pos.add(i)
                 right -= 1
-        
+                
         # then sweep right to left, and store "(" that should be deleted, eg: eg: "(()", the first "(" should be deleted
         left, right = 0, 0
         for i, ch in enumerate(s[::-1]):
-            if ch == ")":
-                right += 1
             if ch == "(":
                 left += 1
+            elif ch == ")":
+                right += 1
             if left > right:
-                idx.add(len(s) - i - 1)
+                pos.add(len(s) - i - 1)     # 注意这里不是add(i)
                 left -= 1
-
+                
         # then delete the prarentheses that should be delted
         res = ""
-        i, j = 0, 0
-        while i < len(s):
-            if i in idx:
-                idx.remove(i)
-                i += 1
+        for i, ch in enumerate(s):
+            if i in pos:
                 continue
-                
-            res += s[i]
-            i += 1
+            res += ch
             
         return res
