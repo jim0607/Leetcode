d58027c160371aec15dde3c6f2e85f4350193a27
Jim0607 <jinjun.ding@gmail.com>
2020-08-22 20:21:27 -0700

    Update 1094.Car-Pooling.py

--- Solutions/1094.Car-Pooling.py
+++ Solutions/1094.Car-Pooling.py
@@ -1,8 +1,11 @@
 1094. Car Pooling
 
-You are driving a vehicle that has capacity empty seats initially available for passengers.  The vehicle only drives east (ie. it cannot turn around and drive west.)
+You are driving a vehicle that has capacity empty seats initially available for passengers.  
+The vehicle only drives east (ie. it cannot turn around and drive west.)
 
-Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains information about the i-th trip: the number of passengers that must be picked up, and the locations to pick them up and drop them off.  The locations are given as the number of kilometers due east from your vehicle's initial location.
+Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains information about the i-th trip: 
+the number of passengers that must be picked up, and the locations to pick them up and drop them off.  
+The locations are given as the number of kilometers due east from your vehicle's initial location.
 
 Return true if and only if it is possible to pick up and drop off all passengers for all the given trips. 
 
@@ -30,23 +33,23 @@ Output: true
 """
 这题可以叫meeting root III. 我们以end pos构造一个heapq, 每次把end pos小于start pos的pop出来
 """
-from heapq import heappush, heappop
-
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key = lambda trip: (trip[1], trip[2]))   # step 1: sort the intervals
-
-        end_pos = []        # 以end pos构造一个heapq
-        person = 0          # record how many persons are there on the bus
+        # step 1: sort the intervals - interval 题目的第一步！
+        trips.sort(key = lambda x: (x[1], x[2]))    
+        
+        end_hq = []     # a min heapq based on end location
+        person = 0      # record how many persons are there on the bus
         for num, start, end in trips:
-            heappush(end_pos, (end, start, num))
+            # 让这帮乘客上车吧
+            heappush(end_hq, (end, start, num))
+            person += num
             
-            while start >= end_pos[0][0]:   # 只要是end < start, 说明那些人早就到了，让他们下车
-                person -= heappop(end_pos)[2]       
-                
-            person += num           # 别忘了新的一拨人要上车 
+            # 让end time < start的乘客都下车
+            while len(end_hq) > 0 and start >= end_hq[0][0]:
+                person -= heappop(end_hq)[2]
             
             if person > capacity:
                 return False
-            
+        
         return True
