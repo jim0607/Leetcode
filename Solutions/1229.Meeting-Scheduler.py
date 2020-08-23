1c845470a6d9627a2b5b3e6ac521c87b660d12f7
Jim0607 <jinjun.ding@gmail.com>
2020-08-23 13:55:18 -0700

    Update 1229.Meeting-Scheduler.py

--- Solutions/1229.Meeting-Scheduler.py
+++ Solutions/1229.Meeting-Scheduler.py
@@ -20,7 +20,51 @@ Input: slots1 = [[10,50],[60,120],[140,210]], slots2 = [[0,15],[60,70]], duratio
 Output: []
 
 
-
+ """
+to find the overlap of two intervals. We loop over the two intervals. 
+一个interval与另一个interval的位置关系就三种情况(1. 没有交集; 2. 一个包含了另一个; 3. 有交集但是没有谁能完全包含谁)
+"""
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        slots1.sort()
+        slots2.sort()
+        res = []
+        i, j = 0, 0
+        while i < len(slots1) and j < len(slots2):
+            start1, end1 = slots1[i][0], slots1[i][1]
+            start2, end2 = slots2[j][0], slots2[j][1]
+            
+            # case 1: 两个interval不相交
+            if start1 > end2:
+                j += 1
+            elif start2 > end1:
+                i += 1
+            
+            # case 2: 一个interval包含另一个
+            elif start1 <= start2 and end1 >= end2:
+                res = [start2, end2]
+                j += 1
+            elif start2 <= start1 and end2 >= end1:
+                res = [start1, end1]
+                i += 1
+                
+            # case 3: 相交但并不包含
+            elif start2 <= end1 <= end2:
+                res = [start2, end1]
+                i += 1
+            elif start1 <= end2 <= end1:
+                res = [start1, end2]
+                j += 1
+                
+            if len(res) > 0 and res[1] - res[0] >= duration:
+                return [res[0], res[0]+duration]
+            
+        return []
+ 
+ 
+"""
+solution 2: 代码更短但是逻辑没那么清楚
+"""
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
         slots1.sort()
