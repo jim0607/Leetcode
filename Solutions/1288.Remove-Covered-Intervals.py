abe5d127430f501305ec6a69a04ba8fa40df6a63
Jim0607 <jinjun.ding@gmail.com>
2020-08-23 20:51:52 -0700

    Update 1288.Remove-Covered-Intervals.py

--- Solutions/1288.Remove-Covered-Intervals.py
+++ Solutions/1288.Remove-Covered-Intervals.py
@@ -19,16 +19,15 @@ to see if curr interval has a smaller end time than any of the previous interval
 we only need to compare with the largest end time in previous intervals.
 we can maintain a hq for the end time of the previous intervals 
 """
-
-from heapq import heappush, heappop
-
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key = lambda x: (x[0], -x[1]))   # 注意这里要按end_time逆序排列，因为我们希望max_end time go first
 
-        max_end = []
+        hq = []     # maintain a max heap for end time
+        remove = 0
         for start, end in intervals:
-            if not max_end or end > -max_end[0][0]:    # should not be removed
-                heappush(max_end, (-end, start))
+            if len(hq) > 0 and end <= -hq[0]:   # since all the intervals already in the hq have start time less than start
+                remove += 1    # then it is safe to say that this interval should be removed if end less than the max end time in hq
+            heappush(hq, -end)
             
-        return len(max_end)
+        return len(intervals) - remove
