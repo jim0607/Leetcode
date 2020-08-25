f297357113bea487f111e5e1d9be17f3b9e722b7
Jim0607 <jinjun.ding@gmail.com>
2020-08-24 20:18:05 -0700

    Update 1472.Design-Browser-History.py

--- Solutions/1472.Design-Browser-History.py
+++ Solutions/1472.Design-Browser-History.py
@@ -6,8 +6,10 @@ Implement the BrowserHistory class:
 
 BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
 void visit(string url) visits url from the current page. It clears up all the forward history.
-string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
-string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. 
+Return the current url after moving back in history at most steps.
+string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. 
+Return the current url after forwarding in history at most steps.
  
 
 Example:
@@ -41,10 +43,33 @@ homepage and url consist of  '.' or lower case English letters.
 At most 5000 calls will be made to visit, back, and forward.
 
 
+"""
+soution 1: simple: one list store the browse history, one pointer point at where we are at the list.
+"""
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.idx = 0
+
+    def visit(self, url: str) -> None:
+        self.history = self.history[:self.idx + 1]
+        self.history.append(url)
+        self.idx += 1
+
+    def back(self, steps: int) -> str:
+        back_idx = max(self.idx - steps, 0)
+        self.idx = back_idx
+        return self.history[self.idx]
+
+    def forward(self, steps: int) -> str:
+        forward_idx = min(self.idx + steps, len(self.history) - 1)
+        self.idx = forward_idx
+        return self.history[self.idx]
+
 
 """
-solution 1: two stacks.
-Solution 2: one arr and two pointers.
+solution 2: two stacks.
 """
 class BrowserHistory:
 
@@ -75,10 +100,3 @@ class BrowserHistory:
             steps -= 1
             
         return self.curr_url           
-
-
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
