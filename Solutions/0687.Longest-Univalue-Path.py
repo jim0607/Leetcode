bbeb08aa6f52986b4ac82e673cd9c3ab838dbf5f
Jim0607 <jinjun.ding@gmail.com>
2020-08-25 16:48:56 -0700

    Update 0687.Longest-Univalue-Path.py

--- Solutions/0687.Longest-Univalue-Path.py
+++ Solutions/0687.Longest-Univalue-Path.py
@@ -35,6 +35,38 @@ Output: 2
 Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
 
 
+"""
+在binary tree里求longest path问题，如果任何path都算数的话，那么我们在divide and conquer的时候要分成两种情况讨论：
+case 1. path ended with root; case 2: path not ended with root
+我们往往需要在helper函数中返回end_w和end_wo两种情况的值
+有时候也可以将case 2细分为: I. path pass the root, and II. path without the root.
+"""
+class Solution:
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        w_root = self._longest_path(root)   # number of same nodes start with root
+        pass_root = 0                       # number of same nodes pass root
+        if root.left and root.right and root.left.val == root.val == root.right.val:
+            pass_root = self._longest_path(root.left) + self._longest_path(root.right) + 1
+        wo_root = max(self.longestUnivaluePath(root.left), self.longestUnivaluePath(root.right))  # number of same path without root  
+        
+        return max(w_root - 1, pass_root - 1, wo_root)
+        
+    def _longest_path(self, root):
+        """
+        return the longest path starts with root
+        """
+        if not root:
+            return 0
+
+        left = self._longest_path(root.left) if root.left and root.left.val == root.val else 0
+        right = self._longest_path(root.right) if root.right and root.right.val == root.val else 0
+               
+        return max(left, right) + 1
+ 
+ 
 
 """
 在binary tree里求longest path问题，如果任何path都算数的话，那么我们在divide and conquer的时候要分成两种情况讨论：
