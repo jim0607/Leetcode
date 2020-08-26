d385648cd5abebd71b49d6d23d6f09f654439d03
Jim0607 <jinjun.ding@gmail.com>
2020-08-25 20:10:39 -0700

    Update 0235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py

--- Solutions/0235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Solutions/0235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -25,31 +25,19 @@ All of the nodes' values will be unique.
 p and q are different and both values will exist in the BST.
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        """return the LCA of two nodes: p and q"""
-        
-        # 1. 递归的出口
         if not root:
-            return
+            return None
         
-        # 1. 递归的出口：养成好习惯判断叶子节点
-        if not root.left and not root.right:
-            return
-        
-        # 2. 递归的拆解: divide and conquer
-        if (p.val < root.val < q.val or p.val > root.val > q.val) or (p.val == root.val or q.val == root.val):
+        if p.val == root.val or q.val == root.val:
             return root
         
-        if root.val < p.val and root.val < q.val:
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        if p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
         
-        if root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val < root.val < q.val or q.val < root.val < p.val:
+            return root
