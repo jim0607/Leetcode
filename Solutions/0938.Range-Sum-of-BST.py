3d42fd5f8f7c5717a005eca7f0466bbeea17de71
Jim0607 <jinjun.ding@gmail.com>
2020-08-27 11:49:17 -0700

    Update 0938.Range-Sum-of-BST.py

--- Solutions/0938.Range-Sum-of-BST.py
+++ Solutions/0938.Range-Sum-of-BST.py
@@ -69,7 +69,20 @@ class Solution:
         else:
             return leftSum + rightSum + root.val
 
-
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        
+        sum = 0
+        if root.val > R:
+            sum += self.rangeSumBST(root.left, L, R)
+        elif root.val < L:
+            sum += self.rangeSumBST(root.right, L, R)
+        else:
+            sum = root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
+        
+        return sum
 
 
 class Solution:
