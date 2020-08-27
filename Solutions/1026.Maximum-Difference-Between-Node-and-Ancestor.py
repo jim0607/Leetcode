de8bf31ebc7be37403556237eed105d041ed245e
Jim0607 <jinjun.ding@gmail.com>
2020-08-26 21:28:57 -0700

    Update 1026.Maximum-Difference-Between-Node-and-Ancestor.py

--- Solutions/1026.Maximum-Difference-Between-Node-and-Ancestor.py
+++ Solutions/1026.Maximum-Difference-Between-Node-and-Ancestor.py
@@ -17,6 +17,47 @@ We have various ancestor-node differences, some of which are given below :
 |10 - 13| = 3
 Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
 
+"""
+solution 1: dfs visit every node, at each node, stop there and find the max and min of its subtree to get its max_diff.
+since finding max and min of subtee takes O(N), so the overall time comlexity is O(N^2).
+"""
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        left_max_diff = self.maxAncestorDiff(root.left)
+        right_max_diff = self.maxAncestorDiff(root.right)
+        
+        left_max, left_min = self._find_maxmin(root.left)
+        right_max, right_min = self._find_maxmin(root.right)
+        
+        root_max_diff = 0
+        if left_max is not None:   # 最好是乖乖写is not None, 因为left_max可能等于0, 导致两个test case过不了
+            root_max_diff = max(root_max_diff, abs(left_max - root.val), abs(left_min - root.val))
+        if right_max is not None:
+            root_max_diff = max(root_max_diff, abs(right_max - root.val), abs(right_min - root.val))
+        
+        return max(root_max_diff, left_max_diff, right_max_diff)
+    
+    
+    def _find_maxmin(self, root):
+        if not root:
+            return None, None
+        if not root.left and not root.right:
+            return root.val, root.val
+        
+        left_max, left_min = self._find_maxmin(root.left)
+        right_max, right_min = self._find_maxmin(root.right)
+        
+        root_max, root_min = root.val, root.val
+        if left_max is not None:    # 最好是乖乖写is not None, 因为left_max可能等于0, 导致两个test case过不了
+            root_max, root_min = max(root_max, left_max), min(root_min, left_min)
+        if right_max is not None:
+            root_max, root_min = max(root_max, right_max), min(root_min, right_min)
+
+        return root_max, root_min
+
 
 
 """
