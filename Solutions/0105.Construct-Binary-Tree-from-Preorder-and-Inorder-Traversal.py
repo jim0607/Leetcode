28a775a0e1f6a9782c6d6ab9d36d1144f51ca8b6
Jim0607 <jinjun.ding@gmail.com>
2020-08-28 12:13:50 -0700

    Update 0105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py

--- Solutions/0105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
+++ Solutions/0105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
@@ -27,16 +27,8 @@ class Solution:
         if not preorder or not inorder:
             return None
         
-        root = TreeNode()
-        root.val = preorder.pop(0)  # this takes O(N), so overall the algorithm takes O(N^2)
-        
-        # find the index where root.val is in inorder list
-        # this takes O(N), so overall the algorithm takes O(N^2)
-        idx = 0         
-        for i, num in enumerate(inorder):
-            if num == root.val:
-                idx = i
-                break
+        root = TreeNode(preorder.pop(0))    # this takes O(N), so overall the algorithm takes O(N^2)
+        idx = inorder.index(root.val)       # this takes O(N), so overall the algorithm takes O(N^2)
             
         root.left = self.buildTree(preorder, inorder[:idx])
         root.right = self.buildTree(preorder, inorder[idx+1:])
@@ -46,43 +38,30 @@ class Solution:
         
         
 """
-solution 1中需要O(N^2)的原因是1. preorder.pop(0) takes O(N). We can convert preorder into a deque and popleft.
-2. finding the idx in inorder list takes O(N). We can use a hash table to store num-to-idx pair in advance.
-This leads to solution 2, which takes O(N) instead of O(N^2).
-"""
-"""
-solution 1中需要O(N^2)的原因是1. preorder.pop(0) takes O(N). We can convert preorder into a deque and popleft.
+solution 1中需要O(N^2)的原因是1. preorder.pop(0) takes O(N). We can convert preorder into a q and popleft.
 2. finding the idx in inorder list takes O(N). We can use a hash table to store num-to-idx pair in advance.
 This leads to solution 2, which takes O(N) instead of O(N^2).
 """
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        if not preorder or not inorder:
-            return None
-        
-        preorder_dq = collections.deque(preorder)       # use a deque to store preorder list
-        inorder_idxmap = collections.defaultdict(int)   # use a hash table to store num-to-idx pair
-        for i, num in enumerate(inorder):
-            inorder_idxmap[num] = i
-            
-            
-        def helper(preorder_dq, i, j):     # i, j are idx in inorder list
-            if not preorder_dq:
+        def helper(preorder_q, start, end):
+            if start >= end:
                 return None
             
-            if i >= j:
-                return None
+            root = TreeNode(preorder_q.popleft())   # now this takes O(1)
+            idx = inorder_idx[root.val]             # now this takes O(1)
             
-            root = TreeNode()
-            root.val = preorder_dq.popleft()  # this takes O(1)
-        
-            # find the index where root.val is in inorder list - O(1)
-            idx = inorder_idxmap[root.val]
+            # 注意此时必须把start和idx都传进helper function, 因为如果传inorder[:idx]的话，
+            # 会导致后面找inorder[:idx]这个subarray中的idx的时候和hash table中的不一样
+            root.left = helper(preorder_q, start, idx)  
+            root.right = helper(preorder_q, idx + 1, end)
             
-            root.left = helper(preorder_dq, i, idx)
-            root.right = helper(preorder_dq, idx + 1, j)
-        
             return root
         
         
-        return helper(preorder_dq, 0, len(inorder))
+        preorder_q = collections.deque(preorder)    # use a queue to store preorder list
+        inorder_idx = collections.defaultdict(int)  # use a hash table to store num-to-idx pair
+        for i, num in enumerate(inorder):
+            inorder_idx[num] = i
+            
+        return helper(preorder_q, 0, len(inorder))
