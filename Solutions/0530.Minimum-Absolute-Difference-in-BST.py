49d2ce7deb9fdfe7099043ef3a7cd0b6e245a4bd
Jim0607 <jinjun.ding@gmail.com>
2020-08-28 16:18:19 -0700

    Update 0530.Minimum-Absolute-Difference-in-BST.py

--- Solutions/0530.Minimum-Absolute-Difference-in-BST.py
+++ Solutions/0530.Minimum-Absolute-Difference-in-BST.py
@@ -19,9 +19,33 @@ Explanation:
 The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
 
 
+"""
+solution 1: in order traversal the BST and compare the prev_node and curr_node as we go. 
+Need to use a global prev_node while doing in_order traversal. maintain a global prev_node的思想非常重要
+"""
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        self.min_diff = float("inf")
+        self.prev = None
+        self._in_order(root)
+        return self.min_diff
+    
+    def _in_order(self, root):
+        if not root:
+            return
+        
+        self._in_order(root.left)
+        
+        if self.prev and abs(self.prev.val - root.val) < self.min_diff:
+            self.min_diff = abs(self.prev.val - root.val)
+            
+        self.prev = root
+        
+        self._in_order(root.right)
+
 
 """
-just in order traversal to turn the tree into a list, and then compare the adjacent elements in a list.
+solutio 2: in order traversal to turn the tree into a list, and then compare the adjacent elements in a list.
 """
 class Solution:
     def getMinimumDifference(self, root: TreeNode) -> int:
