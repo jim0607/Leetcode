24fdb488ed432686d1bd2e05a5df5e49321e5c68
Jim0607 <jinjun.ding@gmail.com>
2020-08-30 14:05:33 -0700

    Update 0304.Range-Sum-Query-2D-Immutable.py

--- Solutions/0304.Range-Sum-Query-2D-Immutable.py
+++ Solutions/0304.Range-Sum-Query-2D-Immutable.py
@@ -26,6 +26,27 @@ a 2D version prefix sum. O(1) for query.
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
+        m, n = len(matrix), len(matrix[0])
+        self.presum = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                self.presum[i+1][j+1] = self.presum[i][j+1] + matrix[i][j]
+
+        for j in range(n):
+            for i in range(m):
+                self.presum[i+1][j+1] += self.presum[i+1][j]    # 这里很容易出错
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.presum[row2+1][col2+1] - self.presum[row1][col2+1] - self.presum[row2+1][col1] + self.presum[row1][col1]   # 这里很容易出错
+
+
+
+"""
+另一种不太简洁的写法
+"""
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
         if not matrix or not matrix[0]:
             return
         
