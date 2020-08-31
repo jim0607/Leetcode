cd84069095e78ede210d8c93188e6497804b5a1e
Jim0607 <jinjun.ding@gmail.com>
2020-08-31 00:42:50 -0700

    Update 0699.Falling-Squares.py

--- Solutions/0699.Falling-Squares.py
+++ Solutions/0699.Falling-Squares.py
@@ -62,4 +62,87 @@ class Solution:
         return res
         
         
-solution 2: segment tree O(NlogN) https://leetcode.com/problems/falling-squares/discuss/409304/Python-Diffenrent-Concise-Solutions
+solution 2: segment tree O(NlogN) 
+"""
+因为每进来一个squre我们都需要query 在这个interval里的max_num, 所以我们segement tree to enable O(logN) query.
+下面的solution是对的，但是由于需要build的tree太大了，所以TLE.
+"""
+
+class SegmentTree:
+    
+    def __init__(self, start, end, max_num):  
+        """
+        initialize a segment tree, a segment tree node has start idx of the arr, end idx of the arr, 
+        range sum from start to end (inclussive), left node, right node
+        """
+        self.start = start 
+        self.end = end
+        self.max_num = max_num
+        self.left = None
+        self.right = None
+        
+    def build(self, start, end, nums):
+        """
+        build the segment tree, return the root of the tree - O(N)
+        """
+        if start > end:
+            return None
+            
+        root = SegmentTree(start, end, nums[start])
+        if start == end:
+            return root
+
+        mid = start + (end - start) // 2
+        root.left = self.build(start, mid, nums)
+        root.right = self.build(mid + 1, end, nums)
+        root.max_num = max(root.left.max_num, root.right.max_num)
+        
+        return root
+    
+    def update(self, root, left, right, val):
+        """
+        update the segment tree when there is an update in nums (change to val in an interval) - O(logN)
+        update 函数其实就是update max_num
+        """
+        if not root:
+            return
+        
+        if left <= root.end and right >= root.start:    # two intervals intersect
+            root.max_num = val
+            self.update(root.left, left, right, val)
+            self.update(root.right, left, right, val)
+        
+    def query(self, root, start, end):
+        """
+        given a range, return the max_num covered by the range - O(logN)
+        """
+        if end < root.start or start > root.end:
+            return 0
+            
+        if start <= root.start and end >= root.end:
+            return root.max_num
+        
+        return max(self.query(root.left, start, end), self.query(root.right, start, end))
+    
+    
+class Solution:
+    def fallingSquares(self, positions: List[List[int]]) -> List[int]:
+        # step 1: build a segment tree
+        min_left = min(left for left, _ in positions)
+        max_right = max(left + lens - 1 for left, lens in positions)
+        sgtree = SegmentTree(min_left, max_right, 0)
+        root = sgtree.build(min_left, max_right, [0 for _ in range(max_right + 1)])
+        
+        # step 2: query from the segment tree while updating a tree
+        res = []
+        max_h = 0
+        for left, lens in positions:
+            left, right = left, left + lens - 1
+            
+            prev_max_h = sgtree.query(root, left, right)  # this takes O(logN)
+            max_h = max(max_h, prev_max_h + lens)
+            res.append(max_h)
+            
+            sgtree.update(root, left, right, prev_max_h + lens)
+            
+        return res
