d1bc0caa96a0e6006207cadbf2002afd6fd9198b
Jim0607 <jinjun.ding@gmail.com>
2020-08-31 10:02:36 -0700

    Update 0591.Connecting-Graph-III.py

--- Solutions/0591.Connecting-Graph-III.py
+++ Solutions/0591.Connecting-Graph-III.py
@@ -39,27 +39,26 @@ Output:
 
 
 class ConnectingGraph3:
-    
     def __init__(self, n):
-        self.father = collections.defaultdict()
-        self.cnt = n
+        self.father = collections.defaultdict(int)
+        self.cnt = 0
+        for i in range(1, n+1):
+            self._add(i)
+            
+    def _add(self, x):
+        self.father[x] = x
+        self.cnt += 1
         
-        for i in range(1, n + 1):
-            self.father[i] = i
-        
-    def find(self, x):
+    def _find(self, x):
         if self.father[x] == x:
             return x
-            
-        self.father[x] = self.find(self.father[x])
-        
+        self.father[x] = self._find(self.father[x])
         return self.father[x]
     
     def connect(self, a, b):
-        father_a, father_b = self.find(a), self.find(b)
-        
-        if father_a != father_b:
-            self.father[father_b] = father_a
+        root_a, root_b = self._find(a), self._find(b)
+        if root_a != root_b:
+            self.father[root_a] = root_b
             self.cnt -= 1
 
     def query(self):
