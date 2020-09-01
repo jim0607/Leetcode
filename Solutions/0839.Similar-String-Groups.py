e831997524ca358d3042df561f3fc7d09f2a8767
Jim0607 <jinjun.ding@gmail.com>
2020-09-01 08:46:42 -0700

    Update 0839.Similar-String-Groups.py

--- Solutions/0839.Similar-String-Groups.py
+++ Solutions/0839.Similar-String-Groups.py
@@ -21,20 +21,45 @@ Output: 2
 can use bfs/dfs/union-find to traverse the graph.
 since we want to output disconnected components, union-find is quite straight forward - O(L*N^2)
 """
+class Solution:
+    def numSimilarGroups(self, A: List[str]) -> int:
+        uf = UnionFind(A)
+        for i in range(len(A)-1):       # 这种两两相邻的node相互比较的思想非常重要
+            for j in range(i+1, len(A)):
+                if uf.connected(A[i], A[j]):
+                    continue
+                if self._is_similar(A[i], A[j]):
+                    uf.union(A[i], A[j])
+        return uf.cnt
+    
+    def _is_similar(self, s, t):  # 利用了s1和s2是anagram的前提将复杂度降为O(L)
+        diff_cnt = 0
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                diff_cnt += 1
+            if diff_cnt > 2:
+                return False
+        return True
+    
+    
 class UnionFind:
     
     def __init__(self, A):
         self.father = collections.defaultdict(str)
-        self.disjoint_cnt = 0
-        for s in A:
-            if s not in self.father[s]:  # 注意如果A中有重复的话disjoint_cnt += 1不能做两次
-                self.disjoint_cnt += 1
-            self.father[s] = s   
+        self.cnt = 0
         
+        for s in A:
+            if s not in self.father:   # 注意这里的判断至关重要
+                self.add(s)            # 因为如果A中有重复的话cnt += 1不能做两次
+                
+    def add(self, x):
+        self.father[x] = x
+        self.cnt += 1
+    
     def find(self, x):
         if self.father[x] == x:
             return x
-        self.father[x] = self.find(self.father[x])  # path compression
+        self.father[x] = self.find(self.father[x])    # path compression
         return self.father[x]
     
     def connected(self, a, b):
@@ -44,37 +69,4 @@ class UnionFind:
         root_a, root_b = self.find(a), self.find(b)
         if root_a != root_b:
             self.father[root_a] = root_b
-            self.disjoint_cnt -= 1
-            
-            
-class Solution:
-    def numSimilarGroups(self, A: List[str]) -> int:
-        uf = UnionFind(A)
-        for i in range(len(A) - 1):
-            for j in range(i + 1, len(A)):
-                if uf.connected(A[i], A[j]):
-                    continue
-                if self._is_similar(A[i], A[j]):
-                    uf.union(A[i], A[j])
-        return uf.disjoint_cnt
-    
-    def _is_similar(self, s1, s2):
-        lens = len(s1)
-        diff_cnt = 0
-        for i in range(lens):   # 利用了s1和s2是anagram的前提将复杂度降为O(L)
-            if s1[i] != s2[i]:
-                diff_cnt += 1
-                if diff_cnt > 2:
-                    return False
-        return True
-    
-    # 下面这个_is_similar funciton takes O(L^2)
-#     def _is_similar(self, s1, s2):
-#         if s1 == s2:
-#             return True
-#         lens = len(s1)
-#         for i in range(lens - 1):
-#             for j in range(i + 1, lens):
-#                 if s1[:i] + s1[j] + s1[i+1:j] + s1[i] + s1[j+1:] == s2:
-#                     return True
-#         return False
+            self.cnt -= 1
