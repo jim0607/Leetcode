fe76363b32b9e742b8e6ca2630149dbe7314e77d
Jim0607 <jinjun.ding@gmail.com>
2020-09-01 09:29:10 -0700

    Update 1168.Optimize-Water-Distribution-in-a-Village.py

--- Solutions/1168.Optimize-Water-Distribution-in-a-Village.py
+++ Solutions/1168.Optimize-Water-Distribution-in-a-Village.py
@@ -21,14 +21,37 @@ The best strategy is to build a well in the first house with cost 1 and connect 
 """
 Kruskal's 模板
 """
-class UnionFind:
-    def __init__(self, N):
-        self.father = collections.defaultdict()
-        self.disjoint_cnt = 0
+class Solution:
+    def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+        uf = UnionFind(n + 1)
         
-        for i in range(N + 1):     # O(V) sapce for Union-Find
-            self.father[i] = i
-            self.disjoint_cnt += 1
+        edges = pipes                       # 这个题目比较tricky的地方是需要想像有一个虚拟的house_0, house_0是出水的house 
+        for i, cost in enumerate(wells):    # 这样house_1自己打井需要的cost就相当于从house_0连接到house_1所需的cost了
+            edges.append([0, i+1, cost])
+            
+        edges.sort(key = lambda x: x[2])
+        
+        costs = 0
+        for u, v, cost in edges:
+            if uf.connected(u, v):
+                continue
+            uf.union(u, v)
+            costs += cost
+        
+        return costs
+    
+    
+class UnionFind:
+    
+    def __init__(self, N):
+        self.father = collections.defaultdict(int)
+        self.cnt = 0
+        for i in range(N):
+            self.add(i)
+            
+    def add(self, x):
+        self.father[x] = x
+        self.cnt += 1
         
     def find(self, x):
         if self.father[x] == x:
@@ -37,36 +60,10 @@ class UnionFind:
         return self.father[x]
     
     def connected(self, a, b):
-        root_a, root_b = self.find(a), self.find(b)
-        if root_a == root_b:
-            return True
-        return False
+        return self.find(a) == self.find(b)
     
     def union(self, a, b):
         root_a, root_b = self.find(a), self.find(b)
         if root_a != root_b:
             self.father[root_a] = root_b
-            self.disjoint_cnt -= 1
-
-
-class Solution:
-    def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
-        uf = UnionFind(n)
-        
-        # 这个题目比较tricky的地方是需要想像有一个虚拟的house_0, house_0是出水的house 
-        # 这样house_1自己打井需要的cost就相当于从house_0连接到house_1所需的cost了
-        imaginary_pipes = []
-        for i, well in enumerate(wells):
-            imaginary_pipes.append([0, i + 1, well])
-            
-        pipes += imaginary_pipes    
-        pipes.sort(key = lambda pipe: pipe[2])
-        
-        total_cost = 0
-        for house_1, house_2, cost in pipes:
-            if uf.connected(house_1, house_2):
-                continue
-            uf.union(house_1, house_2)
-            total_cost += cost
-        
-        return total_cost if uf.disjoint_cnt == 1 else -1
+            self.cnt -= 1
