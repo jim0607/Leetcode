3f34ddf73f664480e31c1e0a067699cf8608afe5
Jim0607 <jinjun.ding@gmail.com>
2020-09-01 10:05:55 -0700

    Update 0102.Binary-Tree-Level-Order-Traversal.py

--- Solutions/0102.Binary-Tree-Level-Order-Traversal.py
+++ Solutions/0102.Binary-Tree-Level-Order-Traversal.py
@@ -42,30 +42,33 @@
 """Time complexity : O(N) since each node is processed exactly once.
 Space complexity : O(N) to keep the output structure which contains N node values."""
 
-from collections import deque
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
-        # 非常typical的BFS解binary tree的模板
+        
+        # 下面是非常typical的BFS解binary tree的模板
         res = []
-        q = deque()
-        q.append(root)
-        while q:
-            level = []
-            lens = len(q)  # important, 注意这里定义一个lens而不是在59行里直接用len(q)是因为每次q.popleft()之后q的长度会变！
-            # 用一个for循环来处理每一层
+        q = collections.deque()
+        q.append(root)      # 注意进q的永远是node, 而不是node.val，永远永远！
+        while len(q) > 0:
+            lens = len(q)   # important, 注意这里定义一个lens而不是在59行里直接用len(q)是因为每次q.popleft()之后q的长度会变!
+            level = []      # level 记录每一层的信息
             for _ in range(lens):
                 # 在这一层要做两件事情：1. 处理这一层：将该层的所有的node.val依次放入level中
-                node = q.popleft()
-                level.append(node.val)
+                curr_node = q.popleft()
+                level.append(curr_node.val)
                 # 2. 处理下一层：将该层所有的node的左右子节点依次入队列
-                if node.left:  # 注意这里判断是为了不把None放到队列里去，这样res出来的结果就没有None了。
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+                if curr_node.left:      # 注意这里判断是为了不把None放到队列里去，这样res出来的结果就没有None了
+                    q.append(curr_node.left)
+                if curr_node.right:
+                    q.append(curr_node.right)
             res.append(level)
+            
         return res
-
-# @lc code=end
