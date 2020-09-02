280f80c0c94802be3616a703e562cda35f6b256b
Jim0607 <jinjun.ding@gmail.com>
2020-09-01 23:18:25 -0700

    Update 0994.Rotting-Oranges.py

--- Solutions/0994.Rotting-Oranges.py
+++ Solutions/0994.Rotting-Oranges.py
@@ -77,66 +77,31 @@ class Solution:
     EMPTY = 0
     FRESH = 1
     ROTTEN = 2
-    MOVES = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        
-        # 如果本来就没有FRESH的，就直接返回0
-        found = False
-        m, n = len(grid), len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == self.FRESH:
-                    found = True
-                    
-        if not found:
-            return 0  
-        
-        visited = set()
-        minites = self.bfs(grid, visited)
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == self.FRESH:
-                    return -1
-        
-        return minites
-    
-    def bfs(self, grid, visited):
-        """bfs to reach the fresh ones and change them into rotten ones, return the minites to reach all fresh ones"""
-        
-        # 1. append the rotten ones to the first level, 由于需要做层序遍历，所以要想将所有的ROTTEN都放进q （作为第一层），然后再开始q的遍历
         m, n = len(grid), len(grid[0])
         q = collections.deque()
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == self.ROTTEN:
                     q.append((i, j))
-                    visited.add((i, j))     # 一对孪生兄弟
-        
-        # 2. bfs
-        minites = -1        # 需要分层遍历，所以需要加入一个参数minutes
-        while q:
-            minites += 1
+                    
+        # 层序遍历模板
+        minutes = -1
+        while len(q) > 0:        
+            minutes += 1
             lens = len(q)
-            for _ in range(lens):   # 一层一层的来
-                (x, y) = q.popleft()
-                for delta_x, delta_y in self.MOVES:
-                    neighbor_x, neighbor_y = x + delta_x, y + delta_y
-                    if self.inBound(grid, neighbor_x, neighbor_y) and \
-                    grid[neighbor_x][neighbor_y] == self.FRESH and \
-                    (neighbor_x, neighbor_y) not in visited:
-                        grid[neighbor_x][neighbor_y] = self.ROTTEN
-                        q.append((neighbor_x, neighbor_y))
-                        visited.add((neighbor_x, neighbor_y))       # 一对孪生兄弟
-
-        return minites
-                        
-    def inBound(self, grid, x, y):
-        m, n = len(grid), len(grid[0])
-        if 0 <= x < m and 0 <= y < n:
-            return True
-        
-        return False
+            for _ in range(lens):       
+                curr_i, curr_j = q.popleft()
+                for delta_i, delta_j in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                    if 0 <= next_i < m and 0 <= next_j < n:
+                        if grid[next_i][next_j] == self.FRESH:
+                            q.append((next_i, next_j))
+                            grid[next_i][next_j] = self.ROTTEN    # 就地修改grid, 所以不用visited set, 因为不会有重复访问
+                            
+        for i in range(m):
+            for j in range(n):  
+                if grid[i][j] == self.FRESH:    # 如果还有访问不到的FRESH, 那就return -1
+                    return -1
+                
+        return 0 if minutes == -1 else minutes  # 如果minutes=-1, 那说明最开始就没有ROTTEN的进q里去
