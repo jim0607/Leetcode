757ba242142e1f8bb07fed2d82a9787e57b5fc54
Jim0607 <jinjun.ding@gmail.com>
2020-09-02 13:23:49 -0700

    Update 0317.Shortest-Distance-from-All-Buildings.py

--- Solutions/0317.Shortest-Distance-from-All-Buildings.py
+++ Solutions/0317.Shortest-Distance-from-All-Buildings.py
@@ -49,75 +49,80 @@
 #
 """
 很重要的一题：
-可以选空地践行bfs (也可以选1的地方)，选择0的地方很好想，但是我们选择1的地方会节省一些时间。
+可以选空地trigger a bfs，选择空地的地方很好想，但是我们选择building的地方trigger a bfs会节省一些时间。
+"""
+"""
+Use reachable_cnt[i][j] to record how many times a 0 grid has been reached 
+and use dist[][] to record the sum of distance from all 1 grids to this 0 grid. 
+Linear scan the 2d grid map, if a node contains a '1', then it is a root node that triggers a BFS. 
+in the bfs, we do level bfs and update the reachable_cnt matrix and dist matrix. 遇到obstacle不放进q就可以了. 
+each bfs, all position are visited, so O(MNk) where k is how many building are there or how many bfs are triggered. 
+Finnaly return the min of dist[i][j] if reachable_cnt[i][j] = total number of buildings. 
+Strong Prune: if starting from building (i, j), can reach all other building? 
+if not, that means at least one building is isolated and can not be reached, then return -1 directly: 
+in each BFS we use reachableBuildings to count how many 1s we reached. If reachableBuldings != totalBuildings - 1,
+then we know not all 1s are connected are we can return -1 immediately, which greatly improved speed.
 """
 class Solution:
-    
     EMPTY = 0
     BUILDING = 1
     OBSTACLE = 2
-    MOVES = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    
     def shortestDistance(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         
-        # find the total buldings need to reach
-        self.totalBuildings = 0
+        # 算出matrix里有多少个buildings
+        total_buildings = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == self.BUILDING:
-                    self.totalBuildings += 1
+                    total_buildings += 1
                     
-        self.reachable_cnt = [[0 for _ in range(n)] for _ in range(m)]      # how many buildings can an empty reach
-        self.dist = [[0 for _ in range(n)] for _ in range(m)]               # what is the distance to all buildings for an empty
-        
+        # trigger a bfs at each bulding, in bfs, update reachable_cnt and dist
+        # rechable_cnt records how many buildings can each EMPTY reach
+        # dist records what is the distance from each EMPTY to all buildings
+        reachable_cnt = [[0 for _ in range(n)] for _ in range(m)]
+        dist = [[0 for _ in range(n)] for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == self.BUILDING:     # Linear scan the 2d grid map, if a node contains a '1', then it is a root node that triggers a BFS
-                    allReachable = self.bfs(grid, i, j)
-                    if not allReachable:        # if starting from building (i, j), can reach all other building? if not, that means at least one building is isolated and can not be reached - STRONG PRUNING
-                        return -1
-              
-        minDist = float("inf")
+                if grid[i][j] == self.BUILDING:
+                    if not self._bfs(grid, i, j, total_buildings, reachable_cnt, dist): # if starting from building (i, j), can reach all other buildings?
+                        return -1     # if not, that means at least one building is isolated and can not be reached - STRONG PRUNING
+                 
+        # get the mid_dist from dist matrix and reachable matrix
+        min_dist = float("inf")
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == self.EMPTY and self.reachable_cnt[i][j] == self.totalBuildings:
-                    minDist = min(minDist, self.dist[i][j])
+                if grid[i][j] == self.EMPTY and reachable_cnt[i][j] == total_buildings: # only EMPTY that can reach all buildings count
+                    min_dist = min(min_dist, dist[i][j])
                     
-        return minDist if minDist != float("inf") else -1
+        return min_dist if min_dist != float("inf") else -1
     
-    def bfs(self, grid, i, j):
+    def _bfs(self, grid, i, j, total_buildings, reachable_cnt, dist):
         m, n = len(grid), len(grid[0])
-        
         q = collections.deque()
         visited = set()
         q.append((i, j))
         visited.add((i, j))
         
-        level = 0
-        reachable = 0
-        while q:
-            level += 1
+        steps = 0
+        can_reach_buldings = 0      # record how many buldings can be reached from BUILDING (i, j)
+        while len(q) > 0:
+            steps += 1
             lens = len(q)
-            
             for _ in range(lens):
-                curr_x, curr_y = q.popleft()
-                
-                for delta_x, delta_y in self.MOVES:
-                    next_x, next_y = curr_x + delta_x, curr_y + delta_y
-                    
-                    if 0 <= next_x < m and 0 <= next_y < n and \
-                    grid[next_x][next_y] == self.EMPTY and \
-                    (next_x, next_y) not in visited:
-                        self.reachable_cnt[next_x][next_y] += 1
-                        self.dist[next_x][next_y] += level
-                        q.append((next_x, next_y))
-                        visited.add((next_x, next_y))
-                        
-                    if 0 <= next_x < m and 0 <= next_y < n and \
-                    grid[next_x][next_y] == self.BUILDING and \
-                    (next_x, next_y) not in visited:
-                        visited.add((next_x, next_y))       # 不要q.append((neighbor_x, neighbor_y))是因为Each 1 marks a building which you cannot pass through.
-                        reachable += 1
-  
-        return reachable == self.totalBuildings - 1     # STRONG PRUNING: if some bulding can not be reached from (i, j) bulding, then return -1 
+                curr_i, curr_j = q.popleft()
+                for delta_i, delta_j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                    if 0 <= next_i < m and 0 <= next_j < n:
+                        if (next_i, next_j) not in visited:
+                            if grid[next_i][next_j] == self.BUILDING:
+                                can_reach_buldings += 1     
+                                visited.add((next_i, next_j))   # **** 注意这里要将(next_i, next_j) add到visited中, 但不要append到q中
+                                                                # 这是因为each 1 marks a building which you cannot pass through.
+                            elif grid[next_i][next_j] == self.EMPTY:
+                                reachable_cnt[next_i][next_j] += 1      # update reachable_cnt
+                                dist[next_i][next_j] += steps           # update dist
+                                q.append((next_i, next_j))
+                                visited.add((next_i, next_j))
+                                
+        return can_reach_buldings == total_buildings - 1    # STRONG PRUNING: if some bulding can not be reached from bulding (i, j), then return -1
