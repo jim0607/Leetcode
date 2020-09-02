2601917ba3631efab7ee954e4d3ae70bce8e10ad
Jim0607 <jinjun.ding@gmail.com>
2020-09-02 11:42:40 -0700

    Update 0815.Bus-Routes.py

--- Solutions/0815.Bus-Routes.py
+++ Solutions/0815.Bus-Routes.py
@@ -15,7 +15,8 @@ The best strategy is take the first bus to the bus stop 7, then take the second 
 
 
 """
-Shortest path problem: bfs. 与word ladder那题类似，word ladder是one-to-one的bfs, 这个是多源节点出发的bfs
+Shortest path problem: bfs. 与word ladder那题类似，word ladder是one-to-one的bfs, 这个是多源节点出发的bfs.
+solution 1: q里面放route idx.
 """
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], S: int, T: int) -> int:
@@ -23,7 +24,7 @@ class Solution:
             return 0
         
         # 首先建立一个stop_to_route的dictionary, 方便从当前stop找下一个stop
-        stop_to_route = collections.defaultdict(lambda: set())
+        stop_to_route = collections.defaultdict(set)
         for i, route in enumerate(routes):
             for stop in route:
                 stop_to_route[stop].add(i)
@@ -50,3 +51,44 @@ class Solution:
                             visited.add(next_idx)
                             
         return -1
+
+    
+    
+"""
+solution 2: q里面放stop
+"""
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], start: int, end: int) -> int:
+        routes = [set(route) for route in routes]
+        
+        # constructe stop_to_route_idx for fast route loop up
+        stop_to_route = collections.defaultdict(list)
+        for idx, route in enumerate(routes):
+            for stop in route:
+                stop_to_route[stop].append(idx)
+        
+        q = collections.deque()
+        visited = set()
+        q.append(start)
+        visited.add(start)
+        
+        stops = -1
+        while len(q) > 0:
+            stops += 1
+            lens = len(q)
+            for _ in range(lens):
+                curr_stop = q.popleft()
+                if curr_stop == end:
+                    return stops
+                for next_stop in self._get_next(routes, stop_to_route, visited, curr_stop):
+                    q.append(next_stop)
+                    visited.add(next_stop)
+        return -1
+                    
+    def _get_next(self, routes, stop_to_route, visited, curr_stop):
+        res = []
+        for idx in stop_to_route[curr_stop]:    # get the route that inludes the stop
+            for stop in routes[idx]:            # get the stop that is in the route
+                if stop not in visited:
+                    res.append(stop)
+        return res
