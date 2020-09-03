b65621f2624b4ce335da4a332e2643ac5a79addc
Jim0607 <jinjun.ding@gmail.com>
2020-09-02 20:18:14 -0700

    Update 0854.K-Similar-Strings.py

--- Solutions/0854.K-Similar-Strings.py
+++ Solutions/0854.K-Similar-Strings.py
@@ -72,33 +72,34 @@ solution 2: prune: How to prune?  there are so many swaps, how to make sure we c
 """
 """
 class Solution:
-    def kSimilarity(self, A: str, B: str) -> int:
+    def kSimilarity(self, start: str, end: str) -> int:
         q = collections.deque()
         visited = set()
-        q.append(A)
-        visited.add(A)
+        q.append(start)
+        visited.add(start)
         
-        step = -1
-        while q:
-            step += 1
+        steps = -1
+        while len(q) > 0:
+            steps += 1
             lens = len(q)
             for _ in range(lens):
-                curr = q.popleft()
-                if curr == B:
-                    return step
-                for next in self._swap(curr, B):   # takes O(N)
-                    if next in visited:
-                        continue
-                    q.append(next)
-                    visited.add(next)
-                
-    def _swap(self, s, B):    # now only takes O(N)
+                curr_node = q.popleft()
+                if curr_node == end:
+                    return steps
+                for next_node in self._get_next(curr_node, end):
+                    if next_node not in visited:
+                        q.append(next_node)
+                        visited.add(next_node)
+    
+    def _get_next(self, curr, end):     # takes O(N)
         i = 0
-        while s[i] == B[i]:   # if S[i]==B[i], we don't need to swap them - strong prune to makes sure swapped string always get more and more similar with B
+        # 寻找第一个不相同的ch的位置
+        # if S[i]==B[i], we don't need to swap them - strong prune to makes sure swapped string always get more and more similar with B
+        while i < len(curr) and curr[i] == end[i]:    
             i += 1
-        for j in range(i + 1, len(s)):
-            if s[j] == B[i]:  # since B[i]!=s[i], if we swap s[j] to s[i], now B[i]=s[i]: this is how every swap make sure we get more and more closer to B
-                yield s[:i] + s[j] + s[i+1:j] + s[i] + s[j+1:]
+        for j in range(i+1, len(curr)):     # 找到第一个不相同的ch后, 我们需要在curr后面找等于那个ch的位置进行交换，这样就把curr[i]那个位置的ch换成与end[i]一样了，这样就离end又更近了一步
+            if curr[j] == end[i]:
+                yield curr[:i] + curr[j] + curr[i+1:j] + curr[i] + curr[j+1:]
 
 """
 这个strong pruning 可以work的原因是我们的candidate_pool是所有的anagram, 而不是给你一个list里面包含一些candidate_pool 的情况，
