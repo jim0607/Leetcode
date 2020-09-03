8ade80af70145403b3e499edd2cbe0623ef52584
Jim0607 <jinjun.ding@gmail.com>
2020-09-03 13:49:49 -0700

    Update 0743.Network-Delay-Time.py

--- Solutions/0743.Network-Delay-Time.py
+++ Solutions/0743.Network-Delay-Time.py
@@ -19,22 +19,25 @@ The length of times will be in the range [1, 6000].
 All edges times[i] = (u, v, w) will have 1 <= u, v <= N and 0 <= w <= 100.
 
 
-
-from heapq import *
 class Solution:
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        # 1. construct a graph represented by a dictionary of adjaency list
         graph = collections.defaultdict(list)       # 根据边构建图 O(E)
-        for u, v, w in times:
-            graph[u].append((v, w))
+        for u, v, time in times:
+            graph[u].append((v, time))
             
-        hq = [(0, K)]   # maintain a min heap to keep track of min time
-        distance = collections.defaultdict()    # distance store the distance of all the nodes to node K, 这个extra space必不可少是用来换时间的。
-        while hq:       # O(NlogN)
-            dist, currNode = heappop(hq)    # instead of popping the closest to source node, we pop the best (lowest cost) node first. Greedy algorithm.
-            if currNode in distance:        # 可以continue的前提是我们每次pop出来的都是最lowest cost的路径，如果已经用最优路径访问过currNode了，接下来再次访问该node肯定不会是最low cost的了，所以可以continue
-                continue
-            distance[currNode] = dist
-            for neighbor, neighbor_dist in graph[currNode]:  # Dijkstra算法里：neighbor这一层只干一件事：那就是把这个nextNode push到hq中
-                heappush(hq, (dist + neighbor_dist, neighbor))  # 千万不要在这里更新distance, 因为不能保证哪个neighbor是low cost的选择，只有push进去之后再pop出来的才是最low cost的
-                    
-        return max(distance.values()) if len(distance) == N else -1
+        # 2. bfs using heapq
+        hq = [(0, K)]   # maintian a min heap to keep track of min time
+        distance = collections.defaultdict(int) # store the distance of each node to node K, 这个extra space必不可少是用来换时间的
+        
+        while len(hq) > 0:          # O(VlogV)
+            curr_dist, curr_node = heappop(hq)
+            
+            if curr_node in distance:   # 可以continue的前提是我们每次pop出来的都是最lowest cost的路径，
+                continue  # 如果已经用最优路径访问过currNode了，接下来再次访问该node肯定不会是最low cost的了，所以可以continue
+            distance[curr_node] = curr_dist
+            
+            for next_node, next_dist in graph[curr_node]:   # Dijkstra算法里：next这一层只干一件事：那就是把这个nextNode push到hq中
+                heappush(hq, (curr_dist + next_dist, next_node))    # 千万不要在这里更新distance, 
+                                            # 因为不能保证哪个neighbor是low cost的选择，只有push进去之后再pop出来的才是最low cost的
+        return max(dist for dist in distance.values()) if len(distance) == N else -1  # len(distance) == N 判断每个点都能访问到
