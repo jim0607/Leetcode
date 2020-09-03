dac08487d2c120f19d48a4932f40bda6f6151378
Jim0607 <jinjun.ding@gmail.com>
2020-09-02 22:52:46 -0700

    Update 0127.Topological-Sorting.py

--- Solutions/0127.Topological-Sorting.py
+++ Solutions/0127.Topological-Sorting.py
@@ -25,64 +25,50 @@ You can assume that there is at least one topological order in the graph.
 
 
 
-"""
+""""
 Definition for a Directed graph node
 class DirectedGraphNode:
     def __init__(self, x):
         self.label = x
         self.neighbors = []
 """
+
+
 class Solution:
     """
     @param: graph: A list of Directed graph node
     @return: Any topological order for the given graph.
     """
     def topSort(self, graph):
-        # write your code here
-        if not graph:
-            return
-        
         # 1. collect indgree information of each node an store in a dict
-        inDegrees = collections.defaultdict(int)
-        inDegrees = self.getInDegrees(graph)
+        in_degrees = collections.defaultdict(int)
+        for node in graph:
+            if node not in in_degrees:
+                in_degrees[node] = 0
+            for n in node.neighbors:
+                in_degrees[n] += 1
         
         # 2. topological sorting - bfs
-        res = []
         q = collections.deque()
-        # 初始化q，一般是q.append(根)，这里是把indegree==0的node 入队列
-        for node, inDegree in inDegrees.items():
-            if inDegree == 0:
+        for node in graph:
+            if in_degrees[node] == 0:   # 初始化q，一般是q.append(根)，这里是把indegree==0的node 入队列
                 q.append(node)
-                res.append(node)        # 孪生兄弟
-        
-        # 模板时间
-        while q:
-            currNode = q.popleft()
-            # 一层一层来
-            for neighbor in currNode.neighbors:
-                inDegrees[neighbor] -= 1
-                if inDegrees[neighbor] == 0:   # 这个判断相当于模板里visited的判断，因为没有一个节点的indgree能两次成为0，所以这个判断可以保证每个节点被访问一次。
-                    q.append(neighbor)
-                    res.append(neighbor)        # 孪生兄弟
-        
+                
+        res = []
+        while len(q) > 0:
+            curr_node = q.popleft()
+            res.append(curr_node)    # 孪生兄弟
+            for next_node in curr_node.neighbors:
+                in_degrees[next_node] -= 1
+                if in_degrees[next_node] == 0:  # 这个判断相当于模板里visited的判断，因为没有一个节点的indgree能两次成为0，所以这个判断可以保证每个节点被访问一次
+                    q.append(next_node)     # always append the in_degree = 0 nodes into queue
         return res
     
-        """follow up question: what if topological order 在这个图中不存在，怎么办？也就是说如果出现了环状依赖的节点怎么办？
-        if len(order) == len(graph):
-            return order
+    
+        """
+        follow up question: what if topological order 在这个图中不存在，怎么办？也就是说如果出现了环状依赖的节点怎么办？
+        if len(res) == len(graph):
+            return res
         else:
             return None
         """
-        
-    # 写完主程序之后再来写子程序
-    def getInDegrees(self, graph):
-        inDegrees = collections.defaultdict(int)
-        # 初始化indgree
-        for n in graph:
-            inDegrees[n] = 0
-            
-        for node in graph:
-            for neighbor in node.neighbors:
-                inDegrees[neighbor] += 1
-        
-        return inDegrees
