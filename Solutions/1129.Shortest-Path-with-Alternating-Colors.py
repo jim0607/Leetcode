712c69427a361aa561915ed6df4d722ec47eb5e9
Jim0607 <jinjun.ding@gmail.com>
2020-09-02 19:56:19 -0700

    Update 1129.Shortest-Path-with-Alternating-Colors.py

--- Solutions/1129.Shortest-Path-with-Alternating-Colors.py
+++ Solutions/1129.Shortest-Path-with-Alternating-Colors.py
@@ -31,11 +31,12 @@ Output: [0,1,1]
 
 
 """
-这一题的题眼是visiting the same node with same color is not allowed, with same color is not.
+这一题的题眼是visiting the same node with same color is allowed, with same color is not.
 所以color信息要放到adjacency list 里，也要放到q里，还要放到visited里
 """
 class Solution:
     def shortestAlternatingPaths(self, n: int, red_edges: List[List[int]], blue_edges: List[List[int]]) -> List[int]:
+    
         # step 1: change the array of edges representation to adjacency list
         graph = collections.defaultdict(list)    # list stores (neighbor, edge color)
         for u, v in red_edges:
@@ -43,25 +44,26 @@ class Solution:
         for u, v in blue_edges:
             graph[u].append((v, "BLUE"))
             
-        # step 2: traversal the graph and update the res
-        # we want to find the shorted steps, so use bfs
+        # step 2: traversal the graph and update the res, we want to find the shortest steps, so use bfs
         res = [-1 for _ in range(n)]
         q = collections.deque()
         visited = set()
         q.append((0, "ORIGIN"))
         visited.add((0, "ORIGIN"))   # visiting the same node with same color is not allowed, with same color is not
         step = -1
-        while q:
+        while len(q) > 0:
             step += 1
             lens = len(q)
             for _ in range(lens):
                 curr_node, curr_color = q.popleft()
-                if res[curr_node] == -1: res[curr_node] = step      # update res
+                if res[curr_node] == -1: 
+                    res[curr_node] = step      # update res
                 for next_node, next_color in graph[curr_node]:
                     if (next_node, next_color) in visited:  # visiting the same node with same color is not allowed
                         continue
-                    if next_color == curr_color:    
+                    if next_color == curr_color:    # have to alternate color
                         continue
                     q.append((next_node, next_color))
                     visited.add((next_node, next_color))
+                    
         return res        
