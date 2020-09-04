b61405a5084d34c356faaf77c38088a1d4d22404
Jim0607 <jinjun.ding@gmail.com>
2020-09-04 12:06:06 -0700

    Update 1091.Shortest-Path-in-Binary-Matrix.py

--- Solutions/1091.Shortest-Path-in-Binary-Matrix.py
+++ Solutions/1091.Shortest-Path-in-Binary-Matrix.py
@@ -94,51 +94,37 @@ A* is better than bfs in finding the shorted path from source node to end node. 
 只要可以想到可行的heuristic estimation的方法，都可以尝试将bfs改成更快的A* . 
 bfs 需要visit every node. but A* only greedily choose the best route to go. The best route is estimated by heuristic estimation.
 """
-from heapq import *
 class Solution:
-    MOVES = [(1, 0), (-1, 0), (1, 1), (-1, 1), (0, 1), (0, -1), (1, -1), (-1, -1)]
+    EMPTY = 0
+    BLOCK = 1
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        source = (0, 0)
-        target = (m-1, n-1)
         if grid[0][0] == 1 or grid[m-1][n-1] == 1:
             return -1
         
-        steps = [[999999] * n for _ in range(m)]     # 记录从source到(i, j)的步数，这个extra space在A*是必须的，用空间换时间
-        steps[0][0] = 1
+        destination = (m-1, n-1)
         
-        hq = []
-        # heapq stores (1. heustic estimation of min # of steps from source to target if 经过currNode, 
-        # 2. currNode_pos, 3. steps from source to currNode)
-        # 注意heustic estimation does not overestimate the actual cost to get to the goal 所以不能用Manhatan distance: abs(m-next_x)+abs(n-next_y).
-        heappush(hq, (max(m, n), source, 1))  # max(m, n) is a heuristic ⁄hjuˈrɪstɪk⁄ estimate of the distance of source node to target, not accurate, but works
-        while hq:
-            heuristicEstimate, (curr_x, curr_y), step = heappop(hq)  # 精华，每次都pop出从source到target的最小步数(by heuristic estimation)
-            steps[curr_x][curr_y] = step
+        # (1. curr_heuristic_estimation of min # of steps from source to target if 经过currNode; 2. curr_steps from source to curr_node; 3. curr_pos))
+        # curr_heuristic_estimation = curr_steps + heuristic estimation of minimum distance from curr_pos to desitination.
+        hq = [(max(m, n), 1, (0, 0))]   
+        steps = collections.defaultdict(int)   # position --> steps from source to curr_node. 用一个dictionary记录curr_steps是很有必要的!!
+        
+        while len(hq) > 0:
+            curr_heuristic_estimation, curr_steps, (curr_i, curr_j)  = heappop(hq)
+            if (curr_i, curr_j) == destination:
+                return curr_steps
             
-            if (curr_x, curr_y) == target:
-                return steps[curr_x][curr_y]
-
-            for delta_x, delta_y in self.MOVES:
-                next_x, next_y = curr_x + delta_x, curr_y + delta_y
-                if self.isNotValid(next_x, next_y, grid):
-                    continue
-                # 这一句是精华，如果之前访问过(next_x, next_y)，但是这次访问的时候发现能更快到达，可否举个例子？
-                # 那就把这个(next_x, next_y)重新再次放进去（这就是为什么不要用visited set in A*），并更新从source到(next_x, next_y)的距离
-                if step + 1 < steps[next_x][next_y]:    
-                    heapq.heappush(hq, (step + max(m - next_x, n - next_y), (next_x, next_y), step + 1))
-                    steps[next_x][next_y] = step + 1    # 更新从source到(next_x, next_y)的距离
-       
+            for delta_i, delta_j in [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,-1),(-1,1)]:
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < m and 0 <= next_j < n and grid[next_i][next_j] == self.EMPTY:
+                    
+                    # 这一句是精华，如果之前访问过(next_x, next_y)，但是这次访问的时候发现能更快到达，
+                    # 那就把这个(next_x, next_y)重新再次放进去, 这就是为什么需要用一个dictionary记录curr_steps
+                    if (next_i, next_j) not in steps or curr_steps + 1 < steps[(next_i, next_j)]:
+                        steps[(next_i, next_j)] = curr_steps + 1        # 更新从source到(next_x, next_y)的距离
+                        next_heuristic_estimation = curr_steps + 1 + max(m - next_i, n - next_j)    
+                        heappush(hq, (next_heuristic_estimation, curr_steps + 1, (next_i, next_j)))
         return -1
-    
-    def isNotValid(self, x, y, grid):
-        m, n = len(grid), len(grid[0])
-        if x < 0 or x >= m or y < 0 or y >= n:
-            return True
-        if grid[x][y] == 1:
-            return True
-        
-        return False
 """
 想想这个题的执行顺序for this example: [[0,1,0,1,0],[1,0,0,0,1],[0,0,1,1,1],[0,0,0,0,0],[1,0,1,0,0]]
 还真和wiki上的动画执行顺序一模一样
