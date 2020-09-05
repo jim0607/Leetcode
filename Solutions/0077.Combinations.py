6680f38905bf590269e62af15b165c9c5e611764
Jim0607 <jinjun.ding@gmail.com>
2020-09-05 11:53:26 -0700

    Update 0077.Combinations.py

--- Solutions/0077.Combinations.py
+++ Solutions/0077.Combinations.py
@@ -17,6 +17,27 @@ Output:
 
 
 
+"""
+C(m, n)：m个里面找出n个的组合问题; C_m_n = (n!)/( (m!)* (n-m)! )
+"""
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(curr_idx, curr_combination):
+            if len(curr_combination) == k:
+                res.append(curr_combination.copy())
+                return
+            
+            for next_idx in range(curr_idx + 1, n + 1):
+                curr_combination.append(next_idx)
+                backtrack(next_idx, curr_combination)
+                curr_combination.pop()            
+            
+        res = []
+        backtrack(0, [])
+        return res
+      
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
