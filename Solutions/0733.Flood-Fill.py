c0c88243dc869210a56c1aeb6371fcf08c4a4d7c
Jim0607 <jinjun.ding@gmail.com>
2020-09-05 09:00:41 -0700

    Update 0733.Flood-Fill.py

--- Solutions/0733.Flood-Fill.py
+++ Solutions/0733.Flood-Fill.py
@@ -27,56 +27,53 @@ The value of each color in image[i][j] and newColor will be an integer in [0, 65
 
 
 """
-solution 1: Union-Find
-"""
-class UnionFind:
-    
-    def __init__(self, grid):
-        self.father = collections.defaultdict()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                self.father[(i, j)] = (i, j)
-                
-    def find(self, x):
-        if self.father[x] == x:
-            return x
-        
-        self.father[x] = self.find(self.father[x])
-        
-        return self.father[x]
-    
-    def connected(self, a, b):
-        return self.find(a) == self.find(b)
-    
-    def union(self, a, b):
-        root_a, root_b = self.find(a), self.find(b)
-        if root_a != root_b:
-            self.father[root_a] = root_b
-            
-
+solution 4: dfs recurssively   
+"""    
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        uf = UnionFind(image)
-        
         moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         m, n = len(image), len(image[0])
         s_color = image[sr][sc]
-
-        for i in range(m):
-            for j in range(n):
-                if image[i][j] == s_color:
-                    for delta_i, delta_j in moves:
-                        neighbor_i, neighbor_j = i + delta_i, j + delta_j
-                        if 0 <= neighbor_i < m and 0 <= neighbor_j < n and image[neighbor_i][neighbor_j] == s_color:
-                            uf.union((i, j), (neighbor_i, neighbor_j))
-                            
-        for i in range(m):
-            for j in range(n):
-                if image[i][j] == s_color and uf.connected((i, j), (sr, sc)):
-                    image[i][j] = newColor
-                        
-        return image
         
+        visited = set()
+        visited.add((sr, sc))
+        
+        def dfs(curr_i, curr_j):
+            image[curr_i][curr_j] = newColor
+            
+            for delta_i, delta_j in moves:
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < m and 0 <= next_j < n and image[next_i][next_j] == s_color and \
+                (next_i, next_j) not in visited:
+                    visited.add((next_i, next_j))
+                    dfs(next_i, next_j)
+                    image[next_i][next_j] = newColor
+                    
+        dfs(sr, sc)
+        
+        return image
+    
+    
+"""
+dfs 不需要visited, since we can modify the matrix in place
+"""
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        def dfs(curr_i, curr_j):
+            image[curr_i][curr_j] = newColor
+            for delta_i, delta_j in [(1,0), (0,1), (-1,0), (0,-1)]:
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < len(image) and 0 <= next_j < len(image[0]):
+                    if image[next_i][next_j] == color_should_change:
+                        dfs(next_i, next_j)
+                       
+        color_should_change = image[sr][sc]     # we should only change cells with original color
+        if color_should_change != newColor:
+            dfs(sr, sc)
+        return image
+
+
 
 """
 solution 2: bfs
@@ -133,30 +130,57 @@ class Solution:
                     
         return image
         
+
         
+
 """
-solution 4: dfs recurssively   
-"""    
+solution 1: Union-Find
+"""
+class UnionFind:
+    
+    def __init__(self, grid):
+        self.father = collections.defaultdict()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                self.father[(i, j)] = (i, j)
+                
+    def find(self, x):
+        if self.father[x] == x:
+            return x
+        
+        self.father[x] = self.find(self.father[x])
+        
+        return self.father[x]
+    
+    def connected(self, a, b):
+        return self.find(a) == self.find(b)
+    
+    def union(self, a, b):
+        root_a, root_b = self.find(a), self.find(b)
+        if root_a != root_b:
+            self.father[root_a] = root_b
+            
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        uf = UnionFind(image)
+        
         moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         m, n = len(image), len(image[0])
         s_color = image[sr][sc]
-        
-        visited = set()
-        visited.add((sr, sc))
-        
-        def dfs(curr_i, curr_j):
-            image[curr_i][curr_j] = newColor
-            
-            for delta_i, delta_j in moves:
-                next_i, next_j = curr_i + delta_i, curr_j + delta_j
-                if 0 <= next_i < m and 0 <= next_j < n and image[next_i][next_j] == s_color and \
-                (next_i, next_j) not in visited:
-                    visited.add((next_i, next_j))
-                    dfs(next_i, next_j)
-                    image[next_i][next_j] = newColor
-                    
-        dfs(sr, sc)
-        
+
+        for i in range(m):
+            for j in range(n):
+                if image[i][j] == s_color:
+                    for delta_i, delta_j in moves:
+                        neighbor_i, neighbor_j = i + delta_i, j + delta_j
+                        if 0 <= neighbor_i < m and 0 <= neighbor_j < n and image[neighbor_i][neighbor_j] == s_color:
+                            uf.union((i, j), (neighbor_i, neighbor_j))
+                            
+        for i in range(m):
+            for j in range(n):
+                if image[i][j] == s_color and uf.connected((i, j), (sr, sc)):
+                    image[i][j] = newColor
+                        
         return image
+        
