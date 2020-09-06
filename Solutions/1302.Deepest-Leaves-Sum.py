0834b2e147d3398f39d647ffc2ac1f003ccab0cc
Jim0607 <jinjun.ding@gmail.com>
2020-09-06 08:59:33 -0700

    Update 1302.Deepest-Leaves-Sum.py

--- Solutions/1302.Deepest-Leaves-Sum.py
+++ Solutions/1302.Deepest-Leaves-Sum.py
@@ -16,28 +16,35 @@ Output: 15
 first dfs find the max depth, 2nd dfs get the sum of all nodes with max depth.
 solution 2: level order bfs, 每次都在while循环里初始化max_depth_sums就可以保证输出的是最后一层的sums了，只需一次遍历
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+dfs find leaf and put it in the res
+"""
 class Solution:
     def deepestLeavesSum(self, root: TreeNode) -> int:
-        max_depth = self._find_max_depth(root, -1)
-        self.sums = 0
-        self._find_sum(root, 0, max_depth)
-        return self.sums
-    
-    def _find_max_depth(self, root, curr_depth):
         if not root:
-            return curr_depth
-        
-        max_depth = curr_depth
-        max_depth = max(max_depth, self._find_max_depth(root.left, curr_depth + 1))
-        max_depth = max(max_depth, self._find_max_depth(root.right, curr_depth + 1))
-        
-        return max_depth
+            return 0
+        max_depth = self._get_max_depth(root)
+        return self._helper(root, 1, max_depth)
     
-    def _find_sum(self, root, curr_depth, max_depth):
+    def _get_max_depth(self, root):
         if not root:
-            return
-        if curr_depth == max_depth:
-            self.sums += root.val
-            return
-        self._find_sum(root.left, curr_depth + 1, max_depth)
-        self._find_sum(root.right, curr_depth + 1, max_depth)       
+            return 0
+        return 1 + max(self._get_max_depth(root.left), self._get_max_depth(root.right))
+    
+    def _helper(self, root, curr_depth, max_depth):
+        if not root:
+            return 0
+        if not root.left and not root.right and curr_depth == max_depth:
+            return root.val
+        
+        left_sum = self._helper(root.left, curr_depth + 1, max_depth)
+        right_sum = self._helper(root.right, curr_depth + 1, max_depth)
+        
+        return left_sum + right_sum
