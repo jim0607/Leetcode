b7d5e94046e6d695427f11000abfa3da21ab495d
Jim0607 <jinjun.ding@gmail.com>
2020-09-06 11:46:51 -0700

    Update 0968.Binary-Tree-Cameras.py

--- Solutions/0968.Binary-Tree-Cameras.py
+++ Solutions/0968.Binary-Tree-Cameras.py
@@ -19,6 +19,38 @@ Output: 2
 Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
 
 
+    
+    
+class Solution:
+    def minCameraCover(self, root: TreeNode) -> int:
+        root_not_cv, root_cv_w, root_cv_wo = self._helper(root)
+        return min(root_cv_w, root_cv_wo)
+    
+    def _helper(self, root):
+        """
+        Return minimum number of cameras needed to cover all the node's children with 
+        (node not covered, node covered: 1. with a camera on node, 2. without a camera on node)
+        """
+        if not root:
+            return 0, 1, 0
+        
+        # 注意left_not_cv, left_cv_w, left_cv_wo经保证root.left的下一层已经cover好了
+        # 注意left_not_cv, left_cv_w, left_cv_wo都是从下面一层得到的信息, 与root层无关, 注意left_not_cv表示没有被自己或下一层cover到
+        left_not_cv, left_cv_w, left_cv_wo = self._helper(root.left)
+        right_not_cv, right_cv_w, right_cv_wo = self._helper(root.right)
+        
+        # 接下来看看root层, 首先root_not_cv肯定是left has not no camera and right has no camera
+        root_not_cv = left_cv_wo + right_cv_wo
+        # 然后root covered without a camera肯定是left has a camera or right has a camera, or both have camera
+        root_cv_wo = min(left_cv_w + min(right_cv_w, right_cv_wo), right_cv_w + min(left_cv_w, left_cv_wo))
+        # 最后root covered with a camera on it肯定是1 + 左边不管有没有被left或left的下一层cover到的最小值 + 右边同样
+        root_cv_w = 1 + min(left_not_cv, left_cv_w, left_cv_wo) + min(right_not_cv, right_cv_w, right_cv_wo)
+        
+        return root_not_cv, root_cv_w, root_cv_wo
+    
+    
+    
+    
 
 """
 solution 1: dp. bottom up:
