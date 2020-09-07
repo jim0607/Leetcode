9404880d01e59b4b71f6f7e22fba30a45b77f765
Jim0607 <jinjun.ding@gmail.com>
2020-09-06 19:03:46 -0700

    Update 0784.Letter-Case-Permutation.py

--- Solutions/0784.Letter-Case-Permutation.py
+++ Solutions/0784.Letter-Case-Permutation.py
@@ -18,6 +18,32 @@ Output: ["12345"]
 backtrack - O(2^N)
 """
 class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        def backtrack(curr_idx, curr_comb):
+            if len(curr_comb) == len(s):
+                res.append("".join(curr_comb.copy()))
+                return
+            for next_idx in range(curr_idx + 1, len(s)):    # 注意这个题目next_idx不能往回找，
+                if s[next_idx].isdigit():                   # 不然的话"a1b2"-->"1a2b"就变成valid答案了
+                    curr_comb.append(s[next_idx])
+                    backtrack(next_idx, curr_comb)
+                    curr_comb.pop()
+                elif s[next_idx].isalpha():
+                    for ch in [s[next_idx].upper(), s[next_idx].lower()]:
+                        curr_comb.append(ch)
+                        backtrack(next_idx, curr_comb)
+                        curr_comb.pop()
+  
+        res = []
+        backtrack(-1, [])
+        return res
+
+
+
+"""
+下面的写法更加简洁
+"""
+class Solution:
     def letterCasePermutation(self, S: str) -> List[str]:
         res = []
         self._backtrack(S, -1, "", res)
@@ -30,7 +56,7 @@ class Solution:
     
         for next_idx in range(curr_idx + 1, len(S)):
             if S[next_idx].isdigit():
-                self._backtrack(S, next_idx, curr_str + S[next_idx], res)
+                self._backtrack(S, next_idx, curr_str + S[next_idx], res)   # 这里不需要append和pop是因为curr_str是immutable的，不会改变所以不能改过来改过去
             else:
                 self._backtrack(S, next_idx, curr_str + S[next_idx].upper(), res)
                 self._backtrack(S, next_idx, curr_str + S[next_idx].lower(), res)
