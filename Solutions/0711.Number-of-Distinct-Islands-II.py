f498fde7005f62e371bd40fe3e84251f17b1b573
Jim0607 <jinjun.ding@gmail.com>
2020-09-07 22:19:31 -0700

    Update 0711.Number-of-Distinct-Islands-II.py

--- Solutions/0711.Number-of-Distinct-Islands-II.py
+++ Solutions/0711.Number-of-Distinct-Islands-II.py
@@ -42,37 +42,57 @@ are considered same island shapes. Because if we flip the first array in the up/
 
 
 
+"""
+step 1: use the relative lacation of each "1" with respect to the staring point as the signature of shape.
+step 2: rotate and reflect+rotate them against (0,0) in 8 directions, to get hte signature of the rotated shapes
+step 3: choose the smallest among 8 directions to hash.
+"""
 class Solution:
     def numDistinctIslands2(self, grid: List[List[int]]) -> int:
-        self.moves = [(1,0,"d"), (0,1,"r"), (-1,0,"u"), (0,-1,"l")]     # 把方向信息也要带进去
-        shapes = []
+   
+        # step 1: use the relative lacation of each "1" with respect to the staring point as the signature of shape.
+        def dfs(org_i, org_j, curr_i, curr_j):
+            shape.append([curr_i - org_i, curr_j - org_j])      # append the relative lacation into shape
+            for delta_i, delta_j in [(1, 0), (-1, 0), (0, 1), (0, -1)]: 
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < m and 0 <= next_j < n:
+                    if grid[next_i][next_j] == 1:
+                        if (next_i, next_j) not in visited:
+                            visited.add((next_i, next_j))
+                            dfs(org_i, org_j, next_i, next_j)
+                            
+        # step 2: rotate and reflect+rotate them against (0,0) in 8 directions, to get hte signature of all the rotated shapes         
+        def rotate(shape):
+            rotated = [[] for _ in range(8)]
+            for x, y in shape:
+                rotated[0].append([x, y])
+                rotated[1].append([-x, y])
+                rotated[2].append([x, -y])
+                rotated[3].append([-x, -y])
+                rotated[4].append([y, x])
+                rotated[5].append([-y, x])
+                rotated[6].append([y, -x])
+                rotated[7].append([-y, -x])
+            for rot in rotated:
+                rot.sort()
+                x0, y0 = rot[0]
+                for pos in rot:
+                    pos[0] -= x0
+                    pos[1] -= y0
+            rotated.sort()
+            return rotated[0]       # rotated[0] is the signature of the rotated shapes now
+                            
+        
+        shapes = set()
         visited = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    if (i, j) not in visited:
-                        visited.add((i, j))
-                        shape = []
-                        self._dfs(grid, i, j, visited, "o", shape)      # "o" means original point
-                        self._transform(shape)          # 与694. Number of Distinct Islands相比就增加了这一句
-                        shapes.append("".join(shape))
-                        
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (i, j) not in visited:
+                    shape = []
+                    visited.add((i, j))
+                    dfs(i, j, i, j)                 # pass original_i and original_j into dfs
+                    rotated_shape = rotate(shape)   # get the signature of the shape
+                    shapes.add(tuple(x for pos in rotated_shape for x in pos))
+
         return len(set(shapes))
-        
-    def _dfs(self, grid, i, j, visited, direction, shape):
-        shape.append(direction)
-        
-        for delta_i, delta_j, next_dir in self.moves:
-            next_i, next_j = i + delta_i, j + delta_j
-            if 0 <= next_i < len(grid) and 0 <= next_j < len(grid[0]):
-                if grid[next_i][next_j] == 1:
-                    if (next_i, next_j) not in visited:
-                        visited.add((next_i, next_j))
-                        self._dfs(grid, next_i, next_j, visited, next_dir, shape)
-                        shape.append("b")       # go back 非常重要！！下面举例讲解
-                        
-    def _trasform(self, shape):
-        """
-        return the transform of the shape, transform includes rotation and reflection
-        """
-        pass
