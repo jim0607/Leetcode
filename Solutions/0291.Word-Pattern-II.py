2d22c3ea70dd8a5b609409ccb258091a904e3589
Jim0607 <jinjun.ding@gmail.com>
2020-09-08 15:53:30 -0700

    Update 0291.Word-Pattern-II.py

--- Solutions/0291.Word-Pattern-II.py
+++ Solutions/0291.Word-Pattern-II.py
@@ -1,3 +1,4 @@
+"""
 291. Word Pattern II
 
 Given a pattern and a string str, find if str follows the same pattern.
@@ -21,6 +22,53 @@ Output: false
 Constraints:
 
 You may assume both pattern and str contains only lowercase letters.
+"""
+
+
+
+"""
+backtrack传入参数(curr_s_idx, curr_p_idx, ch_to_str, str_to_ch).
+backtrack结束条件: if curr_s_idx == len(s) - 1 and curr_p_idx == len(pattern) - 1.
+is valid: if ch_to_str[next_ch] == next_word and str_to_ch[next_word] == next_ch.
+"""
+class Solution:
+    def wordPatternMatch(self, pattern: str, s: str) -> bool:
+        def backtrack(curr_s_idx, curr_p_idx, ch_to_str, str_to_ch):
+            if curr_s_idx == len(s) - 1 and curr_p_idx == len(pattern) - 1:
+                return True
+            if curr_s_idx == len(s) - 1:
+                return False
+            if curr_p_idx == len(pattern) - 1:
+                return False
+            for next_s_idx in range(curr_s_idx + 1, len(s)):
+                next_ch = pattern[curr_p_idx + 1]
+                next_word = s[curr_s_idx + 1: next_s_idx + 1]
+                if next_ch in ch_to_str:
+                    if ch_to_str[next_ch] != next_word or str_to_ch[next_word] != next_ch:
+                        continue
+                    if backtrack(next_s_idx, curr_p_idx + 1, ch_to_str, str_to_ch):
+                        return True
+                else:
+                    if next_word in str_to_ch:
+                        continue
+                    ch_to_str[next_ch] = next_word
+                    str_to_ch[next_word] = next_ch
+                    if backtrack(next_s_idx, curr_p_idx + 1, ch_to_str, str_to_ch):
+                        return True
+                    del ch_to_str[next_ch]      # 这里的del相当于visited.remove - 做backtrack
+                    del str_to_ch[next_word]
+            return False
+                    
+                
+        ch_to_str = collections.defaultdict(str)
+        str_to_ch = collections.defaultdict(str)
+        return backtrack(-1, -1, ch_to_str, str_to_ch)
+
+""" 只要熟练理解背诵backtrack模板, 再难的题也是一遍过 """
+
+
+
+
 
 
 
