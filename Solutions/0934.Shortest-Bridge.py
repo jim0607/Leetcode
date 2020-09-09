0623f4c67b69a1ff1d2b9dd913293f919ced0924
Jim0607 <jinjun.ding@gmail.com>
2020-09-08 21:41:51 -0700

    Update 0934.Shortest-Bridge.py

--- Solutions/0934.Shortest-Bridge.py
+++ Solutions/0934.Shortest-Bridge.py
@@ -22,6 +22,62 @@ Input: A = [[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
 Output: 1
 
 
+ 
+"""
+setp 1: 用outliners_1, outliners_2 = set(), set()找到两个island的outliner. 
+step 2: 接下来是多源节点出发求最短路径问题 - bfs
+"""
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        def dfs(curr_i, curr_j, outliners):
+            visited.add((curr_i, curr_j))
+            for delta_i, delta_j in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < m and 0 <= next_j < n:
+                    if grid[next_i][next_j] == 0:       # ****注意这样找一个连通区域的outliners
+                        outliners.add((next_i, next_j))
+                    elif grid[next_i][next_j] == 1:
+                        if (next_i, next_j) not in visited:
+                            dfs(next_i, next_j, outliners)
+                                       
+        # step 1: dfs找到两个独立岛屿的outliners
+        m, n = len(grid), len(grid[0])
+        visited = set()
+        outliners_1, outliners_2 = set(), set()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (i, j) not in visited:
+                    if len(outliners_1) == 0:
+                        dfs(i, j, outliners_1)
+                    else:
+                        dfs(i, j, outliners_2)
+        
+        # step 2: 接下来是多源节点出发求最短路径问题 - bfs
+        q = collections.deque()
+        visited = set()
+        for i, j in outliners_1:
+            q.append((i, j))
+            visited.add((i, j))
+        steps = 0
+        while len(q) > 0:
+            steps += 1
+            lens = len(q)
+            for _ in range(lens):
+                curr_i, curr_j = q.popleft()
+                if (curr_i, curr_j) in outliners_2:     # when reached desitination
+                    return steps
+                for delta_i, delta_j in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
+                    next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                    if 0 <= next_i < m and 0 <= next_j < n:
+                        if grid[next_i][next_j] == 0:
+                            if (next_i, next_j) not in visited:
+                                q.append((next_i, next_j))
+                                visited.add((next_i, next_j))
+        return steps
+ 
+ 
+ 
+ 
 
 class Solution:
     def shortestBridge(self, A: List[List[int]]) -> int:
@@ -36,7 +92,7 @@ class Solution:
                 next_i, next_j = curr_i + delta_i, curr_j + delta_j
                 if 0 <= next_i < m and 0 <= next_j < n:
                     if A[next_i][next_j] == 0:
-                        outliners.add((curr_i, curr_j))
+                        outliners.add((curr_i, curr_j))   # 也可以outliners.add((next_i, next_j))
                     else:
                         if (next_i, next_j) not in visited:
                             visited.add((next_i, next_j))
