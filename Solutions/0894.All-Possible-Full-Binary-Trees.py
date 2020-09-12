ce9ae35ef401cb8d7a6c0d1f29450484841677cf
Jim0607 <jinjun.ding@gmail.com>
2020-09-11 23:44:36 -0700

    Update 0894.All-Possible-Full-Binary-Trees.py

--- Solutions/0894.All-Possible-Full-Binary-Trees.py
+++ Solutions/0894.All-Possible-Full-Binary-Trees.py
@@ -1,3 +1,4 @@
+"""
 894. All Possible Full Binary Trees
 
 A full binary tree is a binary tree where each node has exactly 0 or 2 children.
@@ -14,7 +15,36 @@ Example 1:
 
 Input: 7
 Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
-
+"""
+ 
+ 
+"""
+good example of recursion
+"""
+class Solution:
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        if N % 2 != 1:    # only odd number of nodes can form a full binary tree
+            return []
+        if N == 1:
+            return [TreeNode(0)]
+        
+        res = []
+        for i in range(1, N, 2):
+            left_roots = self.allPossibleFBT(i)
+            right_roots = self.allPossibleFBT(N-i-1)
+            for left_node in left_roots:
+                for right_node in right_roots:
+                    root = TreeNode(0)
+                    root.left = left_node
+                    root.right = right_node
+                    res.append(root)
+            
+        return res
+ 
+ 
+ 
+ 
+ 
 
 class Solution:
     def allPossibleFBT(self, N: int) -> List[TreeNode]:
