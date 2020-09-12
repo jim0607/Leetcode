6a5575ef31a3b5c2799311759fe446642be42aaa
Jim0607 <jinjun.ding@gmail.com>
2020-09-12 16:29:36 -0700

    Update 0514.Freedom-Trail.py

--- Solutions/0514.Freedom-Trail.py
+++ Solutions/0514.Freedom-Trail.py
@@ -1,3 +1,4 @@
+"""
 514. Freedom Trail
 
 In the video game Fallout 4, the quest "Road to Freedom" requires players to reach a metal dial called the "Freedom Trail Ring", and use the dial to spell a specific keyword in order to open the door.
@@ -11,8 +12,6 @@ At the stage of rotating the ring to spell the key character key[i]:
 You can rotate the ring clockwise or anticlockwise one place, which counts as 1 step. The final purpose of the rotation is to align one of the string ring's characters at the 12:00 direction, where this character must equal to the character key[i].
 If the character key[i] has been aligned at the 12:00 direction, you need to press the center button to spell, which also counts as 1 step. After the pressing, you could begin to spell the next character in the key (next stage), otherwise, you've finished all the spelling.
 Example:
-
-
  
 Input: ring = "godding", key = "gd"
 Output: 4
@@ -21,6 +20,8 @@ For the first key character 'g', since it is already in place, we just need 1 st
 For the second key character 'd', we need to rotate the ring "godding" anticlockwise by two steps to make it become "ddinggo".
 Also, we need 1 more step for spelling.
 So the final output is 4.
+"""
+
 
 
 """
@@ -30,23 +31,26 @@ then memo[(curr_ring, curr_idx)] = min(memo[(curr_ring, curr_idx)], steps + 1 + 
 """
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
-        def dfs(curr_ring, curr_idx, memo):
-            if curr_idx == len(key):
+        def dfs(curr_ring, curr_idx):
+            if curr_idx == len(key) - 1:
                 return 0
-            
+       
             if (curr_ring, curr_idx) in memo:
-                return memo[(curr_ring, curr_idx)]  # memo stores the steps needed if from (curr_ring, curr_idx)
-            
-            memo[(curr_ring, curr_idx)] = float("inf")
-            for i, ch in enumerate(curr_ring):
-                if ch == key[curr_idx]:
+                return memo[(curr_ring, curr_idx)]
+         
+            min_step = float("inf")
+            for i in range(len(curr_ring)):
+                if curr_ring[i] == key[curr_idx+1]:
+                    this_step = min(i, len(curr_ring) - i)
                     next_ring = curr_ring[i:] + curr_ring[:i]
-                    steps = min(i, len(curr_ring) - i)
-                    memo[(curr_ring, curr_idx)] = min(memo[(curr_ring, curr_idx)], steps + 1 + dfs(next_ring, curr_idx + 1, memo))
-                    
-            return memo[(curr_ring, curr_idx)]
+                    min_step = min(min_step, this_step + 1 + dfs(next_ring, curr_idx+1))
+                 
+            memo[(curr_ring, curr_idx)] = min_step
+            return min_step        
         
-        return dfs(ring, 0, {})
+        
+        memo = collections.defaultdict(int) # (curr_ring, curr_idx --> minimum step needed to reach target)
+        return dfs(ring, -1)
         
 """ 
 也可以用dp解: dp[curr_idx in ring][curr_idx in key] = steps needed if from (curr_idx in ring, curr_idx in key)
