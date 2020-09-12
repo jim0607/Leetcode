16f35e62877d0dc05265e07cd63a9b2e37c9c6ee
Jim0607 <jinjun.ding@gmail.com>
2020-09-12 12:50:12 -0700

    Update 0294.Flip-Game-II.py

--- Solutions/0294.Flip-Game-II.py
+++ Solutions/0294.Flip-Game-II.py
@@ -1,3 +1,4 @@
+"""
 294. Flip Game II
 
 You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
@@ -11,6 +12,7 @@ Output: true
 Explanation: The starting player can guarantee a win by flipping the middle "++" to become "+--+".
 Follow up:
 Derive your algorithm's runtime complexity.
+"""
 
 
 
