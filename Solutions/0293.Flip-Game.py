021d167352d13509f178ecd82c653ae6c1986678
Jim0607 <jinjun.ding@gmail.com>
2020-09-12 12:19:11 -0700

    Update 0293.Flip-Game.py

--- Solutions/0293.Flip-Game.py
+++ Solutions/0293.Flip-Game.py
@@ -1,3 +1,4 @@
+"""
 293. Flip Game
 
 You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
@@ -14,12 +15,13 @@ Output:
   "++--"
 ]
 Note: If there is no valid move, return an empty list [].
+"""
 
 
 class Solution:
     def generatePossibleNextMoves(self, s: str) -> List[str]:
         res = []
         for i in range(1, len(s)):
-            if s[i] == "+" and s[i-1] == "+":
+            if s[i] == s[i-1] == "+":
                 res.append(s[:i-1] + "--" + s[i+1:])
         return res
