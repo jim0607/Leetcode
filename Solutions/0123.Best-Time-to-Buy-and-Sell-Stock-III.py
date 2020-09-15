313e2eae4ece6e7890ba178d38825fd4255a347a
Jim0607 <jinjun.ding@gmail.com>
2020-09-14 20:58:31 -0700

    Update 0123.Best-Time-to-Buy-and-Sell-Stock-III.py

--- Solutions/0123.Best-Time-to-Buy-and-Sell-Stock-III.py
+++ Solutions/0123.Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -1,3 +1,4 @@
+"""
 123. Best Time to Buy and Sell Stock III
 
 Say you have an array for which the ith element is the price of a given stock on day i.
@@ -12,6 +13,7 @@ Input: [3,3,5,0,0,3,1,4]
 Output: 6
 Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
              Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+"""
 
 
 
