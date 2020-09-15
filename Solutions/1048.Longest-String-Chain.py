e448c610b8094dfc0661ec0b233e5d3e79cbe129
Jim0607 <jinjun.ding@gmail.com>
2020-09-15 11:00:19 -0700

    Update 1048.Longest-String-Chain.py

--- Solutions/1048.Longest-String-Chain.py
+++ Solutions/1048.Longest-String-Chain.py
@@ -1,3 +1,4 @@
+"""
 1048. Longest String Chain
 
 Given a list of words, each word consists of English lowercase letters.
@@ -8,7 +9,6 @@ A word chain is a sequence of words [word_1, word_2, ..., word_k] with k >= 1, w
 
 Return the longest possible length of a word chain with words chosen from the given list of words.
 
- 
 
 Example 1:
 
@@ -22,6 +22,7 @@ Note:
 1 <= words.length <= 1000
 1 <= words[i].length <= 16
 words[i] only consists of English lowercase letters.
+"""
 
 
 """
@@ -31,40 +32,22 @@ if predecessor in dp: dp[word] = max(dp[redesessor]+1)
 """
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        # 也可以用这么写：words.sort(words, key = lambda word: len(word)) 
-        self.sortByLens(words, 0, len(words) - 1)   # O(NlogN), , where N is length of words list
-        dp = collections.defaultdict(lambda: 1)
-        maxLens = 1
-        for word in words:                          # O(N)
-            for i in range(len(word)):              # O(M), where M is length of word
-                prevWord = word[:i] + word[i+1:]
-                if prevWord in dp:
-                    dp[word] = max(dp[word], dp[prevWord] + 1)
-            maxLens = max(maxLens, dp[word])
-            
-        return maxLens
+        words.sort(key = lambda x: len(x))      # sort by lens of word
         
-    def sortByLens(self, words, start, end):
-        """
-        sort the words by lens using quick sort, in-place
-        """
-        if start >= end:
-            return
-
-        pivot = len(words[(start + end)//2])
-        left, right = start, end
-        while left <= right: 
-            while left <= right and len(words[left]) < pivot:
-                left += 1
-            while left <= right and len(words[right]) > pivot:
-                right -= 1
-            if left <= right:
-                words[left], words[right] = words[right], words[left]
-                left += 1
-                right -= 1
-
-        self.sortByLens(words, start, right)
-        self.sortByLens(words, left, end)
+        # key is word, val is the LSC ended with the word
+        dp = collections.defaultdict(lambda: 1)
+        max_lens = 0
+        for word in words:              # O(N)
+            for i in range(len(word)):  # O(M), where M is length of word
+                predecessor = word[:i] + word[i+1:]
+                if predecessor in dp:
+                    dp[word] = max(dp[word], dp[predecessor] + 1)
+                    
+            max_lens = max(max_lens, dp[word])
+            
+        return max_lens
 
 
+"""
 注意用word ladder类似的bfs是解不了的，因为最长的那个chain不一定是从minLens word开始的，所以根本不知道第一步哪些word append到queue里。
+"""
