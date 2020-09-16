faa985eb3884a80a9302dfe440c8955d227a56c9
Jim0607 <jinjun.ding@gmail.com>
2020-09-15 21:14:56 -0700

    Update 1043.Partition-Array-for-Maximum-Sum.py

--- Solutions/1043.Partition-Array-for-Maximum-Sum.py
+++ Solutions/1043.Partition-Array-for-Maximum-Sum.py
@@ -1,16 +1,16 @@
+"""
 1043. Partition Array for Maximum Sum
 
 Given an integer array A, you partition the array into (contiguous) subarrays of length at most K.  After partitioning, each subarray has their values changed to become the maximum value of that subarray.
 
 Return the largest sum of the given array after partitioning.
 
- 
-
 Example 1:
 
 Input: A = [1,15,7,9,2,5,10], K = 3
 Output: 84
 Explanation: A becomes [15,15,15,9,10,10,10]
+"""
 
 
 
@@ -29,7 +29,7 @@ class Solution:
         dp = [0 for j in range(len(A) + 1)]     # dp[j] is the maxsum before j
         for j in range(1, len(A) + 1):
             curr_max = A[j - 1]
-            for i in range(min(j, K)):       # i 是后退的步数
-                curr_max = max(curr_max, A[j - i - 1])
-                dp[j] = max(dp[j], dp[j - i - 1] + curr_max * (i + 1))
+            for i in range(min(j, K)):          # i 是后退的步数
+                curr_max = max(curr_max, A[j - 1 - i])
+                dp[j] = max(dp[j], dp[j - 1 - i] + curr_max * (i + 1))
         return dp[-1]
