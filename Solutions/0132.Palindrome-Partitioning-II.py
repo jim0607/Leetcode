af59f45ce17d446304b2bc39ad6011e93c5511ac
Jim0607 <jinjun.ding@gmail.com>
2020-09-15 19:56:28 -0700

    Update 0132.Palindrome-Partitioning-II.py

--- Solutions/0132.Palindrome-Partitioning-II.py
+++ Solutions/0132.Palindrome-Partitioning-II.py
@@ -1,3 +1,4 @@
+"""
 132. Palindrome Partitioning II
 
 Given a string s, partition s such that every substring of the partition is a palindrome.
@@ -9,41 +10,37 @@ Example:
 Input: "aab"
 Output: 1
 Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+"""
 
 
-"""f[j]=the minimum number of total palindrom a palindrome partitining till the jth character (not including j meaning the last palindrome should end with the j-1th character)
+
+"""
+f[j]=the minimum number of total palindrom a palindrome partitining till the jth character (not including j meaning the last palindrome should end with the j-1th character)
 f[j]=min(f[i]+1) for i<j and s[i:j] is palindrome
 O(N^3)
 """
 class Solution:
     def minCut(self, s: str) -> int:
-        if not s:
-            return 0
-        
-        lens = len(s)
-        dp = [float("inf")] * (lens + 1)
-        dp[0] = 0
-        
-        for j in range(1, lens + 1):
+        n = len(s)
+        dp = [n for _ in range(n + 1)]
+        dp[0] = -1
+        dp[1] = 0
+        for j in range(2, n+1):
             for i in range(j):
-                if self.isPalindrome(s[i:j]):   # the last palindrome should include jth character
-                    dp[j] = min(dp[i] + 1, dp[j])
-
-        return dp[-1] - 1       # 注意题目问的是最少划分几次
+                if self._is_panlin(s[i:j]):
+                    dp[j] = min(dp[j], dp[i] + 1)
+        return dp[-1]
     
-    def isPalindrome(self, s):
-        if len(s) <= 1:
-            return True
-        
-        i, j = 0, len(s) - 1
-        while i < j:
-            if s[i] != s[j]:
+    def _is_panlin(self, t):
+        left, right = 0, len(t) - 1
+        while left < right:
+            if t[left] != t[right]:
                 return False
-            i += 1
-            j -= 1
-            
+            left += 1
+            right -= 1
         return True
 
+    
     
 """"
 优化为O(N^2), 用一个isPalin[i][j]记录s[i:j]是否是palindrome, 更新isPalin[i][j]的方法与leetcode 5 相同，
@@ -51,28 +48,26 @@ class Solution:
 ""
 class Solution:
     def minCut(self, s: str) -> int:
-        lens = len(s)
-        if lens == 0:
-            return 0
-        if lens == 1:
-            return 0
         
-        # dp[i]=the min cuts needed to partition to ith, not including i
-        dp = [float("inf") for _ in range(lens + 1)]   
-        dp[0] = 0
-        
-        # # isPalin[i][j]=if it is a palin from i->j, not including j, basically s[i:j]
-        isPalin = [[False for _ in range(lens + 1)] for _ in range(lens + 1)]
-        
-        for j in range(1, lens + 1):
-            for i in range(j):
-                if s[i] == s[j - 1]:
-                    if j - 1 - i <= 1:      # 初始化isPalin
-                        isPalin[i][j] = True
+        # step 1: pre-calculate the palindeome and store them in matrix - same as 5. Longest Palindromic Substring
+        n = len(s)
+        palin = [[False for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            palin[i][i] = True
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    if j == i + 1:
+                        palin[i][j] = True
                     else:
-                        isPalin[i][j] = isPalin[i + 1][j - 1]
+                        palin[i][j] = palin[i+1][j-1]    
                         
-                if isPalin[i][j]:
+        # step 2: dp to update minimum cuts
+        dp = [n for _ in range(n + 1)]  # dp[i]=the min cuts needed to partition to ith, not including i
+        dp[0] = -1
+        dp[1] = 0
+        for j in range(2, n+1):
+            for i in range(j):
+                if palin[i][j-1]:
                     dp[j] = min(dp[j], dp[i] + 1)
-                    
-        return dp[lens] - 1
+        return dp[-1]
