94fa326d4cd00efed13afa045d0da1f26de7e694
Jim0607 <jinjun.ding@gmail.com>
2020-09-15 23:33:25 -0700

    Update 0089.k-Sum.py

--- Solutions/0089.k-Sum.py
+++ Solutions/0089.k-Sum.py
@@ -1,4 +1,5 @@
-"""•要求从一些正整数中选出一些，使得和是Target
+"""
+要求从一些正整数中选出一些，使得和是Target
 • 背包问题
 • 数组A：各个物品的重量
 • Target：背包最大称重
@@ -12,9 +13,10 @@ k = 2
 target = 5
 Output: 2
 Explanation: 1 + 4 = 2 + 3 = 5
+"""
 
-
-"""•要求从一些正整数中选出一些，使得和是Target
+"""
+•要求从一些正整数中选出一些，使得和是Target
 • 背包问题
 • 数组A：各个物品的重量
 • Target：背包最大称重
@@ -26,7 +28,8 @@ Explanation: 1 + 4 = 2 + 3 = 5
 Target - An-1
 • 要知道还有几个数可选，以及它们的和需要是多少：序列加状态
 • 状态：f[i][j][s]表示有多少种方法可以在前i个数中选出j个，使得它们的和是s
-f[i][j][s] = f[i-1][j][s]; if s>=A[i-1]: f[i][j][s] += f[i-1][k-1][s-A[i-1]] """
+f[i][j][s] = f[i-1][j][s]; if s>=A[i-1]: f[i][j][s] += f[i-1][k-1][s-A[i-1]]
+"""
 
 """
 采用动态规划（dp）的思想，进行状态转移，记录数字和和出现次数之间的关系。
@@ -36,18 +39,15 @@ initialize: dp[i][0][0] = 1
 return dp[lens][k][target]
 """
 class Solution:
-    def kSum(self, A, k, target):
-        lens = len(A)
-        dp = [[[0 for _ in range(target + 1)] for _ in range(k +  1)] for _ in range(lens + 1)]
-        for i in range(lens + 1):
+    def kSum(self, A, K, target):
+        dp = [[[0 for _ in range(target + 1)] for _ in range(K + 1)] for _ in range(len(A) + 1)]
+        for i in range(len(A) + 1):
             dp[i][0][0] = 1
-        
-        for i in range(1, lens + 1):
-            for j in range(1, min(i + 1, k + 1)):   # 注意j的range
-                for t in range(1, target + 1):
-                    if t >= A[i - 1]:
-                        dp[i][j][t] = dp[i - 1][j - 1][t - A[i - 1]] + dp[i - 1][j][t]
-                    else:
-                        dp[i][j][t] = dp[i - 1][j][t]
-                    
-        return dp[lens][k][target]
+            
+        for i in range(1, len(A) + 1):
+            for k in range(1, min(K + 1, i + 1)):   # 注意k的range
+                for m in range(1, target + 1):
+                    dp[i][k][m] = dp[i-1][k][m]
+                    if m >= A[i-1]:
+                        dp[i][k][m] += dp[i-1][k-1][m-A[i-1]]
+        return dp[-1][-1][-1]
