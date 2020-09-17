b2a5ec9344a31d560b51b79079ea18941e89392b
Jim0607 <jinjun.ding@gmail.com>
2020-09-16 23:25:58 -0700

    Update 0115.Distinct-Subsequences.py

--- Solutions/0115.Distinct-Subsequences.py
+++ Solutions/0115.Distinct-Subsequences.py
@@ -1,6 +1,8 @@
+"""
 Given a string S and a string T, count the number of distinct subsequences of S which equals T.
 
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
 Example 1:
 
@@ -17,6 +19,7 @@ rabbbit
 ^^ ^^^^
 rabbbit
 ^^^ ^^^
+"""
 
 
 # dp[i][j] = the number of discinct subeseq until ith char in s and jth char in t
@@ -24,24 +27,16 @@ rabbbit
 # else: rabb, rab, dp[i][j] = dp[i-1][j] + dp[i-1][j-1], 品，细品！
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        # dp[i][j] = the number of discinct subeseq until ith char in s and jth char in t
-        # if s[i]!=t[j], dp[i][j] = dp[i - 1][j]  eg: rab, ra
-        # else: rabb, rab, dp[i][j] = dp[i-1][j] + dp[i-1][j-1], 品，细品！
-        # dp[0][0]=1 , dp[0][i] = 0, dp[i][0] = 1, dp[i][j>i] = 0
+        m, n = len(s), len(t)
+        if m < n: return 0
         
-        lens1, lens2 = len(s), len(t)
-        if lens1 == 0 or lens2 == 0:
-            return 0
-        
-        dp = [[0] * (lens2 + 1) for _ in range(lens1 + 1)]
-        for i in range(lens1):
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(m + 1):
             dp[i][0] = 1
-                
-        for i in range(1, lens1 + 1):
-            for j in range(1, lens2 + 1):
-                if s[i - 1] != t[j - 1]:
-                    dp[i][j] = dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]  # eg: rabb, rab
-                    
-        return dp[lens1][lens2]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i-1][j]
+                if s[i-1] == t[j-1]:    
+                    dp[i][j] += dp[i-1][j-1]    # eg: rabb, rab
+        return dp[m][n] 
