3a4a9c1cefd2f2521d439b987f609a654664c622
Jim0607 <jinjun.ding@gmail.com>
2020-09-17 14:19:36 -0700

    Update 0516.Longest-Palindromic-Subsequence.py

--- Solutions/0516.Longest-Palindromic-Subsequence.py
+++ Solutions/0516.Longest-Palindromic-Subsequence.py
@@ -23,10 +23,7 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         
         # dp[i] = longest palindr subsequence from i to j, including i and j
-        dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
-        for i in range(len(s)):
-            dp[i][i] = 1
-            
+        dp = [[1 for _ in range(len(s))] for _ in range(len(s))]            
         for i in range(len(s)-2, -1, -1):   # 注意 i 要倒序遍历
             for j in range(i+1, len(s)):
                 if s[i] == s[j]:
