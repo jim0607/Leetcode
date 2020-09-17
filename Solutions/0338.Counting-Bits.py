1e1c26dca97f4a412154ba2ae73a8d2c51a3b53c
Jim0607 <jinjun.ding@gmail.com>
2020-09-16 21:35:04 -0700

    Update 0338.Counting-Bits.py

--- Solutions/0338.Counting-Bits.py
+++ Solutions/0338.Counting-Bits.py
@@ -1,3 +1,4 @@
+"""
 338. Counting Bits
 
 Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
@@ -10,14 +11,17 @@ Example 2:
 
 Input: 5
 Output: [0,1,1,2,1,2]
+"""
 
 
-"""状态f[i]=i的二进制中有多少个1
+"""
+状态f[i]=i的二进制中有多少个1
 和位操作相关的动态规划一般用值作为状态
 转移方程f[i] = f[i>>1] + i%2
 i>>1表示i往右移动一位，最后一位就撞到墙了就撞没了。
 初始条件为f[0]=0
-O(N)， O（N）"""
+O(N)， O（N）
+"""
 class Solution:
     def countBits(self, num: int) -> List[int]:
         dp = [0] * (num + 1)
