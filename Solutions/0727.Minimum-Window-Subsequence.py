6f59ff41d8ed5ec8c154da140e28a31dd28ff1e3
Jim0607 <jinjun.ding@gmail.com>
2020-09-17 11:59:52 -0700

    Update 0727.Minimum-Window-Subsequence.py

--- Solutions/0727.Minimum-Window-Subsequence.py
+++ Solutions/0727.Minimum-Window-Subsequence.py
@@ -1,3 +1,4 @@
+"""
 727. Minimum Window Subsequence
 
 Given strings S and T, find the minimum (contiguous) substring W of S, so that T is a subsequence of W.
@@ -12,6 +13,7 @@ Output: "bcde"
 Explanation: 
 "bcde" is the answer because it occurs before "bdde" which has the same length.
 "deb" is not a smaller window because the elements of T in the window must occur in order.
+"""
 
 
 
@@ -76,4 +78,25 @@ class Solution:
         
 """
 solution 3: dp
+dp[i][j] = the min window subsequence that ends with ith ch in t, and jth ch in s.
+If t[i-1] == s[j-1]: dp[i][j] = dp[i-1][j-1] + 1; else: dp[i][j] = dp[i][j-1] + 1
 """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m, n = len(t), len(s)
+        dp = [[20000 for _ in range(n + 1)] for _ in range(m + 1)]  # 20000是因为题目给的len(s)<20000
+        for j in range(n + 1):
+            dp[0][j] = 0
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if t[i-1] == s[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = dp[i][j-1] + 1
+        
+        min_len = min(dp[-1])
+        if min_len == 20000: return ""
+        for j in range(1, n + 1):
+            if dp[-1][j] == min_len:
+                return s[j-min_len:j]
