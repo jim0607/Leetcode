408272faa920271a138cc7b9f54d0ecd510a3d6d
Jim0607 <jinjun.ding@gmail.com>
2020-09-17 20:18:22 -0700

    Update 1049.Last-Stone-Weight-II.py

--- Solutions/1049.Last-Stone-Weight-II.py
+++ Solutions/1049.Last-Stone-Weight-II.py
@@ -1,3 +1,4 @@
+"""
 We have a collection of rocks, each rock has a positive integer weight.
 
 Each turn, we choose any two rocks and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
@@ -17,20 +18,31 @@ We can combine 2 and 4 to get 2 so the array converts to [2,7,1,8,1] then,
 we can combine 7 and 8 to get 1 so the array converts to [2,1,1,1] then,
 we can combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
 we can combine 1 and 1 to get 0 so the array converts to [1] then that's the optimal value.
+"""
 
 
-"""由于石头拿走还能放回去，因此可以简单地把所有石头看作两堆
-假设总重量为 sum, 则问题转化为背包问题：
-如何使两堆石头总重量接近 sum / 2
-dp[i] 背包容量限制为 i 时能够装载的最大石块总重量
-curStone 存在最优解 dp[i] 时需要考虑的下一块石块重量"""
+
+"""
+每次碰撞，我们都会减去小数乘以2， eg: 3 <--> 5, 最后变成2, 相当于3减去了3, 5也减去了3.
+所以我们的目的是选一组数出来，让2*（sum_of_这些数）尽可能接近sum(nums).
+也就是找一组数使他们的和尽可能接近sum(nums)//2.
+由于一个数只能用一次，所以我们使用二维dp: dp[i][m] = can we use first i nums to add up to m? 
+dp[i][m] = True of dp[i-1][m] == True or dp[i-1][m-A[i]] == True
+"""
 class Solution:
-    def lastStoneWeightII(self, stones: List[int]) -> int:
-        lens = len(stones)
-        maxCapacity = sum(stones)//2
-        dp = [0] * (maxCapacity+1)
-        for i in range(lens):
-            currStone = stones[i]
-            for j in range(maxCapacity, currStone-1, -1):
-                dp[j] = max(dp[j], dp[j-currStone] + currStone)
-        return sum(stones) - 2 * dp[maxCapacity]
+    def lastStoneWeightII(self, nums: List[int]) -> int:
+        target = sum(nums) // 2
+        dp = [[False for _ in range(target + 1)] for _ in range(len(nums) + 1)]
+        for i in range(len(nums) + 1):
+            dp[i][0] = True
+        
+        for i in range(1, len(nums) + 1):
+            for m in range(1, target + 1):
+                dp[i][m] = dp[i-1][m]
+                if m >= nums[i-1]:      # 注意有buffer layer, 所以要比较nums[i-1]
+                    dp[i][m] = dp[i][m] or dp[i-1][m-nums[i-1]]
+        
+        # now loop over dp to find the largest m for dp[-1][m] == True
+        for m in range(target, -1, -1):
+            if dp[-1][m]:
+                return sum(nums) - 2*m
