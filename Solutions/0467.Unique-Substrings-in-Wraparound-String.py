2b754ef8cd183e0ccac3f4db1733fcae0f0a0a9e
Jim0607 <jinjun.ding@gmail.com>
2020-09-18 10:42:26 -0700

    Update 0467.Unique-Substrings-in-Wraparound-String.py

--- Solutions/0467.Unique-Substrings-in-Wraparound-String.py
+++ Solutions/0467.Unique-Substrings-in-Wraparound-String.py
@@ -1,6 +1,9 @@
-Consider the string s to be the infinite wraparound string of "abcdefghijklmnopqrstuvwxyz", so s will look like this: "...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd....".
+"""
+Consider the string s to be the infinite wraparound string of "abcdefghijklmnopqrstuvwxyz", 
+so s will look like this: "...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd....".
 
-Now we have another string p. Your job is to find out how many unique non-empty substrings of p are present in s. In particular, your input is the string p and you need to output the number of different non-empty substrings of p in the string s.
+Now we have another string p. Your job is to find out how many unique non-empty substrings of p are present in s. 
+In particular, your input is the string p and you need to output the number of different non-empty substrings of p in the string s.
 
 Note: p consists of only lowercase English letters and the size of p might be over 10000.
 
@@ -8,26 +11,38 @@ Example 1:
 Input: "a"
 Output: 1
 
-Explanation: Only the substring "a" of string "a" is in the string s.
+Explanation: Only the substring "a" of string "a" is in the string s.
 Example 2:
 Input: "cac"
 Output: 2
 Explanation: There are two substrings "a", "c" of string "cac" in the string s.
+"""
 
 
-"""扫描一边序列，利用动态规划记录到每个字符为止的最长子序列长度。时间复杂度o(n),空间复杂度o(1)
-算法：我们看abcd这个字符串，以d结尾的子字符串有abcd, bcd, cd, d，那么我们可以发现bcd或者cd这些以d结尾的字符串的子字符串都包含在abcd中，那么我们知道以某个字符结束的最大字符串包含其他以该字符结束的字符串的所有子字符串，说起来很拗口，但是理解了我上面举的例子就行。"""
+
+
+"""
+算法：我们看abcd这个字符串，以d结尾的子字符串有abcd, bcd, cd, d，以d结尾的最长字符串是长度是4. 所以以d结尾有4个unique substring，
+所以题目可以转换为分别求出以每个字符(a-z)为结束字符的最长连续字符串就行了，
+我们用一个数组记录下 以每个字符(a-z)为结束字符的最长连续字符串，最后求出数组的所有数字之和就是我们要的结果啦.
+扫描一边序列，利用动态规划记录到每个字符为止的最长子序列长度。时间复杂度O(n),空间复杂度O(1).
+"""
 class Solution:
     def findSubstringInWraproundString(self, p: str) -> int:
-        if not p:
-            return 0
-        lensAll = [0] * 26 # 记录到这个字符为止的最长子序列的长度
-        lensAll[ord(p[0])-ord("a")] = 1
-        lens = 1 # cnt 记录到目前为止的最长子序列的长度
+        if not p: return 0
+        if len(p) == 1: return 1
+        
+        all_lens = [0 for _ in range(26)]       # 记录到这个字符为止的最长子序列的长度
+        all_lens[ord(p[0]) - ord("a")] = 1      # 初始化
+        
+        curr_lens = 1           # 记录到目前为止的最长子序列的长度
         for i in range(1, len(p)):
-            if ord(p[i])-ord(p[i-1]) == 1 or ord(p[i])-ord(p[i-1]) == -25:
-                lens += 1
+            if ord(p[i]) - ord(p[i-1]) == 1 or ord(p[i]) - ord(p[i-1]) == -25:
+                curr_lens += 1
             else:
-                lens = 1
-            lensAll[ord(p[i])-ord("a")] = max(lens, lensAll[ord(p[i])-ord("a")])
-        return sum(lensAll)
+                curr_lens = 1
+                
+            # 更新dp数组
+            all_lens[ord(p[i]) - ord("a")] = max(all_lens[ord(p[i]) - ord("a")], curr_lens)
+            
+        return sum(all_lens)
