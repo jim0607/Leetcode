3c35a243685fd7bdc8d0e8e28afb0d839437cf51
Jim0607 <jinjun.ding@gmail.com>
2020-09-18 01:16:52 -0700

    Update 0464.Can-I-Win.py

--- Solutions/0464.Can-I-Win.py
+++ Solutions/0464.Can-I-Win.py
@@ -1,3 +1,4 @@
+"""
 In the "100 game," two players take turns adding, to a running total, any integer from 1..10. The player who first causes the running total to reach or exceed 100 wins.
 
 What if we change the game so that players cannot re-use integers?
@@ -23,27 +24,49 @@ The first player can choose an integer from 1 up to 10.
 If the first player choose 1, the second player can only choose integers from 2 up to 10.
 The second player will win by choosing 10 and get a total = 11, which is >= desiredTotal.
 Same with other integers chosen by the first player, the second player will always win.
+"""
 
 
 
-"""由于状态不可用数组进行传递【在递归当中会受到改变，不能准确定位当前状态】，故在此处用Int的位表示状态（1表示用过,0表示未用过） 在目前状态取胜的条件为（在此步取得胜利，或下一手会取得失败）,假如status保存已经访问过的状态是为了避免进行重复计算"""
+
+"""
+solution 1: naive recursion - 指数级别复杂度
+"""
 class Solution:
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
-        if maxChoosableInteger>desiredTotal:
+
+        if maxChoosableInteger >= desiredTotal:
             return True
-        if sum(range(maxChoosableInteger+1))< desiredTotal:
+        
+        if any(not self.canIWin(maxChoosableInteger, desiredTotal - num) for num in range(1, maxChoosableInteger + 1)):
+            return True
+        
+        return False
+        
+
+"""
+solution 2: dfs + memo
+"""
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        def dfs(curr_total, curr_choosable):
+            if len(curr_choosable) == 0:
+                return False
+            if curr_choosable[-1] >= curr_total:    # 如果可一次性拿走，那就赢了
+                return True
+            if curr_choosable in memo:
+                return memo[curr_choosable]
+            for i in range(len(curr_choosable)):
+                if not dfs(curr_total - curr_choosable[i], curr_choosable[:i] + curr_choosable[i+1:]):
+                    memo[curr_choosable] = True
+                    return True
+            memo[curr_choosable] = False
             return False
         
-        def win(max_num,total,visited,status):
-            if status.get(visited):
-                return status.get(visited)
-            for i in range(1,max_num+1):
-                tmp=1<<i
-                if visited&tmp==0:                  
-                    if i>=total or not win(max_num,total-i,visited|tmp,status):
-                        status[visited]=True
-                        return True
-            status[visited]=False
+        
+        # 一个数只能用一次，所以如果所有maxChoosableInteger都用了都加不到desiredTotal那就return False
+        if maxChoosableInteger * (maxChoosableInteger + 1) // 2 < desiredTotal:
             return False
-        status={}
-        return win(maxChoosableInteger, desiredTotal,0,status)
+        
+        memo = collections.defaultdict(bool)    # (curr_choosable_numbers --> can_win_with_curr_choosable_numbers?)
+        return dfs(desiredTotal, tuple(range(1, maxChoosableInteger + 1)))  # 这里用了curr_choosable_numbers就不需要visited了
