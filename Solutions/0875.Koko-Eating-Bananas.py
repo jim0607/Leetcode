e8c4c3f2211b49e369b46d6e0b08c3b705dad99f
Jim0607 <jinjun.ding@gmail.com>
2020-09-18 19:17:01 -0700

    Update 0875.Koko-Eating-Bananas.py

--- Solutions/0875.Koko-Eating-Bananas.py
+++ Solutions/0875.Koko-Eating-Bananas.py
@@ -1,12 +1,12 @@
+"""
 Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  The guards have gone and will come back in H hours.
 
-Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K bananas from that pile.  If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
+Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K bananas from that pile.  
+If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
 
 Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
 
 Return the minimum integer K such that she can eat all the bananas within H hours.
-
- 
 
 Example 1:
 
@@ -21,43 +21,39 @@ Example 3:
 Input: piles = [30,11,23,4,20], H = 6
 Output: 23
  
-
 Note:
 
 1 <= piles.length <= 10^4
 piles.length <= H <= 10^9
 1 <= piles[i] <= 10^9
+"""
 
 
-"""If Koko can finish eating all the bananas (within H hours) with an eating speed of K, she can finish with a larger speed too. So it is a OOOXXX problem trying to find the first X. end is set to be max(piles). Every time find if it posible to eat all the bananas with speed mid. if yes, then drop the right part, if no, then drop the left."""
-"""Time Complexity: O(NlogW), where N is the number of piles, and W is the maximum size of a pile."""
+
+
+"""
+If Koko can finish eating all the bananas (within H hours) with an eating speed of K, she can finish with a larger speed too. 
+So it is a OOOXXX problem trying to find the first X. end is set to be max(piles). Every time find if it posible to eat all the bananas with speed mid. 
+if yes, then drop the right part, if no, then drop the left.
+Time Complexity: O(NlogW), where N is the number of piles, and W is the maximum size of a pile.
+"""
 class Solution:
     def minEatingSpeed(self, piles: List[int], H: int) -> int:
-        lens = len(piles)
-        if H < lens:
-            return 
-        if lens == 1:
-            if piles[0] <= H:
-                return 1
-
-        start, end = 0, max(piles)
+        start, end = 1, max(piles) + 1
         while start + 1 < end:
             mid = start + (end - start) // 2
-            if self.canFinish(piles, mid, H):
+            if self._can_finish(piles, mid, H):
                 end = mid
             else:
                 start = mid
-                
-        if self.canFinish(piles, start, H):
-            return start
-        if self.canFinish(piles, end, H):
-            return end
-        
-    def canFinish(self, piles, k, H):
-        cnt = 0
+        return start if self._can_finish(piles, start, H) else end
+    
+    def _can_finish(self, piles, k, H):      # return can finish the piles within H hours with speed k?
+        time_needed = 0
         for pile in piles:
-            cnt += (pile - 1) // k + 1
-            
-        return cnt <= H
+            time_needed += (pile - 1) // k + 1
+        return time_needed <= H
 
-"""Time complexity is O(N*log(W)), N is the length of piles, W is the max(piles)"""
+"""
+Time complexity is O(N*log(W)), N is the length of piles, W is the max(piles)
+"""
