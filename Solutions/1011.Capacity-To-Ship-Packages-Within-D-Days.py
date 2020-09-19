abdb138c52d5d08c5fb7363cf5c15e1cb2ea7c16
Jim0607 <jinjun.ding@gmail.com>
2020-09-18 21:32:52 -0700

    Update 1011.Capacity-To-Ship-Packages-Within-D-Days.py

--- Solutions/1011.Capacity-To-Ship-Packages-Within-D-Days.py
+++ Solutions/1011.Capacity-To-Ship-Packages-Within-D-Days.py
@@ -1,3 +1,4 @@
+"""
 1011. Capacity To Ship Packages Within D Days
 
 A conveyor belt has packages that must be shipped from one port to another within D days.
@@ -39,10 +40,40 @@ Explanation:
 2nd day: 2
 3rd day: 3
 4th day: 1, 1
+"""
 
 """
 similar with copy books
 """
+class Solution:
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        start, end = max(weights), sum(weights) + 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if self._can_ship(weights, mid, D):
+                end = mid
+            else:
+                start = mid
+        return start if self._can_ship(weights, start, D) else end
+    
+    def _can_ship(self, weights, capacity, D):  
+        """
+        Return can we ship all weights with capacity within D days?
+        """
+        days_needed = 0
+        i = 0
+        while i < len(weights):
+            curr_w = 0
+            while i < len(weights) and curr_w + weights[i] <= capacity:
+                curr_w += weights[i]
+                i += 1
+            days_needed += 1
+        return days_needed <= D
+
+
+
+
+
 class Solution:
     def shipWithinDays(self, weights: List[int], D: int) -> int:
         start, end = 0, sum(weights)
