7de3cb41b1dc67b6dfe13c60542e0540d7e7f2e9
Jim0607 <jinjun.ding@gmail.com>
2020-09-18 23:26:21 -0700

    Update 0436.Find-Right-Interval.py

--- Solutions/0436.Find-Right-Interval.py
+++ Solutions/0436.Find-Right-Interval.py
@@ -1,3 +1,4 @@
+"""
 436. Find Right Interval
 
 Given a set of intervals, for each of the interval i, check if there exists an interval j whose start point is bigger than or equal to the end point of the interval i, which can be called that j is on the "right" of i.
@@ -38,37 +39,40 @@ Output: [-1, 2, -1]
 
 Explanation: There is no satisfied "right" interval for [1,4] and [3,4].
 For [2,3], the interval [3,4] has minimum-"right" start point.
-
+"""
 
 
 """
 step 1: include the idx information into the interval;
 step 2: then sort the intervals based on start time;
-step 3: scan the interval and update res.
+step 3: scan the interval and update res, using binary search.
 """
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        for idx, interval in enumerate(intervals):
-            interval.append(idx)
+        info = []
+        for i, (start, end) in enumerate(intervals):
+            info.append((start, end, i))
         
-        intervals.sort(key = lambda x: (x[0], x[2]))
+        info.sort(key = lambda x: x[0])
         
         res = [-1 for _ in range(len(intervals))]
-        for interval in intervals:
-            idx, target = interval[2], interval[1]
-            first_right_idx = self._binary_search(intervals, target)
-            res[idx] = first_right_idx
+        for start, end, i in info:
+            res[i] = self._binary_search(info, end)
         return res
     
-    def _binary_search(self, intervals, target):
-        start, end = 0, len(intervals) - 1
-        while start + 1 < end:
-            mid = start + (end - start) // 2
-            if intervals[mid][0] >= target:
-                end = mid
+    def _binary_search(self, info, target):
+        """
+        Return the idx of the first start that larger than target
+        """
+        left, right = 0, len(info) - 1
+        while left + 1 < right:
+            mid = left + (right - left) // 2
+            if info[mid][0] >= target:  # 往左逼to find the first start that larger than target
+                right = mid
             else:
-                start = mid
-        if intervals[end][0] < target:
-            return -1
-        else:
-            return intervals[start][2] if intervals[start][0] >= target else intervals[end][2]
+                left = mid
+        if info[left][0] >= target:
+            return info[left][2] 
+        if info[right][0] >= target:
+            return info[right][2]
+        return -1
