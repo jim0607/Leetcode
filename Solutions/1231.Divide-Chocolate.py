f478f6459b97f13b175343b2129ba5ac7b09fdd3
Jim0607 <jinjun.ding@gmail.com>
2020-09-18 22:42:02 -0700

    Update 1231.Divide-Chocolate.py

--- Solutions/1231.Divide-Chocolate.py
+++ Solutions/1231.Divide-Chocolate.py
@@ -1,3 +1,4 @@
+"""
 1231. Divide Chocolate
 
 You have one chocolate bar that consists of some chunks. Each chunk has its own sweetness given by the array sweetness.
@@ -8,7 +9,6 @@ Being generous, you will eat the piece with the minimum total sweetness and give
 
 Find the maximum total sweetness of the piece you can get by cutting the chocolate bar optimally.
 
- 
 
 Example 1:
 
@@ -25,37 +25,32 @@ Example 3:
 Input: sweetness = [1,2,2,1,2,2,1,2,2], K = 2
 Output: 5
 Explanation: You can divide the chocolate to [1,2,2], [1,2,2], [1,2,2]
+"""
+
 
 
 """
-If I can get a sweetness of s, we can also get a sweetness less than s. 
-So it's a OOXX problem. The difficult is to check whether or not can get the sweetness mid.
+Divide the nums into K+1 subarrays, and make sure each subarray has a sum at least S.
+Find the max S. so it's a OOXXX problem finding the last O.
 Use greedy to check can get - O(N).  Overall: O(NlogM), where N = len(sweetness), M = sum(sweetness)//(K+1)
 """
 class Solution:
-    def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
-        if K + 1 > len(sweetness):
-            return 0
-        if K == 0:
-            return sum(sweetness)
-        
-        start, end = 0, sum(sweetness) // (K + 1)
+    def maximizeSweetness(self, nums: List[int], K: int) -> int:
+        start, end = min(nums), sum(nums) // (K+1) + 1
         while start + 1 < end:
             mid = start + (end - start) // 2
-            if self._can_get(sweetness, mid, K):
+            if self._can_divide(nums, K+1, mid):
                 start = mid
             else:
                 end = mid
-        
-        return end if self._can_get(sweetness, end, K) else start       # 注意是return end在前面，因为需要求的是能得到的最大值
+        return end if self._can_divide(nums, K+1, end) else start   # 注意是return end在前面，因为需要求的是能得到的最大值
     
-    def _can_get(self, sweetness, sw, K):
-        cnt = 0     # how many people can get at least sw sweetness
-        chunk_sweet = 0
-        for sweet in sweetness:
-            chunk_sweet += sweet
-            if chunk_sweet >= sw:
+    def _can_divide(self, nums, k, min_sum):
+        cnt = 0     # how many subarrays can get a sum at least min_sum
+        curr_sum = 0
+        for num in nums:
+            curr_sum += num
+            if curr_sum >= min_sum:
                 cnt += 1
-                chunk_sweet = 0
-                
-        return cnt >= K + 1
+                curr_sum = 0
+        return cnt >= k
