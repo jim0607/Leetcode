ff604a2146d0ab9ba6d2dd1fb5c849cd9aeb8ae3
Jim0607 <jinjun.ding@gmail.com>
2020-09-20 10:38:37 -0700

    Update 0719.Find-K-th-Smallest-Pair-Distance.py

--- Solutions/0719.Find-K-th-Smallest-Pair-Distance.py
+++ Solutions/0719.Find-K-th-Smallest-Pair-Distance.py
@@ -1,3 +1,4 @@
+"""
 719. Find K-th Smallest Pair Distance
 
 Given an integer array, return the k-th smallest distance among all the pairs. The distance of a pair (A, B) is defined as the absolute difference between A and B.
@@ -17,6 +18,8 @@ Note:
 2 <= len(nums) <= 10000.
 0 <= nums[i] < 1000000.
 1 <= k <= len(nums) * (len(nums) - 1) / 2.
+"""
+
 
 
 """
@@ -27,7 +30,7 @@ The algorithm of helper function is sliding window so it's only O(N)
 """
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        nums.sort()     # sort nums first
         start, end = 0, nums[-1] - nums[0]
         while start + 1 < end:
             mid = start + (end - start) // 2
@@ -39,7 +42,7 @@ class Solution:
     
     def _has_more(self, nums, k, diff):
         """
-        check if there is more than k dist that are smaller than diff
+        Return if there are more than k distance larger than diff. nums is sorted
         """
         lens = len(nums)
         i, j = 0, 0
