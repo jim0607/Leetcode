e09e39a16011fb98e0ae0b9f0085a8154c5ee95e
Jim0607 <jinjun.ding@gmail.com>
2020-09-19 21:14:37 -0700

    Update 0644.Maximum-Average-Subarray-II.py

--- Solutions/0644.Maximum-Average-Subarray-II.py
+++ Solutions/0644.Maximum-Average-Subarray-II.py
@@ -1,3 +1,4 @@
+"""
 644. Maximum Average Subarray II
 
 Given an array consisting of n integers, find the contiguous subarray whose length is greater than or equal to k that has the maximum average value. And you need to output the maximum average value.
@@ -13,43 +14,46 @@ Note:
 1 <= k <= n <= 10,000.
 Elements of the given array will be in range [-10,000, 10,000].
 The answer with the calculation error less than 10-5 will be accepted.
+"""
 
 
 """
-We binary search on the answer二分答案. Let P[i] = A[0] + A[1] + ... + A[i-1], the i-th prefix sum under A.
-Let's focus our attention on possible(x), a function that is true if it is possible to have an average of at least x. 
-Consider the elements B = [a-x for a in A] with corresponding prefix sum Q[i] = P[i] - i*x under B.
-We want to know if there is some >= K length subarray in B with average at least zero. 
-ee, to check if Q[j+1] - Q[i] > 0 for any j, and any i <= j - K + 1, we should check whether Q[j+1] >= min_{i <= j-K+1} Q[i]. 
-Keeping a running minimum m of this array Q, we can check this in linear time.
+The Most important concept here is, given any array and an average window of "k", it is possible to find whether there exists a subarray whose average value is bigger than "x", and find it in O(n) time.
+How to check whether an average bigger than "mid" exists in O(n) time?
+
+1. Minus all elements in the array by "mid".
+2. After that, if we see any subarray that has sum>=0 means the average of this subarray is bigger than "mid".
+3. To find a subarray bigger than "0", We can use prefix sum to find subarray of any target value. 
+   Now we only need to find two index i and j to satisfy pre_sum[j]-pre_sum[i] >= 0.
+   The algorithm is similar with 121. Best Time to Buy and Sell Stock, where we want to find the maximum diff of two numbers in an arr.
+   We only need to remember the smallest pre_sum before current. The current prefix sum minus the smallest prefix sum is the biggest subarray so far.
+With this very important algorithm, we can use binary search between the smallest and biggest average "value" of the array to find final result.
 初始化 left 为原数组的最小值，right 为原数组的最大值 - O(Nlog(max-min))
 """
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        start, end = min(nums), max(nums)       # start, end不是idx,而是avg数值
-        while start + 10**(-5) < end:
+        start, end = min(nums), max(nums)       # 二分答案: start, end不是idx,而是avg数值
+        while start + 10**(-5) < end:       
             mid = start + (end - start) / 2     # 注意这题二分答案可能是小数，所以不要用整除号
-            if self._is_possible(mid, nums, k):       # 判断是否有比mid更大的avg
+            if self._is_possible(nums, mid, k): # 判断是否有比mid更大的avg
                 start = mid
             else:
                 end = mid
-        return end if self._is_possible(end, nums, k) else start
+        return end if self._is_possible(nums, start, k) else start
     
-    def _is_possible(self, avg, nums, k):
+    def _is_possible(self, nums, target_avg, k):
         """
-        if 有比avg number更大的avg, return True - O(N)
-        algorithm is similar with 121. Best Time to Buy and Sell Stock
+        Return if there is an k-window avg that is larger than the target_avg
         """
-        diff_nums = [num - avg for num in nums]
-        prefix_diff_sum = [0 for _ in range(len(nums) + 1)]
+        pre_sum = [0 for _ in range(len(nums) + 1)]
         for i in range(len(nums)):
-            prefix_diff_sum[i + 1] = prefix_diff_sum[i] + diff_nums[i]
+            pre_sum[i+1] = pre_sum[i] + nums[i] - target_avg    # 这里减去target_avg就只需要比较max_avg是不是大于零就可以了
             
-        # next is to check if in diff_nums there is an avg with at least K length that is larger than 0
-        # or to check if there is in diff_nums a sum with at least K length that is larger than 0
-        min_sum = 0     # keep a min_sum and use it for compare, similar with 121. Best Time to Buy and Sell Stock
-        for i in range(len(prefix_diff_sum) - k):
-            min_sum = min(min_sum, prefix_diff_sum[i])
-            if prefix_diff_sum[i + k] - min_sum >= 0:   # 保证prefix_diff_sum[i + k]与min_sum之间至少有k个数
+        # next is similar with 121. Best Time to Buy and Sell Stock, to find the max_diff in pre_sum arr
+        curr_min = nums[0]      # keep a curr_min and use it for compare
+        for i in range(len(pre_sum) - k):
+            curr_min = min(curr_min, pre_sum[i])
+            max_diff = pre_sum[i + k] - curr_min       # pre_sum[i + k]与min_sum之间至少有k个数, 这一点与121有点区别
+            if max_diff >= 0:
                 return True
         return False
