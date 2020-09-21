d77374886ac185d53fb765519be6ade17b8cce5b
Jim0607 <jinjun.ding@gmail.com>
2020-09-21 14:43:54 -0700

    Update 0462.Minimum-Moves-to-Equal-Array-Elements-II.py

--- Solutions/0462.Minimum-Moves-to-Equal-Array-Elements-II.py
+++ Solutions/0462.Minimum-Moves-to-Equal-Array-Elements-II.py
@@ -1,6 +1,8 @@
+"""
 462. Minimum Moves to Equal Array Elements II
 
-Given a non-empty integer array, find the minimum number of moves required to make all array elements equal, where a move is incrementing a selected element by 1 or decrementing a selected element by 1.
+Given a non-empty integer array, find the minimum number of moves required to make all array elements equal, 
+where a move is incrementing a selected element by 1 or decrementing a selected element by 1.
 
 You may assume the array's length is at most 10,000.
 
@@ -16,6 +18,7 @@ Explanation:
 Only two moves are needed (remember each move increments or decrements one element):
 
 [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+"""
 
 
 """
@@ -23,10 +26,12 @@ solution 1: find median by sorting
 """
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
+        median = self._find_median(nums)
+        return sum(abs(num - median) for num in nums)
+    
+    def _find_median(self, nums):
         nums.sort()
-        lens = len(nums)
-        median = nums[lens//2]
-        return sum(abs(median - num) for num in nums)
+        return nums[len(nums)//2]
         
         
 """
@@ -35,15 +40,18 @@ solution 2: find meddian by quick select(kth largest element) - O(N)
 """
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        median = self._quick_select(nums, len(nums)//2, 0, len(nums)-1)
-        return sum(abs(median - num) for num in nums)
+        median = self._find_median(nums)
+        return sum(abs(num - median) for num in nums)
     
-    def _quick_select(self, nums, k, start, end):
-        if start >= end:
-            return nums[start]
+    def _find_median(self, nums):
+        return self._quick_select(nums, 0, len(nums) - 1, len(nums) // 2)
         
+    def _quick_select(self, nums, start, end, k):
+        if start >= end:
+            return nums[k]
+        
+        pivot = nums[(start + end) // 2]
         left, right = start, end
-        pivot = nums[(left + right) // 2]
         while left <= right:
             while left <= right and nums[left] < pivot:
                 left += 1
@@ -52,11 +60,11 @@ class Solution:
             if left <= right:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-                right -=1
+                right -= 1
         
-        if right >= k:
-            self._quick_select(nums, k, start, right)
-        if left <= k:
-            self._quick_select(nums, k, left, end)
-            
+        if k <= right:
+            return self._quick_select(nums, start, right, k)
+        elif k >= left:
+            return self._quick_select(nums, left, end, k)
+        
         return nums[k]
