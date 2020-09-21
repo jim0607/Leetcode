8d08564d6e5d39af03f8da41ddd4b509ab2a6fd7
Jim0607 <jinjun.ding@gmail.com>
2020-09-21 12:00:37 -0700

    Update 0144.Interleaving-Positive-and-Negative-Numbers.py

--- Solutions/0144.Interleaving-Positive-and-Negative-Numbers.py
+++ Solutions/0144.Interleaving-Positive-and-Negative-Numbers.py
@@ -1,3 +1,4 @@
+"""
 144. Interleaving Positive and Negative Numbers
 
 Given an array with positive and negative integers. Re-range it to interleaving with positive and negative integers.
@@ -7,9 +8,13 @@ Input : [-1, -2, -3, 4, 5, 6]
 Outout : [-1, 5, -2, 4, -3, 6]
 Explanation :  any other reasonable answer.
 Challenge: Do it in-place and without extra memory.
+"""
 
-"""STEP 1: 反向双指针（也可以同向双指针）对[-1,-2,4,,5,-3,6]进行partition，负数在左边，正数在右边[-1, -2, -3, 4, 5, 6]
-STEP 2: 再来进行正负正负正负安插"""
+
+"""
+STEP 1: 反向双指针（也可以同向双指针）对[-1,-2,4,,5,-3,6]进行partition，负数在左边，正数在右边[-1, -2, -3, 4, 5, 6]
+STEP 2: 再来进行正负正负正负安插
+"""
 class Solution:
     def rerange(self, A):
         i, j = 0, len(A) - 1
