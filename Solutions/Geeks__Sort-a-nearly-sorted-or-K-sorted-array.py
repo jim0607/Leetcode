72083843cae96802a23be30a54b09011f0dc8b05
Jim0607 <jinjun.ding@gmail.com>
2020-09-21 10:37:11 -0700

    Update Geeks__Sort-a-nearly-sorted-or-K-sorted-array.py

--- Solutions/Geeks__Sort-a-nearly-sorted-or-K-sorted-array.py
+++ Solutions/Geeks__Sort-a-nearly-sorted-or-K-sorted-array.py
@@ -1,3 +1,4 @@
+"""
 Given an array of n elements, where each element is at most k away from its target position, devise an algorithm that sorts in O(n log k) time. 
 For example, let us consider k is 2, an element at index 7 in the sorted array, can be at indexes 5, 6, 7, 8, 9 in the given array.
 
@@ -10,11 +11,15 @@ Output : arr[] = {2, 3, 5, 6, 8, 9, 10}
 Input : arr[] = {10, 9, 8, 7, 4, 70, 60, 50}
          k = 4
 Output : arr[] = {4, 7, 8, 9, 10, 50, 60, 70}
+"""
+
+
     
 """
 solution: 用一个大小为k的heapq存储k个元素，然后i从k开始遍历nums, 遍历的过程中每次都更新nums的最左边: nums[target_idx] = heappop(hq)，
 同时更新hq: heappush(hq, nums[i]), 这么做成立的原因是i是从k开始遍历的，所以nums[i]一定是大于nums[0]的，而nums[0]>=heappop(hq), 
-所以nums[i]及其后面的数一定是大于heappop(hq)的，所以可以放心地把heappop(hq)放到target_idx的位置。时间复杂度是O(nlogk). 当k=1: O(0), 当k=n: O(nlogn)
+所以nums[i]及其后面的数一定是大于heappop(hq)的，所以可以放心地把heappop(hq)放到target_idx的位置。时间复杂度是O(nlogk). 
+当k=1: O(0), 当k=n: O(nlogn). 当k=n时就degrade成了heap sort了
 """
 
 from heapq import heapify, heappop, heappush
