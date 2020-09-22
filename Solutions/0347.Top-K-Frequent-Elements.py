c783998559a88842666c2941638ce5126705735e
Jim0607 <jinjun.ding@gmail.com>
2020-09-21 17:50:36 -0700

    Update 0347.Top-K-Frequent-Elements.py

--- Solutions/0347.Top-K-Frequent-Elements.py
+++ Solutions/0347.Top-K-Frequent-Elements.py
@@ -1,3 +1,4 @@
+"""
 347. Top K Frequent Elements
 
 Given a non-empty array of integers, return the k most frequent elements.
@@ -14,11 +15,35 @@ Note:
 
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+"""
+
+
 
 """
-"""O(N+ClogK+K)
+solution 3: bucket sort - O(N). bucket sort is garanteed to be O(N), while quick select is O(N) ot O(N^2)
+"""
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = collections.Counter(nums)
+        
+        max_cnt = max(cnt for cnt in counter.values())  # we use max_freq as our bucket size
+        freq_to_num = [[] for _ in range(max_cnt + 1)]  
+        for num, cnt in counter.items():
+            freq_to_num[cnt].append(num)    # 把freq当做idx来放入bucket中，
+                                        # 这样一来high freq的ch就天然的放在bucket后面了, 就不需要sort了
+        res = []
+        for i in range(len(freq_to_num) - 1, -1, -1):
+            if len(freq_to_num) > 0:
+                res += freq_to_num[i]
+            if len(res) == k:
+                return res
+
+
+"""
+O(N+ClogK+K)
 heapq, heapq中放入的是(freq, key)对
-需要一个cntDict来记录cnt先"""
+需要一个cntDict来记录cnt先
+"""
 
 from heapq import heappush, heappop, heapify
 
@@ -35,54 +60,11 @@ class Solution:
                 
         return [num for cnt, num in hq]
         
-    
-    
-""""use heapify to realize O(N) solution"""
-
-import heapq
-
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        numsFreq = collections.defaultdict(int)
-        for num in nums:
-            numsFreq[num] += 1
-            
-        hq = []
-        for num, freq in numsFreq.items():
-            hq.append((-freq, num))
-            
-        heapq.heapify(hq)
-        
-        res = []
-        for _ in range(k):
-            res.append(heapq.heappop(hq)[1])
-            
-        return res
-    
+      
     
     
 """
 solution 2: quick select - O(N)
 """
 
-"""
-solution 3: bucket sort - O(N)
-"""
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = collections.Counter(nums)
-        max_freq = max(freq.values())   # 
-        
-        # we use max_freq as our bucket size
-        # we use a list of num with frequency=i at idx i
-        bucket = [[] for _ in range(max_freq + 1)]    
-        
-        for num, cnt in freq.items():
-            bucket[cnt].append(num)   # 把freq当做idx来放入bucket中，这样一来high freq的ch就天然的放在bucket后面了, 就不需要sort了
-        
-        res = []
-        for i in range(len(bucket) - 1, -1, -1):
-            for num in bucket[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+
