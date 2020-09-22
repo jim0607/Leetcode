4b00e3b9ce20edf7b0aeffea14ba64f3151c3bab
Jim0607 <jinjun.ding@gmail.com>
2020-09-22 12:19:43 -0700

    Update 0082.Remove-Duplicates-from-Sorted-List-II.py

--- Solutions/0082.Remove-Duplicates-from-Sorted-List-II.py
+++ Solutions/0082.Remove-Duplicates-from-Sorted-List-II.py
@@ -1,3 +1,4 @@
+"""
 82. Remove Duplicates from Sorted List II
 
 Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
@@ -12,6 +13,7 @@ Example 2:
 
 Input: 1->1->1->2->3
 Output: 2->3
+"""
 
 
 
