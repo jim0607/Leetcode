ad88b0fe0da6856a20a6aafea1fb30284dd92ce3
Jim0607 <jinjun.ding@gmail.com>
2020-09-21 17:18:20 -0700

    Update 0315.Count-of-Smaller-Numbers-After-Self.py

--- Solutions/0315.Count-of-Smaller-Numbers-After-Self.py
+++ Solutions/0315.Count-of-Smaller-Numbers-After-Self.py
@@ -1,3 +1,4 @@
+"""
 315. Count of Smaller Numbers After Self
 
 You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
@@ -11,6 +12,8 @@ To the right of 5 there are 2 smaller elements (2 and 1).
 To the right of 2 there is only 1 smaller element (1).
 To the right of 6 there is 1 smaller element (1).
 To the right of 1 there is 0 smaller element.
+"""
+
 
 
 """
