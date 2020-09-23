3c79ecedc45c33e479881672321e4ce7e4d1783e
Jim0607 <jinjun.ding@gmail.com>
2020-09-22 19:46:52 -0700

    Update 0023.Merge-k-Sorted-Lists.py

--- Solutions/0023.Merge-k-Sorted-Lists.py
+++ Solutions/0023.Merge-k-Sorted-Lists.py
@@ -1,3 +1,4 @@
+"""
 23. Merge k Sorted Lists
 
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
@@ -11,19 +12,49 @@ Input:
   2->6
 ]
 Output: 1->1->2->3->4->4->5->6
+"""
 
 
-solution 1: heapq
+
+"""
+正解 soluiton 1: divide and conquer
+time complexity: each merge takes N operations and we divide/merge logk times, so it's O(Nlogk)
+"""
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists or len(lists) == 0:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        
+        # divide
+        mid = len(lists) // 2
+        left_head = self.mergeKLists(lists[:mid])
+        right_head = self.mergeKLists(lists[mid:])
+        
+        # conquer/merge - same as 21. Merge Two Sorted Lists
+        dummy = ListNode()
+        curr = dummy
+        left, right = left_head, right_head
+        while left and right:
+            if left.val < right.val:
+                curr.next = ListNode(left.val)
+                left = left.next
+                curr = curr.next
+            else:
+                curr.next = ListNode(right.val)
+                right = right.next
+                curr = curr.next
+        curr.next = left if left != None else right
+        
+        return dummy.next
+
+      
+      
+"""
+solution 2: heapq
 O(NlogK), O(K)
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-from heapq import *
-
+"""
 class Solution:
     # first, we should overriding ListNode compare function __lt__ to make customized compare happens: compare ListNode
     def __lt__(self, other):        # re-define the __lt__ function
@@ -46,37 +77,3 @@ class Solution:
                 heappush(hq, (curr.next))
                 
         return dummynode.next
-      
-      
-"""
-soluiton 2: divide and conquer
-time complexity: each merge takes N operations and we divide/merge logk times, so it's O(Nlogk)
-"""
-class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        if len(lists) == 0:
-            return None
-        if len(lists) == 1:
-            return lists[0]
-        
-        # divide
-        mid = len(lists) // 2
-        lefthead = self.mergeKLists(lists[:mid])
-        righthead = self.mergeKLists(lists[mid:])
-        
-        # conquer/merge
-        dummy = ListNode(0)
-        curr = dummy
-        leftcurr, rightcurr = lefthead, righthead
-        while leftcurr and rightcurr:
-            if leftcurr.val < rightcurr.val:
-                curr.next = leftcurr
-                curr = curr.next
-                leftcurr = leftcurr.next
-            else:
-                curr.next = rightcurr
-                curr = curr.next
-                rightcurr = rightcurr.next
-        curr.next = leftcurr if leftcurr else rightcurr
-        
-        return dummy.next
