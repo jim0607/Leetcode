8cf0a1a9093398cc9b3e7856ac477e8855116a9d
Jim0607 <jinjun.ding@gmail.com>
2020-09-23 21:31:57 -0700

    Update 0170.Two-Sum-III-Data-structure-design.py

--- Solutions/0170.Two-Sum-III-Data-structure-design.py
+++ Solutions/0170.Two-Sum-III-Data-structure-design.py
@@ -44,14 +44,30 @@ class TwoSum:
                     
         return False
 
+    
+    
 
-# Your TwoSum object will be instantiated and called as such:
-# obj = TwoSum()
-# obj.add(number)
-# param_2 = obj.find(value)
+class TwoSum:
 
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
 
-# Your TwoSum object will be instantiated and called as such:
-# obj = TwoSum()
-# obj.add(number)
-# param_2 = obj.find(value)
+    def add(self, number: int) -> None:
+        """
+        Add the number to an internal data structure..
+        """
+        self.nums.append(number)
+
+    def find(self, value: int) -> bool:
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+        num_set = set()
+        for i, num in enumerate(self.nums):
+            if value - num in num_set:
+                return True
+            num_set.add(num)
+        return False
