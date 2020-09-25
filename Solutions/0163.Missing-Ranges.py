3411416abafb0f13b508e90600b7616003ff4685
Jim0607 <jinjun.ding@gmail.com>
2020-09-24 22:09:25 -0700

    Update 0163.Missing-Ranges.py

--- Solutions/0163.Missing-Ranges.py
+++ Solutions/0163.Missing-Ranges.py
@@ -1,3 +1,4 @@
+"""
 163. Missing Ranges
 
 Given a sorted integer array nums, where the range of elements are in the inclusive range [lower, upper], return its missing ranges.
@@ -6,18 +7,16 @@ Example:
 
 Input: nums = [0, 1, 3, 50, 75], lower = 0 and upper = 99,
 Output: ["2", "4->49", "51->74", "76->99"]
+"""
 
 
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
         nums = [lower - 1] + nums + [upper + 1]
-        
-        lens = len(nums)
         res = []
-        for i in range(1, lens):
-            if nums[i] == nums[i-1] + 2:
-                res.append(str(nums[i-1] + 1))
-            if nums[i] > nums[i-1] + 2:
-                res.append(str(nums[i-1] + 1) + "->" + str(nums[i] - 1))
-                
+        for i in range(len(nums) - 1):
+            if nums[i+1] - nums[i] == 2:
+                res.append(str(nums[i] + 1))
+            elif nums[i+1] - nums[i] > 2:
+                res.append(str(nums[i] + 1) + "->" + str(nums[i+1] - 1))
         return res
