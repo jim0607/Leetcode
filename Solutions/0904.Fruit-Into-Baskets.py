5b5a33523e61cc5ae6664c986b88e4d5e65eef45
Jim0607 <jinjun.ding@gmail.com>
2020-09-24 17:29:55 -0700

    Update 0904.Fruit-Into-Baskets.py

--- Solutions/0904.Fruit-Into-Baskets.py
+++ Solutions/0904.Fruit-Into-Baskets.py
@@ -1,6 +1,9 @@
+"""
 904. Fruit Into Baskets
 
 In a row of trees, the i-th tree produces fruit with type tree[i].
+"""
+
 
 
 """
@@ -9,23 +12,24 @@ The problem is so poorly described, should be described as following:
 eg: [3,3,3,1,2,1,1,2,3,3,4], we can choose [1,2,1,1,2] because there are only two types of fruit (1 and 2).
 now that we sort it out, it is exactly the same as 159. Longest Substring with At Most Two Distinct Characters
 """
+"""
+longest substring with at most two distinct chars
+"""
 class Solution:
     def totalFruit(self, tree: List[int]) -> int:
+        type_to_cnt = collections.defaultdict(int)
         max_lens = 0
-        j = 0
-        freq = collections.defaultdict(int)
-        for i in range(len(tree)):
-            while j < len(tree) and len(freq) <= 2:
-                if tree[j] not in freq and len(freq) == 2:
-                    break
-                freq[tree[j]] += 1
-                j += 1
+        i = 0
+        for j in range(len(tree)):
+            type_to_cnt[tree[j]] += 1
             
-            if len(freq) <= 2:
-                max_lens = max(max_lens, j - i)
+            while i <= j and len(type_to_cnt) > 2:
+                type_to_cnt[tree[i]] -= 1
+                if type_to_cnt[tree[i]] == 0:
+                    del type_to_cnt[tree[i]]
+                i += 1
                 
-            freq[tree[i]] -= 1
-            if freq[tree[i]] == 0:
-                del freq[tree[i]]
+            if len(type_to_cnt) <= 2:
+                max_lens = max(max_lens, j - i + 1)
                 
         return max_lens
