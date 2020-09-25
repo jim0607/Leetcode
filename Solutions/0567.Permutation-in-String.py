08350aa6cf7d5a4bf9c7b30737159504494d99df
Jim0607 <jinjun.ding@gmail.com>
2020-09-24 22:50:46 -0700

    Update 0567.Permutation-in-String.py

--- Solutions/0567.Permutation-in-String.py
+++ Solutions/0567.Permutation-in-String.py
@@ -1,7 +1,8 @@
 """
 567. Permutation in String
 
-Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. 
+In other words, one of the first string's permutations is the substring of the second string.
 
 Example 1:
 
@@ -22,7 +23,7 @@ The length of both given strings is in range [1, 10,000].
 
 
 """
-solution 1: 由于我们要求的substring时固定长度的，所以最好maintina a fixed size windows
+solution 1: 由于我们要求的substring时固定长度的，所以最好maintina a fixed size window - 套用fix window模板
 """
 class Solution:
     def checkInclusion(self, s2: str, s1: str) -> bool:
@@ -30,13 +31,14 @@ class Solution:
         counter2 = collections.Counter(s2)
         ch_to_cnt = collections.defaultdict(int)
         for i in range(len(s1)):
-            ch_to_cnt[s1[i]] += 1
-            if i >= k:              # maintian a fixed size window
+            ch_to_cnt[s1[i]] += 1   # fixed window模板: step 1: 把ith item加到window
+         
+            if i >= k:              # maintian a fixed size window - 把(i-k)th item 吐出来
                 ch_to_cnt[s1[i-k]] -= 1
                 if ch_to_cnt[s1[i-k]] == 0:
                     del ch_to_cnt[s1[i-k]]
                     
-            if ch_to_cnt == counter2:
+            if ch_to_cnt == counter2:  # step 3: update res
                 return True
             
         return False
