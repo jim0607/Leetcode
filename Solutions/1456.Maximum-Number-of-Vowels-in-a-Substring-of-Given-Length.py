d3a3a988d27f7cade5c9cf9678752034e0938cd0
Jim0607 <jinjun.ding@gmail.com>
2020-09-25 10:31:32 -0700

    Update 1456.Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.py

--- Solutions/1456.Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.py
+++ Solutions/1456.Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.py
@@ -1,3 +1,4 @@
+"""
 1456. Maximum Number of Vowels in a Substring of Given Length
 
 Given a string s and an integer k.
@@ -6,7 +7,6 @@ Return the maximum number of vowel letters in any substring of s with length k.
 
 Vowel letters in English are (a, e, i, o, u).
 
- 
 
 Example 1:
 
@@ -18,23 +18,23 @@ Example 2:
 Input: s = "aeiou", k = 2
 Output: 2
 Explanation: Any substring of length 2 contains 2 vowels.
+"""
 
 
+"""
+套 sliding window with fixed size 模板即可
+"""
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        cnt = 0
         vowels = {"a", "e", "i", "o", "u"}
-        for i in range(k):
-            if s[i] in vowels:
-                cnt += 1
+        max_cnt = 0
+        vowel_cnt = 0
+        for i in range(len(s)):
+            vowel_cnt += 1 if s[i] in vowels else 0         # step 1: 把ith item加进去
+            
+            if i >= k:
+                vowel_cnt -= 1 if s[i-k] in vowels else 0   # step 2: 把(i-k)th item 吐出来
                 
-        max_cnt = cnt
-        
-        for i in range(k, len(s)):
-            if s[i] in vowels:
-                cnt += 1
-            if s[i-k] in vowels:
-                cnt -= 1
-            max_cnt = max(cnt, max_cnt)
+            max_cnt = max(max_cnt, vowel_cnt)               # step 3: 更新res
             
         return max_cnt
