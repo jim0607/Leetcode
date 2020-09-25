0e3348c2649d12ec3336ab6f76976bf05946f57c
Jim0607 <jinjun.ding@gmail.com>
2020-09-24 17:18:10 -0700

    Update 0340.Longest-Substringwith-At-Most-K-Distinct-Characters.py

--- Solutions/0340.Longest-Substringwith-At-Most-K-Distinct-Characters.py
+++ Solutions/0340.Longest-Substringwith-At-Most-K-Distinct-Characters.py
@@ -1,3 +1,4 @@
+"""
 340. Longest Substring with At Most K Distinct Characters
 
 Given a string, find the length of the longest substring T that contains at most k distinct characters.
@@ -12,36 +13,30 @@ Example 2:
 Input: s = "aa", k = 1
 Output: 2
 Explanation: T is "aa" which its length is 2.
+"""
 
 
-# 维护一个charDict, 用来记录i->j中的char的频率，套模板时满足的条件是len(charDict) <= k; 
-# 更新j: charDict[s[j]+=1; 更新i: charDict[s[i]] -= 1, if charDict[s[i]] == 0: del charDict[s[i]]
+"""
+维护一个charDict, 用来记录i->j中的char的频率，这题是sum at most s problem, 
+写法是while loop里让前面的指针去追后面的指针; 更新j: charDict[s[j]+=1; 
+更新i: charDict[s[i]] -= 1, if charDict[s[i]] == 0: del charDict[s[i]]
+套用模板即可
+"""
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        lens = len(s)
-        if lens == 0 or k == 0:
-            return 0
-        
-        charDict = collections.defaultdict(int)
-        
-        j = 0
-        res = 0
-        # 套用模板即可
-        for i in range(lens):
-            while j < lens and len(charDict) <= k:
-                if s[j] not in charDict and len(charDict) == k:  # 保证不再加了，再加就满了
-                    break
-                    
-                charDict[s[j]] += 1   # 更新 j
-                j += 1
+        max_lens = 0
+        ch_to_cnt = collections.defaultdict(int)
+        i = 0
+        for j in range(len(s)):
+            ch_to_cnt[s[j]] += 1        # 不管是哪种模板，都是先更新后面的指针
             
-            # if 满足条件, 跟新res
-            if len(charDict) <= k:
-                res = max(res, j - i)
-            
-            # 更新 i
-            charDict[s[i]] -= 1
-            if charDict[s[i]] == 0:
-                del charDict[s[i]]
-            
-        return res
+            while i <= j and len(ch_to_cnt) > k:
+                ch_to_cnt[s[i]] -= 1
+                if ch_to_cnt[s[i]] == 0:
+                    del ch_to_cnt[s[i]]
+                i += 1
+                
+            if len(ch_to_cnt) <= k:
+                max_lens = max(max_lens, j - i + 1)
+                
+        return max_lens
