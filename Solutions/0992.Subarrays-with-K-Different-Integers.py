1800d8a1eac9e7ccd0eea798edab0ab6b9f15c8c
Jim0607 <jinjun.ding@gmail.com>
2020-09-24 19:04:28 -0700

    Update 0992.Subarrays-with-K-Different-Integers.py

--- Solutions/0992.Subarrays-with-K-Different-Integers.py
+++ Solutions/0992.Subarrays-with-K-Different-Integers.py
@@ -1,12 +1,12 @@
+"""
 992. Subarrays with K Different Integers
 
-Given an array A of positive integers, call a (contiguous, not necessarily distinct) subarray of A good if the number of different integers in that subarray is exactly K.
+Given an array A of positive integers, call a (contiguous, not necessarily distinct) 
+subarray of A good if the number of different integers in that subarray is exactly K.
 
 (For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.)
 
 Return the number of good subarrays of A.
-
- 
 
 Example 1:
 
@@ -18,34 +18,32 @@ Example 2:
 Input: A = [1,2,1,3,4], K = 3
 Output: 3
 Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+"""
+
+
 
 
 """
 exactly(K) = atMost(K) - atMost(K-1)
 """
 class Solution:
-    def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
-        return self._at_most_k(A, K) - self._at_most_k(A, K - 1)
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self._at_most(nums, k) - self._at_most(nums, k - 1)
     
-    def _at_most_k(self, A, K):
-        """
-        Exactly the same as 340. Longest Substring with At Most K Distinct Characters
-        """
-        freq = collections.defaultdict(int)
-        j = 0
+    def _at_most(self, nums, k):
+        num_to_cnt = collections.defaultdict(int)
         res = 0
-        for i in range(len(A)):
-            while j < len(A) and len(freq) <= K:
-                if A[j] not in freq and len(freq) == K:
-                    break
-                freq[A[j]] += 1
-                j += 1
+        i = 0
+        for j in range(len(nums)):
+            num_to_cnt[nums[j]] += 1
             
-            if len(freq) <= K:
-                res += j - i
+            while i <= j and len(num_to_cnt) > k:
+                num_to_cnt[nums[i]] -= 1
+                if num_to_cnt[nums[i]] == 0:
+                    del num_to_cnt[nums[i]]
+                i += 1
                 
-            freq[A[i]] -= 1
-            if freq[A[i]] == 0:
-                del freq[A[i]]
-                
+            if len(num_to_cnt) <= k:
+                res += j - i + 1
+        
         return res
