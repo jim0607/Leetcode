b1a7633dd8fc095eb6e638ce8c8292429ca8847c
Jim0607 <jinjun.ding@gmail.com>
2020-09-24 22:45:31 -0700

    Update 1052.Grumpy-Bookstore-Owner.py

--- Solutions/1052.Grumpy-Bookstore-Owner.py
+++ Solutions/1052.Grumpy-Bookstore-Owner.py
@@ -1,3 +1,4 @@
+"""
 1052. Grumpy Bookstore Owner
 
 Today, the bookstore owner has a store open for customers.length minutes.  Every minute, some number of customers (customers[i]) enter the store, and all those customers leave after the end of that minute.
@@ -8,14 +9,14 @@ The bookstore owner knows a secret technique to keep themselves not grumpy for X
 
 Return the maximum number of customers that can be satisfied throughout the day.
 
- 
-
 Example 1:
 
 Input: customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], X = 3
 Output: 16
 Explanation: The bookstore owner keeps themselves not grumpy for the last 3 minutes. 
 The maximum number of customers that can be satisfied = 1 + 1 + 1 + 1 + 7 + 5 = 16.
+"""
+
 
 
 """
@@ -25,12 +26,15 @@ Since the window size is fixed, the problem is easier to implement. We only need
 which represents how man ymore people can be satisfied if the owner use X minites magic card
 """
 class Solution:
-    def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int) -> int:
-        lens = len(customers)
-        satisfied = sum(customers[i]  * (1 - grumpy[i]) for i in range(lens))   # how many people satisfied initially
-        gain = sum(customers[i] * grumpy[i] for i in range(X))  # how many more people can be satisfied if the owner use X minites magic card
-        max_gain = gain
-        for i in range(X, lens):
-            gain += customers[i] * grumpy[i] - customers[i-X] * grumpy[i-X]
-            max_gain = max(max_gain, gain)
-        return max_gain + satisfied
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], x: int) -> int:
+        satisfy = sum(customers[i] for i in range(len(customers)) if grumpy[i] == 0)  # how many customer could be satisfied if not use magic x minutes.  
+        max_satisfy = satisfy
+        for i in range(len(customers)):
+            satisfy += customers[i] if grumpy[i] == 1 else 0
+            
+            if i >= x:      # maintain a window with fixed size
+                satisfy -= customers[i-x] if grumpy[i-x] == 1 else 0
+                
+            max_satisfy = max(max_satisfy, satisfy)
+            
+        return max_satisfy
