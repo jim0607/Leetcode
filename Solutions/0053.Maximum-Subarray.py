5556ffde041b129b4693fae29ee778329d35fd85
Jim0607 <jinjun.ding@gmail.com>
2020-09-25 17:41:07 -0700

    Update 0053.Maximum-Subarray.py

--- Solutions/0053.Maximum-Subarray.py
+++ Solutions/0053.Maximum-Subarray.py
@@ -31,6 +31,27 @@
 # 
 #
 
+
+"""
+step 1: 构造前缀和pre_sum; 
+step 2: the same as 127. Best time to buy and sell stock
+"""
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pre_sum = [0 for _ in range(len(nums) + 1)]
+        for i, num in enumerate(nums):
+            pre_sum[i+1] = pre_sum[i] + num
+            
+        # next part is the same as 127. Best time to buy and sell stock
+        min_pre = float("inf")
+        max_sum = float("-inf")
+        for pre in pre_sum:
+            max_sum = max(max_sum, pre - min_pre)   # 注意因为subarray cannot be empty, we should update max_sum before min_pre
+            min_pre = min(min_pre, pre)
+        return max_sum
+
+
+
 """使用前缀和的方法，计算每个位置为结尾的subarray的最大值是多少"""
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
