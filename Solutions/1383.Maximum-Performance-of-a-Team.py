de28ccadb2856d0ea6b5abcb12a95b260c187330
Jim0607 <jinjun.ding@gmail.com>
2020-09-27 16:29:37 -0700

    Update 1383.Maximum-Performance-of-a-Team.py

--- Solutions/1383.Maximum-Performance-of-a-Team.py
+++ Solutions/1383.Maximum-Performance-of-a-Team.py
@@ -1,10 +1,12 @@
+"""
 1383. Maximum Performance of a Team
 
-There are n engineers numbered from 1 to n and two arrays: speed and efficiency, where speed[i] and efficiency[i] represent the speed and efficiency for the i-th engineer respectively. Return the maximum performance of a team composed of at most k engineers, since the answer can be a huge number, return this modulo 10^9 + 7.
+There are n engineers numbered from 1 to n and two arrays: speed and efficiency, where speed[i] and efficiency[i] represent 
+the speed and efficiency for the i-th engineer respectively. Return the maximum performance of a team composed of at most k engineers, 
+since the answer can be a huge number, return this modulo 10^9 + 7.
 
 The performance of a team is the sum of their engineers' speeds multiplied by the minimum efficiency among their engineers. 
 
- 
 
 Example 1:
 
@@ -22,6 +24,7 @@ Example 3:
 
 Input: n = 6, speed = [2,10,3,1,5,8], efficiency = [5,4,3,9,7,2], k = 4
 Output: 72
+"""
 
 
 """
@@ -31,23 +34,22 @@ Output: 72
 这个因为他的efficiency一定是这k个worker里面最小的。
 可以保持一个k size的heap来存(他之前所有workers), 如果size>k就把min_speed的worker踢出去
 """
-"""
-from heapq import heappush, heappop, heapify
-
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
         workers = [(speed[i], efficiency[i]) for i in range(len(speed))]
-        workers.sort(key = lambda x: (-x[1], -x[0]))        # 将workers按照efficiency降序排序
+        workers.sort(key = lambda x: (-x[1], -x[0]))   # 将workers按照efficiency降序排序
         
-        min_speed = []
-        max_perform = 0
-        total_speed = 0
+        total_spd = 0
+        max_perf = 0
+        hq = []         # heap 存之前所有workers的speed
+        
+        # 遍历的过程中min_eff在不断减小，但是total_spd在不断增加，
+        # 我们希望total_spd的增加能够弥补min_ef的减小
         for spd, eff in workers:
-            heappush(min_speed, spd)
-            total_speed += spd
-            if len(min_speed) > k:
-                total_speed -= heappop(min_speed)
-            max_perform = max(max_perform, total_speed * eff)
-        
-        MOD = 10 ** 9 + 7
-        return max_perform % (MOD)
+            heappush(hq, spd)
+            total_spd += spd
+            if len(hq) > k:
+                total_spd -= heappop(hq)
+            max_perf = max(max_perf, total_spd * eff)
+            
+        return max_perf % (10**9 + 7)
