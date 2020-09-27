901b0153373a6d308c5ecee877a638dde0c21ff1
Jim0607 <jinjun.ding@gmail.com>
2020-09-27 15:38:02 -0700

    Update 0670.Maximum-Swap.py

--- Solutions/0670.Maximum-Swap.py
+++ Solutions/0670.Maximum-Swap.py
@@ -1,3 +1,4 @@
+"""
 670. Maximum Swap
 
 Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. Return the maximum valued number you could get.
@@ -10,35 +11,25 @@ Example 2:
 Input: 9973
 Output: 9973
 Explanation: No swap.
-
+"""
 
 """
 solution 1: sort and compare - O(nlogn)
 """
-"""
 class Solution:
     def maximumSwap(self, num: int) -> int:
         s = str(num)
-        arr = [ch for ch in s]
-        sorted_arr = sorted(arr, key = lambda x: -ord(x))
-        print(arr, sorted_arr)
-        for i in range(len(arr)):
-            if arr[i] != sorted_arr[i]:
-                temp = arr[i]
-                
-                # step 1: # 找到arr中靠后的那个需要替换的idx
-                idx = 0
-                for j in range(len(arr)-1, -1, -1):  # 找到靠后的那个需要替换的idx
-                    if arr[j] == sorted_arr[i]:
-                        idx = j
-                        break
-                    
-                # step 2: swap 操作
-                arr[i] = sorted_arr[i]     
-                arr[idx] = temp
-                return int("".join(arr))
-                
-        return num
+        num_to_idx = collections.defaultdict(int)
+        for i, num in enumerate(s):
+            num_to_idx[num] = i
+        sorted_s = sorted(s, key = lambda x: -ord(x))
+        res = [ch for ch in s]
+        for i in range(len(s)):
+            if s[i] != sorted_s[i]:
+                idx = num_to_idx[sorted_s[i]]     # 找到arr中靠后的那个需要替换的idx  
+                res[i], res[idx] = res[idx], res[i]
+                break
+        return int("".join(res))
         
 
 """
@@ -46,18 +37,15 @@ solution 2: one pass from backward - O(N)
 """
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        nums = [int(ch) for ch in str(num)]
-        max_idx = len(nums) - 1     # the idx for the max number
-        need_swap_idx_small = 0     # the idx that needs to be swapped
-        need_swap_idx_large = 0
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] > nums[max_idx]:
+        s = [ch for ch in str(num)]
+        num_to_idx = collections.defaultdict(int)
+        max_idx = len(s) - 1
+        need_swap = [-1, -1]
+        for i in range(len(s)-1, -1, -1):
+            if s[i] > s[max_idx]:
                 max_idx = i
-            elif nums[i] < nums[max_idx]:
-                need_swap_idx_small = i     # 如果碰到一个小的，应该与后面已经遍历过的max_idx交换
-                need_swap_idx_large = max_idx
+            elif s[i] < s[max_idx]:     # 如果碰到一个小的，应该与后面已经遍历过的max_idx交换
+                need_swap = (max_idx, i)    
         
-        nums[need_swap_idx_small], nums[need_swap_idx_large] = nums[need_swap_idx_large], nums[need_swap_idx_small]
-        
-        return "".join([str(x) for x in nums])
-        
+        s[need_swap[0]], s[need_swap[1]] = s[need_swap[1]], s[need_swap[0]]
+        return int("".join(s))
