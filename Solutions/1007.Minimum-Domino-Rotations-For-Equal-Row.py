5d2a02d449a2d8dd4a7e198b64e0c0c4e42b9c96
Jim0607 <jinjun.ding@gmail.com>
2020-09-27 11:08:21 -0700

    Update 1007.Minimum-Domino-Rotations-For-Equal-Row.py

--- Solutions/1007.Minimum-Domino-Rotations-For-Equal-Row.py
+++ Solutions/1007.Minimum-Domino-Rotations-For-Equal-Row.py
@@ -1,6 +1,8 @@
+"""
 1007. Minimum Domino Rotations For Equal Row
 
-In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the i-th domino.  (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the i-th domino.  
+(A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
 
 We may rotate the i-th domino, so that A[i] and B[i] swap values.
 
@@ -21,35 +23,36 @@ Input: A = [3,5,1,2,3], B = [3,6,3,3,4]
 Output: -1
 Explanation: 
 In this case, it is not possible to rotate the dominoes to make one row of values equal.
+"""
+
 
 
 """
-让max_freq不动，去rotate freq比较小的
+Try all possibilities from 1 to 6.
+If we can make number i in a whole row,
+it should satisfy that countA[i] + countB[i] - same[i] = n
+
+Take example of
+A = [2,1,2,4,2,2]
+B = [5,2,6,2,3,2]
+
+countA[2] = 4, as A[0] = A[2] = A[4] = A[5] = 2
+countB[2] = 3, as B[1] = B[3] = B[5] = 2
+same[2] = 1, as A[5] = B[5] = 2
+
+We have countA[2] + countB[2] - same[2] = 6,
+so we can make 2 in a whole row.
 """
 class Solution:
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
-        freq_A = collections.Counter(A)
-        freq_B = collections.Counter(B)
-        max_freq_A, max_freq_B = 1, 1
-        max_freq_num_A, max_freq_num_B = A[0], B[0]
-        for num, freq in freq_A.items():
-            if freq > max_freq_A:
-                max_freq_A = freq
-                max_freq_num_A = num
-        for num, freq in freq_B.items():
-            if freq > max_freq_B:   
-                max_freq_B = freq
-                max_freq_num_B = num
-            
-        if max_freq_A >= max_freq_B:
-            return len(A) - max_freq_A if self._swap(A, B, max_freq_num_A) else -1
-        else:
-            return len(B) - max_freq_B if self._swap(B, A, max_freq_num_B) else -1
+        counter_A, counter_B = collections.Counter(A), collections.Counter(B)
         
-    def _swap(self, a, b, stay_put):
-        for i in range(len(a)):
-            if a[i] == stay_put:
-                continue
-            if b[i] != stay_put:
-                return False
-        return True
+        counter_same = collections.Counter()    # record the same num for A and B that appears at the same position,
+        for a, b in zip(A, B):                  # so counter_same[num] means how many positions A[i] == num == B[i]
+            if a == b:
+                counter_same[a] += 1
+        
+        for num in range(1, 7):
+            if counter_A[num] + counter_B[num] - counter_same[num] == len(A):
+                return min(counter_A[num], counter_B[num]) - counter_same[num]
+        return -1
