296852e045209f2ba9549eb11db2d096c8606f75
Jim0607 <jinjun.ding@gmail.com>
2020-09-26 19:33:35 -0700

    Update 1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py

--- Solutions/1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py
+++ Solutions/1326.Minimum-Number-of-Taps-to-Open-to-Water-a-Garden.py
@@ -1,3 +1,4 @@
+"""
 1326. Minimum Number of Taps to Open to Water a Garden
 
 There is a one-dimensional garden on the x-axis. The garden starts at the point 0 and ends at the point n. (i.e The length of the garden is n).
@@ -7,8 +8,6 @@ There are n + 1 taps located at points [0, 1, ..., n] in the garden.
 Given an integer n and an integer array ranges of length n + 1 where ranges[i] (0-indexed) means the i-th tap can water the area [i - ranges[i], i + ranges[i]] if it was open.
 
 Return the minimum number of taps that should be open to water the whole garden, If the garden cannot be watered return -1.
-
- 
 
 Example 1:
 
@@ -39,6 +38,7 @@ Example 5:
 
 Input: n = 8, ranges = [4,0,0,0,4,0,0,0,4]
 Output: 1
+"""
 
 
 
@@ -52,7 +52,7 @@ class Solution:
         # step 1: build the reachable list
         reachable = [0 for _ in range(n + 1)]
         for i, ran in enumerate(ranges):
-            idx = max(0, i - ran)
+            idx = max(0, i - ran)      # 注意这里idx的定义是start point, reachable[idx]=from start point, where we can reach.
             can_reach = min(i + ran, n)     # 注意这里can_reach的含义是可以到达哪里，而不是jump game中的可以跳几步
             reachable[idx] = max(reachable[idx], can_reach)      # 注意这里要比较一下，因为可能会出现很多idx相同的情况，比如可能很多idx都等于0
             
