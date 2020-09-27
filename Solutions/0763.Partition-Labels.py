64ba462c74daa5d4d9e4fba8574e5d9400763be4
Jim0607 <jinjun.ding@gmail.com>
2020-09-26 21:00:32 -0700

    Update 0763.Partition-Labels.py

--- Solutions/0763.Partition-Labels.py
+++ Solutions/0763.Partition-Labels.py
@@ -1,7 +1,7 @@
+"""
 763. Partition Labels
 
 A string S of lowercase English letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
-
 
 Example 1:
 
@@ -11,6 +11,47 @@ Explanation:
 The partition is "ababcbaca", "defegde", "hijhklij".
 This is a partition so that each letter appears in at most one part.
 A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+"""
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        """
+        step 1: use a hashmap to store the last time a ch appears. mapping[ch] = the last idx the ch appears
+        step 2: construct has_to_reach[idx] = starting from idx, where we have to reach.
+        step 3: jump game II
+        """
+        # step 1: use a hashmap to store the last time a ch appears. mapping[ch] = the last idx the ch appears
+        mapping = collections.defaultdict(int)
+        for idx, ch in enumerate(s):
+            mapping[ch] = idx
+            
+        # step 2: construct has_to_reach[idx] = starting from idx, where we have to reach.
+        has_to_reach = [0 for _ in range(len(s))]
+        for idx, ch in enumerate(s):
+            has_to_reach[idx] = mapping[ch]
+            
+        # step 3: jump game II
+        res = []
+        i = 0
+        last_coverage = 0
+        next_coverage = has_to_reach[0]
+        start = 0
+        while i < len(s):
+            while i <= last_coverage:
+                next_coverage = max(next_coverage, has_to_reach[i])
+                i += 1
+
+            if next_coverage == last_coverage:      # meaning if we stop here, no ch in current window will appear later
+                res.append(next_coverage - start + 1)
+                start = next_coverage + 1           # 更新下一段的start position
+                if i < len(s): 
+                    next_coverage = has_to_reach[i] # 更新next_coverage, last coverage保持不变就可以了
+            else:
+                last_coverage = next_coverage
+            
+        return res
+
 
 
 
