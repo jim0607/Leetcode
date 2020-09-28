b812257a9296b025cce02fb3133d94c9f696d422
Jim0607 <jinjun.ding@gmail.com>
2020-09-27 23:06:38 -0700

    Update 1213.Intersection-of-Three-Sorted-Arrays.py

--- Solutions/1213.Intersection-of-Three-Sorted-Arrays.py
+++ Solutions/1213.Intersection-of-Three-Sorted-Arrays.py
@@ -1,3 +1,4 @@
+"""
 1213. Intersection of Three Sorted Arrays
 
 Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, return a sorted array of only the integers that appeared in all three arrays.
@@ -7,11 +8,13 @@ Example 1:
 Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
 Output: [1,5]
 Explanation: Only 1 and 5 appeared in the three arrays.
+"""
 
 
 """
 solution 1: use a freq dictionary: the elements that appeared in all the arrays would have a frequency of 3.
-solution 2: three pointers since the arrs are sorted
+solution 2: three pointers since the arrs are sorted.
+solution 3: find intersection inter of arr1 and arr2 first, then find the intersection of inter with arr3.
 """
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
