4d49d70631fb9ce5f4f30ba8bc7e263690977aa2
Jim0607 <jinjun.ding@gmail.com>
2020-09-28 13:42:24 -0700

    Update 0069.Sqrt(x).py

--- Solutions/0069.Sqrt(x).py
+++ Solutions/0069.Sqrt(x).py
@@ -36,3 +36,34 @@ cclass Solution:
             sqrt = (sqrt + x / sqrt) / 2
             
         return int(sqrt)
+      
+      
+"""
+Gradient descent solution. 
+If we keep out learning rate alpha to be a constant ie. 1, the time complexity will be O(x).
+Since we boost the gradient descent by "Adaptive Learning Rates" method - keep reducing steps by half,
+the time complexity will be O(logx)
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x <= 3:
+            return 1
+        
+        return self.gradient_descent(x)
+    
+    def gradient_descent(self, x):
+        y = 0                                       # start from 0, not x
+        alpha = x / 2                               # steps to take at each iteration
+        gradient = self.calculate_gradient(x, y)    # gradient at each iteration
+        while abs(y**2 - x) >= 0.1:                 # alpha * gradient is the decay rate
+            alpha = alpha / 2          # boost the gradient descent by "Adaptive Learning Rates" method - keep reducing steps by half
+            gradient = self.calculate_gradient(x, y)
+            y -= alpha * gradient
+
+        return int(y + 1) if int(y + 1)**2 == x else int(y)
+    
+    def calculate_gradient(self, x, y):
+        cost_func = y**2 - x                 # use MSE as cost function, gradient = d(cost) / dx
+        return 1 if cost_func > 0 else -1    # gradient is which way we should go - increase res of decrease res
