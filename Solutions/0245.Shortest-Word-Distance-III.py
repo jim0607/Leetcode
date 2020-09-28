95c82b523c10cb8f0a613107d67fa140f2b03d65
Jim0607 <jinjun.ding@gmail.com>
2020-09-27 20:59:55 -0700

    Update 0245.Shortest-Word-Distance-III.py

--- Solutions/0245.Shortest-Word-Distance-III.py
+++ Solutions/0245.Shortest-Word-Distance-III.py
@@ -1,3 +1,4 @@
+"""
 245. Shortest Word Distance III
 
 Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
@@ -13,31 +14,30 @@ Input: word1 = "makes", word2 = "makes"
 Output: 3
 Note:
 You may assume word1 and word2 are both in the list.
-
+"""
 
 
 
 class Solution:
     def shortestWordDistance(self, words: List[str], word1: str, word2: str) -> int:
-        minDist = float("inf")
-        idx1, idx2 = -1, -1
-        if word1 != word2:
+        min_dist = float("inf")
+        if word1 == word2:
+            prev, curr = float("inf"), float("inf")
+            for i, word in enumerate(words):
+                if word == word1:
+                    if curr == float("inf"):
+                        curr = i
+                    else:
+                        prev = curr
+                        curr = i
+                        min_dist = min(min_dist, abs(curr - prev))
+        else:
+            idx1, idx2 = float("inf"), float("inf")
             for i, word in enumerate(words):
                 if word == word1:
                     idx1 = i
-                if word == word2:
+                    min_dist = min(min_dist, abs(idx2 - idx1))
+                elif word == word2:
                     idx2 = i
-                
-                if idx1 != -1 and idx2 != -1:
-                    minDist = min(minDist, abs(idx1 - idx2))
-        
-        else:
-            prev, curr = -1, -1
-            for i, word in enumerate(words):
-                if word == word1:
-                    curr = i
-                    if prev != -1:
-                        minDist = min(minDist, curr - prev)
-                    prev = curr
-                    
-        return minDist
+                    min_dist = min(min_dist, abs(idx2 - idx1))
+        return min_dist
