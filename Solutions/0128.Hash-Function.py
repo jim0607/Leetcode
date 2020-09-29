d4f9efc8af20c85e9fb13aa23321fb08678bc056
Jim0607 <jinjun.ding@gmail.com>
2020-09-29 15:48:31 -0700

    Update 0128.Hash-Function.py

--- Solutions/0128.Hash-Function.py
+++ Solutions/0128.Hash-Function.py
@@ -1,6 +1,10 @@
+"""
 128. Hash Function
 
-In data structure Hash, hash function is used to convert a string(or any other type) into an integer smaller than hash size and bigger or equal to zero. The objective of designing a hash function is to "hash" the key as unreasonable as possible. A good hash function can avoid collision as less as possible. A widely used hash function algorithm is using a magic number 33, consider any string as a 33 based big integer like follow:
+In data structure Hash, hash function is used to convert a string(or any other type) into an integer smaller than hash size and bigger or equal to zero. 
+The objective of designing a hash function is to "hash" the key as unreasonable as possible. 
+A good hash function can avoid collision as less as possible. A widely used hash function algorithm is using a magic number 33, 
+consider any string as a 33 based big integer like follow:
 
 hashcode("abcd") = (ascii(a) * 333 + ascii(b) * 332 + ascii(c) *33 + ascii(d)) % HASH_SIZE 
 
@@ -11,7 +15,6 @@ hashcode("abcd") = (ascii(a) * 333 + ascii(b) * 332 + ascii(c) *33 + ascii(d)) %
 here HASH_SIZE is the capacity of the hash table (you can assume a hash table is like an array with index 0 ~ HASH_SIZE-1).
 
 Given a string as a key and the size of hash table, return the hash value of this key.
-
 
 Example
 Example 1:
@@ -27,7 +30,7 @@ Explanation: (97*33^3 + 98*33^2 + 99*33 + 100*1)%100 = 78
 Clarification
 For this problem, you are not necessary to design your own hash algorithm or consider any collision issue, 
 you just need to implement the algorithm as described.
-
+"""
 
 
 """
