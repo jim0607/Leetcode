5f4210fd0c07e654507fd72a3414e3a6387a839c
Jim0607 <jinjun.ding@gmail.com>
2020-09-28 20:48:58 -0700

    Update 0069.Sqrt(x).py

--- Solutions/0069.Sqrt(x).py
+++ Solutions/0069.Sqrt(x).py
@@ -40,9 +40,16 @@ cclass Solution:
       
 """
 Gradient descent solution. 
+
+Gradient descent 核心算法:
+while alpha > eps:
+    prediction -= alpha * gradient, where gradient = d(cost_func)/dx, where cost_func = MSE;
+    alpha *= 0.2
+return prediction
+
 If we keep out learning rate alpha to be a constant ie. 1, the time complexity will be O(x).
 Since we boost the gradient descent by "Adaptive Learning Rates" method - keep reducing steps by half,
-the time complexity will be O(logx)
+the time complexity will be O(logx).
 """
 class Solution:
     def mySqrt(self, x: int) -> int:
@@ -54,11 +61,43 @@ class Solution:
         return self.gradient_descent(x)
     
     def gradient_descent(self, x):
+        y = 0                                       # y is "prediction" - start from 0, not x
+        alpha = x / 2                                  # alpha is "learnign rate" - steps to take at each iteration
+        eps = 1e-5
+        while alpha >= eps:                         # alpha * gradient is the decay rate
+            gradient = self.calculate_gradient(x, y)
+            y -= alpha * gradient
+            alpha = alpha / 2          # boost the gradient descent by "Adaptive Learning Rates" method - keep reducing steps by half
+
+        return int(y + 1) if int(y + 1)**2 == x else int(y)
+    
+    def calculate_gradient(self, x, y):
+        cost_func = y**2 - x                 # use MSE as "cost function", gradient = d(cost) / dx
+        return 1 if cost_func > 0 else -1    # gradient is which way we should go - increase res of decrease res
+      
+      
+      
+      
+"""
+since we know it's going to converge to an answer, we can use while abs(y**2 - x) >= eps:
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x <= 3:
+            return 1
+        if x <= 8:
+            return 2
+        
+        return self.gradient_descent(x)
+    
+    def gradient_descent(self, x):
         y = 0                                       # start from 0, not x
         alpha = x / 2                               # steps to take at each iteration
-        gradient = self.calculate_gradient(x, y)    # gradient at each iteration
-        while abs(y**2 - x) >= 0.1:                 # alpha * gradient is the decay rate
-            alpha = alpha / 2          # boost the gradient descent by "Adaptive Learning Rates" method - keep reducing steps by half
+        eps = 1e-1
+        while abs(y**2 - x) >= eps:                 # alpha * gradient is the decay rate
+            alpha *= 0.5          # boost the gradient descent by "Adaptive Learning Rates" method - keep reducing steps by half
             gradient = self.calculate_gradient(x, y)
             y -= alpha * gradient
 
@@ -67,3 +106,7 @@ class Solution:
     def calculate_gradient(self, x, y):
         cost_func = y**2 - x                 # use MSE as cost function, gradient = d(cost) / dx
         return 1 if cost_func > 0 else -1    # gradient is which way we should go - increase res of decrease res
+    
+"""
+这里用alpha *= 0.5, 如果用alpha *= 0.4就会太慢, 如果用alpha *= 0.8也会很慢因为会在Minimum附近左右跳动.
+"""
