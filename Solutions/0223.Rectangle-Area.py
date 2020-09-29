d2d17a1ca85c9a80c1198d7614b5ea8eea03e02a
Jim0607 <jinjun.ding@gmail.com>
2020-09-28 22:18:34 -0700

    Update 0223.Rectangle-Area.py

--- Solutions/0223.Rectangle-Area.py
+++ Solutions/0223.Rectangle-Area.py
@@ -1,3 +1,4 @@
+"""
 223. Rectangle Area
 
 Find the total area covered by two rectilinear rectangles in a 2D plane.
@@ -10,16 +11,19 @@ Example:
 
 Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
 Output: 45
+"""
 
 
 class Solution:
     def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
-        if not self.overlap(A, B, C, D, E, F, G, H):
-            return (C-A)*(D-B) + (G-E)*(H-F)
-        else:
-            return (C-A)*(D-B) + (G-E)*(H-F) - abs((min(H, D)-max(B, F))*(min(C, G)-max(E, A)))
+        total = (C - A) * (D - B) + (G - E) * (H - F)
+        if not self.isRectangleOverlap([A,B,C,D], [E,F,G,H]):
+            return total
         
-    def overlap(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> bool:
-        if D <= F or C <= E or A >= G or B >= H:
+        overlap = (min(C, G) - max(A, E)) * (min(H, D) - max(B, F))
+        return total - overlap
+        
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if rec2[0] >= rec1[2] or rec2[2] <= rec1[0] or rec2[3] <= rec1[1] or rec2[1] >= rec1[3]:
             return False
         return True
