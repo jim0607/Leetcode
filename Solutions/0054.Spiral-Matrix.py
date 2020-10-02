f201d5b970620269bdc7db8da76cbbc95b2e83fd
Jim0607 <jinjun.ding@gmail.com>
2020-10-01 19:11:59 -0700

    Update 0054.Spiral-Matrix.py

--- Solutions/0054.Spiral-Matrix.py
+++ Solutions/0054.Spiral-Matrix.py
@@ -1,3 +1,4 @@
+"""
 54. Spiral Matrix
 
 Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
@@ -20,54 +21,38 @@ Input:
   [9,10,11,12]
 ]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+"""
 
 
-
+"""
+每一个转弯的点是dfs的node, dfs helper function 需要传入的参数有(当前的位置, 当前的方向，当前的row_range and col_range)
+"""
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        def dfs(curr_i, curr_j, curr_dir, min_row, max_row, min_col, max_col):
+            if len(res) == m * n:
+                return
+            if curr_dir == "r":
+                for j in range(min_col, max_col + 1):
+                    res.append(matrix[curr_i][j])
+                dfs(curr_i, j, "d", min_row + 1, max_row, min_col, max_col)
+            elif curr_dir == "d":
+                for i in range(min_row, max_row + 1):
+                    res.append(matrix[i][curr_j])
+                dfs(i, curr_j, "l", min_row, max_row, min_col, max_col - 1)
+            elif curr_dir == "l":
+                for j in range(max_col, min_col - 1, -1):
+                    res.append(matrix[curr_i][j])
+                dfs(curr_i, j, "u", min_row, max_row - 1, min_col, max_col)
+            elif curr_dir == "u":
+                for i in range(max_row, min_row - 1, -1):
+                    res.append(matrix[i][curr_j])
+                dfs(i, curr_j, "r", min_row, max_row, min_col + 1, max_col)        
+        
+        
         if not matrix or not matrix[0]:
             return []
         m, n = len(matrix), len(matrix[0])
         res = []
-        self.next_dir = {"right": "down", "down": "left", "left": "up", "up": "right"}
-        self._helper(matrix, 0, 0, [0, m - 1], [0, n - 1], "right", res)
+        dfs(0, 0, "r", 0, m - 1, 0, n - 1)
         return res
-    
-    def _helper(self, matrix, curr_row, curr_col, curr_row_range, curr_col_range, curr_dir, res):
-        """
-        需要传入的参数有，当前的位置，当前的row_range and col_range, 当前的方向
-        """
-        if len(res) == len(matrix) * len(matrix[0]):
-            return 
-        
-        row_start, row_end = curr_row_range[0], curr_row_range[1]
-        col_start, col_end = curr_col_range[0], curr_col_range[1]
-        
-        if curr_dir == "right":
-            print(curr_row, col_start, col_end)
-            res += matrix[curr_row][col_start:col_end + 1]
-            curr_row += 1
-            curr_col = col_end
-            curr_dir = self.next_dir[curr_dir]
-            self._helper(matrix, curr_row, curr_col, [curr_row, row_end], curr_col_range, curr_dir, res)
-        
-        elif curr_dir == "down":
-            res += [matrix[row][curr_col] for row in range(row_start, row_end + 1)]
-            curr_col -= 1
-            curr_row = row_end
-            curr_dir = self.next_dir[curr_dir]
-            self._helper(matrix, curr_row, curr_col, curr_row_range, [col_start, curr_col], curr_dir, res)
-            
-        elif curr_dir == "left":
-            res += matrix[curr_row][col_start:col_end + 1][::-1]
-            curr_row -= 1
-            curr_col = col_start
-            curr_dir = self.next_dir[curr_dir]
-            self._helper(matrix, curr_row, curr_col, [row_start, curr_row], curr_col_range, curr_dir, res)
-        
-        elif curr_dir == "up":
-            res += [matrix[row][curr_col] for row in range(row_start, row_end + 1)][::-1]
-            curr_col += 1
-            curr_row = row_start
-            curr_dir = self.next_dir[curr_dir]
-            self._helper(matrix, curr_row, curr_col, curr_row_range, [curr_col, col_end], curr_dir, res)
