220ffd1b7bdb21ce4bc895223fcfa2c78896f3c0
Jim0607 <jinjun.ding@gmail.com>
2020-10-02 11:52:40 -0700

    Update Pramp__070820-Busiest-Time-in-The-Mall.py

--- Solutions/Pramp__070820-Busiest-Time-in-The-Mall.py
+++ Solutions/Pramp__070820-Busiest-Time-in-The-Mall.py
@@ -1,10 +1,16 @@
+"""
 Busiest Time in The Mall
 
-The Westfield Mall management is trying to figure out what the busiest moment at the mall was last year. You’re given data extracted from the mall’s door detectors. Each data point is represented as an integer array whose size is 3. The values at indices 0, 1 and 2 are the timestamp, the count of visitors, and whether the visitors entered or exited the mall (0 for exit and 1 for entrance), respectively. Here’s an example of a data point: [ 1440084737, 4, 0 ].
+The Westfield Mall management is trying to figure out what the busiest moment at the mall was last year. 
+You’re given data extracted from the mall’s door detectors. Each data point is represented as an integer array whose size is 3. 
+The values at indices 0, 1 and 2 are the timestamp, the count of visitors, and whether the visitors entered or exited the mall (0 for exit and 1 for entrance), respectively. 
+Here’s an example of a data point: [ 1440084737, 4, 0 ].
 
-Note that time is given in a Unix format called Epoch, which is a nonnegative integer holding the number of seconds that have elapsed since 00:00:00 UTC, Thursday, 1 January 1970.
+Note that time is given in a Unix format called Epoch, 
+which is a nonnegative integer holding the number of seconds that have elapsed since 00:00:00 UTC, Thursday, 1 January 1970.
 
-Given an array, data, of data points, write a function findBusiestPeriod that returns the time at which the mall reached its busiest moment last year. The return value is the timestamp, e.g. 1480640292. Note that if there is more than one period with the same visitor peak, return the earliest one.
+Given an array, data, of data points, write a function findBusiestPeriod that returns the time at which the mall reached its busiest moment last year. 
+The return value is the timestamp, e.g. 1480640292. Note that if there is more than one period with the same visitor peak, return the earliest one.
 
 Assume that the array data is sorted in an ascending order by the timestamp. Explain your solution and analyze its time and space complexities.
 
@@ -30,12 +36,11 @@ Constraints:
 
 1 ≤ data.length ≤ 100
 [output] integer
-
+"""
 
 
 """
 finding the busiest people is find the largest number in the presum list
-"""
 """
 def find_busiest_period(data):
     curr_num = data[0][1] if data[0][2] == 1 else -data[0][1]
@@ -44,7 +49,7 @@ def find_busiest_period(data):
     i = 1
     while i < len(data):
         temp_num = curr_num
-        while i < len(data) and data[i][0] == data[i-1][0]:  # merge the data that has hte same time
+        while i < len(data) and data[i][0] == data[i-1][0]:  # merge the data that has the same time
             temp_num = temp_num + data[i][1] if data[i][2] == 1 else temp_num - data[i][1]
             i += 1
         curr_num = temp_num
