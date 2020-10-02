9c503194a81ab0598445f05dc75504b83bde4dc4
Jim0607 <jinjun.ding@gmail.com>
2020-10-02 11:49:35 -0700

    Update 0605.Can-Place-Flowers.py

--- Solutions/0605.Can-Place-Flowers.py
+++ Solutions/0605.Can-Place-Flowers.py
@@ -1,8 +1,11 @@
+"""
 605. Can Place Flowers
 
-Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+Suppose you have a long flowerbed in which some of the plots are planted and some are not. 
+However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
 
-Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, 
+return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
 
 Example 1:
 Input: flowerbed = [1,0,0,0,1], n = 1
@@ -14,6 +17,7 @@ Note:
 The input array won't violate no-adjacent-flowers rule.
 The input array size is in the range of [1, 20000].
 n is a non-negative integer which won't exceed the input array size.
+"""
 
 
 """
@@ -32,3 +36,31 @@ class Solution:
                 return True
             
         return False
+    
+    
+    
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        if len(flowerbed) == 1:
+            return True if flowerbed[0] == 0 and n <= 1 else False
+        
+        cnt = 0
+        if flowerbed[0] == 0 and flowerbed[1] == 0:
+            cnt += 1
+            flowerbed[0] = 1
+        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+            cnt += 1
+            flowerbed[-1] = 1
+        
+        i = 1
+        while i < len(flowerbed) - 1:
+            if flowerbed[i] == 0:
+                j = i
+                while j < len(flowerbed) - 1 and flowerbed[j] == 0:
+                    j += 1
+                cnt += (j - i - 1) // 2
+                i = j
+            i += 1
+        return n <= cnt
