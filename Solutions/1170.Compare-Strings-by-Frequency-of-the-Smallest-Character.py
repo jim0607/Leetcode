3f561d188c28e24fd80f494d96668bd9cb52a4f0
Jim0607 <jinjun.ding@gmail.com>
2020-10-11 17:10:28 -0700

    Create 1170.Compare-Strings-by-Frequency-of-the-Smallest-Character.py

---
+++ Solutions/1170.Compare-Strings-by-Frequency-of-the-Smallest-Character.py
@@ -0,0 +1,58 @@
+"""
+1170. Compare Strings by Frequency of the Smallest Character
+
+Let's define a function f(s) over a non-empty string s, 
+which calculates the frequency of the smallest character in s. 
+For example, if s = "dcce" then f(s) = 2 because the smallest character is "c" and its frequency is 2.
+
+Now, given string arrays queries and words, return an integer array answer, 
+where each answer[i] is the number of words such that f(queries[i]) < f(W), where W is a word in words.
+
+Example 1:
+
+Input: queries = ["cbd"], words = ["zaaaz"]
+Output: [1]
+Explanation: On the first query we have f("cbd") = 1, f("zaaaz") = 3 so f("cbd") < f("zaaaz").
+Example 2:
+
+Input: queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
+Output: [1,2]
+Explanation: On the first query only f("bbb") < f("aaaa"). 
+On the second query both f("aaa") and f("aaaa") are both > f("cc").
+ 
+Constraints:
+
+1 <= queries.length <= 2000
+1 <= words.length <= 2000
+1 <= queries[i].length, words[i].length <= 10
+queries[i][j], words[i][j] are English lowercase letters.
+"""
+
+
+
+"""
+step 1: calculate the f(q) for each q in queries f_q, and f(w) for each w in words f_w.
+step 2: sort the f_w.
+step 3: ieterate queries and do binary search in f_w to update res.
+"""
+class Solution:
+    def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        f_q = self._freq_smallest_ch(queries)
+        w_q = self._freq_smallest_ch(words)
+        
+        w_q.sort()
+        
+        res = []
+        for freq in f_q:
+            idx = bisect.bisect_right(w_q, freq)
+            res.append(len(w_q) - idx)
+        return res
+    
+    def _freq_smallest_ch(self, words):
+        res = []
+        for word in words:
+            cnter = collections.Counter(word)
+            smallest_ch = min(word)
+            smallest_cnt = cnter[smallest_ch]
+            res.append(smallest_cnt)
+        return res
