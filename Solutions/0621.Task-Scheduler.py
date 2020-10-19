13ab1bec05eeabccc530046b496cfe937492bd0f
Jim0607 <jinjun.ding@gmail.com>
2020-10-19 12:24:40 -0700

    Update 0621.Task-Scheduler.py

--- Solutions/0621.Task-Scheduler.py
+++ Solutions/0621.Task-Scheduler.py
@@ -1,8 +1,11 @@
+"""
 621. Task Scheduler
 
-Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks. Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
+Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks. 
+Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
 
-However, there is a non-negative cooling interval n that means between two same tasks, there must be at least n intervals that CPU are doing different tasks or just be idle.
+However, there is a non-negative cooling interval n that means between two same tasks, 
+there must be at least n intervals that CPU are doing different tasks or just be idle.
 
 You need to return the least number of intervals the CPU will take to finish all the given tasks.
 
@@ -11,9 +14,12 @@ Example:
 Input: tasks = ["A","A","A","B","B","B"], n = 2
 Output: 8
 Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+"""
 
-
-
+"""
+always put the high freq first, and 2nd highest freq until we hit n seats.
+in order to dynmically update the freq, we maintain a maxhq.
+"""
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         # 题目真他妈难理解
