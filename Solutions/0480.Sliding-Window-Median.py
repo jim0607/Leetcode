d54fb3da2d2f854b7a9be91563403f5e6b5277e8
Jim0607 <jinjun.ding@gmail.com>
2020-10-19 19:13:47 -0700

    Update 0480.Sliding-Window-Median.py

--- Solutions/0480.Sliding-Window-Median.py
+++ Solutions/0480.Sliding-Window-Median.py
@@ -25,36 +25,32 @@ Therefore, return the median sliding window as [1,-1,-1,3,5,6].
 """
 
 """
-Solution 1: maitain a sorted window.  We can use binary search for remove and insert. 
-the overall time complexity is O(nk), because insert takes O(k).
-Solution 2: similar with LC 295, we need to maintain two heaps in the window, leftHq and rightHq. 
+Solution 1:
+maitain a sorted window. We can use binary search for remove and insert.
+Because insert takes O(k), the overall time complexity is O(nk).
+
+Solution 2:
+similar with LC 295, we need to maintain two heaps in the window, leftHq and rightHq.
 To slide one step is actually to do two things: step 1. add a number, which is exactly the same as that in LC 295, which is O(logk)
-step 2. remove the number that is outside the window; there is not a remove method in heapq, so it takes O(k).
-Solution 3: use a SortedList structure, which was implemented using self-balanced tree.  
-SortedList enables O(logk) add and remove.  So the total time complexity is O(nlogk) 
+step 2. remove the number that is outside the window; there is not a remove method in heapq, so it takes O(k). So overall the heapq solution will take O(nk).
+
+Solution 3:
+use a SortedList structure, which was implemented using self-balanced tree.
+SortedList enables O(logk) add and O(logk) remove. So the total time complexity is O(nlogk).
 """
 
 from sortedcontainers import SortedList
 
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        odd = k % 2 == 1    # there are odd numbers of items in the window
-        
-        lst = SortedList()  # maintain a sorted list
-        
+        lst = SortedList()    # maintain a sorted list
         
         res = []
-        for num in nums[:k]:    # step 1: 把前k个数先放进去
-            lst.add(num)
-        median = lst[k//2] if odd else (lst[k//2-1] + lst[k//2]) / 2
-        res.append(median)
-        
-        i = k
-        while i < len(nums):
-            lst.remove(nums[i-k])   # 如果用heapq的话这里是O(k)的, 用sortedList是O(logk)
-            lst.add(nums[i])
-            median = lst[k//2] if odd else (lst[k//2-1] + lst[k//2]) / 2
-            res.append(median)
-            i += 1
-            
+        for i in range(len(nums)):
+            lst.add(nums[i])            # O(logk)
+            if len(lst) > k:
+                lst.remove(nums[i-k])   # if we use heapq here, it takes O(k) here, but for sortedList, it takes O(logk)
+            if len(lst) == k:
+                median = lst[k//2] if k%2 == 1 else (lst[k//2-1] + lst[k//2]) / 2
+                res.append(median)
         return res
