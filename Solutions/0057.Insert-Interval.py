abc0e67937206f1ff579eddbfff94ff8fb5478d9
Jim0607 <jinjun.ding@gmail.com>
2020-10-19 22:06:50 -0700

    Update 0057.Insert-Interval.py

--- Solutions/0057.Insert-Interval.py
+++ Solutions/0057.Insert-Interval.py
@@ -1,3 +1,4 @@
+"""
 57. Insert Interval
 
 Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
@@ -13,11 +14,11 @@ Example 2:
 Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 Output: [[1,2],[3,10],[12,16]]
 Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
-
+"""
 
 
 """
-Append the new interval to the intervals, and then do the merge interval problem
+Append the new interval to the intervals, and then do the 56. merge interval problem
 """
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
@@ -32,7 +33,6 @@ class Solution:
                 merged.append(interval)
             else:
                 merged[-1][-1] = max(res[-1][-1], interval[1])
-        
         return merged
     
     
