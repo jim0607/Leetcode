0550570e9d5e7cd3e1b79f0bc5db80170e890f9a
Jim0607 <jinjun.ding@gmail.com>
2020-10-20 12:10:56 -0700

    Update 0547.Friend-Circles.py

--- Solutions/0547.Friend-Circles.py
+++ Solutions/0547.Friend-Circles.py
@@ -1,8 +1,13 @@
+"""
 547. Friend Circles
 
-There are N students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. And we defined a friend circle is a group of students who are direct or indirect friends.
+There are N students in a class. Some of them are friends, while some are not. 
+Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. 
+And we defined a friend circle is a group of students who are direct or indirect friends.
 
-Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, then the ith and jth students are direct friends with each other, otherwise not. And you have to output the total number of friend circles among all the students.
+Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, 
+then the ith and jth students are direct friends with each other, 
+otherwise not. And you have to output the total number of friend circles among all the students.
 
 Example 1:
 Input: 
@@ -20,9 +25,7 @@ Input:
 Output: 1
 Explanation:The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends, 
 so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.
-
-
-
+"""
 
 
 
