08988659af70c100c90f9b41d3b8f044cffbe3b4
Jim0607 <jinjun.ding@gmail.com>
2020-10-20 17:17:41 -0700

    Update 0297.Serialize-and-Deserialize-Binary-Tree.py

--- Solutions/0297.Serialize-and-Deserialize-Binary-Tree.py
+++ Solutions/0297.Serialize-and-Deserialize-Binary-Tree.py
@@ -46,14 +46,6 @@
 # 
 #
 
-# @lc code=start
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
