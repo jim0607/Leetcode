5b54cf5a07a8fa8094d24dfd56493c5bcf065f60
Jim0607 <jinjun.ding@gmail.com>
2020-10-23 12:40:19 -0700

    Update 0146.LRU-Cache.py

--- Solutions/0146.LRU-Cache.py
+++ Solutions/0146.LRU-Cache.py
@@ -81,7 +81,7 @@ class LRUCache:
 
     def put(self, key: int, value: int) -> None:
         self.cache[key] = value
-        self.cache.move_to_end(key)
+        self.cache.move_to_end(key)         
         if len(self.cache) > self.capacity:
             self.cache.popitem(last = False)
 
