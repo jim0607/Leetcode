d902f4095fbf96d301cb8762986869f49cdd3a33
Jim0607 <jinjun.ding@gmail.com>
2020-10-23 16:10:30 -0700

    Update 0460.LFU-Cache.py

--- Solutions/0460.LFU-Cache.py
+++ Solutions/0460.LFU-Cache.py
@@ -4,10 +4,12 @@
 Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, 
+it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, 
+when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
 
-Note that the number of times an item is used is the number of calls to the get and put functions for that item since it was inserted. This number is set to zero when the item is removed.
-
+Note that the number of times an item is used is the number of calls to the get and put functions for that item since it was inserted. 
+This number is set to zero when the item is removed.
 
 Follow up:
 Could you do both operations in O(1) time complexity?
@@ -85,79 +87,108 @@ class LFUCache:
         freq += 1                               
         self.key_to_freq[key] = freq            # then update key_to_freq and freq_to_orddict using the new freq
         self.freq_to_orddict[freq][key] = value
-                
 
-
-
-
-
-
-
-
-class LFUCache:
-    
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self._cache = collections.defaultdict(int)   # key is key, val is freq  
-        
-        # key is freq, val is a list of keys corresponding to the freq.  In order to maintain the list ordered, we use DLL (ordered dictionary)
-        # in the ordered dictionary, key is key, val is value corresponding to the key. We put the LRU key at the beginning of the DLL.
-        self._freqs = collections.defaultdict(lambda: collections.OrderedDict())    
-        self._min_freq = 0      # the smallest freq in the cache 
-
-    def get(self, key: int) -> int:
-        if key not in self._cache:
-            return -1
-        
-        self._update(key, None)
-        
-        return self._freqs[self._cache[key]][key]
-
-    def put(self, key: int, value: int) -> None:
-        if self.capacity == 0:
-            return None
-        
-        if key in self._cache:
-            self._update(key, value)
-        else:
-            if len(self._cache) == self.capacity:       # if cache is full, remove the LFU key
-                # del the least used key from the cache, as well as it's coreesponding freq in the ordered dictonary
-                # the leased frequenctly used num should have min freq
-                
-                remove_key = self._freqs[self._min_freq].popitem(last = False)[0]    # ordered dictionary pop a key value pair (key, val) from the beginning of the DLL: .popitem(last = False)
-                del self._cache[remove_key]                                          # pop出来的是一个(key, value) pair
-                
-            # add the new key
-            self._min_freq = 1
-            self._cache[key] = 1
-            self._freqs[1][key] = value    # OrderedDict添加到末尾, 新放进去的都默认放到最后面
-            
-    def _update(self, key, value):
-        """
-        1. update freq, freq+=1
-        2. update val 
-        3. update min_freq if neccessarry
-        """
-        freq = self._cache[key] 
-        val = self._freqs[freq].pop(key)    # remove current key and get the val - pop(key) is O(1) for OrderedDict
-        
-        if self._min_freq == freq and not self._freqs[freq]:        # 3. update min_freq if neccessarry
-            self._min_freq = freq + 1    
-            
-        freq += 1
-        self._cache[key] = freq       # 1. update freq, freq+=1
-        self._freqs[freq][key] = value if value != None else val        # 2. update val - OrderedDict添加到末尾
-
-        
 
 """
 非常常考的题套路就是先给一个题求Kth largest, 然后follow up是data stream, 这个问题的solution 是LFU - O(1)
-July 2020 的Google 面经就有:
-Given logs from a YouTube service, and an Integer K. Return K most searched strings from the logs. 
-Discussed creating map of unique strings and counts and going over the map to find the K strings with highest counts by sorting the unique strings by their count. 
-Talked about improving the sort from O(NlogN) to O(NlogK) by using a min heap to sort.
-Heap O(NlogK)
-Quick Select O(N)
-Follow ups: How would you change your code for a stream of logs, Interviewer said that my points and solution were acceptable.
-Solution: LFU - O(1)
+July 2020 的Google 面经: Given logs from a YouTube service, and an Integer K. Return K most searched strings from the logs.
+Solution: Heap O(NlogK), or Quick Select O(N).
+
+Follow ups: How would you change your code for a stream of logs?
+Let's re-state the problem: 
+Implement the TopKFreq class:
+TopKFreq(K): Initializes the object with the capacity of the data structure.
+int get(): return the K most frequently searched strings in a data stream
+void put(s): Sets or inserts the value if the s is not already present. When the cache reaches its capacity, 
+it should invalidate the least frequently used item before inserting a new item. For this problem, 
+when there is a tie (i.e., two or more keys with the same frequency), the least recently used key would be evicted. 
+When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item.
 """
+class TopKFreq:
+
+    def __init__(self, K):
+        self.K = K   # keep the size of str_to_freq to be K, so that all the strings in str_to_freq are top K frequently searched
+        self.str_to_freq = defaultdict(int)
+        self.freq_to_set(OrderedSet)        # 这里只需要OrderedSet, 不需要ordered dictionary, 因为key是string, 没有val
+        self.min_freq = 0
+
+    def search(self, s):
+        if s in self.str_to_freq:
+            freq = self.str_to_freq[s]
+
+            if freq == self.min_freq and len(self.freq_to_set[freq]) == 1:
+                self.min_freq += 1
+
+            self.freq_to_set[freq].remove(s)  # since we need to remove, we cannot just use a list, instead, we have to use OrderedSet (actually DLL)
+            freq += 1
+            self.str_to_freq[s] = freq
+            self.freq_to_set[freq].insert_to_end(s)
+
+        else:
+            # When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item
+            if len(self.str_to_freq) == self.K:
+                min_s = self.freq_to_set[self.min_freq].pop_first()
+                del self.str_to_freq[min_s]
+            self.min_freq = 1
+            self.str_to_freq[s] = 1
+            self.freq_to_set[1].append(s)
+
+    def get(self):
+        """
+        Return the K most frequently searched strings. Here we don't update frequency.
+        """
+        return [s for s in self.str_to_freq]
+
+
+def DLLNode:
+
+    def __init__(self, key):
+        self.key = key
+        self.prev = None
+        self.next = None
+
+
+def OrderedSet:
+
+    def __init__(self):
+        """
+        实现OrderedSet需要两个数据结构：
+        1. A DLL, so that we can move a node to end, and pop first node in O(1) time
+        2. A dictionary, for fast key --> node look up
+        """
+        self.key_to_node = defaultdict(DLLNode)
+
+        self.dummy_head = DLLNode(-1, -1)
+        self.dummy_tail = DLLNode(-1, -1)
+        self.dummy_head.next = self.dummy_tail
+        self.dummy_tail.prev = self.dummy_head
+
+    def remove(self, key):
+        """
+        Remove an already existed node from the DDL
+        """
+        node = self.key_to_node[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        del self.key_to_node[key]
+
+    def insert_to_end(self, key):
+        """
+        Insert a new node to the end of the DDL
+        """
+        new_node = DLLNode(key)
+        self.key_to_node[key] = new_node
+
+        self.dummy_tail.prev.next = new_node
+        new_node.prev = self.dummy_tail.prev
+        new_node.next = self.dummy_tail
+        self.dummy_tail.prev = new_node
+
+    def pop_first(self, key):
+        """
+        Pop the first node in the DLL (return the key)
+        """
+        first_node = self.dummy_head.next
+        first_node_key = first_node.key
+        self.remove_node(first_node_key)
+        return first_node_key
