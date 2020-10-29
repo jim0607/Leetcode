0a22ebe061b5021903da5a5bb84e9a5ccb741048
Jim0607 <jinjun.ding@gmail.com>
2020-10-28 21:29:17 -0700

    Create Google__similar_with_0674.Longest-Continuous-Increasing-Subsequence.py

---
+++ Solutions/Google__similar_with_0674.Longest-Continuous-Increasing-Subsequence.py
@@ -0,0 +1,17 @@
+"""
+Given a string str and a string t where t represents a custom alphabetical order of lowercase letters, 
+find the length of the longest sorted substring in s. 
+t: [a-z] permuation
+s: lower case string, 
+	Input: s = "pierywq", t = "qwertyuiopasdfghjklzxcvbnm"
+
+Output: "ery"
+	             i
+	p i e r y w q
+	             J
+	Solution 1: sliding window
+	Solution 2: dp[i] = the longest lens ended with s[i].
+	 Dp[i] = 0 if hashmap[s[i]] < hashmap[s[i-1]] else dp[i-1] + 1
+Returtn max(dp)
+---> O(1) space 
+"""
