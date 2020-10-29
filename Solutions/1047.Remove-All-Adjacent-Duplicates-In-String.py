6a3582591dedf203e81a87753f2188f0c8624f52
Jim0607 <jinjun.ding@gmail.com>
2020-10-28 21:15:24 -0700

    Update 1047.Remove-All-Adjacent-Duplicates-In-String.py

--- Solutions/1047.Remove-All-Adjacent-Duplicates-In-String.py
+++ Solutions/1047.Remove-All-Adjacent-Duplicates-In-String.py
@@ -1,3 +1,4 @@
+"""
 1047. Remove All Adjacent Duplicates In String
 
 Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
@@ -11,7 +12,9 @@ Example 1:
 Input: "abbaca"
 Output: "ca"
 Explanation: 
-For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  
+The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+"""
 
 
 """
