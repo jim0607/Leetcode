160d0780c065760002310713627d1d90d29edc80
Jim0607 <jinjun.ding@gmail.com>
2020-10-28 21:28:11 -0700

    Update 0674.Longest-Continuous-Increasing-Subsequence.py

--- Solutions/0674.Longest-Continuous-Increasing-Subsequence.py
+++ Solutions/0674.Longest-Continuous-Increasing-Subsequence.py
@@ -1,3 +1,4 @@
+"""
 674. Longest Continuous Increasing Subsequence
 
 Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
@@ -7,6 +8,7 @@ Input: [1,3,5,4,7]
 Output: 3
 Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. 
 Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4. 
+"""
 
 
 """给定一个序列，求子序列的最大值问题：典型的坐标型动态规划问题
