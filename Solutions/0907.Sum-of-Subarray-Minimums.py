731a15c706f720428164e46c8480929cbf75d34d
Jim0607 <jinjun.ding@gmail.com>
2020-10-29 20:31:39 -0700

    Update 0907.Sum-of-Subarray-Minimums.py

--- Solutions/0907.Sum-of-Subarray-Minimums.py
+++ Solutions/0907.Sum-of-Subarray-Minimums.py
@@ -1,10 +1,9 @@
+"""
 907. Sum of Subarray Minimums
 
 Given an array of integers A, find the sum of min(B), where B ranges over every (contiguous) subarray of A.
 
 Since the answer may be large, return the answer modulo 10^9 + 7.
-
- 
 
 Example 1:
 
@@ -12,7 +11,7 @@ Input: [3,1,2,4]
 Output: 17
 Explanation: Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
 Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.  Sum is 17.
-
+"""
 
 
 """
