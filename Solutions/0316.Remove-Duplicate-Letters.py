e539742fa899cef171468855a917a7a77e43a8a1
Jim0607 <jinjun.ding@gmail.com>
2020-10-29 20:32:13 -0700

    Update 0316.Remove-Duplicate-Letters.py

--- Solutions/0316.Remove-Duplicate-Letters.py
+++ Solutions/0316.Remove-Duplicate-Letters.py
@@ -1,6 +1,8 @@
+"""
 316. Remove Duplicate Letters
 
-Given a string which contains only lowercase letters, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+Given a string which contains only lowercase letters, remove duplicate letters so that every letter appears once and only once. 
+You must make sure your result is the smallest in lexicographical order among all possible results.
 
 Example 1:
 
@@ -10,7 +12,7 @@ Example 2:
 
 Input: "cbacdcbc"
 Output: "acdb"
-
+"""
 
 
 """
