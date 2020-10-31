98a6f7d7874008d97a73aa563353758a258e0b6d
Jim0607 <jinjun.ding@gmail.com>
2020-10-31 16:58:55 -0700

    Update 0437.Path-Sum-III.py

--- Solutions/0437.Path-Sum-III.py
+++ Solutions/0437.Path-Sum-III.py
@@ -1,3 +1,4 @@
+"""
 You are given a binary tree in which each node contains an integer value.
 
 Find the number of paths that sum to a given value.
@@ -23,6 +24,7 @@ Return 3. The paths that sum to 8 are:
 1.  5 -> 3
 2.  5 -> 2 -> 1
 3. -3 -> 11
+"""
 
 
 """
