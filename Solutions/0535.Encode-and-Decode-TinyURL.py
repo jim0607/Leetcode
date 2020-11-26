f876639adf7f42d32191cd14291a19e04610c192
Jim0607 <jinjun.ding@gmail.com>
2020-11-26 14:15:30 -0800

    Update 0535.Encode-and-Decode-TinyURL.py

--- Solutions/0535.Encode-and-Decode-TinyURL.py
+++ Solutions/0535.Encode-and-Decode-TinyURL.py
@@ -13,7 +13,7 @@ You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL
 
 
 """
-Convert long url to short url via hashing. 
+Convert long url to short url via hashing of the long url use hash function. 
 Look up long url from short url in hash table.
 """
 class Codec:
@@ -21,16 +21,18 @@ class Codec:
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        self.mapping = collections.defaultdict(str)  # short_url --> long_url
-        key = str(hash(longUrl))    # hash(str) returns the hash_code for the str
-        self.mapping[key] = longUrl
-        shortUrl = "http://tinyurl.com/" + key
+        self.mapping = defaultdict(str)     # hash_code --> longUrl
+        hash_code = hash(longUrl)
+        self.mapping[hash_code] = longUrl   # hash(str) returns the hash_code for the str
+        shortUrl = "http://tinyurl.com/" + str(hash_code)
         return shortUrl
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return self.mapping[shortUrl.split("/")[-1]]
+        hash_code = int(shortUrl.split("/")[-1])
+        longUrl = self.mapping[hash_code]
+        return longUrl
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
