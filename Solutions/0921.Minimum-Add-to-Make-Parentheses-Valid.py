4cfa0f54bc3c8d741d71c322b3115814c9278840
Jim0607 <jinjun.ding@gmail.com>
2020-11-27 12:20:54 -0800

    Update 0921.Minimum-Add-to-Make-Parentheses-Valid.py

--- Solutions/0921.Minimum-Add-to-Make-Parentheses-Valid.py
+++ Solutions/0921.Minimum-Add-to-Make-Parentheses-Valid.py
@@ -1,3 +1,4 @@
+"""
 921. Minimum Add to Make Parentheses Valid
 
 Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
@@ -47,25 +48,22 @@ class Solution:
         add_cnt += len(st)     # for case ")))(("
         
         return add_cnt
- 
+"""
+
  
 """
-借鉴32. Longest Valid Parentheses的做法：从左往右扫描，记录left和right，
-如果right大于left了,就表明前面需要添加左括号
+简化一下，就用left_cnt代替stack
 """
 class Solution:
-    def minAddToMakeValid(self, S: str) -> int:
-        left, right = 0, 0
+    def minAddToMakeValid(self, s: str) -> int:
+        left_cnt = 0
         add_cnt = 0
-        for ch in S:
+        for ch in s:
             if ch == "(":
-                left += 1
-            elif ch == ")":
-                right += 1
-            if right > left:
-                add_cnt += 1
-                left += 1
-            
-        add_cnt += left - right     # for case ")))(("
-        
-        return add_cnt
+                left_cnt += 1
+            else:
+                if left_cnt == 0:
+                    add_cnt += 1
+                else:
+                    left_cnt -= 1
+        return add_cnt + left_cnt
