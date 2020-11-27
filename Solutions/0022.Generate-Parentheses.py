8d8c2a9db11371d6e2c0f16fcfc2d0ef1cae1f85
Jim0607 <jinjun.ding@gmail.com>
2020-11-27 11:21:05 -0800

    Update 0022.Generate-Parentheses.py

--- Solutions/0022.Generate-Parentheses.py
+++ Solutions/0022.Generate-Parentheses.py
@@ -1,3 +1,4 @@
+"""
 22. Generate Parentheses
 
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
@@ -11,25 +12,24 @@ For example, given n = 3, a solution set is:
   "()(())",
   "()()()"
 ]
+"""
 
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         self.res = []
-        self.backtrack(n, 0, 0, "")
-        
+        self._backtrack(n, 0, 0, "")
         return self.res
     
-    def backtrack(self, n, leftCnt, rightCnt, curr):
-        if leftCnt == n and rightCnt == n:
-            self.res.append(curr)   # string doesn't need deep copy because it is immutable
+    def _backtrack(self, n, left_cnt, right_cnt, curr_comb):
+        if left_cnt == right_cnt == n:
+            self.res.append(curr_comb)  # string doesn't need deep copy because it is immutable
             return
         
-        if leftCnt < rightCnt:  # 这个判断尤为关键！
+        if left_cnt < right_cnt:        # 这个判断尤为关键 - strong pruning
             return
         
-        if leftCnt < n:
-            self.backtrack(n, leftCnt + 1, rightCnt, curr + "(")
-            
-        if rightCnt < n:
-            self.backtrack(n, leftCnt, rightCnt + 1, curr + ")")
+        if left_cnt < n:
+            self._backtrack(n, left_cnt + 1, right_cnt, curr_comb + "(")
+        if right_cnt < n:
+            self._backtrack(n, left_cnt, right_cnt + 1, curr_comb + ")")
