7e58f5f32864cb369c04939ecd01fc9f1d34abea
Jim0607 <jinjun.ding@gmail.com>
2020-11-27 11:42:24 -0800

    Update 0301.Remove-Invalid-Parentheses.py

--- Solutions/0301.Remove-Invalid-Parentheses.py
+++ Solutions/0301.Remove-Invalid-Parentheses.py
@@ -1,3 +1,4 @@
+"""
 301. Remove Invalid Parentheses
 
 Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
@@ -16,6 +17,7 @@ Example 3:
 
 Input: ")("
 Output: [""]
+"""
 
 
 """
@@ -25,51 +27,44 @@ solution: dfs 解法跟22. Generate parentheses是一样的，给你这么多括
 
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        
-        # idx is the idx in s, left, right are how many "(" and how many ")" are there in curr_path
-        # left_remain is how many "(" remaining so that we can take from s to put into curr_path
-        # curr_path is the valid parenethes we want to build
-        def dfs(start_idx, left, right, left_remain, right_remain, curr_path):
-            if start_idx == len(s) - 1 and left == right and left_remain == right_remain == 0:
-                res.add(curr_path)
-                return
-
-            if left_remain < 0 or right_remain < 0:
-                return
-            if left < right:    # 借鉴了22. Generate parentheses
+        def backtrack(curr_idx, left_cnt, right_cnt, curr_comb):
+            if curr_idx == len(s) - 1 and left_cnt == right_cnt:
+                res.add(curr_comb)
                 return
             
-            for idx in range(start_idx + 1, len(s)):
-                if s[idx] == "(":       
-                    dfs(idx, left + 1, right, left_remain - 1, right_remain, curr_path + "(")   # add "(" to result
-                    dfs(idx, left, right, left_remain - 1, right_remain, curr_path)             # not add "(" to result
-
-                elif s[idx] == ")":
-                    dfs(idx, left, right + 1, left_remain, right_remain - 1, curr_path + ")")   # add ")" to result
-                    dfs(idx, left, right, left_remain, right_remain - 1, curr_path)             # not add ")" to result
-
-                else:                                               # if it is a letter, then simply put it into curr
-                    dfs(idx, left, right, left_remain, right_remain, curr_path + s[idx])        # add "a" to result
+            if left_cnt > left_total or right_cnt > right_total:
+                return
+            if left_cnt < right_cnt:
+                return
+            
+            for next_idx in range(curr_idx + 1, len(s)):
+                if s[next_idx] == "(":
+                    backtrack(next_idx, left_cnt, right_cnt, curr_comb)             # not add "(" to result
+                    backtrack(next_idx, left_cnt + 1, right_cnt, curr_comb + "(")   # add "(" to result
+                elif s[next_idx] == ")":
+                    backtrack(next_idx, left_cnt, right_cnt, curr_comb)
+                    backtrack(next_idx, left_cnt, right_cnt + 1, curr_comb + ")")
+                else:                                  # if it is a letter, then simply put it into curr    
+                    backtrack(next_idx, left_cnt, right_cnt, curr_comb + s[next_idx])
                 
-        res = set()
-        
-        left_cnt, right_cnt = 0, 0
+          
+        left_total, right_total = 0, 0
         for ch in s:
             if ch == "(":
-                left_cnt += 1
+                left_total += 1
             elif ch == ")":
-                right_cnt += 1
-                
-        dfs(-1, 0, 0, left_cnt, right_cnt, "")
+                right_total += 1
         
-        max_lens = max(len(s) for s in res)
+        res = set()
+        backtrack(-1, 0, 0, "")
+        
         ans = []
-        for s in res:
-            if len(s) == max_lens:
-                ans.append(s)
-                
+        max_len = max(len(comb) for comb in res)
+        for comb in res:
+            if len(comb) == max_len:
+                ans.append(comb)
         return ans
-
+    
 
 
 """
