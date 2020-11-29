805b72f53f509b3a2d0f146bad25b8889a289b3c
Jim0607 <jinjun.ding@gmail.com>
2020-11-28 17:29:55 -0800

    Update 0456.132-Pattern.py

--- Solutions/0456.132-Pattern.py
+++ Solutions/0456.132-Pattern.py
@@ -1,6 +1,9 @@
+"""
 456. 132 Pattern
 
-Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak such that i < j < k and ai < ak < aj. Design an algorithm that takes a list of n numbers as input and checks whether there is a 132 pattern in the list.
+Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak 
+such that i < j < k and ai < ak < aj. Design an algorithm that takes a list of n numbers as 
+input and checks whether there is a 132 pattern in the list.
 
 Note: n will be less than 15,000.
 
@@ -22,12 +25,12 @@ Input: [-1, 3, 2, 0]
 Output: True
 
 Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+"""
 
 
 
 """
 solution: 从右往左，维护一个单调递减栈，while loop是为了保证选出一个最接近num的two, 这样num作为three, 下一个进来的num就更容易小于two了！
-"""
 """
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
