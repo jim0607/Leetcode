9ee002cf0522e0eeaabea5d142f0c806f2792ebb
Jim0607 <jinjun.ding@gmail.com>
2020-11-30 20:40:45 -0800

    Update 0724.Find-Pivot-Index.py

--- Solutions/0724.Find-Pivot-Index.py
+++ Solutions/0724.Find-Pivot-Index.py
@@ -72,3 +72,17 @@ class Solution:
             if pre_sum[i-1] == pre_sum[-1] - pre_sum[i]:
                 return i - 1
         return -1
+
+    
+    
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        right_sum = sum(nums)
+        for i in range(len(nums)):
+            left_sum += nums[i]
+            if i > 0:
+                right_sum -= nums[i-1]
+            if left_sum == right_sum:
+                return i
+        return -1
