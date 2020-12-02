5b3469cf53154bc927f45039645cf1b77419b5b0
Jim0607 <jinjun.ding@gmail.com>
2020-12-01 20:39:17 -0800

    Update 0313.Super-Ugly-Number.py

--- Solutions/0313.Super-Ugly-Number.py
+++ Solutions/0313.Super-Ugly-Number.py
@@ -1,3 +1,4 @@
+"""
 313. Super Ugly Number
 
 Write a program to find the nth super ugly number.
@@ -10,24 +11,21 @@ Input: n = 12, primes = [2,7,13,19]
 Output: 32 
 Explanation: [1,2,4,7,8,13,14,16,19,26,28,32] is the sequence of the first 12 
              super ugly numbers given primes = [2,7,13,19] of size 4.
+"""
 
 
 """
 heap solution: O(nlog(kn)) where k is the lens of primes list
 """
-
 class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        added = set()
+        added.add(1)
         hq = [1]
-        included = set()
-        included.add(1)
-        
-        for _ in range(n - 1):
-            currmin = heappop(hq)
+        for _ in range(n):
+            curr_min = heappop(hq)
             for prime in primes:
-                nextnum = currmin * prime
-                if nextnum not in included:
-                    heappush(hq, nextnum)
-                    included.add(nextnum)
-                    
-        return heappop(hq)
+                if prime * curr_min not in added:
+                    heappush(hq, prime * curr_min)
+                    added.add(prime * curr_min)
+        return curr_min
