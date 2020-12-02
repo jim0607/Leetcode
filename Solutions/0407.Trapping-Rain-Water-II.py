f7094186d749a822357f9132fa5bef07e66f1d1a
Jim0607 <jinjun.ding@gmail.com>
2020-12-01 20:58:56 -0800

    Update 0407.Trapping-Rain-Water-II.py

--- Solutions/0407.Trapping-Rain-Water-II.py
+++ Solutions/0407.Trapping-Rain-Water-II.py
@@ -1,3 +1,4 @@
+"""
 407. Trapping Rain Water II
 
 Given an m x n matrix of positive integers representing the height of each unit cell in a 2D elevation map, compute the volume of water it is able to trap after raining.
@@ -16,6 +17,8 @@ Return 4.
 The above image represents the elevation map [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]] before the rain.
 
 After the rain, water is trapped between the blocks. The total volume of water trapped is 4.
+"""
+
 
 """
 Recall 1D trapping water problem.  We are using the two pointers left and right, from each end of the array.
@@ -59,17 +62,18 @@ class Solution:
             leaking_h, (i, j) = heappop(hq)
             for delta_i, delta_j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 next_i, next_j = i + delta_i, j + delta_j
-                if 0 < next_i < m - 1 and 0 < next_j < n - 1:
-                    if (next_i, next_j) not in visited:
-                        if matrix[next_i][next_j] < leaking_h:
-                            res += leaking_h - matrix[next_i][next_j]
-                            visited.add((next_i, next_j))
-                            heappush(hq, (leaking_h, (next_i, next_j)))     # 注意这里是push进去(next_i, next_j)
-                        else:
-                            heappush(hq, (matrix[next_i][next_j], (next_i, next_j)))
-                            visited.add((next_i, next_j))
+                if 0 < next_i < m - 1 and 0 < next_j < n - 1 and (next_i, next_j) not in visited:
+                    visited.add((next_i, next_j))
+                    if matrix[next_i][next_j] < leaking_h:
+                        res += leaking_h - matrix[next_i][next_j]
+                        heappush(hq, (leaking_h, (next_i, next_j)))     # 注意这里是push进去(next_i, next_j)
+                    else:
+                        heappush(hq, (matrix[next_i][next_j], (next_i, next_j)))     
         return res
 
+      
+      
+      
 """
 稍微简洁一点的写法
 """
