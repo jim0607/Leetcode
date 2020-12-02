ed8f6462776bd9b8105dfe0e2400e5fad1b8f743
Jim0607 <jinjun.ding@gmail.com>
2020-12-02 00:06:10 -0800

    Update 0480.Sliding-Window-Median.py

--- Solutions/0480.Sliding-Window-Median.py
+++ Solutions/0480.Sliding-Window-Median.py
@@ -43,14 +43,13 @@ from sortedcontainers import SortedList
 
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        lst = SortedList()    # maintain a sorted list
-        
+        lst = SortedList()              # maintain a sorted list
         res = []
         for i in range(len(nums)):
             lst.add(nums[i])            # O(logk)
-            if len(lst) > k:
+            if i >= k:
                 lst.remove(nums[i-k])   # if we use heapq here, it takes O(k) here, but for sortedList, it takes O(logk)
-            if len(lst) == k:
-                median = lst[k//2] if k%2 == 1 else (lst[k//2-1] + lst[k//2]) / 2
+            if i >= k - 1:
+                median = lst[k//2] if k % 2 == 1 else (lst[k//2] + lst[k//2-1]) / 2
                 res.append(median)
         return res
