9b62231acc94c87a15529642de68b2e0f35f5c57
Jim0607 <jinjun.ding@gmail.com>
2020-12-03 10:14:59 -0800

    Update 0352.Data-Stream-as-Disjoint-Intervals.py

--- Solutions/0352.Data-Stream-as-Disjoint-Intervals.py
+++ Solutions/0352.Data-Stream-as-Disjoint-Intervals.py
@@ -1,3 +1,4 @@
+"""
 352. Data Stream as Disjoint Intervals
 
 Given a data stream input of non-negative integers a1, a2, ..., an, ..., summarize the numbers seen so far as a list of disjoint intervals.
@@ -10,11 +11,10 @@ For example, suppose the integers from the data stream are 1, 3, 7, 2, 6, ..., t
 [1, 3], [7, 7]
 [1, 3], [6, 7]
  
-
 Follow up:
 
 What if there are lots of merges and the number of disjoint intervals are small compared to the data stream's size?
-
+"""
 
 
 """
