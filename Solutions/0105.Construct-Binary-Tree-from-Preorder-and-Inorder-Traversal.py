0e3c0323ecb90d4271209dbe48f376caa956c6aa
Jim0607 <jinjun.ding@gmail.com>
2020-12-05 12:09:10 -0800

    Update 0105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py

--- Solutions/0105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
+++ Solutions/0105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
@@ -65,3 +65,29 @@ class Solution:
             inorder_idx[num] = i
             
         return helper(preorder_q, 0, len(inorder))
+    
+    
+    
+    
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def build(in_start, in_end):
+            if in_start >= in_end:
+                return None
+            
+            root = TreeNode(preorder[self.pre_idx])
+            self.pre_idx += 1
+            in_idx = num_idx[root.val]
+            
+            root.left = build(in_start, in_idx)
+            root.right = build(in_idx + 1, in_end)
+            
+            return root
+        
+        
+        num_idx = defaultdict(int)
+        for idx, num in enumerate(inorder):
+            num_idx[num] = idx
+        
+        self.pre_idx = 0
+        return build(0, len(inorder))
