42606ec96b4840d9e7632b9f59cf0e0c4e903e70
Jim0607 <jinjun.ding@gmail.com>
2020-12-04 17:29:35 -0800

    Update 0236.Lowest-Common-Ancestor-of-a-Binary-Tree.py

--- Solutions/0236.Lowest-Common-Ancestor-of-a-Binary-Tree.py
+++ Solutions/0236.Lowest-Common-Ancestor-of-a-Binary-Tree.py
@@ -1,6 +1,8 @@
+"""
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node 
+in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
 Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
 
@@ -20,9 +22,16 @@ Note:
 
 All of the nodes' values will be unique.
 p and q are different and both values will exist in the binary tree.
-
+"""
 
 # Time complexity O(N) 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root:
@@ -33,16 +42,15 @@ class Solution:
         if root == q:
             return q
         
-        leftLCA = self.lowestCommonAncestor(root.left, p, q)
-        rightLCA = self.lowestCommonAncestor(root.right, p, q)
+        left_LCA = self.lowestCommonAncestor(root.left, p, q)
+        right_LCA = self.lowestCommonAncestor(root.right, p, q)
         
-        # 如果左边有一个node等于p或q, and右边有一个node等于q或p
-        if leftLCA and rightLCA:    
+        if left_LCA and right_LCA:  # 如果左边有一个node等于p或q, and右边有一个node等于q或p
             return root
-        if not leftLCA:     # 左边没有node等于p或q 
-            return rightLCA
-        if not rightLCA:    # 右边没有node等于p或q 
-            return leftLCA
+        if left_LCA:
+            return left_LCA
+        if right_LCA:
+            return right_LCA
         
         
 
