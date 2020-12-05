76d2a1034e07c46013fdd1bf4e3b2899f7f47bcd
Jim0607 <jinjun.ding@gmail.com>
2020-12-05 00:01:42 -0800

    Update 0366.Find-Leaves-of-Binary-Tree.py

--- Solutions/0366.Find-Leaves-of-Binary-Tree.py
+++ Solutions/0366.Find-Leaves-of-Binary-Tree.py
@@ -1,8 +1,7 @@
+"""
 366. Find Leaves of Binary Tree
 
 Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
-
- 
 
 Example:
 
@@ -16,7 +15,6 @@ Input: [1,2,3,4,5]
 
 Output: [[4,5,3],[2],[1]]
  
-
 Explanation:
 
 1. Removing the leaves [4,5,3] would result in this tree:
@@ -25,16 +23,15 @@ Explanation:
          / 
         2          
  
-
 2. Now removing the leaf [2] would result in this tree:
 
           1          
  
-
 3. Now removing the leaf [1] would result in the empty tree:
 
           []         
 [[3,5,4],[2],[1]], [[3,4,5],[2],[1]], etc, are also consider correct answers since per each level it doesn't matter the order on which elements are returned.
+"""
 
 
 """
@@ -44,25 +41,28 @@ helper function returns the current level.
 """
 class Solution:
     def findLeaves(self, root: TreeNode) -> List[List[int]]:
-        if not root:
-            return []
-        
-        def helper(root):
+        def find_depth(root):
             if not root:
                 return 0
+            return 1 + max(find_depth(root.left), find_depth(root.right))
             
-            left_level = helper(root.left)
-            right_level = helper(root.right)
+        def dfs(root):
+            """
+            return level of the node
+            """
+            if not root:
+                return -1
             
-            root_level = max(left_level, right_level) + 1
-            levels[root_level].append(root.val)
+            left_level = dfs(root.left)
+            right_level = dfs(root.right)
+            
+            root_level = 1 + max(left_level, right_level)
+            res[root_level].append(root.val)
             
             return root_level
         
-        levels = collections.defaultdict(list)  # key is level, val is a list of nodes on the level
-        helper(root)
         
-        res = []
-        for level in sorted(levels):
-            res.append(levels[level])
+        depth = find_depth(root)
+        res = [[] for _ in range(depth)]
+        dfs(root)
         return res
