6a84addb3fd277ea9bafe9c9a81300be40569517
Jim0607 <jinjun.ding@gmail.com>
2020-12-07 11:39:49 -0800

    Update 0684.Redundant-Connection.py

--- Solutions/0684.Redundant-Connection.py
+++ Solutions/0684.Redundant-Connection.py
@@ -1,12 +1,16 @@
+"""
 684. Redundant Connection
 
 In this problem, a tree is an undirected graph that is connected and has no cycles.
 
-The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N), with one additional edge added. The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
+The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N), with one additional edge added. 
+The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
 
-The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
+The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with u < v, 
+that represents an undirected edge connecting nodes u and v.
 
-Return an edge that can be removed so that the resulting graph is a tree of N nodes. If there are multiple answers, return the answer that occurs last in the given 2D-array. The answer edge [u, v] should be in the same format, with u < v.
+Return an edge that can be removed so that the resulting graph is a tree of N nodes. 
+If there are multiple answers, return the answer that occurs last in the given 2D-array. The answer edge [u, v] should be in the same format, with u < v.
 
 Example 1:
 Input: [[1,2], [1,3], [2,3]]
@@ -22,40 +26,39 @@ Explanation: The given undirected graph will be like this:
 5 - 1 - 2
     |   |
     4 - 3
+"""
 
 
-
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind()
+        for u, v in edges:      # O(E)
+            uf.add(u)
+            uf.add(v)
+            if uf.connected(u, v):  
+                return [u, v]
+            uf.union(u, v)
+            
+        
 class UnionFind:
     
-    def __init__(self, edges):
-        self.father = collections.defaultdict()     # O(E)
-        for a, b in edges:
-            self.father[a] = a
-            self.father[b] = b
+    def __init__(self):
+        self.father = defaultdict(int)
+        
+    def add(self, x):
+        if x not in self.father:
+            self.father[x] = x
             
     def find(self, x):
         if self.father[x] == x:
             return x
-        
         self.father[x] = self.find(self.father[x])
-        
         return self.father[x]
-    
-    def connected(self, a, b):
-        return self.find(a) == self.find(b)
     
     def union(self, a, b):
         root_a, root_b = self.find(a), self.find(b)
         if root_a != root_b:
             self.father[root_a] = root_b
-
             
-class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        uf = UnionFind(edges)
-        
-        for a, b in edges:          # O(E)
-            if uf.connected(a, b):
-                return [a, b]
-            
-            uf.union(a, b)
+    def connected(self, a, b):
+        return self.find(a) == self.find(b)
