81c9c8eb015829156e6ffff01f9058247a4910ef
Jim0607 <jinjun.ding@gmail.com>
2020-12-06 18:04:38 -0800

    Update 0425.Word-Squares.py

--- Solutions/0425.Word-Squares.py
+++ Solutions/0425.Word-Squares.py
@@ -212,3 +212,63 @@ class Solution:
             curr_square.append(next_word)
             self._backtrack(trie, N, curr_row + 1, curr_square, res)
             curr_square.pop()
+
+            
+            
+            
+            
+class TrieNode:
+    
+    def __init__(self):
+        self.child = defaultdict(TrieNode)
+        self.words = []
+        
+
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        curr_node = self.root
+        for ch in word:
+            curr_node = curr_node.child[ch]
+            curr_node.words.append(word)
+
+    def start_with(self, prefix):
+        """
+        return a list of words that starts with prefix
+        """
+        curr_node = self.root
+        for ch in prefix:
+            curr_node = curr_node.child[ch]
+            
+        return curr_node.words
+    
+
+class Solution:
+    def wordSquares(self, words: List[str]) -> List[List[str]]:
+        def backtrack(row, curr_comb):
+            if row == n - 1:
+                res.append(curr_comb.copy())
+                return
+            
+            # find next_possible_words
+            prefix = ""
+            for i in range(row + 1):    # 我们想加在第五行加单词，那这个单词必须满足prefix是前4行的第四列组成的
+                prefix += curr_comb[i][row + 1]
+            for next_word in trie.start_with(prefix):
+                curr_comb.append(next_word)
+                backtrack(row + 1, curr_comb)
+                curr_comb.pop()            
+        
+        
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        
+        m, n = len(words), len(words[0])
+        res = []
+        for word in words:
+            backtrack(0, [word])
+        return res 
