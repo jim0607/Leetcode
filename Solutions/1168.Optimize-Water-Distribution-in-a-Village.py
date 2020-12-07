46e80a96e742b69d4b2ee9ef97eadb30f484f630
Jim0607 <jinjun.ding@gmail.com>
2020-12-07 15:13:20 -0800

    Update 1168.Optimize-Water-Distribution-in-a-Village.py

--- Solutions/1168.Optimize-Water-Distribution-in-a-Village.py
+++ Solutions/1168.Optimize-Water-Distribution-in-a-Village.py
@@ -1,12 +1,12 @@
+"""
 1168. Optimize Water Distribution in a Village
 
 There are n houses in a village. We want to supply water for all the houses by building wells and laying pipes.
 
-For each house i, we can either build a well inside it directly with cost wells[i], or pipe in water from another well to it. The costs to lay pipes between houses are given by the array pipes, where each pipes[i] = [house1, house2, cost] represents the cost to connect house1 and house2 together using a pipe. Connections are bidirectional.
+For each house i, we can either build a well inside it directly with cost wells[i], or pipe in water from another well to it. 
+The costs to lay pipes between houses are given by the array pipes, where each pipes[i] = [house1, house2, cost] represents the cost to connect house1 and house2 together using a pipe. Connections are bidirectional.
 
 Find the minimum total cost to supply water to all houses.
-
- 
 
 Example 1:
 
@@ -15,7 +15,7 @@ Output: 3
 Explanation: 
 The image shows the costs of connecting houses using pipes.
 The best strategy is to build a well in the first house with cost 1 and connect the other houses to it with cost 2 so the total cost is 3.
-
+"""
 
 
 """
