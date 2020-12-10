eb7fa806be3bbe658e78cd67ea17781f696a2009
Jim0607 <jinjun.ding@gmail.com>
2020-12-09 22:29:22 -0800

    Update 0054.Spiral-Matrix.py

--- Solutions/0054.Spiral-Matrix.py
+++ Solutions/0054.Spiral-Matrix.py
@@ -25,34 +25,36 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 
 """
-每一个转弯的点是dfs的node, dfs helper function 需要传入的参数有(当前的位置, 当前的方向，当前的row_range and col_range)
+每一个转弯的点是dfs的node, dfs helper function 需要传入的参数有(min_row, max_row, min_col, max_col, curr_dir)
 """
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        def dfs(curr_i, curr_j, curr_dir, min_row, max_row, min_col, max_col):
+        def dfs(min_row, max_row, min_col, max_col, curr_dir):
             if len(res) == m * n:
                 return
+            
             if curr_dir == "r":
                 for j in range(min_col, max_col + 1):
-                    res.append(matrix[curr_i][j])
-                dfs(curr_i, j, "d", min_row + 1, max_row, min_col, max_col)
-            elif curr_dir == "d":
+                    res.append(matrix[min_row][j])
+                dfs(min_row + 1, max_row, min_col, max_col, "d")
+                
+            if curr_dir == "d":
                 for i in range(min_row, max_row + 1):
-                    res.append(matrix[i][curr_j])
-                dfs(i, curr_j, "l", min_row, max_row, min_col, max_col - 1)
-            elif curr_dir == "l":
+                    res.append(matrix[i][max_col])
+                dfs(min_row, max_row, min_col, max_col - 1, "l")
+                
+            if curr_dir == "l":
                 for j in range(max_col, min_col - 1, -1):
-                    res.append(matrix[curr_i][j])
-                dfs(curr_i, j, "u", min_row, max_row - 1, min_col, max_col)
-            elif curr_dir == "u":
+                    res.append(matrix[max_row][j])
+                dfs(min_row, max_row - 1, min_col, max_col, "u")
+                
+            if curr_dir == "u":
                 for i in range(max_row, min_row - 1, -1):
-                    res.append(matrix[i][curr_j])
-                dfs(i, curr_j, "r", min_row, max_row, min_col + 1, max_col)        
-        
-        
-        if not matrix or not matrix[0]:
-            return []
+                    res.append(matrix[i][min_col])
+                dfs(min_row, max_row, min_col + 1, max_col, "r")
+            
+            
         m, n = len(matrix), len(matrix[0])
         res = []
-        dfs(0, 0, "r", 0, m - 1, 0, n - 1)
+        dfs(0, m - 1, 0, n - 1, "r")
         return res
