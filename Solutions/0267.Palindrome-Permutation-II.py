73a600d683b31b6a47c9c059373782f26442de2f
Jim0607 <jinjun.ding@gmail.com>
2020-12-09 21:36:14 -0800

    Update 0267.Palindrome-Permutation-II.py

--- Solutions/0267.Palindrome-Permutation-II.py
+++ Solutions/0267.Palindrome-Permutation-II.py
@@ -1,3 +1,4 @@
+"""
 266. Palindrome Permutation
 
 Given a string, determine if a permutation of the string could form a palindrome.
@@ -14,7 +15,7 @@ Example 3:
 
 Input: "carerac"
 Output: true
-
+"""
 
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
@@ -33,7 +34,7 @@ class Solution:
         return True
 
 
-    
+"""
 267. Palindrome Permutation II
 Given a string s, return all the palindromic permutations (without duplicates) of it. Return an empty list if no palindromic permutation could be form.
 
@@ -45,47 +46,46 @@ Example 2:
 
 Input: "abc"
 Output: []
-
+"""
     
+"""
 O(N*N!), N is for checking isPalin, N! is the number of solutions.  This solution TLE
+"""
 class Solution:
     def generatePalindromes(self, s: str) -> List[str]:
-        if not self.canPermutePalindrome(s):
-            return False
+        def backtrack(curr_comb):
+            if len(curr_comb) == len(s):
+                if is_palin(curr_comb):
+                    res.append("".join(curr_comb.copy()))
+                return
+            
+            for next_idx in range(len(s)):
+                if next_idx in visited:
+                    continue
+                if next_idx > 0 and s[next_idx] == s[next_idx-1] and next_idx - 1 not in visited:   # 去重
+                    continue
+                curr_comb.append(s[next_idx])
+                visited.add(next_idx)
+                backtrack(curr_comb)
+                visited.remove(next_idx)
+                curr_comb.pop()
+                
+                
+        def is_palin(comb):
+            i, j = 0, len(comb) - 1
+            while i < j:
+                if comb[i] != comb[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True        
         
-        s = sorted(s)    # 去重需要先sort, string 不能用s.sorted()来sort
         
+        s = sorted(s)       # 去重需要先sort, string 不能用s.sorted()来sort
         res = []
-        visited = {i: False for i in range(len(s))}
-        self.dfs(s, [], res, visited)
-        
+        visited = set()
+        backtrack([])
         return res
-    
-    def dfs(self, s, curr, res, visited):
-        if len(curr) == len(s) and self.isPalin("".join(curr)):
-            res.append("".join(curr.copy()))
-            return
-        
-        for i in range(len(s)):
-            if visited[i] or (i >= 1 and s[i] == s[i -1] and not visited[i - 1]):   # 注意需要去重
-                continue
-            
-            curr.append(s[i])
-            visited[i] = True
-            self.dfs(s, curr, res, visited)
-            curr.pop()
-            visited[i] = False
-            
-    def isPalin(self, s):
-        i, j = 0, len(s) - 1
-        while i < j:
-            if s[i] != s[j]:
-                return False
-            
-            i += 1
-            j -= 1
-            
-        return True
 
 
     
