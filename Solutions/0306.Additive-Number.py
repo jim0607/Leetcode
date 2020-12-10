0d2128e8539b146062cb7403323cd827fdb0fd64
Jim0607 <jinjun.ding@gmail.com>
2020-12-10 15:09:19 -0800

    Update 0306.Additive-Number.py

--- Solutions/0306.Additive-Number.py
+++ Solutions/0306.Additive-Number.py
@@ -3,13 +3,12 @@
 
 Additive number is a string whose digits can form additive sequence.
 
-A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
+A valid additive sequence should contain at least three numbers. 
+Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
 
 Given a string containing only digits '0'-'9', write a function to determine if it's an additive number.
 
 Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
-
- 
 
 Example 1:
 
@@ -28,31 +27,31 @@ Explanation: The additive sequence is: 1, 99, 100, 199.
 
    
 """
-套backtrack模板即可，backtrack传入的参数有(curr_idx, prev_num, curr_num, curr_cnt). 结束条件是if curr_idx == len(s) - 1 and curr_cnt > 2
+套backtrack模板即可
+end condition is next_idx == len(s) - 1.
+constraint is: 1. not leading zero; 2. has to equal to the prev two num add together.
+backtrack function pass: (curr_idx, curr_comb)
+backtrack function should return whether or not it is valid
 """
 class Solution:
     def isAdditiveNumber(self, s: str) -> bool:
-        def backtrack(curr_idx, prev_num, curr_num, curr_cnt):
-            if curr_idx == len(s) - 1 and curr_cnt > 2:    # 必须至少有3个数才算valid, 所以目前的数字个数curr_cnt要入参数
+        def backtrack(curr_idx, curr_comb):
+            if curr_idx == len(s) - 1 and len(curr_comb) >= 3:
                 return True
-            for next_idx in range(curr_idx + 1, len(s)):
-                if s[curr_idx + 1] == "0" and next_idx != curr_idx + 1:  # "01" is not valid, "0" is valid
-                    continue        
-                next_num = int(s[curr_idx + 1: next_idx + 1])
-                if curr_num == None:       # 如果前面一个num都不存在
-                    if backtrack(next_idx, None, next_num, 1):
-                        return True
-                elif prev_num == None:     # 如果前只存在一个num
-                    if backtrack(next_idx, curr_num, next_num, 2):
-                        return True
-                else:                      # 如果前面两个num都存在
-                    if next_num == prev_num + curr_num:
-                        if backtrack(next_idx, curr_num, next_num, curr_cnt + 1):
-                            return True
-            return False                
-                
             
-        return backtrack(-1, None, None, 0)
+            for next_idx in range(curr_idx + 1, len(s)):
+                next_num = s[curr_idx + 1 : next_idx + 1]
+                if next_num[0] == "0" and len(next_num) > 1:        # no leading zero
+                    continue
+                if len(curr_comb) < 2 or (len(curr_comb) >= 2 and int(next_num) == int(curr_comb[-1]) + int(curr_comb[-2])):
+                    curr_comb.append(next_num)
+                    if backtrack(next_idx, curr_comb):
+                        return True
+                    curr_comb.remove(next_num)
+            return False
+        
+        
+        return backtrack(-1, [])
    
    
    
