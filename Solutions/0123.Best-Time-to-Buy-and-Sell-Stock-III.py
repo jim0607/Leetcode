6b7ed8966d5d107d7aeb8fea0ce2515e79cdea7a
Jim0607 <jinjun.ding@gmail.com>
2020-12-12 14:01:57 -0800

    Update 0123.Best-Time-to-Buy-and-Sell-Stock-III.py

--- Solutions/0123.Best-Time-to-Buy-and-Sell-Stock-III.py
+++ Solutions/0123.Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -15,7 +15,21 @@ Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-
              Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
 """
 
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy1 = -sys.maxsize    # the max profit with the first buy
+        sell1 = -sys.maxsize   # the max profit with the first sell
+        buy2 = -sys.maxsize 
+        sell2 = -sys.maxsize 
+        
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, price + buy1)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, price + buy2)
+            
+        return max(sell1, sell2)
+      
 
 
 """O(N), O(1) solution using DP"""
