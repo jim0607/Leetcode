7d8e384be580c3393929501b203d9430a5e5154b
Jim0607 <jinjun.ding@gmail.com>
2020-12-14 09:41:51 -0800

    Update 0719.Find-K-th-Smallest-Pair-Distance.py

--- Solutions/0719.Find-K-th-Smallest-Pair-Distance.py
+++ Solutions/0719.Find-K-th-Smallest-Pair-Distance.py
@@ -1,7 +1,8 @@
 """
 719. Find K-th Smallest Pair Distance
 
-Given an integer array, return the k-th smallest distance among all the pairs. The distance of a pair (A, B) is defined as the absolute difference between A and B.
+Given an integer array, return the k-th smallest distance among all the pairs. 
+The distance of a pair (A, B) is defined as the absolute difference between A and B.
 
 Example 1:
 Input:
@@ -28,30 +29,37 @@ let helper function return if there is more than k distance smaller than mid.
 we use two pointers to go through the list to check if there is more than k distance smaller than mid.
 The algorithm of helper function is sliding window so it's only O(N)
 """
+"""
+sort the nums
+binary search on distance
+helper function returns if there are k distances smaller than mid
+"""
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        nums.sort()     # sort nums first
+        nums.sort()         # sort nums first
+        
         start, end = 0, nums[-1] - nums[0]
         while start + 1 < end:
             mid = start + (end - start) // 2
-            if self._has_more(nums, k, mid):
+            if self._there_are_more_than_k(nums, mid, k):
                 end = mid
             else:
                 start = mid
-        return start if self._has_more(nums, k, start) else end
+        
+        return start if self._there_are_more_than_k(nums, start, k) else end
     
-    def _has_more(self, nums, k, diff):
+    def _there_are_more_than_k(self, nums, dist, k):
         """
-        Return if there are more than k distance larger than diff. nums is sorted
+        return if there are k or more than k distances smaller than of equals mid
+        套用at most problem的模板即可 - O(N)
         """
-        lens = len(nums)
-        i, j = 0, 0
         cnt = 0
-        while i < lens and j < lens:
-            if j == i:
-                j += 1
-            while j < lens and nums[j] - nums[i] <= diff:
+        i = 0
+        for j in range(1, len(nums)):
+            while i < j and nums[j] - nums[i] > dist:
+                i += 1
+            
+            if i < j and nums[j] - nums[i] <= dist:
                 cnt += j - i
-                j += 1
-            i += 1
+                
         return cnt >= k
