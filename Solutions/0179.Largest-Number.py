891ddae68adc0bb4fe52bea0617afcc4ae9f0083
Jim0607 <jinjun.ding@gmail.com>
2020-12-14 17:16:15 -0800

    Update 0179.Largest-Number.py

--- Solutions/0179.Largest-Number.py
+++ Solutions/0179.Largest-Number.py
@@ -10,27 +10,27 @@ Input: [3,30,34,5,9]
 Output: "9534330"
 
 
-cclass Solution:
+class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        self.quickSort(nums, 0, len(nums)-1)
-        return "".join(map(str, nums)) if nums[0] != 0 else "0"
+        arr = [str(num) for num in nums]
+        self.quick_sort(arr, 0, len(nums) - 1)
+        return "".join(arr) if arr[0] != "0" else "0"
     
-    def quickSort(self, nums, start, end):
+    def quick_sort(self, arr, start, end):
         if start >= end:
             return
         
+        pivot = arr[(start + end) // 2]
         left, right = start, end
-        pivot = nums[(start+end)//2]
-        
         while left <= right:
-            while left <= right and str(nums[left]) + str(pivot) > str(pivot) + str(nums[left]):
+            while left <= right and arr[left] + pivot > pivot + arr[left]:
                 left += 1
-            while left <= right and str(nums[right]) + str(pivot) < str(pivot) + str(nums[right]):
+            while left <= right and arr[right] + pivot < pivot + arr[right]:
                 right -= 1
             if left <= right:
-                nums[left], nums[right] = nums[right], nums[left]
+                arr[left], arr[right] = arr[right], arr[left]
                 left += 1
                 right -= 1
-                
-        self.quickSort(nums, start, right)
-        self.quickSort(nums, left, end)           
+        
+        self.quick_sort(arr, start, right)
+        self.quick_sort(arr, left, end)
