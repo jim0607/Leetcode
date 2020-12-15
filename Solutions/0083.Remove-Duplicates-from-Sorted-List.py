901fd2b43500193cbd4a6b0fa1b4dab5d99921e9
Jim0607 <jinjun.ding@gmail.com>
2020-12-15 11:32:43 -0800

    Update 0083.Remove-Duplicates-from-Sorted-List.py

--- Solutions/0083.Remove-Duplicates-from-Sorted-List.py
+++ Solutions/0083.Remove-Duplicates-from-Sorted-List.py
@@ -1,3 +1,4 @@
+"""
 83. Remove Duplicates from Sorted List
 
 Given a sorted linked list, delete all duplicates such that each element appear only once.
@@ -10,7 +11,7 @@ Example 2:
 
 Input: 1->1->2->3->3
 Output: 1->2->3
-
+"""
 
 
 # Definition for singly-linked list.
@@ -23,13 +24,14 @@ class Solution:
         if not head or not head.next:
             return head
         
-        anchor, curr = head, head.next
-        while curr:
-            if anchor.val == curr.val:
-                anchor.next = curr.next
-                curr = curr.next    # 注意curr往前挪，anchor不要往前挪
-            else:
-                anchor = curr
-                curr = curr.next
+        anchor = head
+        curr = head
+        while curr.next:
+            if curr.val != curr.next.val:
+                anchor = anchor.next
+                anchor.val = curr.next.val
+            curr = curr.next
+        
+        anchor.next = None
         
         return head
