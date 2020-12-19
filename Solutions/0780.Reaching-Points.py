f49bece090563e343fc368f3fcd895211b0c0212
Jim0607 <jinjun.ding@gmail.com>
2020-12-18 20:20:34 -0800

    Create 0780.Reaching-Points.py

---
+++ Solutions/0780.Reaching-Points.py
@@ -0,0 +1,65 @@
+"""
+780. Reaching Points
+
+A move consists of taking a point (x, y) and transforming it to either (x, x+y) or (x+y, y).
+
+Given a starting point (sx, sy) and a target point (tx, ty), 
+return True if and only if a sequence of moves exists to transform the point (sx, sy) to (tx, ty). Otherwise, return False.
+
+Examples:
+Input: sx = 1, sy = 1, tx = 3, ty = 5
+Output: True
+Explanation:
+One series of moves that transforms the starting point to the target is:
+(1, 1) -> (1, 2)
+(1, 2) -> (3, 2)
+(3, 2) -> (3, 5)
+
+Input: sx = 1, sy = 1, tx = 2, ty = 2
+Output: False
+
+Input: sx = 1, sy = 1, tx = 1, ty = 1
+Output: True
+"""
+
+
+
+
+"""
+work backwards: we compare tx and ty, whenever we found tx > ty, we update tx: tx -= ty. 
+else we update ty: ty -= tx.  we update tx and ty all the way until we reach sx and sy.
+O(max(tx, ty))
+"""
+class Solution:
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        while tx >= sx and ty >= sy:
+            if sx == tx and sy == ty:
+                return True
+            if tx > ty:
+                tx -= ty
+            else:
+                ty -= tx
+        return False
+        
+     
+     
+"""        
+O(log(max(tx, ty)))
+"""
+class Solution:
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        while tx >= sx and ty >= sy:
+            if tx == ty:
+                break
+            elif tx > ty:
+                if ty > sy:
+                    tx %= ty
+                else:
+                    return (tx - sx) % ty == 0
+            else:
+                if tx > sx:
+                    ty %= tx
+                else:
+                    return (ty - sy) % tx == 0
+
+        return tx == sx and ty == sy
