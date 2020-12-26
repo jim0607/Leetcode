d673ca6cb6bb2466ea010e445f0bcc53b1603382
Jim0607 <jinjun.ding@gmail.com>
2020-12-26 09:52:43 -0800

    Update 0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.py

--- Solutions/0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.py
+++ Solutions/0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.py
@@ -1,15 +1,15 @@
+"""
 889. Construct Binary Tree from Preorder and Postorder Traversal
 
 Return any binary tree that matches the given preorder and postorder traversals.
 
 Values in the traversals pre and post are distinct positive integers.
 
- 
-
 Example 1:
 
 Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
 Output: [1,2,3,4,5,6,7]
+"""
 
 
 """
@@ -37,32 +37,33 @@ class Solution:
        
 """
 solution 2: O(N)
+1. find the idx for root: pre[start]
+2. find the idx for left tree: pre_root_idx + 1
+3. find the idx for right tree: post_root_idx - 1
+4. recurssive call
 """
 class Solution:
     def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
-        if not pre or not post:
-            return None
-    
-        post_idxmap = collections.defaultdict(int)  # use a hashmap to store num->idx in post
-        for i, num in enumerate(post):
-            post_idxmap[num] = i
-    
-        def helper(pre_left, pre_right, post_left, post_right):
-            if pre_left >= pre_right or post_left >= post_right:
+        def helper(pre_start, pre_end, post_start, post_end):
+            if pre_start > pre_end:
                 return None
+            if pre_start == pre_end:
+                return TreeNode(pre[pre_start])
             
-            root = TreeNode(pre[pre_left])
-            pre_left += 1
-            post_right -= 1
+            root = TreeNode(pre[pre_start])
+            post_idx = post_idxdict[pre[pre_start+1]]
+            pre_idx = pre_idxdict[post[post_end-1]]
             
-            if pre_left >= pre_right or post_left >= post_right:
-                return root
-            idx = post_idxmap[pre[pre_left]]
-            lens = idx + 1 - post_left      # 左子树区间长度为
-            
-            root.left = helper(pre_left, pre_left + lens, post_left, idx+1)
-            root.right = helper(pre_left + lens, pre_right, idx+1, post_right)
+            root.left = helper(pre_start + 1, pre_idx - 1, post_start, post_idx)
+            root.right = helper(pre_idx, pre_end, post_idx + 1, post_end - 1)
             
             return root
         
-        return helper(0, len(pre), 0, len(post))
+        pre_idxdict = defaultdict(int)
+        for idx, num in enumerate(pre):
+            pre_idxdict[num] = idx
+        post_idxdict = defaultdict(int)
+        for idx, num in enumerate(post):
+            post_idxdict[num] = idx
+        
+        return helper(0, len(pre) - 1, 0, len(post) - 1)
