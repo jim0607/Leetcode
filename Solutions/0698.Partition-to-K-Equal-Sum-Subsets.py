c8d7bc190aa4ee3323bf8849f04bf91a17af1db6
Jim0607 <jinjun.ding@gmail.com>
2020-12-27 11:41:45 -0800

    Update 0698.Partition-to-K-Equal-Sum-Subsets.py

--- Solutions/0698.Partition-to-K-Equal-Sum-Subsets.py
+++ Solutions/0698.Partition-to-K-Equal-Sum-Subsets.py
@@ -18,39 +18,35 @@ Time complexity: we basically iterate over nums and for each element either use 
 which is O(2^n). We are doing the same for each subset. Total subsets are k. 
 So Time Complexity becomes O(k*(2^n))
 """
+"""
+这题要求把一个arr分成k个subsets, 每个subsets的sum都是target. 这是一个NP-complete problem, 只能backtrack - O(k*2^n).
+与之类似的，要求把一个arr分成k个subarray, 每个subarray的sum都是target. 这是一个dp问题 - O(k*n*target)
+"""
 class Solution:
-    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:       
         def backtrack(curr_idx, curr_sum, curr_cnt):
-            if curr_cnt == k:       # 结束条件是已有curr_cnt=k段满足条件了
-                return True
-            if curr_cnt > k:        # 提前return False - strong pruning
+            if curr_cnt == k:    # 为什么只需要curr_cnt=k就return True了？ 
+                return True      # 这是因为target = sum(nums) // k, 所以如果k个subsets满足加起来等于target，
+                                 # 那说明nums里面的所有num都已经用上了
+            if curr_sum > target:
                 return False
-            
-            if curr_sum == target:  # 拼出一段之后，从头开始去拼下一段
-                return backtrack(-1, 0, curr_cnt + 1)
-            if curr_sum > target:   
-                return False
-
-            for next_idx in range(curr_idx + 1, len(nums)):   # 去后面找数来拼凑 - O(2^N)
-                if next_idx in visited: # 正因为拼出一段之后，需要从"头!!"开始去拼下一段，所以需要visited
-                    continue
-                    
-                visited.add(next_idx)
-                if backtrack(next_idx, curr_sum + nums[next_idx], curr_cnt):
+            elif curr_sum == target:                # 拼出一段之后，从头开始去拼下一段
+                if backtrack(-1, 0, curr_cnt + 1):  # 注意这里要从 -1 开始搜索
                     return True
-                visited.remove(next_idx)
+            elif curr_sum < target:
+                for next_idx in range(curr_idx + 1, len(nums)):     # 去后面找数来拼凑 - O(2^N)
+                    if next_idx not in visited:
+                        visited.add(next_idx)
+                        if backtrack(next_idx, curr_sum + nums[next_idx], curr_cnt):
+                            return True
+                        visited.remove(next_idx)
             
-            return False       
-            
-            
-        if k == 1:
-            return True
+            return False
+        
+        
         if sum(nums) % k != 0:
             return False
-        
         target = sum(nums) // k
-        if max(nums) > target:
-            return False
         
-        visited = set()
+        visited = set()     # 因为拼出一段之后，需要从"头!!!!"开始去拼下一段，所以需要visited标记
         return backtrack(-1, 0, 0)
