e62401cd880b2c80d2867d62583fb85fd518acb9
Jim0607 <jinjun.ding@gmail.com>
2020-12-31 19:21:39 -0800

    Update 0471.Encode-String-with-Shortest-Length.py

--- Solutions/0471.Encode-String-with-Shortest-Length.py
+++ Solutions/0471.Encode-String-with-Shortest-Length.py
@@ -46,8 +46,8 @@ Explanation: "abbbabbbc" occurs twice, but "abbbabbbc" can also be encoded to "2
 
 """
 dp[i][j] is the encode of substring including index i to index j.
-dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j], potential_candidate) in terms of length.
-potential_candidate = "k[repeating_pattern]", 
+dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j], potential_concadenation) in terms of length.
+potential_concadenation = "k[repeating_pattern]", 
 where pattern is the repeating string in substring s[i:j+1] and k is the number of repeating times.
 
 initializaton: dp[i][j] = s[i:j+1] originally
@@ -62,53 +62,22 @@ class Solution:
             for j in range(i, n):
                 substr = s[i:j+1]
                 dp[i][j] = substr       # initialization
-                if j - i >= 4:          # 如果长度小于5的话就没有必要进行压缩了
-                    for k in range(i, j):
-                        if len(dp[i][k] + dp[k+1][j]) < len(dp[i][j]):
-                            dp[i][j] = dp[i][k] + dp[k+1][j]
-                    
-                    # Next: find the posible compression for s. eg: "aabcdaabcd" --> "2[aabcd]"
-                    # algorithm: step 1: double the string "aabcdaabcdaabcdaabcd";
-                    # step 2: start from idx 1, get the idx for the first s in the doubled string, in this eg: idx = 5.
-                    double_s = substr + substr
-                    repeat_idx = double_s.index(substr, 1)      # start from idx 1, find the position of substr in double_s
-                    repeat_time = len(substr) // repeat_idx
-                    candidate = str(repeat_time) + "[" + dp[i][i+repeat_idx-1] + "]"  # 注意这里repeated pattern是dp[i:i+repeat_idx-1]
-                    if len(candidate) < len(dp[i][j]):
-                        dp[i][j] = candidate
-                        
-        return dp[0][n-1]
-        
-        
-"""
-如果substr是重复的，是不是就不需要再看len(dp[i][k] + dp[k+1][j]) < len(dp[i][j])了。
-例如substr是abcabcabcabcabc, 最终肯定是5[abc]，不需要再看3[abc]+abcabc或者abcabc+3[abc]。
-对于一个本身就重复的字符串，最小的长度肯定是n[REPEATED]，不会是某个left+right。
-所以应该把k的那个循环放在t和replace那部分代码的后面。这样的确提高了一些运算效率的
-"""
-class Solution:
-    def encode(self, s: str) -> str:
-        n = len(s)
-        dp = [["" for _ in range(n)] for _ in range(n)]
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                substr = s[i:j+1]
-                dp[i][j] = substr       # initialization
-                if j - i >= 4:          # 如果长度小于5的话就没有必要进行压缩了
-                    
-                    # Next: find the posible compression for s. eg: "aabcdaabcd" --> "2[aabcd]"
-                    # algorithm: step 1: double the string "aabcdaabcdaabcdaabcd";
-                    # step 2: start from idx 1, get the idx for the first s in the doubled string, in this eg: idx = 5.
-                    double_s = substr + substr
-                    repeat_idx = double_s.index(substr, 1)      # start from idx 1, find the position of substr in double_s
-                    repeat_time = len(substr) // repeat_idx
-                    candidate = str(repeat_time) + "[" + dp[i][i+repeat_idx-1] + "]"  # 注意这里repeated pattern是dp[i:i+repeat_idx-1]
-                    if len(candidate) < len(dp[i][j]):
-                        dp[i][j] = candidate
-                        continue        # 可以直接continue掉后面的for k in range(i, j)循环，从而提高效率
-                        
-                    for k in range(i, j):
-                        if len(dp[i][k] + dp[k+1][j]) < len(dp[i][j]):
-                            dp[i][j] = dp[i][k] + dp[k+1][j]
-                        
+                if j - i < 4:          # 如果长度小于5的话就没有必要进行压缩了
+                    continue
+             
+                # firstly: find the posible compression for s. eg: "aabcdaabcd" --> "2[aabcd]", algorithm: step 1: double the string "aabcdaabcdaabcdaabcd";
+                # step 2: start from idx 1, get the idx for the first s in the doubled string, in this eg: idx = 5.
+                double_s = substr + substr
+                repeat_idx = double_s.index(substr, 1)      # start from idx 1, find the position of substr in double_s
+                repeat_time = len(substr) // repeat_idx
+                candidate = str(repeat_time) + "[" + dp[i][i+repeat_idx-1] + "]"  # 注意这里repeated pattern是dp[i:i+repeat_idx-1], 这样"abbbabbbcabbbabbbc" --> "2[2[abbb]c]"
+                if len(candidate) < len(dp[i][j]):
+                    dp[i][j] = candidate            
+                    continue              # 如果"aabcdaabcd" 可以直接变成 "2[aabcd]", 那就没有必要进行后面的for k in range(i, j)循环
+                   
+                # secondly: 找到一个最好的分隔点
+                for k in range(i, j):
+                    if len(dp[i][k] + dp[k+1][j]) < len(dp[i][j]):
+                        dp[i][j] = dp[i][k] + dp[k+1][j]
+
         return dp[0][n-1]
