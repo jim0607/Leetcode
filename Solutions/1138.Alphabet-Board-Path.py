20e0c4639ba1eb6dc1a7e81454f5459e7d9f940d
Jim0607 <jinjun.ding@gmail.com>
2021-01-01 22:05:35 -0800

    Update 1138.Alphabet-Board-Path.py

--- Solutions/1138.Alphabet-Board-Path.py
+++ Solutions/1138.Alphabet-Board-Path.py
@@ -34,38 +34,39 @@ each time we start from target[i] and do bfs to find target[i+1].
 """
 class Solution:
     def alphabetBoardPath(self, target: str) -> str:
-        def bfs(start_pos, target_pos):
-            q = collections.deque()
-            visited = set()
-            curr_path = ""
-            q.append((start_pos, curr_path))
-            visited.add(start_pos)
-            while len(q) > 0:
-                lens = len(q)
-                for _ in range(lens):
-                    (curr_i, curr_j), curr_path = q.popleft()
-                    if (curr_i, curr_j) == target_pos:
-                        curr_path += "!"
-                        return curr_path
-                    for dr, (delta_i, delta_j) in directions.items():
-                        next_i, next_j = curr_i + delta_i, curr_j + delta_j
-                        if (next_i, next_j) in mapping.values():
-                            if (next_i, next_j) not in visited:
-                                next_path = curr_path + dr
-                                q.append(((next_i, next_j), next_path))
-                                visited.add((next_i, next_j))
-
+        mapping = defaultdict(tuple)   # ch --> the pos of the ch
+        board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"]
+        for i, s in enumerate(board):
+            for j, ch in enumerate(s):
+                mapping[ch] = (i, j)
         
-        mapping = {"a": (0, 0), "b": (0, 1), "c": (0, 2), "d": (0, 3), "e": (0, 4),
-                   "f": (1, 0), "g": (1, 1), "h": (1, 2), "i": (1, 3), "j": (1, 4),
-                   "k": (2, 0), "l": (2, 1), "m": (2, 2), "n": (2, 3), "o": (2, 4),
-                   "p": (3, 0), "q": (3, 1), "r": (3, 2), "s": (3, 3), "t": (3, 4),
-                   "u": (4, 0), "v": (4, 1), "w": (4, 2), "x": (4, 3), "y": (4, 4),
-                   "z": (5, 0)}
         directions = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+        
         res = ""
-        start_pos = (0, 0)
-        for target_ch in target:
-            res += bfs(start_pos, mapping[target_ch])
-            start_pos = mapping[target_ch]
+        start_ch = "a"
+        for ch in target:
+            res += self.find_path(start_ch, ch, mapping, directions)
+            start_ch = ch
         return res
+    
+    def find_path(self, start_ch, end_ch, mapping, directions):
+        start_pos, end_pos = mapping[start_ch], mapping[end_ch]
+        
+        q = deque()
+        visited = set()
+        q.append((start_pos[0], start_pos[1], ""))
+        visited.add(start_pos)
+        
+        while len(q) > 0:
+            lens = len(q)
+            for _ in range(lens):
+                curr_i, curr_j, curr_path = q.popleft()
+                if (curr_i, curr_j) == end_pos:
+                    return curr_path + "!"
+                
+                for direction, (delta_i, delta_j) in directions.items():
+                    next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                    if 0 <= next_i < 5 and 0 <= next_j < 5 or (next_i == 5 and next_j == 0):
+                        if (next_i, next_j) not in visited:
+                            q.append((next_i, next_j, curr_path + direction))
+                            visited.add((next_i, next_j))
