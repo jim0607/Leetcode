91c86683dfd468bee8b65ca51cdc2bee25d242e3
Jim0607 <jinjun.ding@gmail.com>
2021-01-02 14:51:23 -0800

    Update 1146.Snapshot-Array.py

--- Solutions/1146.Snapshot-Array.py
+++ Solutions/1146.Snapshot-Array.py
@@ -41,7 +41,7 @@ class SnapshotArray:
     def __init__(self, length: int):
         self.arr = [0 for _ in range(length)]
         self.snap_cnt = 0
-        self.mapping = collections.defaultdict(list)
+        self.mapping = defaultdict(list)
 
     def set(self, index: int, val: int) -> None:   
         self.arr[index] = val
