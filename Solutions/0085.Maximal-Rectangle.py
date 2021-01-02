eb69ff4e5b8de0846f6037a0600edae8ebb38a42
Jim0607 <jinjun.ding@gmail.com>
2021-01-02 14:08:08 -0800

    Update 0085.Maximal-Rectangle.py

--- Solutions/0085.Maximal-Rectangle.py
+++ Solutions/0085.Maximal-Rectangle.py
@@ -35,3 +35,52 @@ class Solution:
             res = max(res, self.largestRectangularHistogram(heights))
             
         return res
+      
+      
+      
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        
+        m, n = len(matrix), len(matrix[0])
+        heights = [0 for _ in range(n)]
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                heights[j] = 0 if matrix[i][j] == "0" else heights[j] + 1
+            
+            max_area = max(max_area, self.max_area(heights))
+            
+        return max_area
+    
+    def max_area(self, heights):
+        n = len(heights)
+        left_idx = [-1 for _ in range(n)]
+        st = []     # store (h, idx)
+        for i, h in enumerate(heights): 
+            while len(st) > 0 and st[-1][0] >= h:
+                st.pop()
+            
+            if len(st) > 0:
+                left_idx[i] = st[-1][1]
+            
+            st.append((h, i))
+            
+        right_idx = [n for _ in range(n)]
+        st = []
+        for i in range(n - 1, -1, -1):
+            h = heights[i]
+            while len(st) > 0 and st[-1][0] >= h:
+                st.pop()
+                
+            if len(st) > 0:
+                right_idx[i] = st[-1][1]
+                
+            st.append((h, i))
+            
+        max_area = 0
+        for i, h in enumerate(heights):
+            area = h * (right_idx[i] - left_idx[i] - 1)
+            max_area = max(max_area, area)
+        return max_area
