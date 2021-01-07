071aad77753d8c29d1f2783a8184ad6364b2cb9d
Jim0607 <jinjun.ding@gmail.com>
2021-01-06 20:28:37 -0800

    Update 0768.Max-Chunks-To-Make-Sorted-II.py

--- Solutions/0768.Max-Chunks-To-Make-Sorted-II.py
+++ Solutions/0768.Max-Chunks-To-Make-Sorted-II.py
@@ -27,6 +27,43 @@ However, splitting into [2, 1], [3], [4], [4] is the highest number of chunks po
 
 
 """
+O(N) solution: pre-calculation of left_max and right_min
+Assume we have an array A of length n. The objective is to split A into as many chunks as possible, 
+such that when we sort all these chunks and concatenate them, the result will be a fully sorted array in ascending order.
+Notice that as we traverse A, we can split A at index i into A[:i+1] and A[i+1:] if and only if max(A[:i+1]) <= min(A[i+1:]). 
+This to maintain the invariant that sorted(A[:i+1]) + sorted(A[i+1:]) == sorted(A).
+Therefore, all we need to do is iterate over the array, and every time we encounter max(A[:i+1]) <= min(A[i+1:]), we split the array.
+Now, suppose I split at some index i (where 0 <= i < n-1, and then save this index as begin := i. 
+You may be thinking, on the next iteration, I should be comparing max(A[begin : i+1] with min(A[i+1:]) instead of max(A[:i+1]) with min(A[i+1]:).
+However, notice that both max_val and min_val must be sorted in ascending order (just look at how they're created). 
+So, by definition max(A[begin : i+1]) = max(A[:i+1]), for all begin <= i < n. So, we don't even need the begin pointer, 
+we just compare max(A[:i+1]) with min(A[i+1:]) on every iteration.
+"""
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n = len(arr)
+        left_max = [num for num in arr]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i-1], arr[i])
+            
+        right_min = [num for num in arr]
+        for i in range(n - 2, -1, -1):
+            right_min[i] = min(right_min[i+1], arr[i])
+            
+        cnt = 0
+        for i in range(n - 1):
+            if left_max[i] <= right_min[i+1]:
+                cnt += 1
+        return cnt + 1
+
+
+
+
+
+
+
+
+"""
 Maintain two sums, one for arr, one for sorted arr.
 for num_1, num_2 in zip(arr, sorted(arr)): if sum_1 == sum_2: cnt += 1.
 """
