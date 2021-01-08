adf934f67b04f2154be079dffc442b52c5c9ad8f
Jim0607 <jinjun.ding@gmail.com>
2021-01-08 15:03:09 -0800

    Update 0022.Generate-Parentheses.py

--- Solutions/0022.Generate-Parentheses.py
+++ Solutions/0022.Generate-Parentheses.py
@@ -15,21 +15,27 @@ For example, given n = 3, a solution set is:
 """
 
 
+"""
+backtrack结束条件: left_cnt == right_cnt == n
+constraints on next_candidates: can only be left_parentheses or right_parentheses, left_cnt cannot exceed right_cnt
+arguments pass into backtrack function: left_cnt, right_cnt, curr_comb
+"""
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.res = []
-        self._backtrack(n, 0, 0, "")
-        return self.res
-    
-    def _backtrack(self, n, left_cnt, right_cnt, curr_comb):
-        if left_cnt == right_cnt == n:
-            self.res.append(curr_comb)  # string doesn't need deep copy because it is immutable
-            return
-        
-        if left_cnt < right_cnt:        # 这个判断尤为关键 - strong pruning
-            return
-        
-        if left_cnt < n:
-            self._backtrack(n, left_cnt + 1, right_cnt, curr_comb + "(")
-        if right_cnt < n:
-            self._backtrack(n, left_cnt, right_cnt + 1, curr_comb + ")")
+        def backtrack(left_cnt, right_cnt, curr_comb):
+            if left_cnt == right_cnt == n:  
+                res.append(curr_comb)   # string doesn't need deep copy because it is immutable
+                return
+            
+            if right_cnt > left_cnt:    # 这个判断尤为关键 - strong pruning
+                return
+            
+            if left_cnt < n:
+                backtrack(left_cnt + 1, right_cnt, curr_comb + "(")
+            if right_cnt < n:
+                backtrack(left_cnt, right_cnt + 1, curr_comb + ")")
+                
+            
+        res = []
+        backtrack(0, 0, "")
+        return res
