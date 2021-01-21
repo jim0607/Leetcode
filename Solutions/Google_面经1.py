79b7267628e67cd7310cb2e54f8904c92bbea0f0
Jim0607 <jinjun.ding@gmail.com>
2021-01-20 21:46:58 -0800

    Update Google_面经1.py

--- Solutions/Google_面经1.py
+++ Solutions/Google_面经1.py
@@ -4,53 +4,48 @@
 
 solution 1: Backtrack - O(2^N) in worst case.
 backtrack结束条件: curr_idx = len(nums) - 1
-constraints on next_candidate: next_idx in range(curr_idx + 1, len(nums)), nums[next_idx] != last_num + 1
-arguments pass into backtrack function: curr_idx, last_num, curr_sum
+constraints on next_candidate: next_idx in range(curr_idx + 1, len(nums)), nums[next_idx] != nums[curr_idx] + 1
+arguments pass into backtrack function: curr_idx, curr_sum
 """
 import sys
 
 class SelectNumber:
     def select_number(self, nums) -> int:
-        def backtrack(curr_idx, last_num, curr_sum):
-            if curr_idx == len(nums) - 1:
-                self.max_sum = max(self.max_sum, curr_sum)
-                return
+        def backtrack(curr_idx, curr_sum):
+            self.max_sum = max(self.max_sum, curr_sum)
 
             for next_idx in range(curr_idx + 1, len(nums)):
-                if nums[next_idx] == last_num + 1:
+                if nums[next_idx] == nums[curr_idx] + 1:
                     continue
-                backtrack(next_idx, nums[next_idx], curr_sum + nums[next_idx])
+                backtrack(next_idx, curr_sum + nums[next_idx])
 
 
         nums.sort()
         self.max_sum = -sys.maxsize
-        backtrack(-1, sys.maxsize, 0)
+        backtrack(-1, 0)
         return self.max_sum
 
 
 
 """
-solution 2: recursion + memorization - O(N^2)
+solution 2: recursion + memorization - O(N)
 """
 class SelectNumber:
     def select_number(self, nums) -> int:
-        def backtrack(curr_idx, last_num):
-            if curr_idx == len(nums) - 1:
-                return 0
-
-            if (curr_idx, last_num) in memo:
-                return memo[(curr_idx, last_num)]
+        def backtrack(curr_idx):
+            if curr_idx in memo:
+                return memo[curr_idx]
 
             res = -sys.maxsize
             for next_idx in range(curr_idx + 1, len(nums)):
-                if nums[next_idx] == last_num + 1:
+                if nums[next_idx] == nums[curr_idx] + 1:
                     continue
-                res = max(res, nums[next_idx] + backtrack(next_idx, nums[next_idx]))
+                res = max(res, nums[next_idx] + backtrack(next_idx))
 
-            memo[(curr_idx, last_num)] = res
+            memo[curr_idx] = res
             return res
 
 
         nums.sort()
-        memo = defaultdict(int)     # (curr_idx, curr_last_num) --> from (curr_idx, curr_last_num), what is the max_sum we can get
-        return backtrack(-1, sys.maxsize)  # returns from (curr_idx, curr_last_num), what is the max_sum we can get
+        memo = defaultdict(int)     # curr_idx --> from curr_idx, what is the max_sum we can get
+        return backtrack(-1)        # returns from curr_idx, what is the max_sum we can get
