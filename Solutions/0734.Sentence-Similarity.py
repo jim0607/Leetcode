036d5bbe2cc6a3c7b18b8d1b19eb928b789e315d
Jim0607 <jinjun.ding@gmail.com>
2021-01-23 21:18:31 -0800

    Update 0734.Sentence-Similarity.py

--- Solutions/0734.Sentence-Similarity.py
+++ Solutions/0734.Sentence-Similarity.py
@@ -1,23 +1,43 @@
+"""
 734. Sentence Similarity
 
 Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.
-
-For example, "great acting skills" and "fine drama talent" are similar, if the similar word pairs are pairs = [["great", "fine"], ["acting","drama"], ["skills","talent"]].
-
-Note that the similarity relation is not transitive. For example, if "great" and "fine" are similar, and "fine" and "good" are similar, "great" and "good" are not necessarily similar.
+For example, "great acting skills" and "fine drama talent" are similar, 
+if the similar word pairs are pairs = [["great", "fine"], ["acting","drama"], ["skills","talent"]].
+Note that the similarity relation is not transitive. 
+For example, if "great" and "fine" are similar, and "fine" and "good" are similar, "great" and "good" are not necessarily similar.
 
 However, similarity is symmetric. For example, "great" and "fine" being similar is the same as "fine" and "great" being similar.
 
-Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
+Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, 
+even though there are no specified similar word pairs.
 
-Finally, sentences can only be similar if they have the same number of words. So a sentence like words1 = ["great"] can never be similar to words2 = ["doubleplus","good"].
+Finally, sentences can only be similar if they have the same number of words. 
+So a sentence like words1 = ["great"] can never be similar to words2 = ["doubleplus","good"].
+
+Example 1:
+
+Input: sentence1 = ["great","acting","skills"], sentence2 = ["fine","drama","talent"], similarPairs = [["great","fine"],["drama","acting"],["skills","talent"]]
+Output: true
+Explanation: The two sentences have the same length and each word i of sentence1 is also similar to the corresponding word in sentence2.
+Example 2:
+
+Input: sentence1 = ["great"], sentence2 = ["great"], similarPairs = []
+Output: true
+Explanation: A word is similar to itself.
+Example 3:
+
+Input: sentence1 = ["great"], sentence2 = ["doubleplus","good"], similarPairs = [["great","doubleplus"]]
+Output: false
+Explanation: As they don't have the same length, we return false.
+
 
 Note:
-
 The length of words1 and words2 will not exceed 1000.
 The length of pairs will not exceed 2000.
 The length of each pairs[i] will be 2.
 The length of each words[i] and pairs[i][j] will be in the range [1, 20].
+"""
 
 
 """
@@ -32,7 +52,7 @@ class Solution:
         if lens1 != lens2:
             return False
         
-        mapping = collections.defaultdict(set)
+        mapping = defaultdict(set)
         for pair in pairs:
             mapping[pair[0]].add(pair[0])   # word与自己也是similar的
             mapping[pair[0]].add(pair[1])
