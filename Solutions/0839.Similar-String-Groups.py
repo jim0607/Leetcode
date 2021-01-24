3c5c3493fbd8b1191e1c1b90648b5bc01a8a83c8
Jim0607 <jinjun.ding@gmail.com>
2021-01-23 22:49:46 -0800

    Update 0839.Similar-String-Groups.py

--- Solutions/0839.Similar-String-Groups.py
+++ Solutions/0839.Similar-String-Groups.py
@@ -8,7 +8,8 @@ For example, "tars" and "rats" are similar (swapping at positions 0 and 2),
 and "rats" and "arts" are similar, but "star" is not similar to "tars", "rats", or "arts".
 
 Together, these form two connected groups by similarity: {"tars", "rats", "arts"} and {"star"}.  
-Notice that "tars" and "arts" are in the same group even though they are not similar.  Formally, each group is such that a word is in the group if and only if it is similar to at least one other word in the group.
+Notice that "tars" and "arts" are in the same group even though they are not similar.  
+Formally, each group is such that a word is in the group if and only if it is similar to at least one other word in the group.
 
 We are given a list A of strings.  Every string in A is an anagram of every other string in A.  
 How many groups are there?
