9b59f41eabea5780d1b8613334d22a9298ed8e42
Jim0607 <jinjun.ding@gmail.com>
2021-01-23 19:25:44 -0800

    Update 0745.Prefix-and-Suffix-Search.py

--- Solutions/0745.Prefix-and-Suffix-Search.py
+++ Solutions/0745.Prefix-and-Suffix-Search.py
@@ -1,8 +1,10 @@
+"""
 745. Prefix and Suffix Search
 
 Given many words, words[i] has weight i.
 
-Design a class WordFilter that supports one function, WordFilter.f(String prefix, String suffix). It will return the word with given prefix and suffix with maximum weight. If no word exists, return -1.
+Design a class WordFilter that supports one function, WordFilter.f(String prefix, String suffix). 
+It will return the word with given prefix and suffix with maximum weight. If no word exists, return -1.
 
 Examples:
 
@@ -10,7 +12,7 @@ Input:
 WordFilter(["apple"])
 WordFilter.f("a", "e") // returns 0
 WordFilter.f("b", "") // returns -1
-
+"""
 
 
 """
