20489d1369aec2b8c54bcf71e9d9285738724cc5
Jim0607 <jinjun.ding@gmail.com>
2021-01-24 22:34:36 -0800

    Update 0841.Keys-and-Rooms.py

--- Solutions/0841.Keys-and-Rooms.py
+++ Solutions/0841.Keys-and-Rooms.py
@@ -1,8 +1,10 @@
 """841. Keys and Rooms
 
-There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room. 
+There are N rooms and you start in room 0.  
+Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room. 
 
-Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
+Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  
+A key rooms[i][j] = v opens the room with number v.
 
 Initially, all the rooms start locked (except for room 0). 
 
@@ -25,6 +27,7 @@ Input: [[1,3],[3,0,1],[2],[0]]
 Output: false
 Explanation: We can't enter the room with number 2.
 """
+
 
 """
 solution 1: dfs - O(N)
