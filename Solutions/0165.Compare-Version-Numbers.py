dbd9db5c33bb365cb49a12019f8ee2fb5a2617f1
Jim0607 <jinjun.ding@gmail.com>
2021-01-26 14:32:39 -0800

    Update 0165.Compare-Version-Numbers.py

--- Solutions/0165.Compare-Version-Numbers.py
+++ Solutions/0165.Compare-Version-Numbers.py
@@ -3,9 +3,15 @@
 
 Given two version numbers, version1 and version2, compare them.
 
-Version numbers consist of one or more revisions joined by a dot '.'. Each revision consists of digits and may contain leading zeros. Every revision contains at least one character. Revisions are 0-indexed from left to right, with the leftmost revision being revision 0, the next revision being revision 1, and so on. For example 2.5.33 and 0.1 are valid version numbers.
+Version numbers consist of one or more revisions joined by a dot '.'. 
+Each revision consists of digits and may contain leading zeros. Every revision contains at least one character. 
+Revisions are 0-indexed from left to right, with the leftmost revision being revision 0, 
+the next revision being revision 1, and so on. For example 2.5.33 and 0.1 are valid version numbers.
 
-To compare version numbers, compare their revisions in left-to-right order. Revisions are compared using their integer value ignoring any leading zeros. This means that revisions 1 and 001 are considered equal. If a version number does not specify a revision at an index, then treat the revision as 0. For example, version 1.0 is less than version 1.1 because their revision 0s are the same, but their revision 1s are 0 and 1 respectively, and 0 < 1.
+To compare version numbers, compare their revisions in left-to-right order. 
+Revisions are compared using their integer value ignoring any leading zeros. This means that revisions 1 and 001 are considered equal. 
+If a version number does not specify a revision at an index, then treat the revision as 0. For example, 
+version 1.0 is less than version 1.1 because their revision 0s are the same, but their revision 1s are 0 and 1 respectively, and 0 < 1.
 
 Return the following:
 
@@ -52,29 +58,22 @@ class Solution:
         v2 = version2.split(".")
         
         for i in range(min(len(v1), len(v2))):
-            num1, num2 = v1[i], v2[i]
-            
-            p = 0
             n1 = 0
-            while p < len(num1):
-                n1 = 10 * n1 + ord(num1[p]) - ord("0")
-                p += 1
+            for digit in v1[i]:
+                n1 = 10 * n1 + ord(digit) - ord("0")
                 
-            q = 0
             n2 = 0
-            while q < len(num2):
-                n2 = 10 * n2 + ord(num2[q]) - ord("0")
-                q += 1
+            for digit in v2[i]:
+                n2 = 10 * n2 + ord(digit) - ord("0")
                 
             if n1 > n2:
-                print(n1, n2)
                 return 1
             elif n1 < n2:
                 return -1
                 
-        if i != len(v1) - 1 and any(ch != "0" for s in v1[i+1:] for ch in s):
+        if i != len(v1) - 1 and any(digit != "0" for s in v1[i+1:] for digit in s):
             return 1
-        if i != len(v2) - 1 and any(ch != "0" for s in v2[i+1:] for ch in s):
+        if i != len(v2) - 1 and any(digit != "0" for s in v2[i+1:] for digit in s):
             return -1
         
         return 0
