6170b5fbedf0f6db82f1eabe2a3f38c4ab4e1ccc
Jim0607 <jinjun.ding@gmail.com>
2021-01-26 15:36:18 -0800

    Update 0248.Strobogrammatic-Number-III.py

--- Solutions/0248.Strobogrammatic-Number-III.py
+++ Solutions/0248.Strobogrammatic-Number-III.py
@@ -16,9 +16,35 @@ Because the range might be a large number, the low and high numbers are represen
 
 
 
+
 """
-想想如果 low = 99, high = 1000, 那所有的valid nums都是由三个数字组成的。
-这就变成了 247. Strobogrammatic Number II 中 n = 3 的情况，只是这题不需要输出组合，只需要求多少种可能组合
-可能的组合数其实就是247. Strobogrammatic Number II的数间复杂度: O(5^(n//2))
-所以我们只需要算组合个数就行，不需要做 backtrack
+解法：每次都按照一个lens做backtrack. 每次添加next_ch都在curr_comb前面和后面添加
+backtrack end consition: if len(curr_comb) == lens, check if curr_comb in range(low, high)
+constraints on next_candidates: next_comb = ch + curr_comb + mapping[ch], cannot have leading zero
+arguments pass into backtrack function: curr_comb
 """
+class Solution:
+    def strobogrammaticInRange(self, low: str, high: str) -> int:
+        def backtrack(curr_comb, n):
+            if len(curr_comb) == n:
+                if int(low) <= int(curr_comb) <= int(high):
+                    res.add(curr_comb)
+                return
+            
+            for next_ch in mapping:
+                next_comb = next_ch + curr_comb + mapping[next_ch]
+                if len(next_comb) == n and next_comb[0] == "0":
+                    continue
+                backtrack(next_comb, n)       
+        
+        
+        mapping = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}
+        res = set()
+        for lens in range(len(low), len(high) + 1):    # 对于每一个lens, 做backtrack寻找等于lens的comb
+            if lens % 2 == 0:       # 如果lens是偶数，那中间就没有ch
+                backtrack("", lens)
+            else:                   # 如果lens是奇数，中间只能是"0", "1", "8"
+                backtrack("0", lens)
+                backtrack("1", lens)
+                backtrack("8", lens)
+        return len(res)
