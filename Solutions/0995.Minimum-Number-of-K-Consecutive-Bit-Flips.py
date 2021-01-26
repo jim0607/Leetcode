35e3bcfae139f42830e85caa5c3c3535da88077e
Jim0607 <jinjun.ding@gmail.com>
2021-01-26 11:43:20 -0800

    Update 0995.Minimum-Number-of-K-Consecutive-Bit-Flips.py

--- Solutions/0995.Minimum-Number-of-K-Consecutive-Bit-Flips.py
+++ Solutions/0995.Minimum-Number-of-K-Consecutive-Bit-Flips.py
@@ -1,10 +1,10 @@
+"""
 995. Minimum Number of K Consecutive Bit Flips
 
-In an array A containing only 0s and 1s, a K-bit flip consists of choosing a (contiguous) subarray of length K and simultaneously changing every 0 in the subarray to 1, and every 1 in the subarray to 0.
+In an array A containing only 0s and 1s, a K-bit flip consists of choosing a (contiguous) subarray of 
+length K and simultaneously changing every 0 in the subarray to 1, and every 1 in the subarray to 0.
 
 Return the minimum number of K-bit flips required so that there is no 0 in the array.  If it is not possible, return -1.
-
- 
 
 Example 1:
 
@@ -24,6 +24,7 @@ Explanation:
 Flip A[0],A[1],A[2]: A becomes [1,1,1,1,0,1,1,0]
 Flip A[4],A[5],A[6]: A becomes [1,1,1,1,1,0,0,0]
 Flip A[5],A[6],A[7]: A becomes [1,1,1,1,1,1,1,1]
+"""
 
  
 """
